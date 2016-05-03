053adcac8cab495c99dc9e3c631c86fb9e0b255f
Navjot Kukreja <navjotk@gmail.com>
2016-05-03 12:57:39 +0100

    Propagator now supports stencils for time loop
    We can now set certain stencils to be evaluated only once per time step

--- propagator.py
+++ propagator.py
@@ -19,6 +19,7 @@ class Propagator(object):
         self.time_steppers = []
         self.time_order = time_order
         self.nt = nt
+        self.time_loop_stencils = []
         # Start with the assumption that the propagator needs to save the field in memory at every time step
         self._save = True
         # This might be changed later when parameters are being set
@@ -73,15 +74,16 @@ class Propagator(object):
         stmts = []
         for equality, args in zip(stencils, stencil_args):
             equality = equality.subs(dict(zip(subs, args)))
-            equality = self.time_substitutions(equality)
-            equality = equality.xreplace(self._var_map)
-            stencil = cgen.Assign(ccode(equality.lhs), ccode(equality.rhs))
+            stencil = self.convert_equality_to_cgen(equality)
             stmts.append(stencil)
         kernel = self._pre_kernel_steps
         kernel += stmts
         kernel += self._post_kernel_steps
         return self.prepare_loop(cgen.Block(kernel))
-
+    
+    def convert_equality_to_cgen(self, equality):
+        return cgen.Assign(ccode(self.time_substitutions(equality.lhs).xreplace(self._var_map)), ccode(self.time_substitutions(equality.rhs).xreplace(self._var_map)))
+    
     def prepare_loop(self, loop_body):
         num_spac_dim = len(self.space_dims)
         for dim_ind in range(1, num_spac_dim+1):
@@ -97,7 +99,8 @@ class Propagator(object):
             time_stepping = self.get_time_stepping()
         else:
             time_stepping = []
-        loop_body = cgen.Block(time_stepping + [loop_body])
+        time_loop_stencils = [self.convert_equality_to_cgen(x) for x in self.time_loop_stencils]
+        loop_body = cgen.Block(time_stepping + [loop_body]+time_loop_stencils)
         loop_body = cgen.For(cgen.InlineInitializer(cgen.Value("int", t_var), str(t_loop_limits[0])), t_var + cond_op + str(t_loop_limits[1]), t_var + "+=" + str(self._time_step), loop_body)
         def_time_step = [cgen.Value("int", t_var_def.name) for t_var_def in self.time_steppers]
         body = def_time_step + [loop_body]
@@ -105,8 +108,8 @@ class Propagator(object):
 
     def add_loop_step(self, sympy_condition, true_assign, false_assign=None, before=False):
         condition = ccode(sympy_condition.lhs.xreplace(self._var_map)) + " == " + ccode(sympy_condition.rhs.xreplace(self._var_map))
-        true_str = cgen.Assign(ccode(self.time_substitutions(true_assign.lhs).xreplace(self._var_map)), ccode(self.time_substitutions(true_assign.rhs).xreplace(self._var_map)))
-        false_str = cgen.Assign(ccode(self.time_substitutions(false_assign.lhs).xreplace(self._var_map)), ccode(self.time_substitutions(false_assign.rhs).xreplace(self._var_map))) if false_assign is not None else None
+        true_str = self.convert_equality_to_cgen(true_assign)
+        false_str = self.convert_equality_to_cgen(false_assign) if false_assign is not None else None
         statement = cgen.If(condition, true_str, false_str)
         if before:
             self._pre_kernel_steps.append(statement)
@@ -180,3 +183,6 @@ class Propagator(object):
             for arg in sympy_expr.args:
                 sympy_expr = sympy_expr.subs(arg, self.time_substitutions(arg))
         return sympy_expr
+    
+    def add_time_loop_stencil(self, stencil):
+        self.time_loop_stencils.append(stencil)
