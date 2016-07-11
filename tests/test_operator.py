7a36bc908cd4cd44ec0b0bdcf6f347846a1d9900
Navjot Kukreja <navjotk@gmail.com>
2016-07-11 16:02:54 -0300

    Small nitpicks 1. Changed a for loop to list comprehension 2. Changed the test to use the SimpleOperator already defined in main devito code.

--- tests/test_operator.py
+++ tests/test_operator.py
@@ -1,42 +1,38 @@
 from devito.interfaces import DenseData
 import numpy as np
-from devito.operators import Operator
-from sympy import Eq, symbols
-
-
-class SimpleOperator(Operator):
-    def __init__(self, input_grid):
-        nt = input_grid.shape[0]
-        shape = input_grid.shape[1:]
-        output_grid = DenseData("output_grid", input_grid.shape, input_grid.dtype)
-        self.input_params = [input_grid]
-        self.output_params = [output_grid]
-        x, t = symbols("x t")
-        kernel = Eq(output_grid[t, x], input_grid[t, x] + 3)
-        self.stencils = [(kernel, [])]
-        super(SimpleOperator, self).__init__([], nt, shape, 0, 0, True, input_grid.dtype)
+from devito.operators import SimpleOperator
+from sympy import Eq
+from sympy.abc import t, x
 
 
 class Test_Operator(object):
     def test_codegen(self):
         input_grid = DenseData("input_grid", (3, 2), np.float64)
         input_grid.data[:] = np.arange(6, dtype=np.float64).reshape((3, 2))
-        op = SimpleOperator(input_grid)
-        output_grid = op.apply()[0]
-        assert(output_grid[2][1] == 8)
+        output_grid = DenseData("output_grid", input_grid.shape, input_grid.dtype)
+        eq = Eq(output_grid[t, x], input_grid[t, x] + 3)
+        op = SimpleOperator(input_grid, output_grid, [eq])
+        op.apply()
+        assert(output_grid.data[2][1] == 8)
 
     def test_python(self):
         input_grid = DenseData("input_grid", (3, 2), np.float64)
         input_grid.data[:] = np.arange(6, dtype=np.float64).reshape((3, 2))
-        op = SimpleOperator(input_grid)
-        output_grid = op.apply(debug=True)[0]
-        assert(output_grid[2][1] == 8)
+        output_grid = DenseData("output_grid", input_grid.shape, input_grid.dtype)
+        eq = Eq(output_grid[t, x], input_grid[t, x] + 3)
+        op = SimpleOperator(input_grid, output_grid, [eq])
+        op.apply(debug=True)
+        assert(output_grid.data[2][1] == 8)
 
     def test_compare(self):
         input_grid = DenseData("input_grid", (3, 2), np.float64)
         input_grid.data[:] = np.arange(6, dtype=np.float64).reshape((3, 2))
-        op_cg = SimpleOperator(input_grid)
-        op_py = SimpleOperator(input_grid)
-        output_grid_cg = op_cg.apply()[0]
-        output_grid_py = op_py.apply(debug=True)[0]
-        assert(np.linalg.norm(output_grid_cg) == np.linalg.norm(output_grid_py))
+        output_grid_py = DenseData("output_grid_py", input_grid.shape, input_grid.dtype)
+        output_grid_cg = DenseData("output_grid_cg", input_grid.shape, input_grid.dtype)
+        eq_py = Eq(output_grid_py[t, x], input_grid[t, x] + 3)
+        eq_cg = Eq(output_grid_cg[t, x], input_grid[t, x] + 3)
+        op_cg = SimpleOperator(input_grid, output_grid_cg, [eq_cg])
+        op_py = SimpleOperator(input_grid, output_grid_py, [eq_py])
+        op_cg.apply()
+        op_py.apply(debug=True)
+        assert(np.equal(output_grid_cg.data, output_grid_py.data).all())
