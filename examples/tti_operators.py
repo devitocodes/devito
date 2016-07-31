28a4aaa8ddf9eaccb6ba63e976065ccd70717b01
Mathias Louboutin <Mathias.Louboutin@gmail.com>
2016-07-31 15:48:57 +0100

    2D dim fix

--- examples/tti_operators.py
+++ examples/tti_operators.py
@@ -182,34 +182,46 @@ class ForwardOperator(Operator):
             Gy2p = ang3 * u.dxr - ang2 * u.dyr
             Gyy2 = first_derivative(Gy2p, ang3, dim=x, side=-1, order=spc_order/2) -\
                    first_derivative(Gy2p, ang2, dim=y, side=-1, order=spc_order/2)
-            parm = [m, damp, A, B, th, ph, u, v]a
-            dim2 = y
-            dim3 = z
+
+            Gx1p = ang0 * ang2 * u.dxl + ang0 * ang3 * u.dyl - ang1 * u.dzl
+            Gz1r = ang1 * ang2 * v.dxl + ang1 * ang3 * v.dyl + ang0 * v.dzl
+            Gxx1 = first_derivative(Gx1p, ang0 * ang2, dim=x, side=1, order=spc_order/2) +\
+               first_derivative(Gx1p, ang0 * ang3,  dim=y, side=1, order=spc_order/2) -\
+               first_derivative(Gx1p, ang1, dim=z, side=1, order=spc_order/2)
+            Gzz1 = first_derivative(Gz1r, ang1 * ang2, dim=x, side=1, order=spc_order/2) +\
+               first_derivative(Gz1r, ang1 * ang3, dim=y, side=1, order=spc_order/2) +\
+               first_derivative(Gz1r, ang0, dim=z, side=1, order=spc_order/2)
+
+            Gx2p = ang0 * ang2 * u.dxr + ang0 * ang3 * u.dyr - ang1 * u.dzr
+            Gz2r = ang1 * ang2 * v.dxr + ang1 * ang3 * v.dyr + ang0 * v.dzr
+
+            Gxx2 = first_derivative(Gx2p, ang0 * ang2, dim=x, side=-1, order=spc_order/2) +\
+               first_derivative(Gx2p, ang0 * ang3, dim=y, side=-1, order=spc_order/2) -\
+               first_derivative(Gx2p, ang1, dim=z, side=-1, order=spc_order/2)
+            Gzz2 = first_derivative(Gz2r, ang1 * ang2, dim=x, side=-1, order=spc_order/2) +\
+                   first_derivative(Gz2r, ang1 * ang3, dim=y, side=-1, order=spc_order/2) +\
+                   first_derivative(Gz2r, ang0, dim=z, side=-1, order=spc_order/2)
+
+                parm = [m, damp, A, B, th, ph, u, v]
         else:
             Gyy2 = 0
             Gyy1 = 0
-            ang2 = 1
-            ang3 = 0
             parm = [m, damp, A, B, th, u, v]
+            Gx1p = ang0 * u.dxl - ang1 * u.dyl
+            Gz1r = ang1 * v.dxl + ang0 * v.dyl
+            Gxx1 = first_derivative(Gx1p, ang0, dim=x, side=1, order=spc_order/2) -\
+               first_derivative(Gx1p, ang1, dim=y, side=1, order=spc_order/2)
+            Gzz1 = first_derivative(Gz1r, ang1, dim=x, side=1, order=spc_order/2) +\
+               first_derivative(Gz1r, ang0, dim=y, side=1, order=spc_order/2)
 
-        Gx1p = ang0 * ang2 * u.dxl + ang0 * ang3 * u.dyl - ang1 * u.dzl
-        Gz1r = ang1 * ang2 * v.dxl + ang1 * ang3 * v.dyl + ang0 * v.dzl
-        Gxx1 = first_derivative(Gx1p, ang0 * ang2, dim=x, side=1, order=spc_order/2) +\
-               first_derivative(Gx1p, ang0 * ang3,  dim=y, side=1, order=spc_order/2) -\
-               first_derivative(Gx1p, ang1, dim=z, side=1, order=spc_order/2)
-        Gzz1 = first_derivative(Gz1r, ang1 * ang2, dim=x, side=1, order=spc_order/2) +\
-               first_derivative(Gz1r, ang1 * ang3, dim=y, side=1, order=spc_order/2) -\
-               first_derivative(Gz1r, ang0, dim=z, side=1, order=spc_order/2)
+            Gx2p = ang0 * u.dxr - ang1 * u.dyr
+            Gz2r = ang1 * v.dxr + ang0 * v.dyr
 
-        Gx2p = ang0 * ang2 * u.dxr + ang0 * ang3 * u.dyr - ang1 * u.dzr
-        Gz2r = ang1 * ang2 * v.dxr + ang1 * ang3 * v.dyr + ang0 * v.dzr
+            Gxx2 = first_derivative(Gx2p, ang0, dim=x, side=-1, order=spc_order/2) -\
+               first_derivative(Gx2p, ang1, dim=y, side=-1, order=spc_order/2)
+            Gzz2 = first_derivative(Gz2r, ang1, dim=x, side=-1, order=spc_order/2) +\
+                   first_derivative(Gz2r, ang0, dim=z, side=-1, order=spc_order/2)
 
-        Gxx2 = first_derivative(Gx2p, ang0 * ang2, dim=x, side=-1, order=spc_order/2) +\
-               first_derivative(Gx2p, ang0 * ang3, dim=y, side=-1, order=spc_order/2) -\
-               first_derivative(Gx2p, ang1, dim=z, side=-1, order=spc_order/2)
-        Gzz2 = first_derivative(Gz2r, ang1 * ang2, dim=x, side=-1, order=spc_order/2) +\
-               first_derivative(Gz2r, ang1 * ang3, dim=y, side=-1, order=spc_order/2) -\
-               first_derivative(Gz2r, ang0, dim=z, side=-1, order=spc_order/2)
         stencilp = 2 * s**2 / (2 * m + s * damp) * (2 * m / s**2 * u + (s * damp - 2 * m) / (2 * s**2) * u.backward + A * Hp + B * Hzr)
         stencilr = 2 * s**2 / (2 * m + s * damp) * (2 * m / s**2 * v + (s * damp - 2 * m) / (2 * s**2) * v.backward + B * Hp + Hzr)
         Hp = -.5 * Gxx1 - .5 * Gxx2 - .5 * Gyy1 - .5 * Gyy2
