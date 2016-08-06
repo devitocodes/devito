7a68c18e5d56ac2a8e61fde666b964a67b516dc7
mloubout <mathias.louboutin@gmail.com>
2016-08-06 15:08:28 +0100

    tab fix

--- devito/operator.py
+++ devito/operator.py
@@ -175,7 +175,7 @@ class Operator(object):
         self.symbol_to_data = {}
         for param in self.signature:
             self.propagator.add_devito_param(param)
-        self.symbol_to_data[param.name] = param
+            self.symbol_to_data[param.name] = param
         self.propagator.stencils = self.stencils
         self.propagator.factorized = factorized
         for name, val in factorized.items():
