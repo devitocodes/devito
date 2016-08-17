50debf5192e710c18c69f28ef5375e7f78de3429
Vincenzo Pandolfo <vp715@ic.ac.uk>
2016-08-17 09:27:21 +0100

    Added package-level method clear_cache

--- devito/__init__.py
+++ devito/__init__.py
@@ -1,5 +1,19 @@
 from __future__ import absolute_import
+import gc
+
+from sympy.core import cache
+
 from devito.interfaces import *  # noqa
+from devito.interfaces import _SymbolCache
 from devito.operator import *  # noqa
 from devito.finite_difference import *  # noqa
 from devito.iteration import *  # noqa
+
+
+def clear_cache():
+    cache.clear_cache()
+    gc.collect()
+
+    for key, val in _SymbolCache.items():
+        if val() is None:
+            del _SymbolCache[key]
