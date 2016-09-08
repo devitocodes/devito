801c929add003f976d9b6673016957178a9cde73
Mathias Louboutin <Mathias.Louboutin@gmail.com>
2016-09-08 14:56:31 -0700

    flake8

--- examples/tti_operators.py
+++ examples/tti_operators.py
@@ -104,21 +104,30 @@ class ForwardOperator(Operator):
 
             Gx1p = (ang0 * ang2 * u.dxl + ang0 * ang3 * u.dyl - ang1 * u.dzl)
             Gz1r = (ang1 * ang2 * v.dxl + ang1 * ang3 * v.dyl + ang0 * v.dzl)
-            Gxx1 = (first_derivative(Gx1p, ang0, ang2, dim=x, side="right", order=spc_brd) +
-                    first_derivative(Gx1p, ang0, ang3, dim=y, side="right", order=spc_brd) -
+            Gxx1 = (first_derivative(Gx1p, ang0, ang2,
+                                     dim=x, side="right", order=spc_brd) +
+                    first_derivative(Gx1p, ang0, ang3,
+                                     dim=y, side="right", order=spc_brd) -
                     first_derivative(Gx1p, ang1, dim=z, side="right", order=spc_brd))
-            Gzz1 = (first_derivative(Gz1r, ang1, ang2, dim=x, side="right", order=spc_brd) +
-                    first_derivative(Gz1r, ang1, ang3, dim=y, side="right", order=spc_brd) +
+            Gzz1 = (first_derivative(Gz1r, ang1, ang2,
+                                     dim=x, side="right", order=spc_brd) +
+                    first_derivative(Gz1r, ang1, ang3,
+                                     dim=y, side="right", order=spc_brd) +
                     first_derivative(Gz1r, ang0, dim=z, side="right", order=spc_brd))
 
             Gx2p = (ang0 * ang2 * u.dxr + ang0 * ang3 * u.dyr - ang1 * u.dzr)
             Gz2r = (ang1 * ang2 * v.dxr + ang1 * ang3 * v.dyr + ang0 * v.dzr)
-            Gxx2 = (first_derivative(Gx2p, ang0, ang2, dim=x, side="left", order=spc_brd) +
-                    first_derivative(Gx2p, ang0, ang3, dim=y, side="left", order=spc_brd) -
+            Gxx2 = (first_derivative(Gx2p, ang0, ang2,
+                                     dim=x, side="left", order=spc_brd) +
+                    first_derivative(Gx2p, ang0, ang3,
+                                     dim=y, side="left", order=spc_brd) -
                     first_derivative(Gx2p, ang1, dim=z, side="left", order=spc_brd))
-            Gzz2 = (first_derivative(Gz2r, ang1, ang2, dim=x, side="left", order=spc_brd) +
-                    first_derivative(Gz2r, ang1, ang3, dim=y, side="left", order=spc_brd) +
-                    first_derivative(Gz2r, ang0, dim=z, side="left", order=spc_brd))
+            Gzz2 = (first_derivative(Gz2r, ang1, ang2,
+                                     dim=x, side="left", order=spc_brd) +
+                    first_derivative(Gz2r, ang1, ang3,
+                                     dim=y, side="left", order=spc_brd) +
+                    first_derivative(Gz2r, ang0,
+                                     dim=z, side="left", order=spc_brd))
             parm = [m, damp, epsilon, delta, theta, phi, u, v]
         else:
             Gyy2 = 0
@@ -126,16 +135,24 @@ class ForwardOperator(Operator):
             parm = [m, damp, epsilon, delta, theta, u, v]
             Gx1p = (ang0 * u.dxr - ang1 * u.dy)
             Gz1r = (ang1 * v.dxr + ang0 * v.dy)
-            Gxx1 = (first_derivative(Gx1p * ang0, dim=x, side="left", order=spc_brd) -
-                    first_derivative(Gx1p * ang1, dim=y, side="centered", order=spc_brd))
-            Gzz1 = (first_derivative(Gz1r * ang1, dim=x, side="left", order=spc_brd) +
-                    first_derivative(Gz1r * ang0, dim=y, side="centered", order=spc_brd))
+            Gxx1 = (first_derivative(Gx1p * ang0, dim=x,
+                                     side="left", order=spc_brd) -
+                    first_derivative(Gx1p * ang1, dim=y,
+                                     side="centered", order=spc_brd))
+            Gzz1 = (first_derivative(Gz1r * ang1, dim=x,
+                                     side="left", order=spc_brd) +
+                    first_derivative(Gz1r * ang0, dim=y,
+                                     side="centered", order=spc_brd))
             Gx2p = (ang0 * u.dx - ang1 * u.dyr)
             Gz2r = (ang1 * v.dx + ang0 * v.dyr)
-            Gxx2 = (first_derivative(Gx2p * ang0, dim=x, side="centered", order=spc_brd) -
-                    first_derivative(Gx2p * ang1, dim=y, side="left", order=spc_brd))
-            Gzz2 = (first_derivative(Gz2r * ang1, dim=x, side="centered", order=spc_brd) +
-                    first_derivative(Gz2r * ang0, dim=y, side="left", order=spc_brd))
+            Gxx2 = (first_derivative(Gx2p * ang0, dim=x,
+                                     side="centered", order=spc_brd) -
+                    first_derivative(Gx2p * ang1, dim=y,
+                                     side="left", order=spc_brd))
+            Gzz2 = (first_derivative(Gz2r * ang1, dim=x,
+                                     side="centered", order=spc_brd) +
+                    first_derivative(Gz2r * ang0, dim=y,
+                                     side="left", order=spc_brd))
 
         stencilp = 1.0 / (2.0 * m + s * damp) * \
             (4.0 * m * u + (s * damp - 2.0 * m) *
@@ -155,7 +172,7 @@ class ForwardOperator(Operator):
         super(ForwardOperator, self).__init__(src.nt, m.shape,
                                               stencils=stencils,
                                               subs=subs,
-                                              spc_border=spc_order/2,
+                                              spc_border=spc_order/2 + 2,
                                               time_order=time_order,
                                               forward=True,
                                               dtype=m.dtype,
