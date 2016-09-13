20846047978c6728ca885bb34b91f5ee34a77ed0
Vincenzo Pandolfo <vp715@ic.ac.uk>
2016-09-13 14:59:03 +0100

    TTI uses the auto_tuning parameter instead of doing AT manually

--- examples/TTI_codegen.py
+++ examples/TTI_codegen.py
@@ -3,7 +3,6 @@ from __future__ import print_function
 
 import numpy as np
 
-from devito.at_controller import AutoTuner
 from examples.tti_operators import *
 
 
@@ -68,16 +67,8 @@ class TTI_cg:
         fw = ForwardOperator(self.model, self.src, self.damp, self.data,
                              time_order=self.t_order, spc_order=self.s_order,
                              profile=True, save=save, cache_blocking=cache_blocking,
-                             cse=cse, compiler=compiler)
-
-        if auto_tuning:
-            fw_new = ForwardOperator(self.model, self.src, self.damp, self.data,
-                                     time_order=self.t_order, spc_order=self.s_order,
-                                     profile=True, save=save, cse=cse, compiler=compiler)
-
-            at = AutoTuner(fw_new)
-            at.auto_tune_blocks(self.s_order + 1, self.s_order * 4 + 2)
-            fw.propagator.cache_blocking = at.block_size
+                             cse=cse, compiler=compiler, auto_tuning=auto_tuning,
+                             at_range=(self.s_order + 1, self.s_order * 4 + 2))
 
         u, v, rec = fw.apply()
         return (rec.data, u.data, v.data,
