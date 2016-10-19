0889faf251686c80f2e231e9af878c09cb3ef825
Michael Lange <michael.lange@imperial.ac.uk>
2016-10-19 06:53:55 +0100

    StencilKernel: Better use of dimension fixtures in arithmetic test

--- tests/test_stencil_arithmetic.py
+++ tests/test_stencil_arithmetic.py
@@ -6,27 +6,23 @@ from devito import DenseData, Dimension, StencilKernel
 
 
 @pytest.fixture
-def x(xdim=4):
-    return Dimension(name='x', size=xdim)
+def i(size=3):
+    return Dimension(name='i', size=size)
 
 
 @pytest.fixture
-def y(ydim=6):
-    return Dimension(name='y', size=ydim)
+def j(size=4):
+    return Dimension(name='j', size=size)
 
 
 @pytest.fixture
-def ai(x, y, name='a', value=2.):
-    a = DenseData(name=name, dimensions=(x, y))
-    a.data[:] = value
-    return a.indexify()
+def k(size=5):
+    return Dimension(name='k', size=size)
 
 
 @pytest.fixture
-def bi(x, y, name='b', value=3.):
-    b = DenseData(name=name, dimensions=(x, y))
-    b.data[:] = value
-    return b.indexify()
+def l(size=6):
+    return Dimension(name='l', size=size)
 
 
 @pytest.mark.parametrize('expr, result', [
@@ -35,8 +31,16 @@ def bi(x, y, name='b', value=3.):
     ('Eq(ai, 4 * (bi * ai))', 24.),
     ('Eq(ai, (6. / bi) + (8. * ai))', 18.),
 ])
-def test_arithmetic_flat(ai, bi, expr, result):
+def test_arithmetic_flat(i, j, expr, result):
     """Tests basic point-wise arithmetic on two-dimensional data"""
+
+    a = DenseData(name='a', dimensions=(i, j))
+    a.data[:] = 2.
+    ai = a.indexify()
+    b = DenseData(name='b', dimensions=(i, j))
+    b.data[:] = 3.
+    bi = b.indexify()
+
     eqn = eval(expr)
     StencilKernel(eqn)(ai.base.function, bi.base.function)
     assert np.allclose(ai.base.function.data, result, rtol=1e-12)
@@ -48,12 +52,8 @@ def test_arithmetic_flat(ai, bi, expr, result):
     ('Eq(ai, 4 * (bi * ai))', 24.),
     ('Eq(ai, (6. / bi) + (8. * ai))', 18.),
 ])
-def test_arithmetic_deep(expr, result):
+def test_arithmetic_deep(i, j, k, l, expr, result):
     """Tests basic point-wise arithmetic on multi-dimensional data"""
-    i = Dimension(name='i', size=3)
-    j = Dimension(name='j', size=4)
-    k = Dimension(name='k', size=5)
-    l = Dimension(name='l', size=6)
 
     a = DenseData(name='a', dimensions=(i, j, k, l))
     a.data[:] = 2.
