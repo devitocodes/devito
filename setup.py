68f07070c829fa2703167e8f1c8161d364e3b800
Michael Lange <michael.lange@imperial.ac.uk>
2016-11-24 10:54:39 +0000

    Pip: Add more install meta-data, including dependency packages

--- setup.py
+++ setup.py
@@ -5,7 +5,18 @@ except ImportError:
 
 setup(name='devito',
       version='2.0.1',
-      description="""Finite Difference DSL for symbolic stencil computation.""",
+      description="Finite Difference DSL for symbolic computation.",
+      long_descritpion="""Devito is a new tool for performing
+      optimised Finite Difference (FD) computation from high-level
+      symbolic problem definitions. Devito performs automated code
+      generation and Just-In-time (JIT) compilation based on symbolic
+      equations defined in SymPy to create and execute highly
+      optimised Finite Difference kernels on multiple computer
+      platforms.""",
+      url='http://www.opesci.org/devito',
       author="Imperial College London",
+      author_email='opesci@imperial.ac.uk',
       license='MIT',
-      packages=['devito'])
+      packages=['devito'],
+      install_requires=['numpy', 'sympy', 'mpmath', 'cgen', 'codepy'],
+      test_requires=['pytest', 'flake8', 'isort'])
