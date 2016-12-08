07ba078bf7c0f64bc126f42e8c3c285d6eb9c6cd
mloubout <mathias.louboutin@gmail.com>
2016-12-08 12:24:40 -0800

    flake8

--- examples/acoustic/Acoustic_codegen.py
+++ examples/acoustic/Acoustic_codegen.py
@@ -28,6 +28,7 @@ class Acoustic_cg(object):
             self.dt = model.get_critical_dt()
         self.model.nbpml = nbpml
         self.model.set_origin(nbpml)
+
         # Fill the dampening field with nbp points in the absorbing layer
         def damp_boundary(damp, nbp):
             h = self.model.get_spacing()
