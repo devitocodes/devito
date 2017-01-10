3e2979a62c8a303b17b6331076997b5f0d95e58c
Fabio Luporini <f.luporini12@imperial.ac.uk>
2017-01-10 14:55:56 +0100

    dse: Add further factorization pass

--- devito/dse/symbolics.py
+++ devito/dse/symbolics.py
@@ -129,6 +129,7 @@ class Rewriter(object):
         self._factorize(state, mode=mode)
         self._optimize_trigonometry(state, mode=mode)
         self._replace_time_invariants(state, mode=mode)
+        self._factorize(state, mode=mode)
 
         self._finalize(state)
 
