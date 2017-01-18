gradient test fixed.

tests/test_gradient.py
@@ -7,7 +7,6 @@ from examples.acoustic.Acoustic_codegen import Acoustic_cg
 from examples.containers import IGrid, IShot
 
 
-@pytest.mark.xfail(reason='Numerical accuracy with np.float32')
 class TestGradient(object):
     @pytest.fixture(params=[(70, 80)])
     def acoustic(self, request, time_order, space_order):
@@ -47,7 +46,7 @@ class TestGradient(object):
         else:
             dt = model.get_critical_dt()
         t0 = 0.0
-        tn = 500.0
+        tn = 750.0
         nt = int(1+(tn-t0)/dt)
         # Set up the source as Ricker wavelet for f0
 
@@ -89,7 +88,7 @@ class TestGradient(object):
     def time_order(self, request):
         return request.param
 
-    @pytest.fixture(params=[2])
+    @pytest.fixture(params=[4])
     def space_order(self, request):
         return request.param
 
@@ -102,7 +101,7 @@ class TestGradient(object):
         # Actual Gradient test
         G = np.dot(gradient.reshape(-1), acoustic[1].model.pad(acoustic[2]).reshape(-1))
         # FWI Gradient test
-        H = [1, 0.1, 0.01, .001, 0.0001, 0.00001, 0.000001]
+        H = [0.5, 0.25, .125, 0.0625, 0.0312, 0.015625, 0.0078125]
         error1 = np.zeros(7)
         error2 = np.zeros(7)
         for i in range(0, 7):
@@ -124,8 +123,8 @@ class TestGradient(object):
         p2 = np.polyfit(np.log10(H), np.log10(error2), 1)
         print(p1)
         print(p2)
-        assert np.isclose(p1[0], 1.0, rtol=0.05)
-        assert np.isclose(p2[0], 2.0, rtol=0.05)
+        assert np.isclose(p1[0], 1.0, rtol=0.1)
+        assert np.isclose(p2[0], 2.0, rtol=0.1)
 
 
 if __name__ == "__main__":