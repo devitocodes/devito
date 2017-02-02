fa49383d1024c56646f0c8ff4278933bd0d157c8
Fabio Luporini <f.luporini12@imperial.ac.uk>
2017-02-02 15:29:35 +0100

    Remove unused sympy_find utils

--- devito/tools.py
+++ devito/tools.py
@@ -67,34 +67,6 @@ def convert_dtype_to_ctype(dtype):
     return conversion_dict[dtype]
 
 
-def sympy_find(expr, term, repl):
-    """Change all terms from function notation to array notation.
-
-    Finds all terms of the form term(x1, x2, x3)
-    and changes them to repl[x1, x2, x3]. i.e. changes from
-    function notation to array notation. It also reorders the indices
-    x1, x2, x3 so that the time index comes first.
-
-    :param expr: The expression to be processed
-    :param term: The pattern to be replaced
-    :param repl: The replacing pattern
-    :returns: The changed expression
-    """
-
-    t = symbols("t")
-
-    if type(expr) == term:
-        args_wo_t = [x for x in expr.args if x != t and t not in x.args]
-        args_t = [x for x in expr.args if x == t or t in x.args]
-        expr = repl[tuple(args_t + args_wo_t)]
-
-    if hasattr(expr, "args"):
-        for a in expr.args:
-            expr = expr.subs(a, sympy_find(a, term, repl))
-
-    return expr
-
-
 def aligned(a, alignment=16):
     """Function to align the memmory
 
