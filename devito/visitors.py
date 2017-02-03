3b3aec2911185bc4ab8545bf9bb5922bd6c27b18
Michael Lange <michael.lange@imperial.ac.uk>
2017-02-03 05:14:10 +0000

    Iteration: For buffered dims use parent variable as primary loop counter

--- devito/visitors.py
+++ devito/visitors.py
@@ -13,9 +13,8 @@ from operator import attrgetter
 import cgen as c
 from sympy import Symbol
 
-from devito.dimension import BufferedDimension
 from devito.dse import estimate_cost, estimate_memory
-from devito.nodes import Block, Iteration, IterationBound
+from devito.nodes import Iteration, IterationBound, List
 from devito.tools import as_tuple, filter_ordered, filter_sorted, flatten
 
 
@@ -444,13 +443,13 @@ class ResolveIterationVariable(Transformer):
 
     def visit_Iteration(self, o, subs={}, offsets=defaultdict(set)):
         nodes = self.visit(o.children, subs=subs, offsets=offsets)
-        if isinstance(o.dim, BufferedDimension):
+        if o.dim.is_Buffered:
             # For buffered dimensions insert the explicit
             # definition of buffere variables, eg. t+1 => t1
             init = []
             for off in filter_ordered(offsets[o.dim]):
                 vname = o.dim.get_varname()
-                value = o.dim + off
+                value = o.dim.parent + off
                 modulo = o.dim.modulo
                 init += [c.Initializer(c.Value('int', vname),
                                        "(%s) %% %d" % (value, modulo))]
@@ -458,8 +457,8 @@ class ResolveIterationVariable(Transformer):
             # Always lower to symbol
             subs[o.dim.parent] = Symbol(o.dim.parent.name)
             # Insert block with modulo initialisations
-            newnodes = (Block(header=init, body=nodes[0]), )
-            return o._rebuild(newnodes)
+            newnodes = (List(header=init, body=nodes[0]), )
+            return o._rebuild(newnodes, index=o.dim.parent.name)
         else:
             vname = o.dim.get_varname()
             subs[o.dim] = Symbol(vname)
