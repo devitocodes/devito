c098cb885e0ec1f8490265749743e863c4ca53ea
Fabio Luporini <f.luporini12@imperial.ac.uk>
2017-02-13 09:39:30 +0100

    benchmark: Add temporary legacy flag

--- examples/benchmark.py
+++ examples/benchmark.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
 
     devito = parser.add_argument_group("Devito")
     devito.add_argument("--no-legacy", dest="legacy", action="store_false",
-                        help="Pass False to run with the new StencilKernel "
+                        help="Use this option to run with the new StencilKernel "
                              "infrastructure, rather than Operator/Propagator")
     devito.add_argument("-dse", default="advanced", nargs="*",
                         choices=["noop", "basic", "factorize", "approx-trigonometry",
