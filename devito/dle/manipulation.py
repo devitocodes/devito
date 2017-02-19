35566421dea840d8f44afbedfe2ea3f0ef17bc3b
Fabio Luporini <f.luporini12@imperial.ac.uk>
2017-02-19 20:08:06 +0100

    dle: Add copy_arrays utility

--- devito/dle/manipulation.py
+++ devito/dle/manipulation.py
@@ -1,4 +1,10 @@
-__all__ = ['compose_nodes']
+import cgen as c
+
+from devito.codeprinter import ccode
+from devito.nodes import Element, Iteration
+from devito.visitors import MergeOuterIterations
+
+__all__ = ['compose_nodes', 'copy_arrays']
 
 
 def compose_nodes(nodes):
@@ -11,3 +17,23 @@ def compose_nodes(nodes):
         body = handle._rebuild(body, **handle.args_frozen)
 
     return body
+
+
+def copy_arrays(mapper):
+    """Build an Iteration/Expression tree performing the copy ``k = v`` for each
+    (k, v) in mapper. (k, v) are expected to be of type :class:`IndexedData`."""
+
+    # Build the Iteration tree for the copy
+    iterations = []
+    for k, v in mapper.items():
+        handle = []
+        indices = k.function.indices
+        for i, j in zip(k.shape, indices):
+            handle.append(Iteration([], dimension=j, limits=j.symbolic_size))
+        handle.append(Element(c.Assign(ccode(k[indices]), ccode(v[indices]))))
+        iterations.append(compose_nodes(handle))
+
+    # Maybe some Iterations are mergeable
+    iterations = MergeOuterIterations().visit(iterations)
+
+    return iterations
