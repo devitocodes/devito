2f4377955f60ab05ec9330f718d5b405cad5996c
Fabio Luporini <f.luporini12@imperial.ac.uk>
2017-02-20 10:25:04 +0100

    dle: Cache simd flag retrieval

--- devito/dle/transformer.py
+++ devito/dle/transformer.py
@@ -823,13 +823,19 @@ simdinfo = {
 
 def get_simd_flag():
     """Retrieve the best SIMD flag on the current architecture."""
-    ordered_known = ('sse', 'sse4_2', 'avx', 'avx2', 'avx512')
-    flags = cpuinfo.get_cpu_info().get('flags')
-    if not flags:
-        return None
-    for i in reversed(ordered_known):
-        if i in flags:
-            return i
+    if get_simd_flag.flag is None:
+        ordered_known = ('sse', 'sse4_2', 'avx', 'avx2', 'avx512')
+        flags = cpuinfo.get_cpu_info().get('flags')
+        if not flags:
+            return None
+        for i in reversed(ordered_known):
+            if i in flags:
+                get_simd_flag.flag = i
+                return i
+    else:
+        # "Cached" because calls to cpuingo are expensive
+        return get_simd_flag.flag
+get_simd_flag.flag = None
 
 
 def get_simd_items(dtype):
