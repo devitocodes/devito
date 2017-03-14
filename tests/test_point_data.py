31bbed992e278a7c545b608eb3f1b1fa703cf65e
Michael Lange <michael.lange@imperial.ac.uk>
2017-03-14 10:06:56 +0000

    PointData: Simple 2D test for point injection into grid

--- tests/test_point_data.py
+++ tests/test_point_data.py
@@ -25,6 +25,15 @@ def points(npoints=20):
                       ndim=2, coordinates=coords, h=0.1, nbpml=0)
 
 
+@pytest.fixture
+def points_horizontal(npoints=19, ycoord=0.45):
+    coords = np.empty((npoints, 2), dtype=np.float32)
+    coords[:, 0] = np.linspace(.05, .95, npoints)
+    coords[:, 1] = ycoord
+    return SourceLike(name='points', nt=1, npoint=npoints,
+                      ndim=2, coordinates=coords, h=0.1, nbpml=0)
+
+
 def test_interpolate_2d(a, points):
     """Test generic point interpolation testing the x-coordinate of an
     abitrary set of points going across the grid.
@@ -34,3 +43,16 @@ def test_interpolate_2d(a, points):
     expr = Eq(points, points.interpolate(a))
     Operator(expr, subs={h: spacing})(t=1)
     assert np.allclose(points.data, xcoords, rtol=1e-7)
+
+
+def test_inject_2d(a, points_horizontal):
+    """Test point injection with a set of points forming a line
+    through the middle of the grid.
+    """
+    np.set_printoptions(precision=10, linewidth=200)
+    points = points_horizontal
+    spacing = a.data[1, 1]
+    a.data[:] = 0.
+    expr = Eq(points, points.inject(1.))
+    Operator(expr, dse=None, dle=None, subs={h: spacing})(t=1)
+    assert np.allclose(a.data[1:-1, 4:6], 1., rtol=1.e-6)
