1fdd975a7e2dfce81643e2bdb3e2e43abbe86d9d
Fabio Luporini <f.luporini12@imperial.ac.uk>
2017-03-15 18:35:53 +0100

    visitors: Fix MergeOuterIteration

--- devito/visitors.py
+++ devito/visitors.py
@@ -482,12 +482,13 @@ class MergeOuterIterations(Transformer):
         between the loops. A deeper analysis is required for this that
         will be added soon.
         """
-        equal = (iter1.dim == iter2.dim and
-                 iter1.index == iter2.index and
-                 iter1.limits == iter2.limits)
-        # Aliasing only works one-way because we left-merge
-        alias = iter1.dim.is_Buffered and iter1.dim.parent == iter2.dim
-        return equal or alias
+        if iter1.dim.is_Buffered:
+            # Aliasing only works one-way because we left-merge
+            if iter1.dim.parent == iter2.dim:
+                return True
+            if iter2.dim.is_Buffered and iter1.dim.parent == iter2.dim.parent:
+                return True
+        return iter1.dim == iter2.dim and iter1.bounds_symbolic == iter2.bounds_symbolic
 
     def merge(self, iter1, iter2):
         """Creates a new merged :class:`Iteration` object from two
