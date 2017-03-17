cbae2d9df975abec02fe56f3d8acce12421e8faa
Fabio Luporini <f.luporini12@imperial.ac.uk>
2017-03-17 13:50:37 +0100

    dse: Move factorizer pass to the end

--- devito/dse/symbolics.py
+++ devito/dse/symbolics.py
@@ -199,10 +199,9 @@ class Rewriter(object):
         self._cse(state, mode=mode)
         self._optimize_trigonometry(state, mode=mode)
         self._split_expressions(state, mode=mode)
-        self._factorize(state, mode=mode)
         for _ in range(2):
             self._capture_redundancies(state, mode=mode)
-        from IPython import embed; embed()
+        self._factorize(state, mode=mode)
 
         self._finalize(state)
 
