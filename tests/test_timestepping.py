8513b6e52581c8a5682941c8fc8efd955ca5fa9c
Michael Lange <michael.lange@imperial.ac.uk>
2017-03-22 12:24:18 +0000

    Timestepping: A dedicated test that uses buffered and unrolled "time"

--- tests/test_timestepping.py
+++ tests/test_timestepping.py
@@ -21,6 +21,13 @@ def b(shape=(11, 11)):
                     time_dim=6, save=True)
 
 
+@pytest.fixture
+def c(shape=(11, 11)):
+    """Forward time data object, buffered (save=False)"""
+    return TimeData(name='c', shape=shape, time_order=1,
+                    save=False, time_axis=Forward)
+
+
 def test_forward(a, nt=5):
     a.data[0, :] = 1.
     eqn = Eq(a.forward, a + 1.)
@@ -35,3 +42,14 @@ def test_backward(b, nt=5):
     StencilKernel(eqn, dle=None, dse=None, time_axis=Backward)(time=nt)
     for i in range(nt + 1):
         assert np.allclose(b.data[i, :], 1. + i, rtol=1.e-12)
+
+
+def test_forward_unroll(a, c, nt=5):
+    """Test forward time marching with a buffered and an unrolled t"""
+    a.data[0, :] = 1.
+    c.data[0, :] = 1.
+    eqn_c = Eq(c.forward, c + 1.)
+    eqn_a = Eq(a.forward, c.forward)
+    StencilKernel([eqn_c, eqn_a], dle=None, dse=None)(time=nt)
+    for i in range(nt):
+        assert np.allclose(a.data[i, :], 1. + i, rtol=1.e-12)
