4dffdc9f1f27439d46d9e89ba83d98d7ed55af53
Michael Lange <michael.lange@imperial.ac.uk>
2017-05-07 06:22:57 +0100

    PointData: Refactor `PointData` tests to test 2D and 3D cases

--- tests/test_point_data.py
+++ tests/test_point_data.py
@@ -15,56 +15,82 @@ def a(shape=(11, 11)):
     return a
 
 
-@pytest.fixture
-def points(npoints=20):
-    x = np.linspace(.05, .9, npoints)
-    y = np.linspace(.01, .8, npoints)
-    coords = np.concatenate((x, y)).reshape(npoints, 2)
-    return PointData(name='points', nt=1, npoint=npoints,
-                     ndim=2, coordinates=coords)
+def unit_box(name='a', shape=(11, 11)):
+    """Create a field with value 0. to 1. in each dimension"""
+    a = DenseData(name='a', shape=shape)
+    dims = tuple([np.linspace(0., 1., d) for d in shape])
+    a.data[:] = np.meshgrid(*dims)[1]
+    return a
 
 
-@pytest.fixture
-def points_horizontal(npoints=19, ycoord=0.45):
-    coords = np.empty((npoints, 2), dtype=np.float32)
-    coords[:, 0] = np.linspace(.05, .95, npoints)
-    coords[:, 1] = ycoord
-    return PointData(name='points', nt=1, npoint=npoints,
-                     ndim=2, coordinates=coords)
+def points(ranges, npoints):
+    """Create a set of sparse points from a set of coordinate
+    ranges for each spatial dimension.
+    """
+    points = PointData(name='points', nt=1, npoint=npoints, ndim=len(ranges))
+    for i, r in enumerate(ranges):
+        points.coordinates.data[:, i] = np.linspace(r[0], r[1], npoints)
+    return points
 
 
-def test_interpolate_2d(a, points):
+@pytest.mark.parametrize('shape, coords', [
+    ((11, 11), [(.05, .9), (.01, .8)]),
+    ((11, 11, 11), [(.05, .9), (.01, .8), (0.07, 0.84)])
+])
+def test_interpolate(shape, coords, npoints=20):
     """Test generic point interpolation testing the x-coordinate of an
     abitrary set of points going across the grid.
     """
-    spacing = a.data[1, 1]
-    xcoords = points.coordinates.data[:, 0]
-    expr = Eq(points, points.interpolate(a))
-    Operator(expr, subs={h: spacing})(t=1)
-    assert np.allclose(points.data, xcoords, rtol=1e-7)
+    a = unit_box(shape=shape)
+    spacing = a.data[tuple([1 for _ in shape])]
+    p = points(coords, npoints=npoints)
+    xcoords = p.coordinates.data[:, 0]
+
+    expr = Eq(p, p.interpolate(a))
+    Operator(expr, subs={h: spacing})(a=a, t=1)
+
+    assert np.allclose(p.data[0, :], xcoords, rtol=1e-6)
 
 
-def test_inject_2d(a, points_horizontal):
+@pytest.mark.parametrize('shape, coords, result', [
+    ((11, 11), [(.05, .95), (.45, .45)], 1.),
+    ((11, 11, 11), [(.05, .95), (.45, .45), (.45, .45)], 0.5)
+])
+def test_inject(shape, coords, result, npoints=19):
     """Test point injection with a set of points forming a line
     through the middle of the grid.
     """
-    points = points_horizontal
-    spacing = a.data[1, 1]
+    a = unit_box(shape=shape)
+    spacing = a.data[tuple([1 for _ in shape])]
     a.data[:] = 0.
-    expr = points.inject(a, Function('FLOAT')(1.))
-    Operator(expr, dse=None, dle=None, subs={h: spacing})(t=1)
-    assert np.allclose(a.data[1:-1, 4:6], 1., rtol=1.e-6)
+    p = points(ranges=coords, npoints=npoints)
+
+    expr = p.inject(a, Function('FLOAT')(1.))
+    Operator(expr, subs={h: spacing})(a=a, t=1)
+
+    indices = [slice(4, 6, 1) for _ in coords]
+    indices[0] = slice(1, -1, 1)
+    assert np.allclose(a.data[indices], result, rtol=1.e-5)
 
 
-def test_inject_from_field_2d(a, points_horizontal):
+@pytest.mark.parametrize('shape, coords, result', [
+    ((11, 11), [(.05, .95), (.45, .45)], 1.),
+    ((11, 11, 11), [(.05, .95), (.45, .45), (.45, .45)], 0.5)
+])
+def test_inject_from_field(shape, coords, result, npoints=19):
     """Test point injection from a second field along a line
     through the middle of the grid.
     """
-    points = points_horizontal
-    spacing = a.data[1, 1]
+    a = unit_box(shape=shape)
+    spacing = a.data[tuple([1 for _ in shape])]
+    a.data[:] = 0.
     b = DenseData(name='b', shape=a.data.shape)
-    b.data[:] = 0.
-    expr = points.inject(field=b, expr=a)
-    StencilKernel(expr, dse=None, dle=None, subs={h: spacing})(t=1)
-    assert np.allclose(a.data[1:-1, 4], np.arange(.1, 1., 0.1), rtol=1.e-6)
-    assert np.allclose(a.data[1:-1, 5], np.arange(.1, 1., 0.1), rtol=1.e-6)
+    b.data[:] = 1.
+    p = points(ranges=coords, npoints=npoints)
+
+    expr = p.inject(field=a, expr=b)
+    Operator(expr, subs={h: spacing})(a=a, b=b, t=1)
+
+    indices = [slice(4, 6, 1) for _ in coords]
+    indices[0] = slice(1, -1, 1)
+    assert np.allclose(a.data[indices], result, rtol=1.e-5)
