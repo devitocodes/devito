960aaea1aa84b800329b22783cc318e5ec5ad472
Fabio Luporini <f.luporini12@imperial.ac.uk>
2017-05-09 16:01:12 +0200

    Use LoweredDimension to wrap modulo iterators

--- devito/dimension.py
+++ devito/dimension.py
@@ -9,6 +9,7 @@ __all__ = ['Dimension', 'x', 'y', 'z', 't', 'p', 'd', 'time']
 class Dimension(Symbol):
 
     is_Buffered = False
+    is_Lowered = False
 
     """Index object that represents a problem dimension and thus
     defines a potential iteration space.
@@ -22,7 +23,6 @@ class Dimension(Symbol):
     def __new__(cls, name, **kwargs):
         newobj = Symbol.__new__(cls, name)
         newobj.size = kwargs.get('size', None)
-        newobj._count = 0
         newobj.reverse = kwargs.get('reverse', False)
         return newobj
 
@@ -54,7 +54,8 @@ class BufferedDimension(Dimension):
 
     is_Buffered = True
 
-    """Dimension symbils that implies modulo buffered iteration.
+    """
+    Dimension symbol that implies modulo buffered iteration.
 
     :param parent: Parent dimension over which to loop in modulo fashion.
     """
@@ -64,7 +65,6 @@ class BufferedDimension(Dimension):
         assert isinstance(parent, Dimension)
         newobj.parent = parent
         newobj.modulo = kwargs.get('modulo', 2)
-        newobj._count = 0
         return newobj
 
     @property
@@ -74,6 +74,38 @@ class BufferedDimension(Dimension):
     @property
     def reverse(self):
         return self.parent.reverse
+
+
+class LoweredDimension(Dimension):
+
+    is_Lowered = True
+
+    """
+    Dimension symbol representing modulo iteration created when resolving a
+    :class:`BufferedDimension`.
+
+    :param buffered: BufferedDimension from which this Dimension originated.
+    :param offset: Offset value used in the modulo iteration.
+    """
+
+    def __new__(cls, name, buffered, offset, **kwargs):
+        newobj = Symbol.__new__(cls, name)
+        assert isinstance(buffered, BufferedDimension)
+        newobj.buffered = buffered
+        newobj.offset = offset
+        return newobj
+
+    @property
+    def origin(self):
+        return self.buffered + self.offset
+
+    @property
+    def size(self):
+        return self.buffered.size
+
+    @property
+    def reverse(self):
+        return self.buffered.reverse
 
 
 # Default dimensions for time
