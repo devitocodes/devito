2ebc1bb8b86b7351dc7604687036ad5e070e03c1
Michael Lange <michael.lange@imperial.ac.uk>
2017-05-18 11:31:00 +0100

    TTI: Forcing space order 4 in example and adding space order warning

--- examples/tti/tti_example.py
+++ examples/tti/tti_example.py
@@ -3,6 +3,7 @@ import numpy as np
 from examples.containers import IShot
 from examples.tti.TTI_codegen import TTI_cg
 from examples.seismic import Model
+from devito.logger import warning
 
 
 def source(t, f0):
@@ -68,6 +69,9 @@ def run(dimensions=(50, 50, 50), spacing=(20.0, 20.0, 20.0), tn=250.0,
         time_order=2, space_order=4, nbpml=10, **kwargs):
 
     TTI = setup(dimensions, spacing, tn, time_order, space_order, nbpml)
+
+    if space_order % 4 != 0:
+        warning('WARNING: TTI requires a space_order that is a multiple of 4!')
 
     rec, u, v, gflopss, oi, timings = TTI.Forward(**kwargs)
 
