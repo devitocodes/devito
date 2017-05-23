e8be62aa8f169c267a54a00703b5cf6d11d3538d
mloubout <mathias.louboutin@gmail.com>
2017-05-23 09:09:47 +0100

    name opp

---
+++ examples/tti/operators.py
@@ -0,0 +1,158 @@
+from sympy import Eq, cos, sin
+from sympy.abc import h, s
+
+from devito import Operator, Forward, TimeData
+from examples.seismic import PointSource, Receiver
+from devito.finite_difference import centered, first_derivative, right, transpose
+from devito.dimension import x, y, z
+
+def ForwardOperator(model, source, receiver, time_order=2, space_order=4,
+                    save=False, **kwargs):
+    """
+    Constructor method for the forward modelling operator in an acoustic media
+
+    :param model: :class:`Model` object containing the physical parameters
+    :param src: None ot IShot() (not currently supported properly)
+    :param data: IShot() object containing the acquisition geometry and field data
+    :param: time_order: Time discretization order
+    :param: spc_order: Space discretization order
+    :param: u_ini : wavefield at the three first time step for non-zero initial condition
+    """
+    dt = model.critical_dt
+
+    m, damp, epsilon, delta, theta, phi = (model.m, model.damp, model.epsilon,
+                                           model.delta, model.theta, model.phi)
+
+    # Create symbols for forward wavefield, source and receivers
+    u = TimeData(name='u', shape=model.shape_domain, time_dim=source.nt,
+                 time_order=time_order, space_order=space_order, save=save,
+                 dtype=model.dtype)
+    v = TimeData(name='v', shape=model.shape_domain, time_dim=source.nt,
+                 time_order=time_order, space_order=space_order, save=save,
+                 dtype=model.dtype)
+    src = PointSource(name='src', ntime=source.nt, ndim=source.ndim,
+                      npoint=source.npoint)
+    rec = Receiver(name='rec', ntime=receiver.nt, ndim=receiver.ndim,
+                   npoint=receiver.npoint)
+
+    ang0 = cos(theta)
+    ang1 = sin(theta)
+    if len(model.shape) == 3:
+        ang2 = cos(phi)
+        ang3 = sin(phi)
+
+        # Derive stencil from symbolic equation
+        Gyp = (ang3 * u.dx - ang2 * u.dyr)
+        Gyy = (first_derivative(Gyp * ang3,
+                                dim=x, side=centered, order=space_order,
+                                matvec=transpose) -
+               first_derivative(Gyp * ang2,
+                                dim=y, side=right, order=space_order,
+                                matvec=transpose))
+        Gyp2 = (ang3 * u.dxr - ang2 * u.dy)
+        Gyy2 = (first_derivative(Gyp2 * ang3,
+                                 dim=x, side=right, order=space_order,
+                                 matvec=transpose) -
+                first_derivative(Gyp2 * ang2,
+                                 dim=y, side=centered, order=space_order,
+                                 matvec=transpose))
+
+        Gxp = (ang0 * ang2 * u.dx + ang0 * ang3 * u.dyr - ang1 * u.dzr)
+        Gzr = (ang1 * ang2 * v.dx + ang1 * ang3 * v.dyr + ang0 * v.dzr)
+        Gxx = (first_derivative(Gxp * ang0 * ang2,
+                                dim=x, side=centered, order=space_order,
+                                matvec=transpose) +
+               first_derivative(Gxp * ang0 * ang3,
+                                dim=y, side=right, order=space_order,
+                                matvec=transpose) -
+               first_derivative(Gxp * ang1,
+                                dim=z, side=right, order=space_order,
+                                matvec=transpose))
+        Gzz = (first_derivative(Gzr * ang1 * ang2,
+                                dim=x, side=centered, order=space_order,
+                                matvec=transpose) +
+               first_derivative(Gzr * ang1 * ang3,
+                                dim=y, side=right, order=space_order,
+                                matvec=transpose) +
+               first_derivative(Gzr * ang0,
+                                dim=z, side=right, order=space_order,
+                                matvec=transpose))
+        Gxp2 = (ang0 * ang2 * u.dxr + ang0 * ang3 * u.dy - ang1 * u.dz)
+        Gzr2 = (ang1 * ang2 * v.dxr + ang1 * ang3 * v.dy + ang0 * v.dz)
+        Gxx2 = (first_derivative(Gxp2 * ang0 * ang2,
+                                 dim=x, side=right, order=space_order,
+                                 matvec=transpose) +
+                first_derivative(Gxp2 * ang0 * ang3,
+                                 dim=y, side=centered, order=space_order,
+                                 matvec=transpose) -
+                first_derivative(Gxp2 * ang1,
+                                 dim=z, side=centered, order=space_order,
+                                 matvec=transpose))
+        Gzz2 = (first_derivative(Gzr2 * ang1 * ang2,
+                                 dim=x, side=right, order=space_order,
+                                 matvec=transpose) +
+                first_derivative(Gzr2 * ang1 * ang3,
+                                 dim=y, side=centered, order=space_order,
+                                 matvec=transpose) +
+                first_derivative(Gzr2 * ang0,
+                                 dim=z, side=centered, order=space_order,
+                                 matvec=transpose))
+        Hp = -(.5*Gxx + .5*Gxx2 + .5 * Gyy + .5*Gyy2)
+        Hzr = -(.5*Gzz + .5 * Gzz2)
+
+    else:
+        Gx1p = (ang0 * u.dxr - ang1 * u.dy)
+        Gz1r = (ang1 * v.dxr + ang0 * v.dy)
+        Gxx1 = (first_derivative(Gx1p * ang0, dim=x,
+                                 side=right, order=space_order,
+                                 matvec=transpose) -
+                first_derivative(Gx1p * ang1, dim=y,
+                                 side=centered, order=space_order,
+                                 matvec=transpose))
+        Gzz1 = (first_derivative(Gz1r * ang1, dim=x,
+                                 side=right, order=space_order,
+                                 matvec=transpose) +
+                first_derivative(Gz1r * ang0, dim=y,
+                                 side=centered, order=space_order,
+                                 matvec=transpose))
+        Gx2p = (ang0 * u.dx - ang1 * u.dyr)
+        Gz2r = (ang1 * v.dx + ang0 * v.dyr)
+        Gxx2 = (first_derivative(Gx2p * ang0, dim=x,
+                                 side=centered, order=space_order,
+                                 matvec=transpose) -
+                first_derivative(Gx2p * ang1, dim=y,
+                                 side=right, order=space_order,
+                                 matvec=transpose))
+        Gzz2 = (first_derivative(Gz2r * ang1, dim=x,
+                                 side=centered, order=space_order,
+                                 matvec=transpose) +
+                first_derivative(Gz2r * ang0, dim=y,
+                                 side=right, order=space_order,
+                                 matvec=transpose))
+
+        Hp = -(.5 * Gxx1 + .5 * Gxx2)
+        Hzr = -(.5 * Gzz1 + .5 * Gzz2)
+
+    stencilp = 1.0 / (2.0 * m + s * damp) * \
+        (4.0 * m * u + (s * damp - 2.0 * m) *
+         u.backward + 2.0 * s**2 * (epsilon * Hp + delta * Hzr))
+    stencilr = 1.0 / (2.0 * m + s * damp) * \
+        (4.0 * m * v + (s * damp - 2.0 * m) *
+         v.backward + 2.0 * s**2 * (delta * Hp + Hzr))
+
+    # Add substitutions for spacing (temporal and spatial)
+    subs = {s: dt, h: model.get_spacing()}
+    first_stencil = Eq(u.forward, stencilp)
+    second_stencil = Eq(v.forward, stencilr)
+    stencils = [first_stencil, second_stencil]
+
+    ti = u.indices[0]
+    stencils += src.inject(field=u, u_t=ti + 1, expr=src * dt * dt / m,
+                           offset=model.nbpml)
+    stencils += src.inject(field=v, u_t=ti + 1, expr=src * dt * dt / m,
+                           offset=model.nbpml)
+    stencils += rec.interpolate(expr=u, u_t=ti, offset=model.nbpml)
+    stencils += rec.interpolate(expr=v, u_t=ti, offset=model.nbpml)
+
+    return Operator(stencils=stencils, subs=subs,
+                    time_axis=Forward, name='ForwardTTI', **kwargs)
---
+++ examples/tti/wavesolver.py
@@ -0,0 +1,113 @@
+# coding: utf-8
+from cached_property import cached_property
+
+from devito.interfaces import DenseData, TimeData
+from examples.tti.operators import ForwardOperator
+from examples.seismic import PointSource, Receiver
+
+
+class AnisotropicWaveSolver(object):
+    """
+    Solver object that provides operators for seismic inversion problems
+    and encapsulates the time and space discretization for a given problem
+    setup.
+
+    :param model: Physical model with domain parameters
+    :param source: Sparse point symbol providing the injected wave
+    :param receiver: Sparse point symbol describing an array of receivers
+    :param time_order: Order of the time-stepping scheme (default: 2)
+    :param space_order: Order of the spatial stencil discretisation (default: 4)
+
+    Note: space_order must always be greater than time_order
+    """
+    def __init__(self, model, source, receiver,
+                 time_order=2, space_order=2, **kwargs):
+        self.model = model
+        self.source = source
+        self.receiver = receiver
+
+        self.time_order = time_order
+        self.space_order = space_order/2
+
+        # Time step can be \sqrt{3}=1.73 bigger with 4th order
+        self.dt = self.model.critical_dt
+        if self.time_order == 4:
+            self.dt *= 1.73
+
+        # Cache compiler options
+        self._kwargs = kwargs
+
+    @cached_property
+    def op_fwd(self):
+        """Cached operator for forward runs with buffered wavefield"""
+        return ForwardOperator(self.model, save=False, source=self.source,
+                               receiver=self.receiver, time_order=self.time_order,
+                               space_order=self.space_order, **self._kwargs)
+
+    @cached_property
+    def op_fwd_save(self):
+        """Cached operator for forward runs with unrolled wavefield"""
+        return ForwardOperator(self.model, save=True, source=self.source,
+                               receiver=self.receiver, time_order=self.time_order,
+                               space_order=self.space_order, **self._kwargs)
+
+    def forward(self, src=None, rec=None, u=None, v=None, m=None,
+                epsilon=None, delta=None, theta=None, phi=None,
+                save=False, **kwargs):
+        """
+        Forward modelling function that creates the necessary
+        data objects for running a forward modelling operator.
+
+        :param src: Symbol with time series data for the injected source term
+        :param rec: Symbol to store interpolated receiver data
+        :param u: (Optional) Symbol to store the computed wavefield
+        :param m: (Optional) Symbol for the time-constant square slowness
+        :param save: Option to store the entire (unrolled) wavefield
+
+        :returns: Receiver, wavefield and performance summary
+        """
+        # Source term is read-only, so re-use the default
+        if src is None:
+            src = self.source
+        # Create a new receiver object to store the result
+        if rec is None:
+            rec = Receiver(name='rec', ntime=self.receiver.nt,
+                           coordinates=self.receiver.coordinates.data)
+
+        # Create the forward wavefield if not provided
+        if u is None:
+            u = TimeData(name='u', shape=self.model.shape_domain,
+                         save=save, time_dim=self.source.nt,
+                         time_order=self.time_order,
+                         space_order=self.space_order,
+                         dtype=self.model.dtype)
+        # Create the forward wavefield if not provided
+        if v is None:
+            v = TimeData(name='v', shape=self.model.shape_domain,
+                         save=save, time_dim=self.source.nt,
+                         time_order=self.time_order,
+                         space_order=self.space_order,
+                         dtype=self.model.dtype)
+        # Pick m from model unless explicitly provided
+        if m is None:
+            m = m or self.model.m
+        if epsilon is None:
+            epsilon = epsilon or self.model.epsilon
+        if delta is None:
+            delta = delta or self.model.delta
+        if theta is None:
+            theta = theta or self.model.theta
+        if phi is None:
+            phi = phi or self.model.phi
+
+        # Execute operator and return wavefield and receiver data
+        if save:
+            summary = self.op_fwd_save.apply(src=src, rec=rec, u=u, v=v, m=m,
+                                             epsilon=epsilon, delta=delta,
+                                             theta=theta, phi=phi, **kwargs)
+        else:
+            summary = self.op_fwd.apply(src=src, rec=rec, u=u, v=v, m=m,
+                                        epsilon=epsilon, delta=delta,
+                                        theta=theta, phi=phi, **kwargs)
+        return rec, u, v, summary
+
