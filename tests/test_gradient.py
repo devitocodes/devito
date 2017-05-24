1e314aec27f53da749a500a37b8e5e89b4d31a8f
mloubout <mathias.louboutin@gmail.com>
2017-05-24 13:41:14 +0100

    Test Born accuracy

--- tests/test_gradient.py
+++ tests/test_gradient.py
@@ -9,7 +9,27 @@ from examples.seismic import Model, PointSource, Receiver
 @pytest.mark.parametrize('space_order', [4])
 @pytest.mark.parametrize('time_order', [2])
 @pytest.mark.parametrize('dimensions', [(70, 80)])
-def test_gradient(dimensions, time_order, space_order):
+def test_gradientFWI(dimensions, time_order, space_order):
+    """
+    This test ensure that the FWI gradient computed with devito
+    satisfies the Taylor expansion property:
+    .. math::
+        \Phi(m0 + h dm) = \Phi(m0) + \O(h) \\
+        \Phi(m0 + h dm) = \Phi(m0) + h \nabla \Phi(m0) + \O(h^2) \\
+        \Phi(m0) = .5* || F(m0 + h dm) - D ||_2^2
+
+    where
+    .. math::
+        \nabla \Phi(m0) = <J^T \delta d, dm> \\
+        \delta d = F(m0+ h dm) - D \\
+
+    with F the Forward modelling operator.
+    :param dimensions: size of the domain in all dimensions
+    in number of grid points
+    :param time_order: order of the time discretization scheme
+    :param space_order: order of the spacial discretization scheme
+    :return: assertion that the Taylor properties are satisfied
+    """
     nbpml = 40
 
     if len(dimensions) == 2:
@@ -140,5 +160,146 @@ def test_gradient(dimensions, time_order, space_order):
     assert np.isclose(p2[0], 2.0, rtol=0.1)
 
 
+@pytest.mark.parametrize('space_order', [4])
+@pytest.mark.parametrize('time_order', [2])
+@pytest.mark.parametrize('dimensions', [(70, 80)])
+def test_gradientJ(dimensions, time_order, space_order):
+    """
+    This test ensure that the Jacobian computed with devito
+    satisfies the Taylor expansion property:
+    .. math::
+        F(m0 + h dm) = F(m0) + \O(h) \\
+        F(m0 + h dm) = F(m0) + J dm + \O(h^2) \\
+
+    with F the Forward modelling operator.
+    :param dimensions: size of the domain in all dimensions
+    in number of grid points
+    :param time_order: order of the time discretization scheme
+    :param space_order: order of the spacial discretization scheme
+    :return: assertion that the Taylor properties are satisfied
+    """
+    nbpml = 40
+
+    if len(dimensions) == 2:
+        # Dimensions in 2D are (x, z)
+        origin = (0., 0.)
+        spacing = (10., 10.)
+
+        # True velocity
+        true_vp = np.ones(dimensions) + .5
+        true_vp[:, int(dimensions[1] / 2):] = 2.8
+
+        # Source location
+        location = np.zeros((1, 2))
+        location[0, 0] = origin[0] + dimensions[0] * spacing[0] * 0.5
+        location[0, 1] = origin[1] + 2 * spacing[1]
+
+        # Receiver coordinates
+        receiver_coords = np.zeros((101, 2))
+        receiver_coords[:, 0] = np.linspace(0, origin[0] +
+                                            dimensions[0] * spacing[0], num=101)
+        receiver_coords[:, 1] = location[0, 1]
+
+    elif len(dimensions) == 3:
+        # Dimensions in 3D are (x, y, z)
+        origin = (0., 0., 0.)
+        spacing = (15., 15., 15.)
+
+        # True velocity
+        true_vp = np.ones(dimensions) + .5
+        true_vp[:, :, int(dimensions[2] / 2):] = 2.8
+
+        # Source location
+        location = np.zeros((1, 3))
+        location[0, 0] = origin[0] + dimensions[0] * spacing[0] * 0.5
+        location[0, 1] = origin[1] + dimensions[1] * spacing[1] * 0.5
+        location[0, 2] = origin[1] + 2 * spacing[2]
+
+        # Receiver coordinates
+        receiver_coords = np.zeros((101, 3))
+        receiver_coords[:, 0] = np.linspace(0, origin[0] +
+                                            dimensions[0] * spacing[0], num=101)
+        receiver_coords[:, 1] = origin[1] + dimensions[1] * spacing[1] * 0.5
+        receiver_coords[:, 2] = location[0, 2]
+
+    # velocity models
+    def smooth10(vel):
+        out = np.zeros(dimensions)
+        out[:] = vel[:]
+        for a in range(5, dimensions[-1]-6):
+            if len(dimensions) == 2:
+                out[:, a] = np.sum(vel[:, a - 5:a + 5], axis=1) / 10
+            else:
+                out[:, :, a] = np.sum(vel[:, :, a - 5:a + 5], axis=2) / 10
+        return out
+
+    # Smooth velocity
+    initial_vp = smooth10(true_vp)
+    m0 = initial_vp**-2
+
+    # Model perturbation
+    dm = true_vp**-2 - initial_vp**-2
+    model = Model(origin, spacing, dimensions, true_vp, nbpml=nbpml)
+    model0 = Model(origin, spacing, dimensions, initial_vp, nbpml=nbpml)
+
+    f0 = .010
+    dt = model.critical_dt
+    if time_order == 4:
+        dt *= 1.73
+    t0 = 0.0
+    tn = 750.0
+    nt = int(1+(tn-t0)/dt)
+
+    # Set up the source as Ricker wavelet for f0
+    def source(t, f0):
+        r = (np.pi * f0 * (t - 1./f0))
+        return (1-2.*r**2)*np.exp(-r**2)
+
+    # Source geometry
+    time_series = np.zeros((nt, 1))
+    time_series[:, 0] = source(np.linspace(t0, tn, nt), f0)
+
+    # Define source and receivers and create acoustic wave solver
+    src = PointSource(name='src', data=time_series, coordinates=location)
+    rec = Receiver(name='rec', ntime=nt, coordinates=receiver_coords)
+    linrec = Receiver(name='rec', ntime=nt, coordinates=receiver_coords)
+    wave = AcousticWaveSolver(model, source=src, receiver=rec,
+                              time_order=time_order, space_order=space_order)
+
+    # Compute receiver data and full wavefield for the smooth velocity
+    rec, u0, _ = wave.forward(m=model0.m, save=False)
+
+    # Gradient: J dm
+    Jdm, _ , _, _ = wave.born(np.float32(model0.pad(dm)),
+                              rec=linrec, m=model0.m)
+    # FWI Gradient test
+    H = [0.5, 0.25, .125, 0.0625, 0.0312, 0.015625, 0.0078125]
+    error1 = np.zeros(7)
+    error2 = np.zeros(7)
+    for i in range(0, 7):
+        # Add the perturbation to the model
+        model0.m.data[:] = model0.pad(m0 + H[i] * dm)
+        # Data for the new model
+        d = wave.forward(m=model0.m)[0]
+        # First order error F(m0 + hdm) - F(m0)
+        error1[i] = np.linalg.norm(d.data - rec.data, 1)
+        # Second order term F(m0 + hdm) - F(m0) - J dm
+        error2[i] = np.linalg.norm(d.data - rec.data - H[i] * Jdm, 1)
+        # print(F0, .5*linalg.norm(d - rec)**2, error1[i], H[i] *G, error2[i])
+        # print('For h = ', H[i], '\nFirst order errors is : ', error1[i],
+              # '\nSecond order errors is ', error2[i])
+
+    hh = np.zeros(7)
+    for i in range(0, 7):
+        hh[i] = H[i] * H[i]
+
+    # Test slope of the  tests
+    p1 = np.polyfit(np.log10(H), np.log10(error1), 1)
+    p2 = np.polyfit(np.log10(H), np.log10(error2), 1)
+    print(p1)
+    print(p2)
+    assert np.isclose(p1[0], 1.0, rtol=0.1)
+    assert np.isclose(p2[0], 2.0, rtol=0.1)
+
 if __name__ == "__main__":
-    test_gradient(dimensions=(60, 70), time_order=2, space_order=4)
+    test_gradientJ(dimensions=(60, 70), time_order=2, space_order=4)
