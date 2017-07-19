45c5adcafa9d73785700666f4bdb37ce7e15cb60
Fabio Luporini <f.luporini12@imperial.ac.uk>
2017-07-19 18:21:43 +0200

    Tweak log_args() function

--- devito/yask/interfaces.py
+++ devito/yask/interfaces.py
@@ -16,11 +16,7 @@ class DenseData(interfaces.DenseData):
 
     def _allocate_memory(self):
         """Allocate memory in terms of Yask grids."""
-        # TODO: Refactor CMemory to be our _data_object, while _data will
-        # be the YaskGrid itself.
-
         debug("Allocating YaskGrid for %s (%s)" % (self.name, str(self.shape)))
-
         self._data_object = YaskGrid(self.name, self.shape, self.indices, self.dtype)
         if self._data_object is None:
             debug("Failed. Reverting to plain allocation...")
@@ -145,3 +141,9 @@ class YaskGrid(object):
     def ndpointer(self):
         # TODO: see corresponding comment in interfaces.py about CMemory
         return self
+
+    def view(self):
+        """
+        View of the YASK grid in standard (i.e., Devito) row-major layout.
+        """
+        return self[:]
