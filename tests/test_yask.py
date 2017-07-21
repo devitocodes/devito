f92cfcc57a96d3d9b14f5daf6256de8b7e9a8819
Fabio Luporini <f.luporini12@imperial.ac.uk>
2017-07-21 14:30:22 +0200

    yask: Update tests

--- tests/test_yask.py
+++ tests/test_yask.py
@@ -5,7 +5,7 @@ import pytest  # noqa
 
 pexpect = pytest.importorskip('yask_compiler')  # Run only if YASK is available
 
-from devito import Operator, DenseData, x, y, z, configuration  # noqa
+from devito import Operator, DenseData, t, x, y, z, configuration  # noqa
 from devito.yask.interfaces import YaskGrid  # noqa
 
 pytestmark = pytest.mark.skipif(configuration['backend'] != 'yask',
@@ -13,14 +13,15 @@ pytestmark = pytest.mark.skipif(configuration['backend'] != 'yask',
 
 
 def test_data_type():
-    u = DenseData(name='yu', shape=(10, 10, 10), dimensions=(x, y, z))
+    u = DenseData(name='yu3D', shape=(16, 16, 16), dimensions=(x, y, z))
     u.data  # Trigger initialization
     assert type(u._data_object) == YaskGrid
 
 
 @pytest.mark.xfail(reason="YASK always seems to use 3D grids")
 def test_data_movement_1D():
-    u = DenseData(name='yu1D', shape=(10,), dimensions=(x,))
+    u = DenseData(name='yu1D', shape=(16,), dimensions=(x,))
+    u.data
     assert type(u._data_object) == YaskGrid
 
     u.data[1] = 1.
@@ -30,7 +31,9 @@ def test_data_movement_1D():
 
 
 def test_data_movement_nD():
-    u = DenseData(name='yu3D', shape=(10, 10, 10), dimensions=(x, y, z))
+    u = DenseData(name='yu3D', shape=(16, 16, 16), dimensions=(x, y, z))
+    u.data
+    assert type(u._data_object) == YaskGrid
 
     # Test simple insertion and extraction
     u.data[0, 1, 1] = 1.
@@ -41,9 +44,9 @@ def test_data_movement_nD():
     assert 1. in u.data[0, 1]
 
     # Test negative indices
-    assert u.data[0, -9, -9] == 1.
+    assert u.data[0, -15, -15] == 1.
     u.data[6, 0, 0] = 1.
-    assert u.data[-4, :, :].sum() == 1.
+    assert u.data[-10, :, :].sum() == 1.
 
     # Test setting whole array to given value
     u.data[:] = 3.
@@ -54,14 +57,14 @@ def test_data_movement_nD():
     assert np.all(u.data[5, :, 5] == 5.)
 
     # Test insertion of block into block
-    block = np.ndarray(shape=(1, 10, 1), dtype=np.float32)
+    block = np.ndarray(shape=(1, 16, 1), dtype=np.float32)
     block.fill(4.)
     u.data[4, :, 4] = block
     assert np.all(u.data[4, :, 4] == block)
 
 
 def test_data_arithmetic_nD():
-    u = DenseData(name='yu3D', shape=(10, 10, 10), dimensions=(x, y, z))
+    u = DenseData(name='yu3D', shape=(16, 16, 16), dimensions=(x, y, z))
 
     # Simple arithmetic
     u.data[:] = 1
@@ -81,16 +84,13 @@ def test_data_arithmetic_nD():
 
     # Right operations __rOP__
     u.data[:] = 1.
-    arr = np.ndarray(shape=(10, 10, 10), dtype=np.float32)
+    arr = np.ndarray(shape=(16, 16, 16), dtype=np.float32)
     arr.fill(2.)
     assert np.all(arr - u.data == -1.)
 
 
-def test_simple_operator():
-    u = DenseData(name='yu', shape=(10, 10, 10), dimensions=(x, y, z))
-    u.data
-    # FIXME: Yask crashes because the support to run Devito operators through
-    # YASK is still mostly missing
-    # op = Operator(Eq(u, 1.))
-    # op.apply(u)
-    # assert np.allclose(u.data, 1)
+def test_trivial_operator():
+    u = DenseData(name='yu4D', shape=(2, 16, 16, 16), dimensions=(t, x, y, z))
+    op = Operator(Eq(u.indexed[t + 1, x, y, z], u.indexed[t, x, y, z] + 1.))
+    op(u, t=1)
+    assert np.all(u.data == 1.)
