ee94d5d86720550afc6d559d5d81ae37da87cc35
Michael Lange <michael.lange@imperial.ac.uk>
2017-08-10 13:34:03 +0100

    Acoustic: Trim the source and receiver creation for the example setup

--- examples/seismic/acoustic/acoustic_example.py
+++ examples/seismic/acoustic/acoustic_example.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from devito.logger import info
 from examples.seismic.acoustic import AcousticWaveSolver
-from examples.seismic import Model, PointSource, Receiver
+from examples.seismic import Model, RickerSource, Receiver
 
 
 # Velocity models
@@ -20,60 +20,44 @@ def smooth10(vel, shape):
     return out
 
 
-# Set up the source as Ricker wavelet for f0
-def source(t, f0):
-    r = (np.pi * f0 * (t - 1./f0))
-
-    return (1-2.*r**2)*np.exp(-r**2)
-
-
 def setup(dimensions=(50, 50, 50), spacing=(15.0, 15.0, 15.0), tn=500.,
           time_order=2, space_order=4, nbpml=10, **kwargs):
 
     ndim = len(dimensions)
+    nrec = dimensions[0]
     origin = tuple([0.]*ndim)
     spacing = spacing[:ndim]
+
     # Velocity model, two layers
     true_vp = np.ones(dimensions) + .5
     true_vp[..., int(dimensions[-1] / 3):dimensions[-1]] = 2.5
-    # Source location
-    location = np.zeros((1, ndim), dtype=np.float32)
-    location[0, :-1] = [origin[i] + dimensions[i] * spacing[i] * .5
-                        for i in range(ndim-1)]
-    location[0, -1] = origin[-1] + 2 * spacing[-1]
-    # Receivers locations
-    receiver_coords = np.zeros((dimensions[0], ndim), dtype=np.float32)
-    receiver_coords[:, 0] = np.linspace(0, origin[0] +
-                                        (dimensions[0]-1) * spacing[0],
-                                        num=dimensions[0])
-    receiver_coords[:, 1:] = location[0, 1:]
 
     # Define seismic data
     model = Model(origin, spacing, dimensions, true_vp, nbpml=int(nbpml))
 
-    f0 = .010
+    # Derive timestepping from model spacing
     dt = model.critical_dt
     if time_order == 4:
         dt *= 1.73
     t0 = 0.0
-    tn = tn
-    nt = int(1+(tn-t0)/dt)
+    nt = int(1 + (tn-t0) / dt)
+    time = np.linspace(t0, tn, nt)
 
-    # Set up the source as Ricker wavelet for f0
-    def source(t, f0):
-        r = (np.pi * f0 * (t - 1./f0))
-        return (1-2.*r**2)*np.exp(-r**2)
+    # Define source geometry (center of domain, just below surface)
+    src = RickerSource(name='src', ndim=ndim, f0=0.01, time=time)
+    src.coordinates.data[0, :] = np.array(model.domain_size) * .5
+    src.coordinates.data[0, -1] = model.origin[-1] + 2 * spacing[-1]
 
-    # Source geometry
-    time_series = np.zeros((nt, 1), dtype=np.float32)
-    time_series[:, 0] = source(np.linspace(t0, tn, nt), f0)
+    # Define receiver geometry (spread across x, lust below surface)
+    rec = Receiver(name='nrec', ntime=nt, npoint=nrec, ndim=ndim)
+    rec.coordinates.data[:, 0] = np.linspace(0., model.domain_size[0], num=nrec)
+    rec.coordinates.data[:, 1:] = src.coordinates.data[0, 1:]
 
-    # Define source and receivers and create acoustic wave solver
-    src = PointSource(name='src', data=time_series, coordinates=location)
-    rec = Receiver(name='rec', ntime=nt, coordinates=receiver_coords)
-    return AcousticWaveSolver(model, source=src, receiver=rec,
-                              time_order=time_order, space_order=space_order,
-                              **kwargs)
+    # Create solver object to provide relevant operators
+    solver = AcousticWaveSolver(model, source=src, receiver=rec,
+                                time_order=time_order,
+                                space_order=space_order, **kwargs)
+    return solver
 
 
 def run(dimensions=(50, 50, 50), spacing=(20.0, 20.0, 20.0), tn=1000.0,
