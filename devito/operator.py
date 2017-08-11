7dc22b3aaa5d01b75a92b9088d869f830770aecd
Fabio Luporini <f.luporini12@imperial.ac.uk>
2017-08-11 12:34:06 +0200

    operator: Refactor parameters derivation

--- devito/operator.py
+++ devito/operator.py
@@ -471,9 +471,12 @@ class OperatorRunnable(Operator):
     def _profile_sections(self, nodes, parameters):
         """Introduce C-level profiling nodes within the Iteration/Expression tree."""
         nodes, profiler = create_profile(nodes)
-        self._globals.append(profiler.cdef)
         parameters.append(Object(profiler.varname, profiler.dtype, profiler.setup()))
         return nodes, profiler
+
+    @property
+    def _cglobals(self):
+        return [self.profiler.cdef, blankline]
 
 
 # Misc helpers
