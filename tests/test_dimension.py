6241de4f048bdb8bc2c6cd8df61a2591e70129c3
Navjot Kukreja <navjotk@gmail.com>
2017-09-19 09:59:08 +0100

    Update tests

--- tests/test_dimension.py
+++ tests/test_dimension.py
@@ -1,15 +1,17 @@
 import devito
+import pytest
 from devito.dimension import FixedDimension
 from devito import DenseData, y
 
 
-def test1():
+@pytest.mark.xfail
+def test_incorrect_usage():
     m = DenseData(name="m", shape=(10, 10))
     devito.x = FixedDimension(name='x', size=10, spacing=devito.x.spacing)
     assert(devito.x in m.indices)
 
 
-def test2():
+def test_correct_usage():
     myx = FixedDimension(name='x', size=10, spacing=devito.x.spacing)
     m = DenseData(name="m", dimensions=(myx, y))
     assert(myx in m.indices)
