f8c07e2f74925d07e5834b922a22d20d74bb6adc
Fabio Luporini <f.luporini12@imperial.ac.uk>
2017-11-04 12:35:02 +0100

    ir: Reimplement Iteration analysis
    *MUCH* more robust, better structured, simpler to understand

--- devito/ir/iet/analysis.py
+++ devito/ir/iet/analysis.py
@@ -1,188 +1,165 @@
 """
 A collection of algorithms to analyze and decorate :class:`Iteration` in an
 Iteration/Expression tree. Decoration comes in the form of :class:`IterationProperty`
-objects, attached to nodes in the Iteration/Expression tree.
-
-The current implementation is based on simplistic analysis of Iteration/Expression
-trees. A better implementation would use a control flow graph, perhaps derived
-the Iteration/Expression tree itself. An ever better implementation would use
-a library such as ISL (the Integer Set Library).
+objects, attached to Iterations in the Iteration/Expression tree. The algorithms
+perform actual data dependence analysis.
 """
 
 from collections import OrderedDict
+from functools import cmp_to_key
 
 from devito.ir.iet import (Iteration, SEQUENTIAL, PARALLEL, VECTOR, WRAPPABLE,
-                           FindSections, IsPerfectIteration, NestedTransformer)
-from devito.symbolics import as_symbol
-from devito.tools import as_tuple, flatten
+                           MapIteration, NestedTransformer, retrieve_iteration_tree)
+from devito.ir.support import Scope
+from devito.tools import as_tuple
 
 __all__ = ['analyze_iterations']
 
 
-def analyze_iterations(nodes):
+class Analysis(object):
+
+    _cache = {}
+
+    def __new__(cls, iet):
+        if iet in Analysis._cache:
+            return Analysis._cache[iet]
+        else:
+            obj = super(Analysis, cls).__new__(cls)
+            obj.__init__(iet)
+            Analysis._cache[iet] = obj
+            return obj
+
+    def __init__(self, iet):
+        self.iet = iet
+        self.properties = OrderedDict()
+
+        self.trees = retrieve_iteration_tree(iet)
+        self.scopes = OrderedDict([(k, Scope([i.expr for i in v]))
+                                   for k, v in MapIteration().visit(iet).items()])
+
+    def update(self, properties):
+        for k, v in properties.items():
+            self.properties.setdefault(k, []).append(v)
+
+
+def propertizer(func):
+    def wrapper(arg):
+        analysis = Analysis(arg) if not isinstance(arg, Analysis) else arg
+        func(analysis)
+        return analysis
+    return wrapper
+
+
+def analyze_iterations(iet):
     """
     Attach :class:`IterationProperty` to :class:`Iteration` objects within
     ``nodes``. The recognized IterationProperty decorators are listed in
     ``nodes.IterationProperty._KNOWN``.
     """
-    sections = FindSections().visit(nodes)
+    analysis = mark_parallel(iet)
+    analysis = mark_vectorizable(analysis)
+    analysis = mark_wrappable(analysis)
 
-    # Local analysis: detect Iteration properties, inspecting trees in isolation
+    # Decorate the Iteration/Expression tree with the found properties
     mapper = OrderedDict()
-    for tree, exprs in sections.items():
-        deps_graph = compute_dependency_graph(exprs)
-
-        mapper = detect_fully_parallel(tree, deps_graph, mapper)
-        mapper = detect_outermost_parallel(tree, deps_graph, mapper)
-        mapper = detect_outermost_sequential_inner_parallel(tree, deps_graph, mapper)
-        mapper = detect_innermost_unitstride(tree, deps_graph, mapper)
-        mapper = detect_wrappable_iterations(tree, deps_graph, mapper)
-
-    # Global analysis
-    for k, v in list(mapper.items()):
+    for k, v in list(analysis.properties.items()):
         args = k.args
-        # SEQUENTIAL kills PARALLEL
-        properties = [i for i in v if i != PARALLEL] if SEQUENTIAL in v else v
-        properties = as_tuple(args.pop('properties')) + as_tuple(properties)
+        properties = as_tuple(args.pop('properties')) + as_tuple(v)
         mapper[k] = Iteration(properties=properties, **args)
-
-    # Store the discovered properties in the Iteration/Expression tree
-    processed = NestedTransformer(mapper).visit(nodes)
+    processed = NestedTransformer(mapper).visit(iet)
 
     return processed
 
 
-def compute_dependency_graph(exprs):
-    """
-    Given an ordered list of :class:`Expression`, build a mapper from lvalues
-    to reads occurring in the rvalues.
-    """
-    deps_graph = OrderedDict()
-    writes = [e.output for e in exprs if e.is_tensor]
-    for i in writes:
-        for e in exprs:
-            i_reads = [j for j in e.reads if as_symbol(i) == as_symbol(j)]
-            deps_graph.setdefault(i, []).extend(i_reads)
-    return deps_graph
+@propertizer
+def mark_parallel(analysis):
+    """Update the ``analysis`` detecting the ``SEQUENTIAL`` and ``PARALLEL``
+    Iterations within ``analysis.iet``."""
+    properties = OrderedDict()
+    for tree in analysis.trees:
+        for depth, i in enumerate(tree):
+            if i in properties:
+                continue
+            dims = [j.dim for j in tree[:depth + 1]]
+            # The i-th Iteration is PARALLEL if for all dependences (d_1, ..., d_n):
+            # (d_1, ..., d_{i-1}) > 0, OR
+            # (d_1, ..., d_i) = 0
+            is_sequential = False
+            for dep in analysis.scopes[i].d_all:
+                if not ((dims[:-1] and any(dep.is_carried(d) for d in dims[:-1])) or
+                        all(dep.is_independent(d) for d in dims)):
+                    is_sequential = True
+                    break
+            properties[i] = SEQUENTIAL if is_sequential else PARALLEL
+    analysis.update(properties)
 
 
-def detect_fully_parallel(tree, deps_graph, mapper=None):
-    """
-    Update ``mapper``, a dictionary from :class:`Iteration`s to
-    :class:`IterationProperty`s, by annotating nested, fully-parallel Iterations.
-    """
-    if mapper is None:
-        mapper = OrderedDict()
-    is_FP = True
-    for k, v in deps_graph.items():
-        is_FP &= all(k.indices == i.indices for i in v)
-    if is_FP:
+@propertizer
+def mark_vectorizable(analysis):
+    """Update the ``analysis`` detecting the ``VECTOR`` Iterations within
+    ``analysis.iet``. An Iteration is VECTOR iff:
+
+        * it's the innermost in an Iteration tree, AND
+        * it's got at least an outer PARALLEL Iteration, AND
+        * it's been marked as PARALLEL or all the accesses along its dimension
+          are unit-strided.
+        """
+    for tree in analysis.trees:
+        if len(tree) == 1:
+            continue
+        else:
+            outer, innermost = tree[-2], tree[-1]
+            if PARALLEL not in analysis.properties.get(outer, []):
+                continue
+            elif PARALLEL in analysis.properties.get(innermost, []):
+                analysis.properties[innermost].append(VECTOR)
+            else:
+                accesses = analysis.scopes[innermost].accesses
+                if not accesses:
+                    continue
+                if all(accesses[0][innermost.dim] == i[innermost.dim] for i in accesses):
+                    analysis.update({innermost: VECTOR})
+
+
+@propertizer
+def mark_wrappable(analysis):
+    """Update the ``analysis`` detecting the ``WRAPPABLE`` Iterations within
+    ``analysis.iet``."""
+    # All potential WRAPPABLEs are Stepping dimensions
+    stepper = None
+    for iteration in analysis.scopes:
+        if not iteration.dim.is_Stepping:
+            continue
+        stepper = iteration
+    if not stepper:
+        return
+    stepping = stepper.dim
+    accesses = [i for i in analysis.scopes[stepper].accesses if stepping in i.findices]
+    if not accesses:
+        return
+    # Pick the /back/ and /front/ slots accessed
+    try:
+        accesses = sorted(accesses, key=cmp_to_key(lambda i, j: i.distance(j, stepping)))
+        back, front = accesses[0][stepping], accesses[-1][stepping]
+    except TypeError:
+        return
+    if back == front:
+        return
+    # Finally check that all data dependences would be honored by using the
+    # /front/ index function in place of the /back/ index function
+    for tree in analysis.trees:
         for i in tree:
-            mapper.setdefault(i, []).append(PARALLEL)
-    return mapper
-
-
-def detect_outermost_parallel(tree, deps_graph, mapper=None):
-    """
-    Update ``mapper``, a dictionary from :class:`Iteration`s to
-    :class:`IterationProperty`s, by annotating the outermost Iteration if this
-    turns out to be parallel.
-    """
-    if mapper is None:
-        mapper = OrderedDict()
-    is_OP = True
-    for k, v in deps_graph.items():
-        for i in v:
-            is_OP &= k.indices[0] == i.indices[0]
-            is_OP &= all(k.indices[0].free_symbols.isdisjoint(j.free_symbols)
-                         for j in i.indices[1:])  # not A[x,y] = A[x,x+1]
-    if is_OP:
-        mapper.setdefault(tree[0], []).append(PARALLEL)
-    return mapper
-
-
-def detect_outermost_sequential_inner_parallel(tree, deps_graph, mapper=None):
-    """
-    Update ``mapper``, a dictionary from :class:`Iteration`s to
-    :class:`IterationProperty`s, by annotating the outermost Iteration if this
-    turns out to be sequential and, if that's the case, by annotating the inner
-    Iterations as parallel.
-    """
-    if mapper is None:
-        mapper = OrderedDict()
-    candidate = tree[0]
-    filtered_mapper = OrderedDict()
-    for k, v in deps_graph.items():
-        if candidate.dim == k.base.function.indices[0] and len(v) > 0:
-            filtered_mapper[k] = v
-    if not filtered_mapper:
-        # The outermost Iteration is actually parallel
-        return mapper
-    if len({i.base.function.indices[0] for i in filtered_mapper}) > 1:
-        # Must be u[t+1] = ... v[t+1] = ... , not u[t+1] = ... v[t+2] = ...
-        return mapper
-    is_OS = True
-    for k, v in filtered_mapper.items():
-        # At least one access along the candidate dimension differs (u[t+1] = u[t] ...)
-        # AND the others either differ too or are identical to the LHS, that is
-        # u[t+1, x] = u[t, x] + u[t+1, x] OK, but u[t+1, x] = u[t, x] + u[t+1, x-1] NO
-        is_OS &= all(k.base.function.indices[0] == i.base.function.indices[0] for i in v)
-        is_OS &= any(k.indices[0] != i.indices[0] for i in v)
-        is_OS &= all(k.indices[0] != i.indices[0] or
-                     k.indices[1:] == i.indices[1:] for i in v)
-    if is_OS:
-        mapper.setdefault(candidate, []).append(SEQUENTIAL)
-        for i in tree[tree.index(candidate) + 1:]:
-            mapper.setdefault(i, []).append(PARALLEL)
-    return mapper
-
-
-def detect_innermost_unitstride(tree, deps_graph, mapper=None):
-    """
-    Update ``mapper``, a dictionary from :class:`Iteration`s to
-    :class:`IterationProperty`s, by annotating the innermost Iteration as
-    vectorizable if all array accesses along its dimension turn out to
-    be unit-strided.
-    """
-    if mapper is None:
-        mapper = OrderedDict()
-    innermost = tree[-1]
-    if not IsPerfectIteration().visit(innermost):
-        return mapper
-    if len(tree) == 1 or SEQUENTIAL in mapper.get(tree[-2], []):
-        # Heuristic: there should be at least an outer parallel Iteration
-        # to mark /innermost/ as vectorizable, otherwise it is preferable
-        # to save it for shared-memory parallelism
-        return mapper
-    is_US = True
-    for k, v in deps_graph.items():
-        is_US &= all(k.indices[-1] == i.indices[-1] for i in v)
-    if is_US or PARALLEL in mapper.get(innermost, []):
-        mapper.setdefault(innermost, []).append(VECTOR)
-    return mapper
-
-
-def detect_wrappable_iterations(tree, deps_graph, mapper=None):
-    """
-    Update ``mapper``, a dictionary from :class:`Iteration`s to
-    :class:`IterationProperty`s, by annotating an Iteration as wrappable
-    if the first and last slots accessed through modulo buffered iteration
-    can be mapped to a single slot, thus reducing the working set.
-    """
-    if mapper is None:
-        mapper = OrderedDict()
-    stepping = [i for i in tree if i.dim.is_Stepping]
-    if len(stepping) != 1:
-        return mapper
-    stepping = stepping[0]
-    is_WP = all(stepping.dim == i.base.function.indices[0] for i in deps_graph)
-    if is_WP:
-        accesses = {i.indices[0] for i in deps_graph}
-        accesses |= {i.indices[0] for i in flatten(deps_graph.values())}
-        candidate = sorted(accesses, key=lambda i: i.subs(stepping.dim, 0))[0]
-        for k, v in deps_graph.items():
-            is_WP &= all(k.indices[1:] == i.indices[1:] for i in v
-                         if candidate == i.indices[0])
-    if is_WP:
-        mapper.setdefault(stepping, []).append(WRAPPABLE)
-    return mapper
+            if i == stepper:
+                continue
+            # There must be NO writes to the /back/ timeslot
+            for access in analysis.scopes[i].accesses:
+                if access.is_write and access[stepping] == back:
+                    return
+            # All reads from the /front/ timeslot must not cause dependences with
+            # the writes in the /back/ timeslot along the /i/ dimension
+            for dep in analysis.scopes[i].d_flow:
+                if dep.source[stepping] != front or dep.sink[stepping] != back:
+                    continue
+                if dep.source.section(stepping) != dep.sink.section(stepping):
+                    return
+    analysis.update({stepper: WRAPPABLE})
