d3dc48a0512746f5dc131acd33bcc51914e1f59d
Navjot Kukreja <navjotk@gmail.com>
2017-11-13 10:09:57 -0700

    Cleanup

--- examples/checkpointing/checkpointing_example.py
+++ examples/checkpointing/checkpointing_example.py
@@ -1,19 +1,18 @@
-import numpy as np
-from numpy import linalg
 from math import floor
 from cached_property import cached_property
 
-from devito import Function, TimeFunction
-from examples.seismic import Receiver, demo_model, RickerSource
-from examples.seismic.acoustic import ForwardOperator, GradientOperator, smooth10
+from devito import TimeFunction
+from examples.seismic.acoustic import GradientOperator
 from examples.checkpointing.checkpoint import DevitoCheckpoint, CheckpointOperator
 from examples.seismic.acoustic.gradient_example import GradientExample
 from pyrevolve import Revolver
 
+
 class CheckpointingExample(GradientExample):
     @cached_property
     def forward_field(self):
-        return TimeFunction(name="u", grid=self.model.grid, time_order=self.time_order, space_order=self.space_order, save=False)
+        return TimeFunction(name="u", grid=self.model.grid, time_order=self.time_order,
+                            space_order=self.space_order, save=False)
 
     @cached_property
     def forward_operator(self):
@@ -22,14 +21,16 @@ class CheckpointingExample(GradientExample):
 
     @cached_property
     def gradient_operator(self):
-        return GradientOperator(self.model, self.src, self.rec_g, time_order=self.time_order,
-                              spc_order=self.space_order, save=False)
+        return GradientOperator(self.model, self.src, self.rec_g,
+                                time_order=self.time_order, spc_order=self.space_order,
+                                save=False)
 
     def gradient(self, m0, maxmem=None):
         cp = DevitoCheckpoint([self.forward_field])
         n_checkpoints = None
         if maxmem is not None:
-            n_checkpoints = int(floor(maxmem*10**6/(cp.size*self.forward_field.data.itemsize)))
+            n_checkpoints = int(floor(maxmem * 10**6/
+                                      (cp.size*self.forward_field.data.itemsize)))
 
         wrap_fw = CheckpointOperator(self.forward_operator, {'u': self.forward_field, 'rec': self.rec, 'm': m0, 'src': self.src, 'dt': self.dt})
         wrap_rev = CheckpointOperator(self.gradient_operator, {'u': self.forward_field, 'v': self.adjoint_field, 'm': m0, 'rec': self.rec_g, 'grad': self.grad, 'dt': self.dt})
