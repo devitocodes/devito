ad938e99f13765022ecacdbf73124ce2f8469e1c
Michael Lange <michael.lange@imperial.ac.uk>
2017-11-28 12:28:59 +0000

    Benchmark: Convert old plotting routine to new setup

--- examples/seismic/benchmark.py
+++ examples/seismic/benchmark.py
@@ -167,6 +167,111 @@ def bench(problem, **kwargs):
     bench.save()
 
 
+@benchmark.command()
+@option_simulation
+@option_performance
+@click.option('-r', '--resultsdir', default='results',
+              help='Directory containing results')
+@click.option('-p', '--plotdir', default='plots',
+              help='Directory containing plots')
+@click.option('--arch', default='unknown',
+              help='Architecture on which the simulation is/was run')
+@click.option('--max_bw', type=float,
+              help='Max GB/s of the DRAM')
+@click.option('--max_flops', type=float,
+              help='Max GFLOPS/s of the CPU')
+@click.option('--point_runtime', is_flag=True,
+              help='Annotate points with runtime values')
+def plot(problem, **kwargs):
+    try:
+        from opescibench import Benchmark, RooflinePlotter
+    except:
+        raise ImportError("Could not import opescibench utility package.\n"
+                          "Please install https://github.com/opesci/opescibench "
+                          "and Matplotlib to plot performance results")
+    resultsdir = kwargs.pop('resultsdir')
+    plotdir = kwargs.pop('plotdir')
+    arch = kwargs.pop('arch')
+    max_bw = kwargs.pop('max_bw')
+    max_flops = kwargs.pop('max_flops')
+    point_runtime = kwargs.pop('point_runtime')
+
+    bench = Benchmark(name=problem, resultsdir=resultsdir, parameters=kwargs)
+    bench.load()
+    if not bench.loaded:
+        warning("Could not load any results, nothing to plot. Exiting...")
+        sys.exit(0)
+
+    gflopss = bench.lookup(params=kwargs, measure="gflopss", event='main')
+    oi = bench.lookup(params=kwargs, measure="oi", event='main')
+    time = bench.lookup(params=kwargs, measure="timings", event='main')
+
+    name = "%s_dim%s_so%s_to%s_arch[%s].pdf" % (problem,
+                                                kwargs['shape'],
+                                                kwargs['space_order'],
+                                                kwargs['time_order'],
+                                                arch)
+    name = name.replace(' ', '')
+    problem_styles = {'acoustic': 'Acoustic', 'tti': 'TTI'}
+    title = "%s [grid=%s, TO=%s, duration=%sms], varying <DSE,DLE> on %s" %\
+        (problem_styles[problem], list(kwargs['shape']), kwargs['time_order'],
+         kwargs['tn'], arch)
+
+    dse_runs = ["basic", "advanced", "speculative", "aggressive"]
+    dle_runs = ["basic", "advanced", "speculative"]
+    styles = {  # (marker, color)
+        # DLE basic
+        ('basic', 'basic'): ('D', 'r'),
+        ('advanced', 'basic'): ('D', 'g'),
+        ('speculative', 'basic'): ('D', 'y'),
+        ('aggressive', 'basic'): ('D', 'b'),
+        # DLE advanced
+        ('basic', 'advanced'): ('o', 'r'),
+        ('advanced', 'advanced'): ('o', 'g'),
+        ('speculative', 'advanced'): ('o', 'y'),
+        ('aggressive', 'advanced'): ('o', 'b'),
+        # DLE speculative
+        ('basic', 'speculative'): ('s', 'r'),
+        ('advanced', 'speculative'): ('s', 'g'),
+        ('speculative', 'speculative'): ('s', 'y'),
+        ('aggressive', 'speculative'): ('s', 'b')
+    }
+
+    # Find min and max runtimes for instances having the same OI
+    min_max = {v: [0, sys.maxsize] for v in oi.values()}
+    for k, v in time.items():
+        i = oi[k]
+        min_max[i][0] = v if min_max[i][0] == 0 else min(v, min_max[i][0])
+        min_max[i][1] = v if min_max[i][1] == sys.maxsize else max(v, min_max[i][1])
+
+    with RooflinePlotter(title=title, figname=name, plotdir=plotdir,
+                         max_bw=max_bw, max_flops=max_flops,
+                         fancycolor=True, legend={'fontsize': 5, 'ncol': 4}) as plot:
+        for key, gflopss in gflopss.items():
+            oi_value = oi[key]
+            time_value = time[key]
+            key = dict(key)
+            run = (key["dse"], key["dle"])
+            label = "<%s,%s>" % run
+            oi_loc = 0.05 if len(str(key["space_order"])) == 1 else 0.06
+            oi_annotate = {'s': 'SO=%s' % key["space_order"],
+                           'size': 4, 'xy': (oi_value, oi_loc)} if run[0] else None
+            if time_value in min_max[oi_value] and point_runtime:
+                # Only annotate min and max runtimes on each OI line, to avoid
+                # polluting the plot too much
+                point_annotate = {'s': "%.1f s" % time_value, 'xytext': (0, 5.2),
+                                  'size': 3.5, 'weight': 'bold', 'rotation': 0}
+            else:
+                point_annotate = None
+            oi_line = time_value == min_max[oi_value][0]
+            if oi_line:
+                perf_annotate = {'size': 4, 'xytext': (-4, 4)}
+            plot.add_point(gflops=gflopss, oi=oi_value, marker=styles[run][0],
+                           color=styles[run][1], oi_line=oi_line, label=label,
+                           perf_annotate=perf_annotate, oi_annotate=oi_annotate,
+                           point_annotate=point_annotate)
+
+
 if __name__ == "__main__":
     benchmark()
 
