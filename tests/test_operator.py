e1eac9e86b2879eaf01368a2881ce75fb01c6497
Fabio Luporini <f.luporini12@imperial.ac.uk>
2017-12-05 16:43:27 +0100

    Share Data tests between core and yask

--- tests/test_operator.py
+++ tests/test_operator.py
@@ -57,9 +57,85 @@ class TestAPI(object):
         assert op.parameters[5].is_PtrArgument
         assert 'a_dense[i] = 2.0F*constant + a_dense[i]' in str(op.ccode)
 
+
+class TestData(object):
+
+    @classmethod
+    def setup_class(cls):
+        clear_cache()
+
+    @pytest.fixture
+    def u(self):
+        grid = Grid(shape=(16, 16, 16))
+        u = Function(name='yu3D', grid=grid, space_order=0)
+        return u
+
+    def test_indexing(self, u):
+        """
+        Tests packing/unpacking :class:`Data` objects.
+        """
+        # Test simple insertion and extraction
+        u.data[0, 1, 1] = 1.
+        assert u.data[0, 0, 0] == 0.
+        assert u.data[0, 1, 1] == 1.
+        assert np.all(u.data == u.data[:, :, :])
+        assert 1. in u.data[0]
+        assert 1. in u.data[0, 1]
+
+        # Test negative indices
+        assert u.data[0, -15, -15] == 1.
+        u.data[6, 0, 0] = 1.
+        assert u.data[-10, :, :].sum() == 1.
+
+        # Test setting whole array to given value
+        u.data[:] = 3.
+        assert np.all(u.data == 3.)
+
+        # Test insertion of single value into block
+        u.data[5, :, 5] = 5.
+        assert np.all(u.data[5, :, 5] == 5.)
+
+        # Test extraction of block with negative indices
+        sliced = u.data[-11, :, -11]
+        assert sliced.shape == (16,)
+        assert np.all(sliced == 5.)
+
+        # Test insertion of block into block
+        block = np.ndarray(shape=(1, 16, 1), dtype=np.float32)
+        block.fill(4.)
+        u.data[4:5, :, 4:5] = block
+        assert np.all(u.data[4, :, 4] == block)
+
+    def test_arithmetic(self, u):
+        """
+        Tests arithmetic operations between :class:`Data` objects and values.
+        """
+        u.data[:] = 1
+
+        # Simple arithmetic
+        assert np.all(u.data == 1)
+        assert np.all(u.data + 2. == 3.)
+        assert np.all(u.data - 2. == -1.)
+        assert np.all(u.data * 2. == 2.)
+        assert np.all(u.data / 2. == 0.5)
+        assert np.all(u.data % 2 == 1.)
+
+        # Increments and partial increments
+        u.data[:] += 2.
+        assert np.all(u.data == 3.)
+        u.data[9, :, :] += 1.
+        assert all(np.all(u.data[i, :, :] == 3.) for i in range(9))
+        assert np.all(u.data[9, :, :] == 4.)
+
+        # Right operations __rOP__
+        u.data[:] = 1.
+        arr = np.ndarray(shape=(16, 16, 16), dtype=np.float32)
+        arr.fill(2.)
+        assert np.all(arr - u.data == 1.)
+
     def test_logic_indexing(self):
         """
-        Tests that logic indexing for stepping dimensions works.
+        Tests logic indexing for stepping dimensions.
         """
         grid = Grid(shape=(4, 4, 4))
         nt = 5
