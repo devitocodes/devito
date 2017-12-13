a28dacb6078e2e5e59289f21218ec7e126e8247e
Fabio Luporini <f.luporini12@imperial.ac.uk>
2017-12-13 16:40:58 +0100

    benchmark: Ease running and plotting

--- examples/seismic/benchmark.py
+++ examples/seismic/benchmark.py
@@ -1,4 +1,6 @@
+from collections import OrderedDict
 import sys
+
 import numpy as np
 import click
 
@@ -82,6 +84,8 @@ def option_performance(f):
                      callback=from_preset, expose_value=False,
                      type=click.Choice(['maxperf', 'dse', 'dle']), default='maxperf',
                      help='Choose what to benchmark; ignored if execmode=run'),
+        click.option('--arch', default='unknown',
+                     help='Architecture on which the simulation is/was run'),
         click.option('--dse', callback=from_value,
                      type=click.Choice(['noop'] + configuration._accepted['dse']),
                      help='Devito symbolic engine (DSE) mode'),
@@ -163,31 +167,12 @@ def bench(problem, **kwargs):
     """
     Complete benchmark with multiple simulation and performance parameters.
     """
-    try:
-        from opescibench import Benchmark, Executor
-    except:
-        raise ImportError('Could not import opescibench utility package.\n'
-                          'Please install https://github.com/opesci/opescibench')
-
     run = tti_run if problem == 'tti' else acoustic_run
     resultsdir = kwargs.pop('resultsdir')
     repeats = kwargs.pop('repeats')
 
-    class BenchExecutor(Executor):
-        """Executor class that defines how to run the benchmark"""
-
-        def run(self, *args, **kwargs):
-            gflopss, oi, timings, _ = run(*args, **kwargs)
-
-            for key in timings.keys():
-                self.register(gflopss[key], measure="gflopss", event=key)
-                self.register(oi[key], measure="oi", event=key)
-                self.register(timings[key], measure="timings", event=key)
-
-            clear_cache()
-
-    bench = Benchmark(name=problem, resultsdir=resultsdir, parameters=kwargs)
-    bench.execute(BenchExecutor(), warmups=0, repeats=repeats)
+    bench = get_ob_bench(problem, resultsdir, kwargs)
+    bench.execute(get_ob_exec(run), warmups=0, repeats=repeats)
     bench.save()
 
 
@@ -196,15 +181,11 @@ def bench(problem, **kwargs):
 @option_performance
 @click.option('-r', '--resultsdir', default='results',
               help='Directory containing results')
-@click.option('-p', '--plotdir', default='plots',
-              help='Directory containing plots')
-@click.option('--arch', default='unknown',
-              help='Architecture on which the simulation is/was run')
-@click.option('--max_bw', type=float,
+@click.option('--max-bw', type=float,
               help='Max GB/s of the DRAM')
-@click.option('--max_flops', type=float,
+@click.option('--max-flops', type=float,
               help='Max GFLOPS/s of the CPU')
-@click.option('--point_runtime', is_flag=True,
+@click.option('--point-runtime', is_flag=True, default=True,
               help='Annotate points with runtime values')
 def cli_plot(problem, **kwargs):
     """
@@ -217,20 +198,15 @@ def plot(problem, **kwargs):
     """
     Plotting mode to generate plots for performance analysis.
     """
-    try:
-        from opescibench import Benchmark, RooflinePlotter
-    except:
-        raise ImportError("Could not import opescibench utility package.\n"
-                          "Please install https://github.com/opesci/opescibench "
-                          "and Matplotlib to plot performance results")
     resultsdir = kwargs.pop('resultsdir')
-    plotdir = kwargs.pop('plotdir')
-    arch = kwargs.pop('arch')
     max_bw = kwargs.pop('max_bw')
     max_flops = kwargs.pop('max_flops')
     point_runtime = kwargs.pop('point_runtime')
 
-    bench = Benchmark(name=problem, resultsdir=resultsdir, parameters=kwargs)
+    # Access opescibench
+    RooflinePlotter = get_ob_plotter()
+    bench = get_ob_bench(problem, resultsdir, kwargs)
+
     bench.load()
     if not bench.loaded:
         warning("Could not load any results, nothing to plot. Exiting...")
@@ -241,15 +217,14 @@ def plot(problem, **kwargs):
     time = bench.lookup(params=kwargs, measure="timings", event='main')
 
     name = "%s_dim%s_so%s_to%s_arch[%s].pdf" % (problem,
-                                                kwargs['shape'],
+                                                list(kwargs['shape']),
                                                 kwargs['space_order'],
                                                 kwargs['time_order'],
-                                                arch)
-    name = name.replace(' ', '')
+                                                kwargs['arch'])
     problem_styles = {'acoustic': 'Acoustic', 'tti': 'TTI'}
     title = "%s [grid=%s, TO=%s, duration=%sms], varying <DSE,DLE> on %s" %\
         (problem_styles[problem], list(kwargs['shape']), kwargs['time_order'],
-         kwargs['tn'], arch)
+         kwargs['tn'], kwargs['arch'])
 
     styles = {  # (marker, color)
         # DLE basic
@@ -276,7 +251,7 @@ def plot(problem, **kwargs):
         min_max[i][0] = v if min_max[i][0] == 0 else min(v, min_max[i][0])
         min_max[i][1] = v if min_max[i][1] == sys.maxsize else max(v, min_max[i][1])
 
-    with RooflinePlotter(title=title, figname=name, plotdir=plotdir,
+    with RooflinePlotter(title=title, figname=name.replace(' ', ''), plotdir=resultsdir,
                          max_bw=max_bw, max_flops=max_flops,
                          fancycolor=True, legend={'fontsize': 5, 'ncol': 4}) as plot:
         for key, gflopss in gflopss.items():
@@ -302,6 +277,70 @@ def plot(problem, **kwargs):
                            color=styles[run][1], oi_line=oi_line, label=label,
                            perf_annotate=perf_annotate, oi_annotate=oi_annotate,
                            point_annotate=point_annotate)
+
+
+def get_ob_bench(problem, resultsdir, parameters):
+    """Return a special :class:`opescibench.Benchmark` to manage performance runs."""
+    try:
+        from opescibench import Benchmark
+    except:
+        raise ImportError('Could not import opescibench utility package.\n'
+                          'Please install https://github.com/opesci/opescibench')
+
+    class DevitoBenchmark(Benchmark):
+
+        def param_string(self, params):
+            devito_params, params = OrderedDict(), dict(params)
+            devito_params['arch'] = params['arch']
+            devito_params['shape'] = ",".join(str(i) for i in params['shape'])
+            devito_params['nbpml'] = params['nbpml']
+            devito_params['tn'] = params['tn']
+            devito_params['so'] = params['space_order']
+            devito_params['to'] = params['time_order']
+            devito_params['dse'] = params['dse']
+            devito_params['dle'] = params['dle']
+            devito_params['at'] = params['autotune'] and configuration.core['autotuning']
+            return '_'.join(['%s[%s]' % (k, v) for k, v in devito_params.items()])
+
+    return DevitoBenchmark(name=problem, resultsdir=resultsdir, parameters=parameters)
+
+
+def get_ob_exec(func):
+    """Return a special :class:`opescibench.Executor` to execute performance runs."""
+    try:
+        from opescibench import Executor
+    except:
+        raise ImportError('Could not import opescibench utility package.\n'
+                          'Please install https://github.com/opesci/opescibench')
+
+    class DevitoExecutor(Executor):
+
+        def __init__(self, func):
+            super(DevitoExecutor, self).__init__()
+            self.func = func
+
+        def run(self, *args, **kwargs):
+            gflopss, oi, timings, _ = self.func(*args, **kwargs)
+
+            for key in timings.keys():
+                self.register(gflopss[key], measure="gflopss", event=key)
+                self.register(oi[key], measure="oi", event=key)
+                self.register(timings[key], measure="timings", event=key)
+
+            clear_cache()
+
+    return DevitoExecutor(func)
+
+
+def get_ob_plotter():
+    try:
+        from opescibench import RooflinePlotter
+    except:
+        raise ImportError('Could not import opescibench utility package.\n'
+                          'Please install https://github.com/opesci/opescibench'
+                          'To plot performance results, make sure to have the'
+                          'Matplotlib package installed')
+    return RooflinePlotter
 
 
 if __name__ == "__main__":
