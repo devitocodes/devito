09993c498ebf5e257ad3c59e2905d7be3f8f6f76
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-01-11 14:49:12 +0100

    tools: Add EnrichedTuple

--- devito/tools.py
+++ devito/tools.py
@@ -553,3 +553,13 @@ class Bunch(object):
     """
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
+
+
+class EnrichedTuple(tuple):
+    """
+    A tuple with an arbitrary number of additional attributes.
+    """
+    def __new__(cls, *items, **kwargs):
+        obj = super(EnrichedTuple, cls).__new__(cls, items)
+        obj.__dict__.update(kwargs)
+        return obj
