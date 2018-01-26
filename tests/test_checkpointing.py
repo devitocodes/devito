de7aa4cdd6b81655e1bdf94920ab79daeab4f661
mloubout <mathias.louboutin@gmail.com>
2018-01-26 17:13:44 -0500

    checkpint test fix

--- tests/test_checkpointing.py
+++ tests/test_checkpointing.py
@@ -119,15 +119,15 @@ def test_segmented_averaging():
 @silencio(log_level='WARNING')
 @skipif_yask
 @pytest.mark.parametrize('space_order', [4])
-@pytest.mark.parametrize('time_order', [2])
+@pytest.mark.parametrize('kernel', ['OT2'])
 @pytest.mark.parametrize('shape', [(70, 80), (50, 50, 50)])
-def test_forward_with_breaks(shape, time_order, space_order):
+def test_forward_with_breaks(shape, kernel, space_order):
     """ Test running forward in one go and "with breaks"
     and ensure they produce the same result
     """
     spacing = tuple([15.0 for _ in shape])
     tn = 500.
-    example = CheckpointingExample(shape, spacing, tn, time_order, space_order)
+    example = CheckpointingExample(shape, spacing, tn, kernel, space_order)
     m0, dm = example.initial_estimate()
 
     cp = DevitoCheckpoint([example.forward_field])
@@ -136,7 +136,7 @@ def test_forward_with_breaks(shape, time_order, space_order):
     wrap_rev = CheckpointOperator(example.gradient_operator, u=example.forward_field,
                                   v=example.adjoint_field, m=m0, rec=example.rec_g,
                                   grad=example.grad, dt=example.dt)
-    wrp = Revolver(cp, wrap_fw, wrap_rev, None, example.nt-time_order)
+    wrp = Revolver(cp, wrap_fw, wrap_rev, None, example.nt-2)
     example.forward_operator.apply(u=example.forward_field, rec=example.rec, m=m0,
                                    src=example.src, dt=example.dt)
     u_temp = np.copy(example.forward_field.data)
@@ -169,13 +169,13 @@ def test_acoustic_save_and_nosave(shape=(50, 50), spacing=(15.0, 15.0), tn=500.,
 @silencio(log_level='WARNING')
 @skipif_yask
 @pytest.mark.parametrize('space_order', [4])
-@pytest.mark.parametrize('time_order', [2])
+@pytest.mark.parametrize('kernel', ['OT2'])
 @pytest.mark.parametrize('shape', [(70, 80), (50, 50, 50)])
-def test_checkpointed_gradient_test(shape, time_order, space_order):
+def test_checkpointed_gradient_test(shape, kernel, space_order):
     """ Run the gradient test but with checkpointing """
     spacing = tuple([15.0 for _ in shape])
     tn = 500.
-    example = CheckpointingExample(shape, spacing, tn, time_order, space_order)
+    example = CheckpointingExample(shape, spacing, tn, kernel, space_order)
     m0, dm = example.initial_estimate()
     gradient, rec_data = example.gradient(m0)
     example.verify(m0, gradient, rec_data, dm)
