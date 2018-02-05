3fcda4b1740ba0b08d7e095064269962043ceef0
FabioLuporini <f.luporini12@imperial.ac.uk>
2018-02-05 10:46:10 +0000

    ir: Use is_Derived, not is_Stepping, where needed

--- devito/ir/equations/equation.py
+++ devito/ir/equations/equation.py
@@ -90,7 +90,7 @@ class LoweredEq(Eq, EqMixin):
         # to build an IterationSpace
         iterators = OrderedDict()
         for i in ordering:
-            if i.is_Stepping:
+            if i.is_Derived:
                 iterators.setdefault(i.parent, []).append(stencil.entry(i))
             else:
                 iterators.setdefault(i, [])
