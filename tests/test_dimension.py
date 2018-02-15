6edbde25ada208c0bba4d03b530240e8227d66a3
mloubout <mathias.louboutin@gmail.com>
2018-02-15 13:13:21 -0500

    IntDiv as Atom to avoid evaluation + test_dimension fix (assert weren't checking anything)

--- tests/test_dimension.py
+++ tests/test_dimension.py
@@ -26,9 +26,38 @@ def test_conditional_basic():
     eqns = [Eq(u.forward, u + 1.), Eq(u2.forward, u2 + 1.), Eq(usave, u)]
     op = Operator(eqns)
     op.apply(t=nt)
-    assert np.all(u.data[(nt-1) % 3] == nt-1)
-    assert np.all(u2.data[i] == i for i in range(nt))
-    assert np.all(usave.data[i] == i*factor for i in range((nt+factor-1)//factor))
+    assert np.all(np.allclose(u.data[(nt-1) % 3], nt-1))
+    assert np.all([np.allclose(u2.data[i], i) for i in range(nt)])
+    assert np.all([np.allclose(usave.data[i], i*factor)
+                  for i in range((nt+factor-1)//factor)])
+
+
+@skipif_yask
+def test_conditional_as_expr():
+    nt = 19
+    grid = Grid(shape=(11, 11))
+    time = grid.time_dim
+
+    u = TimeFunction(name='u', grid=grid)
+    assert(grid.stepping_dim in u.indices)
+
+    u2 = TimeFunction(name='u2', grid=grid, save=nt)
+    assert(time in u2.indices)
+
+    factor = 4
+    time_subsampled = ConditionalDimension('t_sub', parent=time, factor=factor)
+    usave = TimeFunction(name='usave', grid=grid, save=(nt+factor-1)//factor,
+                         time_dim=time_subsampled)
+    assert(time_subsampled in usave.indices)
+
+    eqns = [Eq(u.forward, u + 1.), Eq(u2.forward, u2 + 1.),
+            Eq(usave, time_subsampled * u)]
+    op = Operator(eqns)
+    op.apply(t=nt)
+    assert np.all(np.allclose(u.data[(nt-1) % 3], nt-1))
+    assert np.all([np.allclose(u2.data[i], i) for i in range(nt)])
+    assert np.all([np.allclose(usave.data[i], i*factor*i)
+                  for i in range((nt+factor-1)//factor)])
 
 
 @skipif_yask
@@ -55,7 +84,6 @@ def test_conditional_shifted():
     op = Operator(eqns)
 
     op.apply(time_s=10, time_e=nt, t_sub_shift=3)
-
-    assert np.all(u.data[0] == 8)
-    assert np.all(u2.data[i] == i for i in range(10, nt))
-    assert np.all(usave.data[i] == 2+i*factor for i in range(2))
+    assert np.all(np.allclose(u.data[0], 8))
+    assert np.all([np.allclose(u2.data[i], i - 10) for i in range(10, nt)])
+    assert np.all([np.allclose(usave.data[i], 2+i*factor) for i in range(2)])
