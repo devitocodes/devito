73cbd1f90da1968711d1db8c5113d1108047fc68
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-02-21 10:36:34 +0100

    domain-switch: Adjust save tests

--- tests/test_save.py
+++ tests/test_save.py
@@ -5,19 +5,18 @@ from conftest import skipif_yask
 from devito import Grid, Eq, Operator, TimeFunction
 
 
-def initial(dx=0.01, dy=0.01):
-    nx, ny = int(1 / dx), int(1 / dy)
+def initial(nt, nx, ny):
     xx, yy = np.meshgrid(np.linspace(0., 1., nx, dtype=np.float32),
                          np.linspace(0., 1., ny, dtype=np.float32))
-    ui = np.zeros((nx, ny), dtype=np.float32)
+    ui = np.zeros((nt, nx, ny), dtype=np.float32)
     r = (xx - .5)**2. + (yy - .5)**2.
-    ui[np.logical_and(.05 <= r, r <= .1)] = 1.
+    ui[0, np.logical_and(.05 <= r, r <= .1)] = 1.
 
     return ui
 
 
 def initializer(data):
-    data[0, :] = initial()
+    data[:] = initial(*data.shape)
 
 
 def run_simulation(save=False, dx=0.01, dy=0.01, a=0.5, timesteps=100):
@@ -26,21 +25,15 @@ def run_simulation(save=False, dx=0.01, dy=0.01, a=0.5, timesteps=100):
     dt = dx2 * dy2 / (2 * a * (dx2 + dy2))
 
     grid = Grid(shape=(nx, ny))
-    u = TimeFunction(
-        name='u', grid=grid, save=timesteps if save else None,
-        initializer=initializer,
-        time_order=1, space_order=2
-    )
+    u = TimeFunction(name='u', grid=grid, save=timesteps if save else None,
+                     initializer=initializer, time_order=1, space_order=2)
 
     eqn = Eq(u.dt, a * (u.dx2 + u.dy2))
     stencil = solve(eqn, u.forward)[0]
     op = Operator(Eq(u.forward, stencil))
-    op.apply(time=timesteps, dt=dt)
+    op.apply(time=timesteps-2, dt=dt)
 
-    if save:
-        return u.data[timesteps - 1, :]
-    else:
-        return u.data[(timesteps+1) % 2, :]
+    return u.data[timesteps - 1]
 
 
 @skipif_yask
