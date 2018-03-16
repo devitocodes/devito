d75afabac5f51a01b6af5e80f7c5f1c96c6754b2
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-03-16 15:55:14 +0100

    ir: Fix flow detection

--- tests/test_ir.py
+++ tests/test_ir.py
@@ -2,12 +2,13 @@ import pytest
 
 from conftest import EVAL, time, x, y, z, skipif_yask  # noqa
 
-from devito import Eq  # noqa
+from devito import Eq, Grid, TimeFunction  # noqa
 from devito.ir.equations import LoweredEq
 from devito.ir.iet.nodes import Conditional, Expression
 from devito.ir.support.basic import IterationInstance, TimedAccess, Scope
 from devito.ir.support.space import (NullInterval, Interval, IntervalGroup,
-                                     Forward, Backward)
+                                     Any, Forward, Backward)
+from devito.ir.support.utils import detect_flow_directions
 
 
 @pytest.fixture(scope="session")
@@ -325,6 +326,21 @@ def test_dependences_scope(exprs, expected, ti0, ti1, ti3, fa):
 
     # Sanity check: we did find all of the expected dependences
     assert len(expected) == 0
+
+
+@skipif_yask
+def test_flow_detection():
+    """Test detection of information flow."""
+    grid = Grid((10, 10))
+    u1 = TimeFunction(name="u1", grid=grid, save=10, time_order=1)
+    u2 = TimeFunction(name="u2", grid=grid, time_order=2)
+    u1 = TimeFunction(name="u1", grid=grid, save=10, time_order=2)
+    exprs = [LoweredEq(Eq(u1.forward, u1 + 2.0 - u1.backward)),
+             LoweredEq(Eq(u2.forward, u2 + 2*u2.backward - u1.dt2))]
+    mapper = detect_flow_directions(exprs)
+    assert mapper.get(grid.stepping_dim) == {Forward}
+    assert mapper.get(grid.time_dim) == {Any, Forward}
+    assert all(mapper.get(i) == {Any} for i in grid.dimensions)
 
 
 @skipif_yask
