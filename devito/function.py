b313446bb6db841051be7c4ac75f0103adc9b6fd
mloubout <mathias.louboutin@gmail.com>
2018-03-16 22:26:06 -0400

    function: process data for array input for sparse functions

--- devito/function.py
+++ devito/function.py
@@ -1035,6 +1035,7 @@ class SparseFunction(TensorFunction):
         else:
             # ..., but if not, we simply need to recurse over children.
             values = self.coordinates.argument_values(alias=key, **kwargs)
+            values[key.name] = new
 
         return values
 
