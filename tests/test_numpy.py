fe48e4e9af93ae49c7da45b34a6a1cc9a92f666f
Gerard Gorman <g.gorman@imperial.ac.uk>
2018-03-26 10:44:09 +0100

    Added comment to advice how to work around this bug in numpy.

--- tests/test_numpy.py
+++ tests/test_numpy.py
@@ -6,6 +6,9 @@ import numpy as np
 
 # Checking for bug in numpy.dot
 # https://github.com/ContinuumIO/anaconda-issues/issues/7457
+
+# If you run into this bug then try:
+#  conda install numpy nomkl
 v = rand(1000).astype(np.float64)
 assert np.isclose(np.dot(v, v), (v*v).sum())
 
