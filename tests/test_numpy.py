4069d78f454fe3ad0ca31f7753b0e10598dc0c92
Gerard Gorman <g.gorman@imperial.ac.uk>
2018-03-26 15:15:41 +0100

    fix flake8 issues

--- tests/test_numpy.py
+++ tests/test_numpy.py
@@ -1,8 +1,5 @@
 import numpy as np
 from numpy.random import rand
-from conftest import skipif_yask
-
-import numpy as np
 
 # Checking for bug in numpy.dot
 # https://github.com/ContinuumIO/anaconda-issues/issues/7457
@@ -14,4 +11,3 @@ assert np.isclose(np.dot(v, v), (v*v).sum())
 
 v = rand(1000).astype(np.float32)
 assert np.isclose(np.dot(v, v), (v*v).sum())
-
