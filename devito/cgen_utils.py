dtype seperate + various numerical fixes

devito/cgen_utils.py
@@ -66,8 +66,8 @@ class CodePrinter(C99CodePrinter):
 
     :param settings: A dictionary containing relevant settings
     """
-    def __init__(self, settings={}):
-        self.dtype = settings.pop('dtype', np.float32)
+    def __init__(self, dtype=np.float32, settings={}):
+        self.dtype = dtype
         C99CodePrinter.__init__(self, settings)
         self.known_functions.update(self.custom_functions)
 
@@ -163,14 +163,14 @@ class CodePrinter(C99CodePrinter):
         return str(expr)
 
 
-def ccode(expr, **settings):
+def ccode(expr, dtype=np.float32, **settings):
     """Generate C++ code from an expression calling CodePrinter class
 
     :param expr: The expression
     :param settings: A dictionary of settings for code printing
     :returns: The resulting code as a string. If it fails, then it returns the expr
     """
-    return CodePrinter(settings=settings).doprint(expr, None)
+    return CodePrinter(dtype=dtype, settings=settings).doprint(expr, None)
 
 
 blankline = c.Line("")

examples/seismic/model.py
@@ -315,9 +315,10 @@ class Model(object):
     :param m: The square slowness of the wave
     :param damp: The damping field for absorbing boundarycondition
     """
+    _physical_parameters = ('m', 'epsilon', 'delta', 'theta', 'phi')
+
     def __init__(self, origin, spacing, shape, space_order, vp, nbpml=20,
                  dtype=np.float32, epsilon=None, delta=None, theta=None, phi=None):
-        self._physical_parameters = ('m', 'epsilon', 'delta', 'theta', 'phi')
         self.shape = shape
         self.nbpml = int(nbpml)
         self.origin = tuple([dtype(o) for o in origin])
@@ -390,7 +391,8 @@ class Model(object):
         Return all set physical parameters
         """
         known = [getattr(self, i) for i in self._physical_parameters]
-        return {i.name: kwargs.get(i.name, i) for i in known if i is not None}
+        return {i.name: kwargs.get(i.name, i) or i for i in known
+                if isinstance(i, Function)}
 
     @property
     def dim(self):
@@ -440,7 +442,8 @@ class Model(object):
         # The CFL condtion is then given by
         # dt <= coeff * h / (max(velocity))
         coeff = 0.38 if len(self.shape) == 3 else 0.42
-        return self.dtype(coeff * np.min(self.spacing) / (self.scale*np.max(self.vp)))
+        dt = self.dtype(coeff * np.min(self.spacing) / (self.scale*np.max(self.vp)))
+        return .001 * int(1000 * dt)
 
     @property
     def vp(self):

tests/test_adjointJ.py
@@ -10,7 +10,7 @@ from examples.seismic.acoustic import AcousticWaveSolver
 
 @skipif_yask
 @pytest.mark.parametrize('space_order', [4, 8, 12])
-@pytest.mark.parametrize('shape', [(60, 70), (40, 50, 30)])
+@pytest.mark.parametrize('shape', [(60,), (60, 70), (40, 50, 30)])
 def test_acousticJ(shape, space_order):
     t0 = 0.0  # Start time
     tn = 500.  # Final time
@@ -60,10 +60,10 @@ def test_acousticJ(shape, space_order):
     # Adjoint test: Verify <Ax,y> matches  <x, A^Ty> closely
     term1 = np.dot(im.data.reshape(-1), dm.reshape(-1))
     term2 = linalg.norm(du.data)**2
-    info('<Ax,y>: %f, <x, A^Ty>: %f, difference: %12.12f, ratio: %f'
-         % (term1, term2, term1 - term2, term1 / term2))
+    info('<Ax,y>: %f, <x, A^Ty>: %f, relative error: %4.4e, ratio: %e'
+         % (term1, term2, 1 - term1/term2, term1 / term2))
     assert np.isclose(term1 / term2, 1.0, atol=0.001)
 
 
 if __name__ == "__main__":
-    test_acousticJ(shape=(60, 70), space_order=4)
+    test_acousticJ(shape=(60,), space_order=4)