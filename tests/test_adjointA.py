dtype seperate + various numerical fixes

tests/test_adjointA.py
@@ -69,6 +69,6 @@ def test_acoustic(mkey, shape, kernel, space_order, nbpml):
     # Adjoint test: Verify <Ax,y> matches  <x, A^Ty> closely
     term1 = np.dot(srca.data.reshape(-1), solver.source.data)
     term2 = linalg.norm(rec.data) ** 2
-    info('<Ax,y>: %f, <x, A^Ty>: %f, difference: %12.12f, ratio: %f'
+    info('<Ax,y>: %f, <x, A^Ty>: %f, difference: %4.4e, ratio: %f'
          % (term1, term2, (term1 - term2)/term1, term1 / term2))
     assert np.isclose((term1 - term2)/term1, 0., rtol=1.e-10)