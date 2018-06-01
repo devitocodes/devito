a95357198eee82e428f477b54189157c9b9153e5
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-06-01 17:11:27 +0200

    tools: Move algos to algorithms.py

--- devito/tools/__init__.py
+++ devito/tools/__init__.py
@@ -1,4 +1,6 @@
 from devito.tools.utils import *  # noqa
+
+from devito.tools.algorithms import *  # noqa
 from devito.tools.memoization import *  # noqa
 from devito.tools.mixins import *  # noqa
 from devito.tools.validators import *  # noqa
