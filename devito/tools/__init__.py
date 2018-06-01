978b388c156b60e974cbebe7ccb6160bba4c0091
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-06-01 17:07:29 +0200

    tools: Move GenericVisitor to visitors.py

--- devito/tools/__init__.py
+++ devito/tools/__init__.py
@@ -2,3 +2,4 @@ from devito.tools.utils import *  # noqa
 from devito.tools.memoization import *  # noqa
 from devito.tools.mixins import *  # noqa
 from devito.tools.validators import *  # noqa
+from devito.tools.visitors import *  # noqa
