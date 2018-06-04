e83da64bf9993135ae20b69141979d55886e31c0
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-06-04 09:08:32 +0200

    ir: Fix affine_if_present docstring

--- devito/ir/support/basic.py
+++ devito/ir/support/basic.py
@@ -285,10 +285,8 @@ class IterationInstance(Vector):
         return self.rank == 0
 
     def affine_if_present(self, findices):
-        """
-        Return True if the provided ``findices`` appear in ``self`` and are
-        affine, False otherwise.
-        """
+        """Return False if any of the provided findices appears in self and
+        is not affine, True otherwise."""
         return (set(findices) & set(self.findices)).issubset(set(self.findices_affine))
 
     def distance(self, other, findex=None, view=None):
