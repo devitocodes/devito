26d8188024845055b96597833f1e8b077aaa84a9
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-06-18 10:54:29 +0200

    scipts: Make advisor.py capable of running a proper Advisor analysis

--- scripts/advisor.py
+++ scripts/advisor.py
@@ -2,6 +2,7 @@ import os
 import sys
 from pathlib import Path
 from subprocess import check_call
+from tempfile import gettempdir
 
 import click
 
@@ -27,15 +28,39 @@ def run_with_advisor(path, output, exec_args, advisor_home):
     output = Path(output)
     output.mkdir(parents=True, exist_ok=True)
 
+    # Devito must be told where to find Advisor, because it uses its C API
     if advisor_home:
         os.environ['ADVISOR_HOME'] = advisor_home
     else:
         os.environ['ADVISOR_HOME'] = '/opt/intel/advisor'
 
+    # Tell Devito to instrument the generated code for Advisor
     os.environ['DEVITO_PROFILING'] = 'advisor'
 
-    command = ['python', path.as_posix()]
-    command.extend(exec_args.split())
+    # Prevent NumPy from using threads, which otherwise leads to a deadlock when
+    # used in combination with Advisor. This issue has been described at:
+    #     `software.intel.com/en-us/forums/intel-advisor-xe/topic/780506`
+    # Note: we should rather sniff the BLAS library used by NumPy, and set the
+    # appropriate env var only
+    os.environ['OPENBLAS_NUM_THREADS'] = '1'
+    os.environ['MKL_NUM_THREADS'] = '1'
+    # Note: `Numaexpr`, used by NumPy, also employs threading, so we shall disable
+    # it too via the corresponding env var. See:
+    #     `stackoverflow.com/questions/17053671/python-how-do-you-stop-numpy-from-multithreading`  # noqa
+    os.environ['NUMEXPR_NUM_THREADS'] = '1'
+
+    advisor_command = [
+        'advixe-cl',
+        '-data-limit=500',
+        '-collect survey',
+        '-start-paused',  # The generated code will enable/disable Advisor on a loop basis
+        '-project-dir', str(output),
+        '-search-dir src:r=%s' % gettempdir(),  # Root directory where Devito stores the generated code  # noqa
+        '-run-pass-thru=--no-altstack',  # Avoids `https://software.intel.com/en-us/vtune-amplifier-help-error-message-stack-size-is-too-small`  # noqa
+        '-strategy ldconfig:notrace:notrace'  # Avoids `https://software.intel.com/en-us/forums/intel-vtune-amplifier-xe/topic/779309`  # noqa
+    ]
+    py_command = ['python', str(path)] + exec_args.split()
+    command = advisor_command + ['--'] + py_command
 
     check_call(command)
 
