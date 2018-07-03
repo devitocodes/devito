30786e90010de06076677093a38616c933f78ff8
mloubout <mathias.louboutin@gmail.com>
2018-07-03 15:30:10 -0400

    more flake8

--- examples/seismic/tti/operators.py
+++ examples/seismic/tti/operators.py
@@ -1,4 +1,4 @@
-from sympy import cos, sin, finite_diff_weights
+from sympy import cos, sin
 
 from devito import Eq, Operator, TimeFunction
 from examples.seismic import PointSource, Receiver
@@ -468,38 +468,53 @@ def ForwardOperatorStagg(model, source, receiver, space_order=4,
                       save=source.nt if save else None,
                       time_order=1, space_order=space_order)
     # Stencils
-    u_vx = Eq(vx.forward, damp_loc * vx - damp_loc *s/rho*staggered_diff(ph, dim=x, order=space_order, stagger=left, theta=theta, phi=phi))
-    u_vz = Eq(vz.forward, damp_loc * vz - damp_loc *s/rho*staggered_diff(pv, dim=z, order=space_order, stagger=left, theta=theta, phi=phi))
+    phdx = staggered_diff(ph, dim=x, order=space_order, stagger=left,
+                          theta=theta, phi=phi)
+    u_vx = Eq(vx.forward, damp_loc * vx - damp_loc * s / rho * phdx)
 
-    dvx = staggered_diff(vx.forward, dim=x, order=space_order, stagger=right, theta=theta, phi=phi)
-    dvz = staggered_diff(vz.forward, dim=z, order=space_order, stagger=right, theta=theta, phi=phi)
+    pvdz = staggered_diff(pv, dim=z, order=space_order, stagger=left,
+                          theta=theta, phi=phi)
+    u_vz = Eq(vz.forward, damp_loc * vz - damp_loc * s / rho * pvdz)
+
+    dvx = staggered_diff(vx.forward, dim=x, order=space_order, stagger=right,
+                         theta=theta, phi=phi)
+    dvz = staggered_diff(vz.forward, dim=z, order=space_order, stagger=right,
+                         theta=theta, phi=phi)
 
     u_vy = []
     dvy = 0
     if ndim == 3:
-        u_vy = [Eq(vy.forward, damp_loc * vy - damp_loc *s/rho*staggered_diff(ph, dim=y, order=space_order, stagger=left, theta=theta, phi=phi))]
-        dvy = staggered_diff(vy.forward, dim=y, order=space_order, stagger=right, theta=theta, phi=phi)
+        phdy = staggered_diff(ph, dim=y, order=space_order, stagger=left,
+                              theta=theta, phi=phi)
+        u_vy = [Eq(vy.forward, damp_loc * vy - damp_loc * s / rho * phdy)]
+        dvy = staggered_diff(vy.forward, dim=y, order=space_order, stagger=right,
+                             theta=theta, phi=phi)
 
+    pv_eq = Eq(pv.forward,
+               damp_loc * (pv - s * rho / m * (delta * (dvx + dvy) + dvz)))
 
-    pv_eq = Eq(pv.forward, damp_loc * pv - damp_loc *s * rho / m * (delta*(dvx + dvy) + dvz))
-
-    ph_eq = Eq(ph.forward, damp_loc * ph - damp_loc *s * rho / m * (epsilon*(dvx + dvy) + delta * dvz))
+    ph_eq = Eq(ph.forward,
+               damp_loc * (ph - s * rho / m * (epsilon * (dvx + dvy) + delta * dvz)))
 
     # Source symbol with input wavelet
     src = PointSource(name='src', grid=model.grid, time_range=source.time_range,
                       npoint=source.npoint)
     rec = Receiver(name='rec', grid=model.grid, time_range=receiver.time_range,
                    npoint=receiver.npoint)
-    src_term = src.inject(field=pv.forward, offset=model.nbpml, expr=src * rho * dt / m)
-    src_term += src.inject(field=ph.forward, offset=model.nbpml, expr=src * rho * dt / m)
+    src_term = src.inject(field=pv.forward, offset=model.nbpml,
+                          expr=src * rho * dt / m)
+    src_term += src.inject(field=ph.forward, offset=model.nbpml,
+                           expr=src * rho * dt / m)
     # Data is sampled at receiver locations
     rec_term = rec.interpolate(expr=pv + ph, offset=model.nbpml)
 
     # Substitute spacing terms to reduce flops
-    op = Operator([u_vx, u_vz] + u_vy + rec_term + [pv_eq, ph_eq] + src_term, subs=model.spacing_map,
+    op = Operator([u_vx, u_vz] + u_vy + rec_term + [pv_eq, ph_eq] + src_term,
+                  subs=model.spacing_map,
                   dse='advanced', dle='advanced')
 
     return op
+
 
 def staggered_diff(f, dim, order, stagger=centered, theta=0, phi=0):
     """
@@ -525,16 +540,19 @@ def staggered_diff(f, dim, order, stagger=centered, theta=0, phi=0):
 
     if theta == 0 and phi == 0:
         diff = dim.spacing
-        idx = [(dim + int(i+.5+off[dim])*diff) for i in range(-int(order / 2), int(order / 2))]
+        idx = [(dim + int(i+.5+off[dim])*diff)
+               for i in range(-int(order / 2), int(order / 2))]
         return f.diff(dim).as_finite_difference(idx, x0=dim + off[dim]*dim.spacing)
     else:
         ndim = f.grid.dim
         x = f.grid.dimensions[0]
         z = f.grid.dimensions[-1]
-        idxx = list(set([(x + int(i+.5+off[x])*x.spacing) for i in range(-int(order / 2), int(order / 2))]))
+        idxx = list(set([(x + int(i+.5+off[x])*x.spacing)
+                         for i in range(-int(order / 2), int(order / 2))]))
         dx = f.diff(x).as_finite_difference(idxx, x0=x + off[x]*x.spacing)
 
-        idxz = list(set([(z + int(i+.5+off[z])*z.spacing) for i in range(-int(order / 2), int(order / 2))]))
+        idxz = list(set([(z + int(i+.5+off[z])*z.spacing)
+                         for i in range(-int(order / 2), int(order / 2))]))
         dz = f.diff(z).as_finite_difference(idxz, x0=z + off[z]*z.spacing)
 
         dy = 0
@@ -542,18 +560,22 @@ def staggered_diff(f, dim, order, stagger=centered, theta=0, phi=0):
 
         if ndim == 3:
             y = f.grid.dimensions[1]
-            idxy = list(set([(y + int(i+.5+off[y])*y.spacing) for i in range(-int(order / 2), int(order / 2))]))
+            idxy = list(set([(y + int(i+.5+off[y])*y.spacing)
+                             for i in range(-int(order / 2), int(order / 2))]))
             dy = f.diff(y).as_finite_difference(idxy, x0=y + off[y]*y.spacing)
             is_y = (dim == y)
 
         if dim == x:
-            return cos(theta) * cos(phi) * dx + sin(phi) * cos(theta) * dy - sin(theta) * dz
+            return cos(theta) * cos(phi) * dx + sin(phi) * cos(theta) * dy -\
+                sin(theta) * dz
         elif dim == z:
-            return sin(theta) * cos(phi) * dx + sin(phi) * sin(theta) * dy + cos(theta) * dz
+            return sin(theta) * cos(phi) * dx + sin(phi) * sin(theta) * dy +\
+                cos(theta) * dz
         elif is_y:
-            return -sin(phi) * dx + cos(phi) *  dy
+            return -sin(phi) * dx + cos(phi) * dy
         else:
             return 0
+
 
 kernels = {('shifted', 3): kernel_shifted_3d, ('shifted', 2): kernel_shifted_2d,
            ('centered', 3): kernel_centered_3d, ('centered', 2): kernel_centered_2d}
