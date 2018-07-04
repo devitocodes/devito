993c4c98fb9529946669b4d13e6c5a9ff4ab3f67
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-07-04 12:07:48 +0200

    tests: Check domain decomposition over Functions

--- tests/test_mpi.py
+++ tests/test_mpi.py
@@ -2,7 +2,7 @@ from mpi4py import MPI
 
 import pytest
 
-from devito import Grid, Function, Distributor
+from devito import Grid, Function
 
 
 @pytest.mark.parallel(nprocs=2)
@@ -14,8 +14,14 @@ def test_hello_mpi():
     print("Hello, World! I am rank %d of %d on %s" % (rank, size, name), flush=True)
 
 
-@pytest.mark.parallel(nprocs=2)
+@pytest.mark.parallel(nprocs=[2, 4])
 def test_basic_partitioning():
-    grid = Grid(shape=(10, 10, 10))  # Gonna use a default distributor underneath
+    grid = Grid(shape=(15, 15))  # Gonna use a default distributor underneath
     f = Function(name='f', grid=grid)
-    from IPython import embed; embed()
+
+    distributor = grid._distributor
+    expected = {  # nprocs -> [(rank0 shape), (rank1 shape), ...]
+        2: [(8, 15), (7, 15)],
+        4: [(8, 8), (8, 7), (7, 8), (7, 7)]
+    }
+    assert f.shape == expected[distributor.nprocs][distributor.rank]
