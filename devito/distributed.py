ce06925a5ff8c51c70be0c3b4ed1cd054cc9e224
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-07-05 10:53:16 +0200

    mpi: Fix creation of cartesian topology

--- devito/distributed.py
+++ devito/distributed.py
@@ -12,16 +12,31 @@ class Distributor(object):
     A class to perform domain decomposition for a set of MPI processes.
 
     :param shape: The shape of the domain to be decomposed.
+    :param dimensions: The :class:`Dimension`s defining the domain.
     :param comm: An MPI communicator.
     """
 
-    def __init__(self, shape, input_comm=None):
+    def __init__(self, shape, dimensions, input_comm=None):
         self._glb_shape = shape
+        self._dimensions = dimensions
         self._input_comm = input_comm or MPI.COMM_WORLD
-        self._topology = MPI.Compute_dims(self._input_comm.size, len(shape))
+
+        # `Compute_dims` sets the dimension sizes to be as close to each other
+        # as possible, using an appropriate divisibility algorithm. Thus, in 3D:
+        # * topology[0] >= topology[1] >= topology[2]
+        # * topology[0] * topology[1] * topology[2] == self._input_comm.size
+        topology = MPI.Compute_dims(self._input_comm.size, len(shape))
+        # At this point MPI's dimension 0 corresponds to the rightmost element
+        # in `topology`. This is in reverse to `shape`'s ordering. Hence, we
+        # now restore consistency
+        self._topology = tuple(reversed(topology))
 
         if self._input_comm is MPI.COMM_WORLD:
-            # By default, Devito arranges processes into a virtual cartesian topology
+            # By default, Devito arranges processes into a cartesian topology.
+            # MPI works with numbered dimensions and follows the C row-major
+            # numbering of the ranks, i.e. in a 2x3 Cartesian topology (0,0)
+            # maps to rank 0, (0,1) maps to rank 1, (0,2) maps to rank 2, (1,0)
+            # maps to rank 3, and so on.
             self._comm = self._input_comm.Create_cart(self._topology)
         else:
             self._comm = input_comm
@@ -31,8 +46,12 @@ class Distributor(object):
                            for i, j in zip(shape, self._topology)]
 
     @property
-    def rank(self):
+    def myrank(self):
         return self._comm.rank
+
+    @property
+    def mycoords(self):
+        return tuple(self._comm.coords)
 
     @property
     def nprocs(self):
@@ -58,19 +77,23 @@ class Distributor(object):
         return tuple(len(i) for i in self.glb_numb)
 
     @property
+    def dimensions(self):
+        return self._dimensions
+
+    @property
     def neighbours(self):
         """
         Return the mapper ``proc -> direction``; ``proc`` is the rank of a
         neighboring process, while ``direction`` tells whether ``proc`` is
         logically at right (value=1) or left (value=-1) of ``self``.
         """
-        ret = []
-        for src, dest in [self._comm.Shift(i, 1) for i in range(self.ndim)]:
-            if src != -1:
-                ret.append((src, LEFT))
-            if dest != -1:
-                ret.append((dest, RIGHT))
-        return dict(ret)
+        shifts = {d: self._comm.Shift(i, 1) for i, d in enumerate(self.dimensions)}
+        ret = {}
+        for d, (src, dest) in shifts.items():
+            ret[d] = {}
+            ret[d][LEFT] = src if src != -1 else None
+            ret[d][RIGHT] = dest if dest != -1 else None
+        return ret
 
     def __repr__(self):
         return "Distributor(nprocs=%d)" % self.nprocs
