8d9be1d8be3af660115235cc1c2583a72f63c9c4
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-07-05 15:55:27 +0200

    tests: Clean up MPI tests

--- tests/test_mpi.py
+++ tests/test_mpi.py
@@ -1,8 +1,10 @@
 from mpi4py import MPI
+import numpy as np
 
 import pytest
 
 from devito import Grid, Function
+from devito.distributed import LEFT, RIGHT
 
 
 @pytest.mark.parallel(nprocs=2)
@@ -16,39 +18,86 @@ def test_hello_mpi():
 
 @pytest.mark.parallel(nprocs=[2, 4])
 def test_basic_partitioning():
-    grid = Grid(shape=(15, 15))  # Gonna use a default distributor underneath
+    grid = Grid(shape=(15, 15))
     f = Function(name='f', grid=grid)
 
-    distributor = grid._distributor
+    distributor = grid.distributor
     expected = {  # nprocs -> [(rank0 shape), (rank1 shape), ...]
-        2: [(8, 15), (7, 15)],
+        2: [(15, 8), (15, 7)],
         4: [(8, 8), (8, 7), (7, 8), (7, 7)]
     }
-    assert f.shape == expected[distributor.nprocs][distributor.rank]
+    assert f.shape == expected[distributor.nprocs][distributor.myrank]
 
 
-#@pytest.mark.parallel(nprocs=[9])
+@pytest.mark.parallel(nprocs=9)
 def test_neighborhood_2d():
-    grid = Grid(shape=(3, 3))  # Gonna use a default distributor underneath
+    grid = Grid(shape=(3, 3))
+    x, y = grid.dimensions
 
-    distributor = grid._distributor
-    expected = {  # coords -> [neighbour0 coords, neighbour1 coords, ...]
-        (1, 1): {(0, 0), (0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1), (2, 2)}
+    distributor = grid.distributor
+    # Rank map:
+    # ---------------y
+    # | 0 | 1 | 2 |
+    # -------------
+    # | 3 | 4 | 5 |
+    # -------------
+    # | 6 | 7 | 8 |
+    # -------------
+    # |
+    # x
+    expected = {
+        0: {x: {LEFT: None, RIGHT: 3}, y: {LEFT: None, RIGHT: 1}},
+        1: {x: {LEFT: None, RIGHT: 4}, y: {LEFT: 0, RIGHT: 2}},
+        2: {x: {LEFT: None, RIGHT: 5}, y: {LEFT: 1, RIGHT: None}},
+        3: {x: {LEFT: 0, RIGHT: 6}, y: {LEFT: None, RIGHT: 4}},
+        4: {x: {LEFT: 1, RIGHT: 7}, y: {LEFT: 3, RIGHT: 5}},
+        5: {x: {LEFT: 2, RIGHT: 8}, y: {LEFT: 4, RIGHT: None}},
+        6: {x: {LEFT: 3, RIGHT: None}, y: {LEFT: None, RIGHT: 7}},
+        7: {x: {LEFT: 4, RIGHT: None}, y: {LEFT: 6, RIGHT: 8}},
+        8: {x: {LEFT: 5, RIGHT: None}, y: {LEFT: 7, RIGHT: None}},
     }
-    print (distributor.neighbours)
-    from IPython import embed; embed()
+    assert expected[distributor.myrank] == distributor.neighbours
 
 
-@pytest.mark.parallel(nprocs=[2, 4])
-def test_halo_exchange_1d():
-    pass
+@pytest.mark.parallel(nprocs=2)
+def test_halo_exchange_bilateral():
+    """
+    Test halo exchange between two processes organised in a 1x2 cartesian grid.
 
+    Their initial data_with_halo looks like:
 
-@pytest.mark.parallel(nprocs=[2, 4])
-def test_halo_exchange_2d():
-    grid = Grid(shape=(12, 12))  # Gonna use a default distributor underneath
+           rank0           rank1
+        0 0 0 0 0 0     0 0 0 0 0 0
+        0 1 1 1 1 0     0 2 2 2 2 0
+        0 1 1 1 1 0     0 2 2 2 2 0
+        0 1 1 1 1 0     0 2 2 2 2 0
+        0 1 1 1 1 0     0 2 2 2 2 0
+        0 0 0 0 0 0     0 0 0 0 0 0
+
+    After the halo exchange, the following is expected and tested for:
+
+           rank0           rank1
+        0 0 0 0 0 0     0 0 0 0 0 0
+        0 1 1 1 1 2     1 2 2 2 2 0
+        0 1 1 1 1 2     1 2 2 2 2 0
+        0 1 1 1 1 2     1 2 2 2 2 0
+        0 1 1 1 1 2     1 2 2 2 2 0
+        0 0 0 0 0 0     0 0 0 0 0 0
+    """
+    grid = Grid(shape=(12, 12))
     f = Function(name='f', grid=grid)
 
+    distributor = grid.distributor
+    f.data[:] = distributor.myrank + 1
 
-if __name__ == "__main__":
-    test_neighborhood_2d()
+    # Now trigger a halo exchange...
+    f.data_with_halo   # noqa
+
+    if distributor.myrank == 0:
+        assert np.all(f.data_ro_with_halo[1:-1, -1] == f.data_ro_domain[:, -1] + 1)
+        assert np.all(f.data_ro_with_halo[:, 0] == 0.)
+    else:
+        assert np.all(f.data_ro_with_halo[1:-1, 0] == f.data_ro_domain[:, 0] - 1)
+        assert np.all(f.data_ro_with_halo[:, -1] == 0.)
+    assert np.all(f.data_ro_with_halo[0] == 0.)
+    assert np.all(f.data_ro_with_halo[-1] == 0.)
