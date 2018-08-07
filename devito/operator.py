792b123038659e5922869e082887aa4d760bc698
Tim Burgess <timb@dugeo.com>
2018-08-07 16:22:35 +0800

    restore _lib when unpickling Operator

--- devito/operator.py
+++ devito/operator.py
@@ -650,6 +650,10 @@ class Operator(Callable):
     def __getstate__(self):
         if self._lib:
             state = dict(self.__dict__)
+<<<<<<< HEAD
+=======
+            # state.pop('_soname')
+>>>>>>> restore _lib when unpickling Operator
             # The compiled shared-object will be pickled; upon unpickling, it
             # will be restored into a potentially different temporary directory,
             # so the entire process during which the shared-object is loaded and
