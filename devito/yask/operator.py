2ababa7bc8ead5b16f962e2ff2ff2dbbf780812e
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-08-28 17:07:10 +0200

    yask: Fix IET transformation

--- devito/yask/operator.py
+++ devito/yask/operator.py
@@ -61,6 +61,7 @@ class Operator(OperatorRunnable):
         to generate YASK code. Such YASK code is then called from within the
         transformed Iteration/Expression tree.
         """
+        mapper = {}
         self.yk_solns = OrderedDict()
         for n, (section, trees) in enumerate(find_offloadable_trees(iet).items()):
             dimensions = tuple(filter_ordered(i.dim.root for i in flatten(trees)))
@@ -75,17 +76,16 @@ class Operator(OperatorRunnable):
 
             try:
                 # Generate YASK grids and populate `yc_soln` with equations
-                mapper = yaskit(trees, yc_soln)
-                local_grids = [i for i in mapper if i.is_Array]
+                gridmap = yaskit(trees, yc_soln)
+                local_grids = [i for i in gridmap if i.is_Array]
 
-                # Transform the IET
+                # Build the new IET nodes
                 yk_soln_obj = YaskSolnObject(namespace['code-soln-name'](n))
                 funcall = make_sharedptr_funcall(namespace['code-soln-run'],
                                                  ['time'], yk_soln_obj)
                 funcall = ForeignExpression(funcall, self._dtype)
-                mapper = {trees[0].root: funcall}
+                mapper[trees[0].root] = funcall
                 mapper.update({i.root: mapper.get(i.root) for i in trees})  # Drop trees
-                iet = Transformer(mapper).visit(iet)
 
                 # Mark `funcall` as an external function call
                 self._func_table[namespace['code-soln-run']] = MetaCall(None, False)
@@ -100,6 +100,7 @@ class Operator(OperatorRunnable):
                      yc_soln.get_num_equations()))
             except NotImplementedError as e:
                 log("Unable to offload a candidate tree. Reason: [%s]" % str(e))
+        iet = Transformer(mapper).visit(iet)
 
         if not self.yk_solns:
             log("No offloadable trees found")
