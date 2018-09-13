eb9e55f148480fde4aa7def559814eb22877b751
mloubout <mathias.louboutin@gmail.com>
2018-09-13 10:41:29 -0400

    remove old fd file

--- devito/__init__.py
+++ devito/__init__.py
@@ -11,7 +11,7 @@ from devito.base import *  # noqa
 from devito.data import *  # noqa
 from devito.dimension import *  # noqa
 from devito.equation import *  # noqa
-from devito.finite_difference import *  # noqa
+from devito.finite_differences import *  # noqa
 from devito.function import Buffer # noqa
 from devito.logger import error, warning, info, set_log_level, silencio  # noqa
 from devito.parameters import *  # noqa
