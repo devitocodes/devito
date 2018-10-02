51af5920fec41637c5f7751a5e77866c037ea77e
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-10-02 17:09:17 +0200

    function: Drop .interior view

--- devito/function.py
+++ devito/function.py
@@ -319,23 +319,6 @@ class TensorFunction(AbstractCachedFunction, ArgProvider):
 
     @property
     @_allocate_memory
-    def data_interior(self):
-        """
-        The interior data values.
-
-        Elements are stored in row-major format.
-
-        .. note::
-
-            With this accessor you are claiming that you will modify
-            the values you get back. If you only need to look at the
-            values, use :meth:`data_ro_interior` instead.
-        """
-        self._is_halo_dirty = True
-        return self._data[self._mask_interior]
-
-    @property
-    @_allocate_memory
     def data_with_halo(self):
         """
         The domain+halo data values.
@@ -377,16 +360,6 @@ class TensorFunction(AbstractCachedFunction, ArgProvider):
         A read-only view of the domain data values.
         """
         view = self._data[self._mask_domain]
-        view.setflags(write=False)
-        return view
-
-    @property
-    @_allocate_memory
-    def data_ro_interior(self):
-        """
-        A read-only view of the interior data values.
-        """
-        view = self._data[self._mask_interior]
         view.setflags(write=False)
         return view
 
@@ -456,26 +429,6 @@ class TensorFunction(AbstractCachedFunction, ArgProvider):
         ret = tuple(sympy.Add(i, -j, evaluate=False)
                     for i, j in zip(symbolic_shape, self.staggered))
         return EnrichedTuple(*ret, getters=self.dimensions)
-
-    @property
-    def _mask_interior(self):
-        """A mask to access the interior region of the allocated data."""
-        if self.grid is None:
-            glb_pos_map = {d: [LEFT, RIGHT] for d in self.dimensions}
-        else:
-            glb_pos_map = self.grid.distributor.glb_pos_map
-        ret = []
-        for d, i in zip(self.dimensions, self._mask_domain):
-            if d.is_Space:
-                lshift = int(LEFT in glb_pos_map.get(d, []))
-                rshift = int(RIGHT in glb_pos_map.get(d, []))
-                if i.stop is None:
-                    ret.append(slice(i.start + lshift, -rshift or None))
-                else:
-                    ret.append(slice(i.start + lshift, i.stop - rshift))
-            else:
-                ret.append(i)
-        return tuple(ret)
 
     def _halo_exchange(self):
         """Perform the halo exchange with the neighboring processes."""
