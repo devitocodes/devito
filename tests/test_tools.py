e277d0111dff23e349cd20fb627d12539e363062
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-10-09 11:58:01 +0200

    tools: Add numpy_view_offsets

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -2,8 +2,9 @@ import pytest
 from conftest import skipif_yask
 
 from sympy.abc import a, b, c, d, e
+import numpy as np
 
-from devito.tools import toposort
+from devito.tools import toposort, numpy_view_offsets
 
 
 @skipif_yask
@@ -20,3 +21,15 @@ def test_toposort(elements, expected):
         assert ordering == expected
     except ValueError:
         assert expected is None
+
+
+@skipif_yask
+@pytest.mark.parametrize('arr,index,expected', [
+    (np.zeros(shape=(8, 10)), [slice(2, 6), slice(5, 7)], ((2, 2), (5, 3))),
+    (np.zeros(shape=(8, 10)), [slice(2), slice(2)], ((0, 6), (0, 8))),
+    (np.zeros(shape=(8, 10, 2)), [slice(2, 5), slice(2, 5), slice(1, 2)],
+     ((2, 3), (2, 5), (1, 0)))
+])
+def test_numpy_view_offsets(arr, index, expected):
+    view = arr[index]
+    assert numpy_view_offsets(view) == expected
