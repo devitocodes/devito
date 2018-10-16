a89f3b588d8071d3950b2b112fea13501506302a
beterraba <thesupervisar@gmail.com>
2018-10-16 20:40:44 -0300

    updating operators

--- devito/ops/utils.py
+++ devito/ops/utils.py
@@ -7,15 +7,17 @@ namespace = OrderedDict()
 namespace['call-ops_init'] = 'ops_init'
 
 namespace['name-ops_grid'] = 'grid'
-namespace['type-ops_block'] = 'ops_block'
-namespace['call-ops_block'] = 'ops_decl_block'
+namespace['type-ops_grid'] = 'ops_block'
+namespace['call-ops_grid'] = 'ops_decl_block'
 
 namespace['name-ops_dat'] = lambda i: 'dat_u%s' % i
 namespace['type-ops_dat'] = 'ops_dat'
 namespace['call-ops_dat'] = 'ops_decl_dat'
 
-namespace['call-ops_get_data'] = 'ops_get_data'
+namespace['name-ops_stencil'] = lambda i: 'stencil_%s' % i
+namespace['type-ops_stencil'] = 'ops_stencil'
+namespace['call-ops_stencil'] = 'ops_decl_stencil'
+
+namespace['call-ops_par_loop'] = 'ops_par_loop'
 
 namespace['call-ops_exit'] = 'ops_exit'
-
-
