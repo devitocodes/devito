6cc83c8f4cf86c7cc65827a1aa250ecb75d87729
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-11-05 13:15:25 +0100

    builtins: Add inner + refactoring

--- devito/builtins.py
+++ devito/builtins.py
@@ -7,6 +7,8 @@ import numpy as np
 
 import devito as dv
 
+__all__ = ['assign', 'smooth', 'norm', 'sumall', 'inner']
+
 
 def assign(f, v=0):
     """
@@ -51,6 +53,44 @@ def smooth(f, g, axis=None):
         dv.Operator(dv.Eq(f, g.avg(dims=axis)), name='smoother')()
 
 
+# Reduction-inducing builtins
+
+class MPIReduction(object):
+    """
+    A context manager to build MPI-aware reduction Operators.
+    """
+
+    def __init__(self, *functions):
+        grids = {f.grid for f in functions}
+        if len(grids) == 0:
+            self.grid = None
+        elif len(grids) == 1:
+            self.grid = grids.pop()
+        else:
+            raise ValueError("Multiple Grids found")
+        dtype = {f.dtype for f in functions}
+        if len(dtype) == 1:
+            self.dtype = dtype.pop()
+        else:
+            raise ValueError("Illegal mixed data types")
+        self.v = None
+
+    def __enter__(self):
+        i = dv.Dimension(name='i',)
+        self.n = dv.Function(name='n', shape=(1,), dimensions=(i,),
+                             grid=self.grid, dtype=self.dtype)
+        self.n.data[0] = 0
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.grid is None or not dv.configuration['mpi']:
+            assert self.n.data.size == 1
+            self.v = self.n.data[0]
+        else:
+            comm = self.grid.distributor.comm
+            self.v = comm.allreduce(np.asarray(self.n.data))[0]
+
+
 def norm(f, order=2):
     """
     Compute the norm of a :class:`Function`.
@@ -62,10 +102,6 @@ def norm(f, order=2):
     order : int, optional
         The order of the norm. Defaults to 2.
     """
-    i = dv.Dimension(name='i',)
-    n = dv.Function(name='n', shape=(1,), dimensions=(i,), grid=f.grid, dtype=f.dtype)
-    n.data[0] = 0
-
     kwargs = {}
     if f.is_TimeFunction and f._time_buffering:
         kwargs[f.time_dim.max_name] = f._time_size - 1
@@ -74,18 +110,12 @@ def norm(f, order=2):
     # otherwise we would eventually be summing more than expected
     p, eqns = f.guard() if f.is_SparseFunction else (f, [])
 
-    op = dv.Operator(eqns + [dv.Inc(n[0], Abs(Pow(p, order)))], name='norm%d' % order)
-    op.apply(**kwargs)
+    with MPIReduction(f) as mr:
+        op = dv.Operator(eqns + [dv.Inc(mr.n[0], Abs(Pow(p, order)))],
+                         name='norm%d' % order)
+        op.apply(**kwargs)
 
-    # May need a global reduction over MPI
-    if f.grid is None:
-        assert n.data.size == 1
-        v = n.data[0]
-    else:
-        comm = f.grid.distributor.comm
-        v = comm.allreduce(np.asarray(n.data))[0]
-
-    v = Pow(v, 1/order)
+    v = Pow(mr.v, 1/order)
 
     return np.float(v)
 
@@ -99,10 +129,6 @@ def sumall(f):
     f : Function
         Input Function.
     """
-    i = dv.Dimension(name='i',)
-    n = dv.Function(name='n', shape=(1,), dimensions=(i,), grid=f.grid, dtype=f.dtype)
-    n.data[0] = 0
-
     kwargs = {}
     if f.is_TimeFunction and f._time_buffering:
         kwargs[f.time_dim.max_name] = f._time_size - 1
@@ -111,15 +137,57 @@ def sumall(f):
     # otherwise we would eventually be summing more than expected
     p, eqns = f.guard() if f.is_SparseFunction else (f, [])
 
-    op = dv.Operator(eqns + [dv.Inc(n[0], p)], name='sum')
-    op.apply(**kwargs)
+    with MPIReduction(f) as mr:
+        op = dv.Operator(eqns + [dv.Inc(mr.n[0], p)], name='sum')
+        op.apply(**kwargs)
 
-    # May need a global reduction over MPI
-    if f.grid is None:
-        assert n.data.size == 1
-        v = n.data[0]
-    else:
-        comm = f.grid.distributor.comm
-        v = comm.allreduce(np.asarray(n.data))[0]
+    return np.float(mr.v)
 
-    return np.float(v)
+
+def inner(f, g):
+    """
+    Inner product of two :class:`Function`s.
+
+    Parameters
+    ----------
+    f : Function
+        First input operand
+    g : Function
+        Second input operand
+
+    Raises
+    ------
+    ValueError
+        If the two input Functions are defined over different grids, or have
+        different dimensionality, or their dimension-wise sizes don't match.
+        If in input are two SparseFunctions and their coordinates don't match,
+        the exception is raised.
+
+    Notes
+    -----
+    The inner product is the sum of all dimension-wise products. For 1D Functions,
+    the inner product corresponds to the dot product.
+    """
+    # Input check
+    if f.is_TimeFunction and f._time_buffering != g._time_buffering:
+        raise ValueError("Cannot compute `inner` between save/nosave TimeFunctions")
+    if f.shape != g.shape:
+        raise ValueError("`f` and `g` must have same shape")
+    if f._data is None or g._data is None:
+        raise ValueError("Uninitialized input")
+    if f.is_SparseFunction and not np.all(f.coordinates_data == g.coordinates_data):
+        raise ValueError("Non-matching coordinates")
+
+    kwargs = {}
+    if f.is_TimeFunction and f._time_buffering:
+        kwargs[f.time_dim.max_name] = f._time_size - 1
+
+    # Protect SparseFunctions from accessing duplicated (out-of-domain) data,
+    # otherwise we would eventually be summing more than expected
+    rhs, eqns = f.guard(f*g) if f.is_SparseFunction else (f*g, [])
+
+    with MPIReduction(f, g) as mr:
+        op = dv.Operator(eqns + [dv.Inc(mr.n[0], rhs)], name='inner')
+        op.apply(**kwargs)
+
+    return np.float(mr.v)
