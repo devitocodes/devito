cb6e4d0caa59b4ba94c5e1cc2bf31a098865e107
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-11-13 16:07:05 +0100

    tests: Strengthen autotuning check up

--- tests/test_autotuner.py
+++ tests/test_autotuner.py
@@ -1,16 +1,10 @@
-from __future__ import absolute_import
 from functools import reduce
 from operator import mul
-try:
-    from StringIO import StringIO
-except ImportError:
-    # Python3 compatibility
-    from io import StringIO
 
 import pytest
 import numpy as np
-from devito import Grid, Function, TimeFunction, Eq, Operator, configuration, silencio
-from devito.logger import logger, logging
+from devito import (Grid, Function, TimeFunction, Eq, Operator, configuration,
+                    silencio, ruido)
 
 pytestmark = pytest.mark.skipif(configuration['backend'] == 'yask' or
                                 configuration['backend'] == 'ops',
@@ -28,51 +22,34 @@ def test_at_is_actually_working(shape, expected):
     in both 2D and 3D operators.
     """
     grid = Grid(shape=shape)
-
-    buffer = StringIO()
-    temporary_handler = logging.StreamHandler(buffer)
-    logger.addHandler(temporary_handler)
-
     infield = Function(name='infield', grid=grid)
     infield.data[:] = np.arange(reduce(mul, shape), dtype=np.int32).reshape(shape)
     outfield = Function(name='outfield', grid=grid)
+
     stencil = Eq(outfield.indexify(), outfield.indexify() + infield.indexify()*3.0)
     op = Operator(stencil, dle=('blocking', {'blockinner': True, 'blockalways': True}))
 
     # Run with whatever `configuration` says (by default, basic+preemptive)
     op(infield=infield, outfield=outfield, autotune=True)
-    out = [i for i in buffer.getvalue().split('\n') if 'took' in i]
-    assert len(out) == 4
-
-    buffer.truncate(0)
+    assert op._state['autotuning'][-1]['runs'] == 4
+    assert op._state['autotuning'][-1]['tpr'] == 1
 
     # Now try `aggressive` autotuning
     configuration['autotuning'] = 'aggressive'
     op(infield=infield, outfield=outfield, autotune=True)
-    out = [i for i in buffer.getvalue().split('\n') if 'took' in i]
-    assert len(out) == expected
+    assert op._state['autotuning'][-1]['runs'] == expected
+    assert op._state['autotuning'][-1]['tpr'] == 1
     configuration['autotuning'] = configuration._defaults['autotuning']
-
-    buffer.truncate(0)
 
     # Try again, but using the Operator API directly
     op(infield=infield, outfield=outfield, autotune='aggressive')
-    out = [i for i in buffer.getvalue().split('\n') if 'took' in i]
-    assert len(out) == expected
-
-    buffer.truncate(0)
+    assert op._state['autotuning'][-1]['runs'] == expected
+    assert op._state['autotuning'][-1]['tpr'] == 1
 
     # Similar to above
     op(infield=infield, outfield=outfield, autotune=('aggressive', 'preemptive'))
-    out = [i for i in buffer.getvalue().split('\n') if 'took' in i]
-    assert len(out) == expected
-
-    logger.removeHandler(temporary_handler)
-
-    temporary_handler.flush()
-    temporary_handler.close()
-    buffer.flush()
-    buffer.close()
+    assert op._state['autotuning'][-1]['runs'] == expected
+    assert op._state['autotuning'][-1]['tpr'] == 1
 
 
 @silencio(log_level='DEBUG')
@@ -83,10 +60,6 @@ def test_timesteps_per_at_run():
     performing the increment ``a[t + timeorder, ...] = f(a[t, ...], ...)``.
     """
     from devito.core.autotuning import options
-
-    buffer = StringIO()
-    temporary_handler = logging.StreamHandler(buffer)
-    logger.addHandler(temporary_handler)
 
     shape = (30, 30, 30)
     grid = Grid(shape=shape)
@@ -100,10 +73,8 @@ def test_timesteps_per_at_run():
     stencil = Eq(outfield.indexify(), outfield.indexify() + infield.indexify()*3.0)
     op = Operator(stencil, dle=('blocking', {'blockalways': True}))
     op(infield=infield, outfield=outfield, autotune=True)
-    out = [i for i in buffer.getvalue().split('\n') if 'took' in i]
-    assert len(out) == 4
-    assert all('in 1 timesteps' in i for i in out)
-    buffer.truncate(0)
+    assert op._state['autotuning'][-1]['runs'] == 4
+    assert op._state['autotuning'][-1]['tpr'] == 1
 
     # TimeFunction with increasing time order; increasing the time order
     # shouldn't affect how many iterations the autotuner is gonna run
@@ -116,40 +87,48 @@ def test_timesteps_per_at_run():
                      outfield.indexify() + infield.indexify()*3.0)
         op = Operator(stencil, dle=('blocking', {'blockalways': True}))
         op(infield=infield, outfield=outfield, time=20, autotune=True)
-        out = [i for i in buffer.getvalue().split('\n') if 'took' in i]
-        assert len(out) == 4
-        assert all('in %d timesteps' % (options['at_squeezer'] + 1) in i for i in out)
-        buffer.truncate(0)
-
-    logger.removeHandler(temporary_handler)
-
-    temporary_handler.flush()
-    temporary_handler.close()
-    buffer.flush()
-    buffer.close()
+        assert op._state['autotuning'][-1]['runs'] == 4
+        assert op._state['autotuning'][-1]['tpr'] == options['at_squeezer']+1
 
 
+@ruido(profiling='advanced')
 def test_nondestructive_forward():
     """Test autotuning in non-destructive mode."""
     grid = Grid(shape=(64, 64, 64))
     f = TimeFunction(name='f', grid=grid)
 
-    op = Operator(Eq(f.forward, f + 1))
-    op.apply(time=100, autotune=('basic', 'runtime'))
+    op = Operator(Eq(f.forward, f + 1.))
+    summary = op.apply(time=100, autotune=('basic', 'runtime'))
 
-    # AT is expected to have executed 35 timesteps
+    # At is expected to have attempted 6 block shapes
+    assert op._state['autotuning'][0]['runs'] == 6
+
+    # AT is expected to have executed 30 timesteps
+    assert summary['section0'].itershapes[0][0] == 101-30
     assert np.all(f.data[0] == 100)
     assert np.all(f.data[1] == 101)
 
 
+@ruido(profiling='advanced')
 def test_nondestructive_backward():
     """Test autotuning in non-destructive mode."""
     grid = Grid(shape=(64, 64, 64))
     f = TimeFunction(name='f', grid=grid)
 
-    op = Operator(Eq(f.backward, f + 1))
-    op.apply(time=101, autotune=('basic', 'runtime'))
+    op = Operator(Eq(f.backward, f + 1.))
+    summary = op.apply(time=101, autotune=('basic', 'runtime'))
 
-    # AT is expected to have executed 35 timesteps
+    # At is expected to have attempted 6 block shapes
+    assert op._state['autotuning'][0]['runs'] == 6
+
+    # AT is expected to have executed 30 timesteps
+    assert summary['section0'].itershapes[0][0] == 101-30
     assert np.all(f.data[0] == 101)
     assert np.all(f.data[1] == 100)
+
+
+def test_tti_aggressive():
+    from test_dse import tti_operator
+    op = tti_operator(dse='aggressive').op_fwd(kernel='centered', save=False)
+    op.apply(time=0, autotune='aggressive')
+    assert op._state['autotuning'][0]['runs'] == 33
