3da0f40efb6b7fe412c6e2851f2bbb1cd47136d4
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-12-13 09:54:21 +0100

    operator: Drop OperatorRunnable

--- devito/core/operator.py
+++ devito/core/operator.py
@@ -1,19 +1,17 @@
 from collections import OrderedDict
 
 from devito.core.autotuning import autotune
-from devito.cgen_utils import printmark
-from devito.ir.iet import (Call, List, HaloSpot, MetaCall, FindNodes, Transformer,
-                           filter_iterations, retrieve_iteration_tree)
+from devito.ir.iet import Call, List, HaloSpot, MetaCall, FindNodes, Transformer
 from devito.ir.support import align_accesses
 from devito.parameters import configuration
 from devito.mpi import copy, sendrecv, update_halo
-from devito.operator import OperatorRunnable
+from devito.operator import Operator
 from devito.tools import flatten
 
-__all__ = ['Operator']
+__all__ = ['OperatorCore']
 
 
-class OperatorCore(OperatorRunnable):
+class OperatorCore(Operator):
 
     def _specialize_exprs(self, expressions):
         # Align data accesses to the computational domain
@@ -83,36 +81,3 @@ class OperatorCore(OperatorRunnable):
         self._state.setdefault('autotuning', []).append(summary)
 
         return args
-
-
-class OperatorDebug(OperatorCore):
-    """
-    Decorate the generated code with useful print statements.
-    """
-
-    def __init__(self, expressions, **kwargs):
-        super(OperatorDebug, self).__init__(expressions, **kwargs)
-        self._includes.append('stdio.h')
-
-        # Minimize the trip count of the sequential loops
-        iterations = set(flatten(retrieve_iteration_tree(self.body)))
-        mapper = {i: i._rebuild(limits=(max(i.offsets) + 2))
-                  for i in iterations if i.is_Sequential}
-        self.body = Transformer(mapper).visit(self.body)
-
-        # Mark entry/exit points of each non-sequential Iteration tree in the body
-        iterations = [filter_iterations(i, lambda i: not i.is_Sequential, 'any')
-                      for i in retrieve_iteration_tree(self.body)]
-        iterations = [i[0] for i in iterations if i]
-        mapper = {t: List(header=printmark('In nest %d' % i), body=t)
-                  for i, t in enumerate(iterations)}
-        self.body = Transformer(mapper).visit(self.body)
-
-
-class Operator(object):
-
-    def __new__(cls, *args, **kwargs):
-        cls = OperatorDebug if kwargs.pop('debug', False) else OperatorCore
-        obj = cls.__new__(cls, *args, **kwargs)
-        obj.__init__(*args, **kwargs)
-        return obj
