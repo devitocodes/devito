42b55f2cdd721e53516f3e4577125f3742902879
Fabio Luporini <f.luporini12@imperial.ac.uk>
2019-01-10 16:42:26 +0100

    symbolics: Add q_constant

--- devito/symbolics/queries.py
+++ devito/symbolics/queries.py
@@ -5,7 +5,7 @@ from devito.tools import as_tuple
 
 __all__ = ['q_leaf', 'q_indexed', 'q_terminal', 'q_trigonometry', 'q_op',
            'q_terminalop', 'q_sum_of_product', 'q_indirect', 'q_timedimension',
-           'q_affine', 'q_linear', 'q_identity', 'q_inc', 'q_scalar',
+           'q_constant', 'q_affine', 'q_linear', 'q_identity', 'q_inc', 'q_scalar',
            'iq_timeinvariant', 'iq_timevarying']
 
 
@@ -93,6 +93,20 @@ def q_inc(expr):
         return expr.is_Increment
     except AttributeError:
         return False
+
+
+def q_constant(expr):
+    """
+    Return True if ``expr`` is a constant, possibly symbolic, value, False otherwise.
+    Examples of non-constants are expressions containing Dimensions.
+    """
+    for i in expr.free_symbols:
+        try:
+            if not (i.is_Scalar or i.is_Constant):
+                return False
+        except AttributeError:
+            return False
+    return True
 
 
 def q_affine(expr, vars):
