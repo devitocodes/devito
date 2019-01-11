2e10d40b70bb06e1cf7eac3f9680de6ebb4af195
George Bisbas <g.bisbas18@imperial.ac.uk>
2019-01-11 23:16:47 +0000

    Fix whitespaces, long lines

--- examples/seismic/source.py
+++ examples/seismic/source.py
@@ -272,9 +272,12 @@ class DGaussSource(WaveletSource):
     Symbolic object that encapsulate a set of sources with a
     pre-defined 1st derivative wavelet of a Gaussian Source:
 
-    Note: For visualizing the second or third order derivative of Gaussian wavelets, the convention is to use the negative of the normalized derivative.
-    In the case of the second derivative, scaling by -1 produces a wavelet with its main lobe in the positive y direction.
-    This scaling also makes the Gaussian wavelet resemble the Mexican hat, or Ricker, wavelet. The validity of the wavelet is not affected by the -1 scaling factor.
+    Note: For visualizing the second or third order derivative of Gaussian wavelets,
+    the convention is to use the negative of the normalized derivative.
+    In the case of the second derivative, scaling by -1 produces a wavelet with its main
+    lobe in the positive y direction.
+    This scaling also makes the Gaussian wavelet resemble the Mexican hat, or Ricker, wavelet.
+    The validity of the wavelet is not affected by the -1 scaling factor.
 
     returns the 1st order derivative of the Gaussian wavelet
 
@@ -284,13 +287,12 @@ class DGaussSource(WaveletSource):
     :param time: Discretized values of time in ms
     """
 
-    def wavelet(self, f0, t, a ):
+    def wavelet(self, f0, t, a):
         """
         Defines the 1st derivative of a Gaussian wavelet with a peak frequency f0 at time t.
 
         :param f0: Peak frequency in kHz
         :param t: Discretized values of time in ms
         :param a: Maximum amplitude, real constant defined by user
-
         """
         return -2.*a*(t - 1/f0) * np.exp(-a * (t - 1/f0)**2)
