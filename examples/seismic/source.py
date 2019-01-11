37b168f23a56e69ff25a04c2f8bb618cf0a40937
George Bisbas <g.bisbas18@imperial.ac.uk>
2019-01-11 17:46:33 +0000

    Add files via upload

--- examples/seismic/source.py
+++ examples/seismic/source.py
@@ -10,7 +10,7 @@ from devito.types import Dimension, SparseTimeFunction
 
 
 __all__ = ['PointSource', 'Receiver', 'Shot', 'WaveletSource',
-           'RickerSource', 'GaborSource', 'TimeAxis']
+           'RickerSource', 'GaborSource', 'TimeAxis', 'DGaussSource']
 
 
 class TimeAxis(object):
@@ -265,3 +265,34 @@ class GaborSource(WaveletSource):
         tcut = 1.5 / agauss
         s = (t-tcut) * agauss
         return np.exp(-2*s**2) * np.cos(2 * np.pi * s)
+
+class DGaussSource(WaveletSource):
+    """
+    Symbolic object that encapsulate a set of sources with a
+    pre-defined 1st derivative wavelet of a Gaussian Source:
+
+    Note: For visualizing the second or third order derivative of Gaussian wavelets, the convention is to use the negative of the normalized derivative.
+    In the case of the second derivative, scaling by -1 produces a wavelet with its main lobe in the positive y direction.
+    This scaling also makes the Gaussian wavelet resemble the Mexican hat, or Ricker, wavelet. The validity of the wavelet is not affected by the -1 scaling factor.
+
+    returns the 1st order derivative of the Gaussian wavelet
+
+    :param name: Name for the resulting symbol
+    :param grid: :class:`Grid` object defining the computational domain.
+    :param f0: Peak frequency for wavelet in kHz
+    :param time: Discretized values of time in ms
+    """
+
+
+
+    def wavelet(self, f0, t, a ):
+        """
+        Defines the 1st derivative of a Gaussian wavelet with a peak frequency f0 at time t.
+
+        :param f0: Peak frequency in kHz
+        :param t: Discretized values of time in ms
+        :param a: Maximum amplitude, real constant defined by user
+
+        """
+
+        return -2.*a*(t - 1/f0) * np.exp(-a * (t - 1/f0)**2)
