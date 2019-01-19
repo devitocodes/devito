0248bc2e4fe45e75e5070e951203fef3b073a08f
Fabio Luporini <f.luporini12@imperial.ac.uk>
2019-01-19 11:48:24 +0100

    tests: small MPI test update

--- tests/test_mpi.py
+++ tests/test_mpi.py
@@ -379,7 +379,7 @@ free(bufg);"""
 
         heb = HaloExchangeBuilder(False)
         mock_halo = {(x, LEFT): True, (x, RIGHT): True, (y, LEFT): True, (y, RIGHT): True}
-        haloupdate = heb._make_haloupdate(f, [t], mock_halo)
+        haloupdate = heb._make_haloupdate(f, [t], mock_halo, uniquekey='')
         assert str(haloupdate.parameters) == """\
 (f(t, x, y), comm, nb, otime)"""
         assert str(haloupdate.body[0]) == """\
