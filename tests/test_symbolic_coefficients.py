01ac94efffd5db6349bdf6cf4b5e069dfd5f4357
Rhodri Nelson <rhodri.nelson@gmail.com>
2019-01-23 21:54:56 +0000

    Update test.

--- tests/test_symbolic_coefficients.py
+++ tests/test_symbolic_coefficients.py
@@ -1,20 +1,49 @@
-import weakref
-
 import numpy as np
 import pytest
 
 from conftest import skipif
-from devito import Grid, Function, TimeFunction
+from devito import Grid, Function, TimeFunction, Eq, Coefficient, Coefficients
+from devito.finite_differences import Differentiable
+
+_PRECISION = 9
 
 pytestmark = skipif(['yask', 'ops'])
 
 
-@pytest.mark.parametrize('FunctionToBeNamed', [Function, TimeFunction])
-def test_cache_function_new(FunctionToBeNamed):
-    """Test that ..."""
-    grid = Grid(shape=(2,2))
-    u0 = TimeFunction(name='u', grid=grid)
-    u1 = TimeFunction(name='u', grid=grid, coefficients='symbolic')
-    eq1 = Eq(u1.dt-u1.dx)
-    eq0 = Eq(-u0.dx+u0.dt)
-    assert(eq1.__repr__() == eq0.__repr__())
+class TestSC(object):
+    """
+    Class for testing symbolic FD coefficients
+    functionality
+    """
+
+    @pytest.mark.parametrize('order', [1, 2, 6])
+    def test_default_rules(self, order):
+        """
+        Test that the default replacement rules return the same
+        as standard FD.
+        """
+        grid = Grid(shape=(20, 20))
+        u0 = TimeFunction(name='u', grid=grid, time_order=order, space_order=order)
+        u1 = TimeFunction(name='u', grid=grid, time_order=order, space_order=order,
+                          coefficients='symbolic')
+        eq0 = Eq(-u0.dx+u0.dt)
+        eq1 = Eq(u1.dt-u1.dx)
+        assert(eq0.evalf(_PRECISION).__repr__() == eq1.evalf(_PRECISION).__repr__())
+
+    @pytest.mark.parametrize('expected', [
+        ('0.1*u(x) - 0.6*u(x - h_x) + 0.6*u(x + h_x)'),
+    ])
+    def test_coefficients(self, expected):
+        """Test that custom coefficients return the expected result"""
+        grid = Grid(shape=(10,))
+        u = Function(name='u', grid=grid, space_order=2, coefficients='symbolic')
+        x = grid.dimensions
+
+        # FIXME: Make sure to throw error if len(np.array([-0.6, 0.1, 0.6]))
+        # doesn't match space_order
+        coeffs = Coefficient(1, u, x[0], np.array([-0.6, 0.1, 0.6]))
+
+        eq = Eq(u.dx, coefficients=Coefficients(coeffs))
+
+        assert isinstance(eq.lhs, Differentiable)
+        assert expected == str(eq.lhs)
