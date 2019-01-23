4363d56c8bbfb6093d0997aa6a8a05a03b070a0d
Rhodri Nelson <rhodri.nelson@gmail.com>
2019-01-23 17:03:22 +0000

    Add to test (which is still broken).

--- tests/test_symbolic_coefficients.py
+++ tests/test_symbolic_coefficients.py
@@ -12,5 +12,9 @@ pytestmark = skipif(['yask', 'ops'])
 @pytest.mark.parametrize('FunctionToBeNamed', [Function, TimeFunction])
 def test_cache_function_new(FunctionToBeNamed):
     """Test that ..."""
-    grid = Grid(shape=(1,1))
-    u1 = TimeFunction(name='u', grid=grid)
+    grid = Grid(shape=(2,2))
+    u0 = TimeFunction(name='u', grid=grid)
+    u1 = TimeFunction(name='u', grid=grid, coefficients='symbolic')
+    eq1 = Eq(u1.dt-u1.dx)
+    eq0 = Eq(-u0.dx+u0.dt)
+    assert(eq1.__repr__() == eq0.__repr__())
