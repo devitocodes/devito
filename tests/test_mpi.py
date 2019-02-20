c9750e15a0442edcc893754185f0c5cc21267749
Fabio Luporini <f.luporini12@imperial.ac.uk>
2019-02-20 10:32:05 +0100

    tests: Check haloudpate hoisting in presence of flowdeps

--- tests/test_mpi.py
+++ tests/test_mpi.py
@@ -733,6 +733,32 @@ class TestOperatorSimple(object):
         assert len(calls) == 1
 
     @pytest.mark.parallel(mode=1)
+    def test_hoist_haloupdate_if_no_flowdep(self):
+        grid = Grid(shape=(12,))
+        x = grid.dimensions[0]
+        t = grid.stepping_dim
+
+        i = Dimension(name='i')
+
+        f = TimeFunction(name='f', grid=grid)
+        g = Function(name='g', grid=grid)
+        h = Function(name='h', grid=grid)
+
+        op = Operator([Eq(f.forward, f[t, x-1] + f[t, x+1] + 1.),
+                       Inc(g[i], f[t, h[i]] + 1.)])
+
+        calls = FindNodes(Call).visit(op)
+        assert len(calls) == 1
+
+        # Below, there is a flow-dependence along `x`, so a further halo update
+        # before the Inc is required
+        op = Operator([Eq(f.forward, f[t, x-1] + f[t, x+1] + 1.),
+                       Inc(g[i], f[t+1, h[i]] + 1.)])
+
+        calls = FindNodes(Call).visit(op)
+        assert len(calls) == 2
+
+    @pytest.mark.parallel(mode=1)
     def test_stencil_nowrite_implies_haloupdate_anyway(self):
         grid = Grid(shape=(12,))
         x = grid.dimensions[0]
