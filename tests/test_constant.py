ed5d7bf17d397d29439b9153ee4a4dee595b6f25
Rhodri Nelson <rhodri.nelson@gmail.com>
2019-02-22 15:16:37 +0000

    Add test_constant.py.

--- tests/test_constant.py
+++ tests/test_constant.py
@@ -1,10 +1,10 @@
 import numpy as np
-import pytest
 
 from conftest import skipif
 from devito import Grid, Constant, Function, TimeFunction, Eq, solve, Operator
 
 pytestmark = skipif(['yask', 'ops'])
+
 
 class TestConst(object):
     """
@@ -16,35 +16,25 @@ class TestConst(object):
         Test that the default replacement rules return the same
         as standard FD.
         """
-        
-        from IPython import embed, os
 
         n = 5
         t = Constant(name='t', dtype=np.int32)
 
         grid = Grid(shape=(2, 2))
         x, y = grid.dimensions
-        
+
         f = TimeFunction(name='f', grid=grid, save=n+1)
-        #check = Function(name='check', grid=grid)
-
-        #f.data[:] = 0
+        f.data[:] = 0
         eq = Eq(f.dt-1)
-        #eq_test = Eq(check,f[t,x,y])
-
-        stencil = solve(eq, f.forward)
-        #print(stencil)
-        embed()
+        stencil = Eq(f.forward, solve(eq, f.forward))
         op = Operator([stencil])
-        
-        #op_test = Operator([eq_test])
+        op.apply(time_m=0, time_M=n-1, dt=1)
 
-        #
-        #for j in range(0,n):
-            ##if j>0:
-                ##t.data = j-1
-                ##op_check.apply()
-                ##assert(np.amax(check.data[:], axis=None) == j-1)
-                ##assert(np.amin(check.data[:], axis=None) == j-1)
-            #op.apply(time_m=j, time_M=j, dt=1)
-            #embed()
+        check = Function(name='check', grid=grid)
+        eq_test = Eq(check, f[t, x, y])
+        op_test = Operator([eq_test])
+        for j in range(0, n+1):
+                t.data = j  # Ensure constant is being updated correctly
+                op_test.apply(t=t)
+                assert(np.amax(check.data[:], axis=None) == j)
+                assert(np.amin(check.data[:], axis=None) == j)
