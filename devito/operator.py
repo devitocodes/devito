4f87985367bcb6a49aa27a2e1d895315bfa11946
Rhodri Nelson <rhodri.nelson@gmail.com>
2019-04-05 14:34:04 +0100

    Tweaks/tyding.

--- devito/operator.py
+++ devito/operator.py
@@ -156,7 +156,11 @@ class Operator(Callable):
         # autotuning reports, etc
         self._state = {}
 
-        # Form and gather any required implicit expressions
+        # Form and gather any required implicit expressions.
+        # Implicit expressions are those not explicitly defined by the user
+        # but instead are requisites of some specified functionality. Since they
+        # must be treated in a similar manner to user specified expressions
+        # they are added prior to the creation of the IET.
         expressions = self._add_implicit(expressions)
 
         # Expression lowering: indexification, substitution rules, specialization
@@ -213,34 +217,26 @@ class Operator(Callable):
     # Compilation
 
     def _add_implicit(self, expressions):
-        """
-        Create and add to the expression tree any associated implicit
-        expressions.
-        """
-        updated_expressions = []
+        """Create and add any associated implicit expressions."""
+        processed = []
         for e in expressions:
-            subdomain = e.subdomain
-            if bool(subdomain):
+            if e.subdomain:
+                # FIXME: Need to modify to avoid duplication of implicit equations for
+                # certain equation sets. Also add test for this.
                 try:
-                    dat = e._subdomain._ie_dat
-                except AttributeError:
-                    dat = None
-                if bool(dat):
+                    dat = e._subdomain._implicit_eq_dat
                     dims = [d.root for d in e.free_symbols if isinstance(d, Dimension)]
-                    sub_dims = [d.root for d in subdomain.dimensions]
+                    sub_dims = [d.root for d in e.subdomain.dimensions]
                     dims = list(set(dims).symmetric_difference(set(sub_dims)))
-                    dims.append(subdomain._implicit_dimension)
-                    implicit_expressions = []
-                    for i in dat:
-                        eq = Eq(i['rhs'], i['lhs'], implicit_dims=dims)
-                        implicit_expressions.append(eq)
-                    for ie in implicit_expressions:
-                        updated_expressions.append(ie)
-                    for d in subdomain.dimensions:
-                        dims.append(d)
-                    e._implicit_dims = as_tuple(dims)
-            updated_expressions.append(e)
-        return updated_expressions
+                    dims.append(e.subdomain._implicit_dimension)
+                    implicit_expressions = [Eq(i['rhs'], i['lhs'], implicit_dims=dims) for i in dat]
+                    processed.extend(implicit_expressions)
+                    dims.extend(e.subdomain.dimensions)
+                    e = Eq(e.lhs, e.rhs, subdomain=e.subdomain, implicit_dims=dims)
+                except AttributeError:
+                    pass
+            processed.append(e)
+        return processed
 
     def _apply_substitutions(self, expressions, subs):
         """
