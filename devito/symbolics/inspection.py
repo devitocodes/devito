9907c2a17571ad462a456a540c9daa0ef63c309a
Fabio Luporini <f.luporini12@imperial.ac.uk>
2019-04-16 10:08:58 +0200

    symbolics: Fix estimate_cost

--- devito/symbolics/inspection.py
+++ devito/symbolics/inspection.py
@@ -4,7 +4,7 @@ from sympy import cos, sin
 
 from devito.symbolics.search import retrieve_ops, search
 from devito.logger import warning
-from devito.tools import flatten
+from devito.tools import as_tuple, flatten
 
 __all__ = ['count', 'estimate_cost']
 
@@ -20,39 +20,40 @@ def count(exprs, query):
     return dict(mapper)
 
 
-def estimate_cost(expr, estimate_functions=False):
+def estimate_cost(exprs, estimate_functions=False):
     """
     Estimate the operation count of an expression.
 
     Parameters
     ----------
-    expr : expr-like or list of expr-like
+    exprs : expr-like or list of expr-like
         One or more expressions for which the operation count is calculated.
     estimate_functions : dict, optional
         A mapper from known functions (e.g., sin, cos) to (estimated) operation counts.
     """
     external_functions = {sin: 50, cos: 50}
     try:
-        # Is it a plain SymPy object ?
-        iter(expr)
-    except TypeError:
-        expr = [expr]
+        # Is it a plain symbol/array ?
+        if exprs.is_AbstractFunction or exprs.is_AbstractSymbol:
+            return 0
+    except AttributeError:
+        pass
     try:
         # Is it a dict ?
-        expr = expr.values()
+        exprs = exprs.values()
     except AttributeError:
         try:
-            # Must be a list of dicts then
-            expr = flatten([i.values() for i in expr])
-        except AttributeError:
+            # Could still be a list of dicts
+            exprs = flatten([i.values() for i in exprs])
+        except (AttributeError, TypeError):
             pass
     try:
         # At this point it must be a list of SymPy objects
         # We don't use SymPy's count_ops because we do not count integer arithmetic
         # (e.g., array index functions such as i+1 in A[i+1])
         # Also, the routine below is *much* faster than count_ops
-        expr = [i.rhs if i.is_Equality else i for i in expr]
-        operations = flatten(retrieve_ops(i) for i in expr)
+        exprs = [i.rhs if i.is_Equality else i for i in as_tuple(exprs)]
+        operations = flatten(retrieve_ops(i) for i in exprs)
         flops = 0
         for op in operations:
             if op.is_Function:
@@ -64,4 +65,5 @@ def estimate_cost(expr, estimate_functions=False):
                 flops += len(op.args) - (1 + sum(True for i in op.args if i.is_Integer))
         return flops
     except:
-        warning("Cannot estimate cost of %s" % str(expr))
+        warning("Cannot estimate cost of `%s`" % str(exprs))
+        return 0
