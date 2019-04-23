aae75d74d99ee807a455058147a0e386150b5644
Fabio Luporini <f.luporini12@imperial.ac.uk>
2019-04-23 09:44:17 +0200

    tests: Regroup DLE tests

--- tests/test_dle.py
+++ tests/test_dle.py
@@ -200,118 +200,119 @@ def test_cache_blocking_edge_cases_highorder(shape, blockshape):
     assert np.equal(wo_blocking.data, w_blocking.data).all()
 
 
-@pytest.mark.parametrize('exprs,expected', [
-    # trivial 1D
-    (['Eq(fa[x], fa[x] + fb[x])'],
-     (True, False)),
-    # trivial 1D
-    (['Eq(t0, fa[x] + fb[x])', 'Eq(fa[x], t0 + 1)'],
-     (True, False)),
-    # trivial 2D
-    (['Eq(t0, fc[x,y] + fd[x,y])', 'Eq(fc[x,y], t0 + 1)'],
-     (True, False)),
-    # outermost parallel, innermost sequential
-    (['Eq(t0, fc[x,y] + fd[x,y])', 'Eq(fc[x,y+1], t0 + 1)'],
-     (True, False)),
-    # outermost sequential, innermost parallel
-    (['Eq(t0, fc[x,y] + fd[x,y])', 'Eq(fc[x+1,y], t0 + 1)'],
-     (False, True)),
-    # outermost sequential, innermost parallel
-    (['Eq(fc[x,y], fc[x+1,y+1] + fc[x-1,y])'],
-     (False, True)),
-    # outermost parallel w/ repeated dimensions, but the compiler is conservative
-    # and makes it sequential, as it doesn't like what happens in the inner dims,
-    # where `x`, rather than `y`, is used
-    (['Eq(t0, fc[x,x] + fd[x,y+1])', 'Eq(fc[x,x], t0 + 1)'],
-     (False, False)),
-    # outermost sequential w/ repeated dimensions
-    (['Eq(t0, fc[x,x] + fd[x,y+1])', 'Eq(fc[x,x+1], t0 + 1)'],
-     (False, False)),
-    # outermost sequential, innermost sequential (classic skewing example)
-    (['Eq(fc[x,y], fc[x,y+1] + fc[x-1,y])'],
-     (False, False)),
-    # outermost parallel, innermost sequential w/ double tensor write
-    (['Eq(fc[x,y], fc[x,y+1] + fd[x-1,y])', 'Eq(fd[x-1,y+1], fd[x-1,y] + fc[x,y+1])'],
-     (True, False)),
-    # outermost sequential, innermost parallel w/ mixed dimensions
-    (['Eq(fc[x+1,y], fc[x,y+1] + fc[x,y])', 'Eq(fc[x+1,y], 2. + fc[x,y+1])'],
-     (False, True)),
-])
-def test_loops_ompized(fa, fb, fc, fd, t0, t1, t2, t3, exprs, expected, iters):
-    scope = [fa, fb, fc, fd, t0, t1, t2, t3]
-    node_exprs = [Expression(DummyEq(EVAL(i, *scope))) for i in exprs]
-    ast = iters[6](iters[7](node_exprs))
+class TestNodeParallelism(object):
 
-    ast = iet_analyze(ast)
+    @pytest.mark.parametrize('exprs,expected', [
+        # trivial 1D
+        (['Eq(fa[x], fa[x] + fb[x])'],
+         (True, False)),
+        # trivial 1D
+        (['Eq(t0, fa[x] + fb[x])', 'Eq(fa[x], t0 + 1)'],
+         (True, False)),
+        # trivial 2D
+        (['Eq(t0, fc[x,y] + fd[x,y])', 'Eq(fc[x,y], t0 + 1)'],
+         (True, False)),
+        # outermost parallel, innermost sequential
+        (['Eq(t0, fc[x,y] + fd[x,y])', 'Eq(fc[x,y+1], t0 + 1)'],
+         (True, False)),
+        # outermost sequential, innermost parallel
+        (['Eq(t0, fc[x,y] + fd[x,y])', 'Eq(fc[x+1,y], t0 + 1)'],
+         (False, True)),
+        # outermost sequential, innermost parallel
+        (['Eq(fc[x,y], fc[x+1,y+1] + fc[x-1,y])'],
+         (False, True)),
+        # outermost parallel w/ repeated dimensions, but the compiler is conservative
+        # and makes it sequential, as it doesn't like what happens in the inner dims,
+        # where `x`, rather than `y`, is used
+        (['Eq(t0, fc[x,x] + fd[x,y+1])', 'Eq(fc[x,x], t0 + 1)'],
+         (False, False)),
+        # outermost sequential w/ repeated dimensions
+        (['Eq(t0, fc[x,x] + fd[x,y+1])', 'Eq(fc[x,x+1], t0 + 1)'],
+         (False, False)),
+        # outermost sequential, innermost sequential (classic skewing example)
+        (['Eq(fc[x,y], fc[x,y+1] + fc[x-1,y])'],
+         (False, False)),
+        # outermost parallel, innermost sequential w/ double tensor write
+        (['Eq(fc[x,y], fc[x,y+1] + fd[x-1,y])', 'Eq(fd[x-1,y+1], fd[x-1,y] + fc[x,y+1])'],
+         (True, False)),
+        # outermost sequential, innermost parallel w/ mixed dimensions
+        (['Eq(fc[x+1,y], fc[x,y+1] + fc[x,y])', 'Eq(fc[x+1,y], 2. + fc[x,y+1])'],
+         (False, True)),
+    ])
+    def test_iterations_ompized(self, fa, fb, fc, fd, t0, t1, t2, t3,
+                                exprs, expected, iters):
+        scope = [fa, fb, fc, fd, t0, t1, t2, t3]
+        node_exprs = [Expression(DummyEq(EVAL(i, *scope))) for i in exprs]
+        ast = iters[6](iters[7](node_exprs))
 
-    iet, _ = transform(ast, mode='openmp')
-    iterations = FindNodes(Iteration).visit(iet)
-    assert len(iterations) == len(expected)
+        ast = iet_analyze(ast)
 
-    # Check for presence of pragma omp
-    for i, j in zip(iterations, expected):
-        pragmas = i.pragmas
-        if j is True:
-            assert len(pragmas) == 1
-            pragma = pragmas[0]
-            assert 'omp for' in pragma.value
+        iet, _ = transform(ast, mode='openmp')
+        iterations = FindNodes(Iteration).visit(iet)
+        assert len(iterations) == len(expected)
+
+        # Check for presence of pragma omp
+        for i, j in zip(iterations, expected):
+            pragmas = i.pragmas
+            if j is True:
+                assert len(pragmas) == 1
+                pragma = pragmas[0]
+                assert 'omp for' in pragma.value
+            else:
+                for k in pragmas:
+                    assert 'omp for' not in k.value
+
+    def test_dynamic_nthreads(self):
+        grid = Grid(shape=(16, 16, 16))
+        f = TimeFunction(name='f', grid=grid)
+
+        op = Operator(Eq(f.forward, f + 1.), dle='openmp')
+
+        # Check num_threads appears in the generated code
+        # Not very elegant, but it does the trick
+        assert 'num_threads(nthreads)' in str(op)
+
+        # Check `op` accepts the `nthreads` kwarg
+        op.apply(time=0)
+        op.apply(time_m=1, time_M=1, nthreads=4)
+        assert np.all(f.data[0] == 2.)
+
+        # Check the actual value assumed by `nthreads`
+        assert op.arguments(time=0)['nthreads'] == NThreads.default_value()
+        assert op.arguments(time=0, nthreads=123)['nthreads'] == 123  # user supplied
+
+    @pytest.mark.parametrize('eq,expected,blocking', [
+        ('Eq(f, 2*f)', [2, 0, 0], False),
+        ('Eq(u, 2*u)', [0, 2, 0, 0], False),
+        ('Eq(u, 2*u)', [3, 0, 0, 0, 0, 0], True)
+    ])
+    @patch("devito.dle.parallelizer.Ompizer.COLLAPSE", 1)
+    def test_collapsing(self, eq, expected, blocking):
+        grid = Grid(shape=(3, 3, 3))
+
+        f = Function(name='f', grid=grid)  # noqa
+        u = TimeFunction(name='u', grid=grid)  # noqa
+
+        eq = eval(eq)
+
+        if blocking:
+            op = Operator(eq, dle=('blocking', 'openmp', {'blockinner': True}))
+            iterations = FindNodes(Iteration).visit(op._func_table['bf0'])
         else:
-            for k in pragmas:
-                assert 'omp for' not in k.value
+            op = Operator(eq, dle='openmp')
+            iterations = FindNodes(Iteration).visit(op)
 
+        assert len(iterations) == len(expected)
 
-def test_dynamic_nthreads():
-    grid = Grid(shape=(16, 16, 16))
-    f = TimeFunction(name='f', grid=grid)
-
-    op = Operator(Eq(f.forward, f + 1.), dle='openmp')
-
-    # Check num_threads appears in the generated code
-    # Not very elegant, but it does the trick
-    assert 'num_threads(nthreads)' in str(op)
-
-    # Check `op` accepts the `nthreads` kwarg
-    op.apply(time=0)
-    op.apply(time_m=1, time_M=1, nthreads=4)
-    assert np.all(f.data[0] == 2.)
-
-    # Check the actual value assumed by `nthreads`
-    assert op.arguments(time=0)['nthreads'] == NThreads.default_value()
-    assert op.arguments(time=0, nthreads=123)['nthreads'] == 123  # user supplied
-
-
-@pytest.mark.parametrize('eq,expected,blocking', [
-    ('Eq(f, 2*f)', [2, 0, 0], False),
-    ('Eq(u, 2*u)', [0, 2, 0, 0], False),
-    ('Eq(u, 2*u)', [3, 0, 0, 0, 0, 0], True)
-])
-@patch("devito.dle.parallelizer.Ompizer.COLLAPSE", 1)
-def test_loops_collapsed(eq, expected, blocking):
-    grid = Grid(shape=(3, 3, 3))
-
-    f = Function(name='f', grid=grid)  # noqa
-    u = TimeFunction(name='u', grid=grid)  # noqa
-
-    eq = eval(eq)
-
-    if blocking:
-        op = Operator(eq, dle=('blocking', 'openmp', {'blockinner': True}))
-        iterations = FindNodes(Iteration).visit(op._func_table['bf0'])
-    else:
-        op = Operator(eq, dle='openmp')
-        iterations = FindNodes(Iteration).visit(op)
-
-    assert len(iterations) == len(expected)
-
-    # Check for presence of pragma omp + collapse clause
-    for i, j in zip(iterations, expected):
-        if j > 0:
-            assert len(i.pragmas) == 1
-            pragma = i.pragmas[0]
-            assert 'omp for collapse(%d)' % j in pragma.value
-        else:
-            for k in i.pragmas:
-                assert 'omp for collapse' not in k.value
+        # Check for presence of pragma omp + collapse clause
+        for i, j in zip(iterations, expected):
+            if j > 0:
+                assert len(i.pragmas) == 1
+                pragma = i.pragmas[0]
+                assert 'omp for collapse(%d)' % j in pragma.value
+            else:
+                for k in i.pragmas:
+                    assert 'omp for collapse' not in k.value
 
 
 class TestNestedParallelism(object):
