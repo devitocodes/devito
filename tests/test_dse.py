b49f7c72e8ac766963efb750e5efacef74d882b8
Fabio Luporini <f.luporini12@imperial.ac.uk>
2019-04-28 10:13:54 +0200

    tests: Adjust expected operation counts

--- tests/test_dse.py
+++ tests/test_dse.py
@@ -511,7 +511,7 @@ def test_tti_rewrite_aggressive_wmpi():
 
 @switchconfig(profiling='advanced')
 @pytest.mark.parametrize('space_order,expected', [
-    (8, 152), (16, 268)
+    (8, 150), (16, 266)
 ])
 def test_tti_rewrite_aggressive_opcounts(space_order, expected):
     operator = tti_operator(dse='aggressive', space_order=space_order)
@@ -521,7 +521,7 @@ def test_tti_rewrite_aggressive_opcounts(space_order, expected):
 
 @switchconfig(profiling='advanced')
 @pytest.mark.parametrize('space_order,expected', [
-    (4, 180), (12, 372)
+    (4, 181), (12, 373)
 ])
 def test_tti_v2_rewrite_aggressive_opcounts(space_order, expected):
     grid = Grid(shape=(3, 3, 3))
