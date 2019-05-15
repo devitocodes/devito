c7ee4d9fc87c8227c80c715b5d30e84ed55d500e
Fabio Luporini <f.luporini12@imperial.ac.uk>
2019-05-15 09:22:16 +0200

    dse: Fixup imports

--- devito/dse/__init__.py
+++ devito/dse/__init__.py
@@ -1,4 +1,4 @@
 from devito.dse.aliases import *  # noqa
 from devito.dse.manipulation import *  # noqa
-from devito.dle.rewriters import *  # noqa
+from devito.dse.rewriters import *  # noqa
 from devito.dse.transformer import *  # noqa
