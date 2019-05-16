efb6f8d61bfb34b9df4fc3403c897e7c8e5a116f
Fabio Luporini <f.luporini12@imperial.ac.uk>
2019-05-16 15:28:10 +0200

    tools: Add OrderedSet

--- devito/tools/data_structures.py
+++ devito/tools/data_structures.py
@@ -1,4 +1,4 @@
-from collections import Callable, Iterable, OrderedDict, Mapping, deque
+from collections import Callable, Iterable, OrderedDict, Mapping, MutableSet, deque
 from functools import reduce
 
 import numpy as np
@@ -8,7 +8,7 @@ from devito.tools.utils import as_tuple, filter_ordered
 from devito.tools.algorithms import toposort
 
 __all__ = ['Bunch', 'EnrichedTuple', 'ReducerMap', 'DefaultOrderedDict',
-           'PartialOrderTuple', 'DAG']
+           'OrderedSet', 'PartialOrderTuple', 'DAG']
 
 
 class Bunch(object):
@@ -156,6 +156,61 @@ class DefaultOrderedDict(OrderedDict):
 
     def __copy__(self):
         return type(self)(self.default_factory, self)
+
+
+class OrderedSet(OrderedDict, MutableSet):
+
+    """
+    A simple implementation of an ordered set.
+
+    Notes
+    -----
+    Originally extracted from:
+
+        https://stackoverflow.com/questions/1653970/does-python-have-an-ordered-set
+    """
+
+    def update(self, *args, **kwargs):
+        if kwargs:
+            raise TypeError("update() takes no keyword arguments")
+
+        for s in args:
+            for e in s:
+                 self.add(e)
+
+    def add(self, elem):
+        self[elem] = None
+
+    def discard(self, elem):
+        self.pop(elem, None)
+
+    def __le__(self, other):
+        return all(e in other for e in self)
+
+    def __lt__(self, other):
+        return self <= other and self != other
+
+    def __ge__(self, other):
+        return all(e in self for e in other)
+
+    def __gt__(self, other):
+        return self >= other and self != other
+
+    def __repr__(self):
+        return 'OrderedSet([%s])' % (', '.join(map(repr, self.keys())))
+
+    def __str__(self):
+        return '{%s}' % (', '.join(map(repr, self.keys())))
+
+    difference = property(lambda self: self.__sub__)
+    difference_update = property(lambda self: self.__isub__)
+    intersection = property(lambda self: self.__and__)
+    intersection_update = property(lambda self: self.__iand__)
+    issubset = property(lambda self: self.__le__)
+    issuperset = property(lambda self: self.__ge__)
+    symmetric_difference = property(lambda self: self.__xor__)
+    symmetric_difference_update = property(lambda self: self.__ixor__)
+    union = property(lambda self: self.__or__)
 
 
 class PartialOrderTuple(tuple):
