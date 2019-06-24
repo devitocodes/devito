9cb0a6c27161ecb7a5a05c974f5f3e6201564dbd
George Bisbas <g.bisbas18@imperial.ac.uk>
2019-06-24 23:53:16 +0100

    flake8: fixes

--- devito/dse/rewriters.py
+++ devito/dse/rewriters.py
@@ -13,14 +13,12 @@ from devito.dse.manipulation import (common_subexprs_elimination, collect_nested
                                      compact_temporaries)
 from devito.exceptions import DSEException
 from devito.logger import dse_warning as warning
-from devito.parameters import configuration
 from devito.symbolics import (bhaskara_cos, bhaskara_sin, estimate_cost, freeze,
                               iq_timeinvariant, pow_to_mul, retrieve_indexed,
                               q_affine, q_leaf, q_scalar, q_sum_of_product,
                               q_terminalop, xreplace_constrained, xreplace_indices)
 from devito.tools import flatten, generator
 from devito.types import Array, Scalar
-from devito.types import TimeDimension, SpaceDimension
 
 __all__ = ['BasicRewriter', 'AdvancedRewriter', 'AggressiveRewriter', 'CustomRewriter']
 
@@ -399,7 +397,7 @@ class AggressiveRewriter(AdvancedRewriter):
             else:
                 mapper[i.dim] = i.dim + skew_factor*skew_dim
                 intervals.append(Interval(i.dim, -skew_factor*skew_dim,
-                                         -skew_factor*skew_dim))
+                                          -skew_factor*skew_dim))
 
         ispace = IterationSpace(intervals, sub_iterators, directions)
         cluster = Cluster(cluster.exprs, ispace, cluster.dspace, cluster.atomics,
