9499cae1e8f02ea2cae92e052f0e9077f909ee7e
mloubout <mathias.louboutin@gmail.com>
2019-07-01 11:53:31 -0400

    fd: fix default fd_order from expression

--- devito/finite_differences/derivative.py
+++ devito/finite_differences/derivative.py
@@ -115,7 +115,9 @@ class Derivative(sympy.Derivative, Differentiable):
             orders = as_tuple(orders)
 
         # Finite difference orders
-        fd_orders = kwargs.get('fd_order', tuple([expr.space_order]*len(dims)))
+        fd_orders = kwargs.get('fd_order', tuple([expr.time_order if
+                                                  getattr(d, 'is_Time', False) else
+                                                  expr.space_order for d in dims]))
         if len(dims) == 1 and isinstance(fd_orders, Iterable):
             fd_orders = fd_orders[0]
 
