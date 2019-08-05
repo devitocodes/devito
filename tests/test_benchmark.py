4cef3788a19b9ad7059184a39accd2b551407de4
Fabio Luporini <f.luporini12@imperial.ac.uk>
2019-08-05 12:14:09 +0200

    tests: Check bench mode==run with fixed block shape

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,10 +1,7 @@
-import os
 from subprocess import check_call
 
-import pytest
 
-
-def run(command, problem, so, shape, nbpml, *extra):
+def run_cmd(command, problem, so, shape, nbpml, *extra):
     args = ["python", "../benchmarks/user/benchmark.py", command]
     args.extend(["-P", str(problem)])
     args.extend(["-so", str(so)])
@@ -15,12 +12,12 @@ def run(command, problem, so, shape, nbpml, *extra):
 
 
 def test_test_tti():
-    run('test', 'tti', 4, [20, 20, 20], 5)
+    run_cmd('test', 'tti', 4, [20, 20, 20], 5)
 
 
 def test_test_acoustic():
-    run('test', 'acoustic', 4, [20, 20, 20], 5)
+    run_cmd('test', 'acoustic', 4, [20, 20, 20], 5)
 
 
 def test_run_acoustic_fixed_bs():
-    run('run', 'acoustic', 4, [20, 20, 20], 5, '-bs', '5', '5', '4')
+    run_cmd('run', 'acoustic', 4, [20, 20, 20], 5, '-bs', '5', '5', '4')
