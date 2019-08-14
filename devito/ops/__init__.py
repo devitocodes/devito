168f93213a28fcd7d9c33904c65656ee9fb48caa
Gabriel Sebastian von Conta <mrgabral@gmail.com>
2019-08-14 16:01:04 -0300

    Removed duplicated line on __init__.py where I was importing compiler twice

--- devito/ops/__init__.py
+++ devito/ops/__init__.py
@@ -6,12 +6,6 @@ JIT-compile, and run kernels on multiple architectures.
 from devito.archinfo import Cpu64
 from devito.dle import PlatformRewriter, modes
 from devito.parameters import Parameters, add_sub_configuration
-from devito.ops.compiler import CompilerOPS
-
-ops_configuration = Parameters('ops')
-ops_configuration.add('compiler', CompilerOPS())
-env_vars_mapper = {}
-add_sub_configuration(ops_configuration, env_vars_mapper)
 
 # Add OPS-specific DLE modes
 modes.add(Cpu64, {'noop': PlatformRewriter,
@@ -20,6 +14,12 @@ modes.add(Cpu64, {'noop': PlatformRewriter,
 
 # The following used by backends.backendSelector
 from devito.ops.compiler import CompilerOPS as Compiler # noqa
+
+ops_configuration = Parameters('ops')
+ops_configuration.add('compiler', Compiler())
+env_vars_mapper = {}
+add_sub_configuration(ops_configuration, env_vars_mapper)
+
 from devito.ops.operator import OperatorOPS as Operator  # noqa
 from devito.types.constant import *  # noqa
 from devito.types.dense import *  # noqa
