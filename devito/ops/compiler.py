3f9d00b067d907d4219fc0ee7debf3648f5a41fb
Gabriel Sebastian von Conta <mrgabral@gmail.com>
2019-08-14 18:57:48 -0300

    Simplifying the compiler, removing jit_compilation
    Making the compiler only check for the environment variable and create the
    .h and .c files. Abandonned the call for compilation, which was too soon.
    Further commits will be focusing on calling the ops translator.

--- devito/ops/compiler.py
+++ devito/ops/compiler.py
@@ -1,5 +1,6 @@
+import os
+
 from devito.parameters import configuration
-from devito.logger import warning
 
 
 __all__ = ['CompilerOPS']
@@ -7,12 +8,16 @@ __all__ = ['CompilerOPS']
 
 class CompilerOPS(configuration['compiler'].__class__):
     def __init__(self, *args, **kwargs):
+        self._ops_install_path = os.environ.get('OPS_INSTALL_PATH')
+        if not self._ops_install_path:
+            raise ValueError("Couldn't find OPS_INSTALL_PATH \
+                environment variable, please check your OPS installation")
         super(CompilerOPS, self).__init__(*args, **kwargs)
 
-    def jit_compile(self, soname, code, hcode):
-        target = str(self.get_jit_dir().joinpath(soname))
-        h_file = "%s.h" % (target)
-        kernel_file = open(h_file, "w")
-        kernel_file.write(hcode)
-        super(CompilerOPS, self).jit_compile(soname, code)
-        
+    def create_files(self, soname, ccode, hcode):
+        file_name = str(self.get_jit_dir().joinpath(soname))
+        h_file = open("%s.h" % (file_name), "w")
+        c_file = open("%s.c" % (file_name), "w")
+
+        h_file.write(hcode)
+        c_file.write(ccode)
