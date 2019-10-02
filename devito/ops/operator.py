2852beb1a6899599e0289c54bdfb4ae4f86e0ca4
lucas cavalcante <thesupervisar@gmail.com>
2019-10-02 11:18:24 -0300

    ops/operator: polishing

--- devito/ops/operator.py
+++ devito/ops/operator.py
@@ -30,10 +30,12 @@ class OperatorOPS(Operator):
     def _specialize_iet(self, iet, **kwargs):
         warning("The OPS backend is still work-in-progress")
 
-        # If there is no iteration tree, then there is no loop to be optimized using OPS.
+        # If there is no iteration trees nor affine trees, 
+        # then there is no loop to be optimized using OPS.
         iteration_tree = retrieve_iteration_tree(iet, mode='normal')
-        if not len(iteration_tree):
+        if not len(iteration_tree) or not find_affine_trees(iet):
             return iet
+
         time_upper_bound = iteration_tree[0].dimensions[TimeFunction._time_position]\
             .extreme_max
 
@@ -42,71 +44,65 @@ class OperatorOPS(Operator):
         ops_exit = Call(namespace['ops_exit'])
 
         # Extract all symbols that need to be converted to ops_dat
-        if not(find_affine_trees(iet)):
-            return iet
-        else:
-            dims = []
-            to_dat = set()
-            for section, trees in find_affine_trees(iet).items():
-                dims.append(len(trees[0].dimensions))
-                symbols = set(FindSymbols('symbolics').visit(trees[0].root))
-                symbols -= set(FindSymbols('defines').visit(trees[0].root))
-                to_dat |= symbols
+        dims = []
+        to_dat = set()
+        for section, trees in find_affine_trees(iet).items():
+            dims.append(len(trees[0].dimensions))
+            symbols = set(FindSymbols('symbolics').visit(trees[0].root))
+            symbols -= set(FindSymbols('defines').visit(trees[0].root))
+            to_dat |= symbols
 
-            from IPython import embed
-            embed()
+        # Create the OPS block for this problem
+        ops_block = OpsBlock('block')
+        ops_block_init = Expression(ClusterizedEq(Eq(
+            ops_block,
+            namespace['ops_decl_block'](
+                dims[0],
+                Literal('"block"')
+            )
+        )))
 
-            # Create the OPS block for this problem
-            ops_block = OpsBlock('block')
-            ops_block_init = Expression(ClusterizedEq(Eq(
-                ops_block,
-                namespace['ops_decl_block'](
-                    dims[0],
-                    Literal('"block"')
-                )
-            )))
+        # To ensure deterministic code generation we order the datasets to
+        # be generated (since a set is an unordered collection)
+        to_dat = filter_sorted(to_dat)
 
-            # To ensure deterministic code generation we order the datasets to
-            # be generated (since a set is an unordered collection)
-            to_dat = filter_sorted(to_dat)
+        name_to_ops_dat = {}
+        pre_time_loop = []
+        after_time_loop = []
+        for f in to_dat:
+            if f.is_Constant:
+                continue
 
-            name_to_ops_dat = {}
-            pre_time_loop = []
-            after_time_loop = []
-            for f in to_dat:
-                if f.is_Constant:
-                    continue
+            pre_time_loop.extend(create_ops_dat(f, name_to_ops_dat, ops_block))
+            # To return the result to Devito, it is necessary to copy the data
+            # from the dat object back to the CPU memory.
+            after_time_loop.extend(create_ops_fetch(f, name_to_ops_dat, time_upper_bound))
 
-                pre_time_loop.extend(create_ops_dat(f, name_to_ops_dat, ops_block))
-                # To return the result to Devito, it is necessary to copy the data
-                # from the dat object back to the CPU memory.
-                after_time_loop.extend(create_ops_fetch(f, name_to_ops_dat, time_upper_bound))
+        # Generate ops kernels for each offloadable iteration tree
+        mapper = {}
+        for n, (section, trees) in enumerate(find_affine_trees(iet).items()):
+            pre_loop, ops_kernel, ops_par_loop_call = opsit(
+                trees, n, name_to_ops_dat, ops_block, dims[0]
+            )
 
-            # Generate ops kernels for each offloadable iteration tree
-            mapper = {}
-            for n, (section, trees) in enumerate(find_affine_trees(iet).items()):
-                pre_loop, ops_kernel, ops_par_loop_call = opsit(
-                    trees, n, name_to_ops_dat, ops_block, dims[0]
-                )
+            pre_time_loop.extend(pre_loop)
+            self._ops_kernels.append(ops_kernel)
+            mapper[trees[0].root] = ops_par_loop_call
+            mapper.update({i.root: mapper.get(i.root) for i in trees})  # Drop trees
 
-                pre_time_loop.extend(pre_loop)
-                self._ops_kernels.append(ops_kernel)
-                mapper[trees[0].root] = ops_par_loop_call
-                mapper.update({i.root: mapper.get(i.root) for i in trees})  # Drop trees
+        iet = Transformer(mapper).visit(iet)
 
-            iet = Transformer(mapper).visit(iet)
+        assert (d == dims[0] for d in dims), \
+            "The OPS backend currently assumes that all kernels \
+            have the same number of dimensions"
 
-            assert (d == dims[0] for d in dims), \
-                "The OPS backend currently assumes that all kernels \
-                have the same number of dimensions"
+        self._headers.append(namespace['ops_define_dimension'](dims[0]))
+        self._includes.append('stdio.h')
 
-            self._headers.append(namespace['ops_define_dimension'](dims[0]))
-            self._includes.append('stdio.h')
+        body = [ops_init, ops_block_init, *pre_time_loop,
+                ops_partition, iet, *after_time_loop, ops_exit]
 
-            body = [ops_init, ops_block_init, *pre_time_loop,
-                    ops_partition, iet, *after_time_loop, ops_exit]
-
-            return List(body=body)
+        return List(body=body)
 
     @property
     def hcode(self):
