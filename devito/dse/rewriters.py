19e48b7488df12f04479348b8ffec0d128190d24
George Bisbas-hp-eb <g.bisbas18@imperial.ac.uk>
2019-10-10 15:27:53 +0100

    WIP-saveup- Not for review

--- devito/dse/rewriters.py
+++ devito/dse/rewriters.py
@@ -379,13 +379,28 @@ class AggressiveRewriter(AdvancedRewriter):
     def _skewing(self, cluster, template, **kwargs):
         """
         Skew the accesses along the time Dimension.
+        Example:
+        Transform
+        for i = 2, n-1
+            for j = 2, m-1
+                a[i,j] = (a[a-1,j] + a[i,j-1] + a[i+1,j] + a[i,j+1]) / 4
+            end for
+        end for
+        
+        to
+
+        for i = 2, n-1
+            for j = 2+i, m-1+i
+                a[i,j-i] = (a[a-1,j-i] + a[i,j-1-i] + a[i+1,j-i] + a[i,j+1-i]) / 4
+            end for
+        end for
         """
 
         skew_factor = self.DEFAULT_SKEW_FACTOR
 
         _, sub_iterators, directions = cluster.ispace.args
 
-        skew_dim, mapper, intervals = None, {}, []
+        #skew_dim, mapper, intervals = None, {}, []
 
         skew_dims = {i.dim for i in cluster.ispace.intervals if i.dim.is_Time}
         try:
@@ -396,24 +411,26 @@ class AggressiveRewriter(AdvancedRewriter):
         if len(skew_dims) > 0:
             raise ValueError("More than 1 time dimensions. Aborting tt...")
 
+        mapper, intervals = {}, []
+
         # Initializing a default time_dim index position in loop
         index = 0
-        if skew_dim is not None:
-            for i in cluster.ispace.intervals:
-                if i.dim.is_Time:
-                    intervals.append(Interval(i.dim, 0, 0))
-                    skew_dim = i.dim
-                    index = intervals.index(Interval(i.dim, 0, 0))
-                elif index < cluster.ispace.intervals.index(i):
-                    mapper[i.dim] = i.dim + skew_factor*skew_dim
-                    intervals.append(Interval(i.dim, -skew_factor*skew_dim,
-                                              -skew_factor*skew_dim))
-                else:
-                    intervals.append(i)
+        #if skew_dim is not None:
+        for i in cluster.ispace.intervals:
+            if i.dim.is_Time:
+                intervals.append(Interval(i.dim, 0, 0))
+                skew_dim = i.dim
+                index = intervals.index(Interval(i.dim, 0, 0))
+            elif index < cluster.ispace.intervals.index(i):
+                mapper[i.dim] = i.dim + skew_factor*skew_dim
+                intervals.append(Interval(i.dim, -skew_factor*skew_dim,
+                                           -skew_factor*skew_dim))
+            else:
+                intervals.append(i)
 
-            processed = xreplace_indices(cluster.exprs, mapper)
-            ispace = IterationSpace(intervals, sub_iterators, directions)
-            cluster = Cluster(processed, ispace, cluster.dspace, guards=cluster.guards)
+        processed = xreplace_indices(cluster.exprs, mapper)
+        ispace = IterationSpace(intervals, sub_iterators, directions)
+        cluster = Cluster(processed, ispace, cluster.dspace, guards=cluster.guards)
 
         return cluster
 
