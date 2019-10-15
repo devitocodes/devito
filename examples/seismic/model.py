c75018e48409934062edd6b3ab0ea713ebed0dc9
mloubout <mathias.louboutin@gmail.com>
2019-10-15 09:54:55 -0400

    tti: staggered is broken with dse:advanced but fine with dse=aggressive

--- examples/seismic/model.py
+++ examples/seismic/model.py
@@ -663,7 +663,7 @@ class Model(GenericModel):
 
         self.delta = self._gen_phys_param(delta, 'delta', space_order)
         self.theta = self._gen_phys_param(theta, 'theta', space_order)
-        self.phi = self._gen_phys_param(phi, 'delta', space_order)
+        self.phi = self._gen_phys_param(phi, 'phi', space_order)
 
     @property
     def critical_dt(self):
