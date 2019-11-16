5b7e6fe14a7d723075c926853f8ae798d3f9af8b
Fabio Luporini <f.luporini12@imperial.ac.uk>
2019-11-16 15:22:55 -0700

    dle: Teach OmpizerGPU how to generate mapto/mapfrom

--- devito/dle/parallelizer.py
+++ devito/dle/parallelizer.py
@@ -5,12 +5,14 @@ import numpy as np
 import cgen as c
 from sympy import Function, Or, Max, Not
 
+from devito.data import FULL
 from devito.ir import (DummyEq, Conditional, Block, Expression, List, Prodder,
-                       While, FindSymbols, FindNodes, Return, COLLAPSED, Transformer,
-                       IsPerfectIteration, retrieve_iteration_tree, filter_iterations)
+                       While, FindSymbols, FindNodes, MapNodes, Return, COLLAPSED,
+                       Transformer, IsPerfectIteration, retrieve_iteration_tree,
+                       filter_iterations)
 from devito.symbolics import CondEq, INT
 from devito.parameters import configuration
-from devito.tools import as_tuple, is_integer, prod
+from devito.tools import as_tuple, is_integer, flatten, prod
 from devito.types import Constant, Symbol
 
 
@@ -337,6 +339,10 @@ class Ompizer(object):
 
         return partree
 
+    def _make_data_transfers(self, iet):
+        # No data transfers needed
+        return iet
+
     def make_parallel(self, iet):
         """Transform ``iet`` by introducing shared-memory parallelism."""
         mapper = OrderedDict()
@@ -368,6 +374,9 @@ class Ompizer(object):
 
         iet = Transformer(mapper).visit(iet)
 
+        # Add the data transfers
+        iet = self._make_data_transfers(iet)
+
         # The used `nthreads` arguments
         args = [i for i in FindSymbols().visit(iet) if isinstance(i, (NThreadsMixin))]
 
@@ -381,10 +390,24 @@ class OmpizerGPU(Ompizer):
     Always collapse when possible.
     """
 
+    def map_to(f):
+        omp_pragma = 'omp target enter data map(to: %s%s)'
+        var_name = f.name
+        var_data = ''.join('[0:%s]' % f._C_get_field(FULL, d).size for d in f.dimensions)
+        return c.Pragma(omp_pragma % (var_name, var_data))
+
+    def map_from(f):
+        omp_pragma = 'omp target exit data map(from: %s%s)'
+        var_name = f.name
+        var_data = ''.join('[0:%s]' % f._C_get_field(FULL, d).size for d in f.dimensions)
+        return c.Pragma(omp_pragma % (var_name, var_data))
+
     lang = dict(Ompizer.lang)
     lang.update({
         'par-for-teams': lambda i:
             c.Pragma('omp target teams distribute parallel for collapse(%d)' % i),
+        'map-to': map_to,
+        'map-from': map_from
     })
 
     def __init__(self, key=None):
@@ -431,3 +454,44 @@ class OmpizerGPU(Ompizer):
     def _make_nested_partree(self, partree):
         # no-op for now
         return partree
+
+    def _make_data_transfers(self, iet):
+        data_transfers = {}
+        for iteration, partree in MapNodes(child_types=ParallelTree).visit(iet).items():
+            # The data that need to be transfered between host and device
+            exprs = FindNodes(Expression).visit(partree)
+            reads = set().union(*[i.reads for i in exprs])
+            writes = set([i.write for i in exprs])
+            data = {i for i in reads | writes if i.is_Tensor}
+
+            # At what depth in the IET should the data transfer be performed?
+            candidate = iteration
+            for tree in retrieve_iteration_tree(iet):
+                if iteration not in tree:
+                    continue
+                for i in reversed(tree[:tree.index(iteration)+1]):
+                    found = False
+                    for k, v in MapNodes('any', Expression, 'groupby').visit(i).items():
+                        test0 = any(isinstance(n, ParallelTree) for n in k)
+                        test1 = set().union(*[e.functions for e in v]) & data
+                        if not test0 and test1:
+                            found = True
+                            break
+                    if found:
+                        break
+                    candidate = i
+                break
+
+            # Create the omp pragmas for the data transfer
+            map_tos = [self.lang['map-to'](i) for i in data]
+            map_froms = [self.lang['map-from'](i) for i in writes if i.is_Tensor]
+            data_transfers.setdefault(candidate, []).append((map_tos, map_froms))
+
+        # Now create a new IET with the data transfer
+        mapper = {}
+        for i, v in data_transfers.items():
+            map_tos, map_froms = zip(*v)
+            mapper[i] = List(header=flatten(map_tos), body=i, footer=flatten(map_froms))
+        iet = Transformer(mapper).visit(iet)
+
+        return iet
