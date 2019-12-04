13fedab2502e2ad05f0515d03121ca2b7f3f09a0
Fabio Luporini <f.luporini12@imperial.ac.uk>
2019-12-04 14:37:55 +0100

    towards-targets: Move OmpizerGPU to gpu.py

--- devito/targets/common/__init__.py
+++ devito/targets/common/__init__.py
@@ -1,5 +1,5 @@
 from .engine import *  # noqa
 from .blocking import *  # noqa
-from .openmp import NThreads, NThreadsNested, NThreadsNonaffine, Ompizer, OmpizerGPU  # noqa
+from .openmp import *  # noqa
 from .rewriters import *  # noqa
 from .definitions import *  # noqa
