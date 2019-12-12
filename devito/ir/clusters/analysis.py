a7c49480e1eb530aa4df494709ec1f7edd875e1a
Fabio Luporini <f.luporini12@imperial.ac.uk>
2019-12-12 14:34:26 +0100

    ir: Add machinery to detect Cluster properties

--- devito/ir/clusters/analysis.py
+++ devito/ir/clusters/analysis.py
@@ -1,8 +1,40 @@
+from collections import OrderedDict
+
+from devito.ir.clusters.queue import Queue
 from devito.ir.support import (SEQUENTIAL, PARALLEL, PARALLEL_IF_ATOMIC, VECTOR,
                                TILABLE, WRAPPABLE)
+from devito.tools import timed_pass
 
 __all__ = ['analyze']
 
 
+class State(object):
+
+    def __init__(self):
+        self.properties = OrderedDict()
+        self.scopes = OrderedDict()
+
+
+class Detector(Queue):
+
+    def __init__(self, state):
+        super(Detector, self).__init__()
+        self.state = state
+
+    def callback(self, clusters, prefix):
+        self._callback(clusters, prefix)
+        return clusters
+
+
+class Parallelism(Detector):
+
+    def _callback(self, clusters, prefix):
+        properties = OrderedDict()
+
+
 def analyze(clusters):
+    state = State()
+
+    clusters = Parallelism(state).process(clusters)
+
     return clusters
