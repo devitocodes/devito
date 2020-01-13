e17e07fb082f3573ff30b76c3a44ea71df277f85
mloubout <mathias.louboutin@gmail.com>
2020-01-13 09:47:11 -0500

    ci: split notebooks test

--- .github/workflows/test-notebooks.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Using compiler: $1"
-for notebook in examples/cfd examples/seismic/tutorials examples/compiler examples/userapi
-do
-    DEVITO_ARCH=$1 pytest --nbval $notebook
-done
