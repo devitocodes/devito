319252dad18b639c474276a5a05e86fac5ed1814
George Bisbas <g.bisbas18@imperial.ac.uk>
2020-01-15 11:33:37 +0000

    Test DevitoBench
    Update travis
    
    Update travis
    
    Docker test
    
    Docker test
    
    Add plotting
    
    travis:Fix benchmark.py path
    travis: opescibench to req-opt
    
    tests:Add explicit calls
    
    tests: testing around
    
    flake8
    
    Add tests
    
    Move from sh to subprocess
    
    tests: cleanup
    
    tests:small fix

---
+++ tests/test_benchmark.py
@@ -0,0 +1,20 @@
+import pytest
+from subprocess import CalledProcessError, check_call
+from devito.exceptions import CompilationError
+
+
+@pytest.mark.parametrize('mode', ['test', 'run', 'bench'])
+@pytest.mark.parametrize('problem', ['acoustic', 'tti', 'elastic', 'viscoelastic'])
+def test_bench(mode, problem):
+    """
+    Test combinations of modes and problems.
+    """
+    # expected version
+    command = ['python', 'benchmarks/user/benchmark.py', mode, '-P', problem,
+               '-d', '50', '50', '50', '--tn', '50']
+    try:
+        check_call(command)
+    except CalledProcessError as e:
+        raise CompilationError('Command "%s" return error status %d. '
+                               'Unable to compile code.\n' %
+                               (e.cmd, e.returncode))
