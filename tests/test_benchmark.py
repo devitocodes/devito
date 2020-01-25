5b0b91c02d7fc2257738d48f1b8d8b9c28a0d1db
George Bisbas <g.bisbas18@gmail.com>
2020-01-25 19:47:18 +0000

    Empty
    flake8
    
    tests-fix: 1st pass on comments
    
    2nd pass of fixes
    
    flake 8

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,19 +1,60 @@
 import pytest
-from subprocess import CalledProcessError, check_call
-from devito.exceptions import CompilationError
+from os import path
+from conftest import skipif
+from devito import configuration
+from subprocess import check_call
 
 
-@pytest.mark.parametrize('mode', ['test', 'run', 'bench'])
+pytestmark = skipif(['yask', 'ops'])
+
+
+@pytest.mark.parametrize('mode', ['bench'])
 @pytest.mark.parametrize('problem', ['acoustic', 'tti', 'elastic', 'viscoelastic'])
 def test_bench(mode, problem):
     """
-    Test combinations of modes and problems.
+    Test the Devito benchmark framework on various combinations of modes and problems.
     """
-    command = ['python', 'benchmarks/user/benchmark.py', mode, '-P', problem,
-               '-d', '50', '50', '50', '--tn', '50']
-    try:
-        check_call(command)
-    except CalledProcessError as e:
-        raise CompilationError('Command "%s" return error status %d. '
-                               'Unable to compile code.\n' %
-                               (e.cmd, e.returncode))
+    if configuration['openmp']:
+        nthreads = configuration['platform'].cores_physical
+    else:
+        nthreads = 1
+
+    command_bench = ['python', 'benchmarks/user/benchmark.py', mode, '-P', problem,
+                     '-d', '16', '16', '16', '--tn', '4', '-x', '1']
+    check_call(command_bench)
+
+    dir_name = 'results/'
+
+    base_filename = problem
+    filename_suffix = '.json'
+    arch = 'arch[unknown]'
+    shape = 'shape[16,16,16]'
+    nbl = 'nbl[10]'
+    tn = 'tn[4]'
+    so = 'so[2]'
+    to = 'to[2]'
+    dse = 'dse[advanced]'
+    dle = 'dle[advanced]'
+    at = 'at[aggressive]'
+    nt = 'nt[%d]' % nthreads
+    mpi = 'mpi[False]'
+    np = 'np[1]'
+    rank = 'rank[0]'
+    bkend = 'bkend[core]'
+
+    bench_corename = path.join('_'.join([base_filename, arch, shape, nbl, tn,
+                               so, to, dse, dle, at, nt, mpi, np, rank]))
+
+    bench_filename = "%s%s%s" % (dir_name, bench_corename, filename_suffix)
+    assert path.isfile(bench_filename)
+
+    command_plot = ['python', 'benchmarks/user/benchmark.py', 'plot', '-P', problem,
+                    '-d', '16', '16', '16', '--tn', '4', '--max-bw', '12.8',
+                    '--flop-ceil', '80', 'linpack']
+    check_call(command_plot)
+
+    plot_corename = path.join('_'.join([base_filename, shape, so, to, arch,
+                              bkend, at]))
+    plot_filename = "%s%s" % (dir_name, plot_corename)
+
+    assert path.isfile(plot_filename)
