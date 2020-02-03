d4755efb0fc7307da094d6370c65c3b91dc26427
Fabio Luporini <f.luporini12@imperial.ac.uk>
2020-02-03 09:53:02 +0100

    asv: Tweak merge_history.sh

--- benchmarks/regression/merge_history.sh
+++ benchmarks/regression/merge_history.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+touch merge_history.txt
+
 # Current branch tip
-git log --pretty=format:'%h' -n 1 > merge_history.txt
+git log --pretty=format:'%h' -n 1 >> merge_history.txt
 
 echo ""  >> merge_history.txt
 
-# Last ten PRs merged into master
-git log --merges --first-parent master --pretty=format:"%h" -n 10 >> merge_history.txt
+# Last nine PRs merged into master
+git log --merges --first-parent master --pretty=format:"%h" -n 9 >> merge_history.txt
+
+echo ""  >> merge_history.txt
