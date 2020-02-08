be63d0c2d48cb7c7de636200848f02c32a301c2a
Fabio Luporini <f.luporini12@imperial.ac.uk>
2020-02-08 11:13:52 +0100

    passes: Tweak blocking to handle guarded Clusters separately

--- devito/passes/clusters/blocking.py
+++ devito/passes/clusters/blocking.py
@@ -23,6 +23,9 @@ class Blocking(Queue):
 
         super(Blocking, self).__init__()
 
+    def _make_key_hook(self, cluster, level):
+        return (tuple(cluster.guards.get(i.dim) for i in cluster.itintervals[:level]),)
+
     @timed_pass(name='specializing.Clusters.blocking')
     def process(self, clusters):
         # Preprocess: heuristic: drop TILABLE from innermost Dimensions to
