774ca0a6702f9a413de971e1c6a00d175e4f5082
Fabio Luporini <f.luporini12@imperial.ac.uk>
2020-04-03 11:52:39 +0200

    operator: Fixup registration of Custom Operators

--- devito/core/__init__.py
+++ devito/core/__init__.py
@@ -13,38 +13,39 @@ from devito.parameters import Parameters, add_sub_configuration
 core_configuration = Parameters('core')
 add_sub_configuration(core_configuration)
 
-# Add core-specific Operators
+# Register CPU Operators
+operator_registry.add(CustomOperator, Cpu64, 'custom', 'C')
+operator_registry.add(CustomOperator, Cpu64, 'custom', 'openmp')
+
 operator_registry.add(CPU64NoopOperator, Cpu64, 'noop', 'C')
 operator_registry.add(CPU64Operator, Cpu64, 'advanced', 'C')
 operator_registry.add(CPU64OpenMPOperator, Cpu64, 'advanced', 'openmp')
-operator_registry.add(CustomOperator, Cpu64, 'custom', 'C')
 
 operator_registry.add(CPU64NoopOperator, Intel64, 'noop', 'C')
 operator_registry.add(Intel64Operator, Intel64, 'advanced', 'C')
 operator_registry.add(Intel64OpenMPOperator, Intel64, 'advanced', 'openmp')
 operator_registry.add(Intel64FSGOperator, Intel64, 'advanced-fsg', 'C')
 operator_registry.add(Intel64FSGOpenMPOperator, Intel64, 'advanced-fsg', 'openmp')
-operator_registry.add(CustomOperator, Intel64, 'custom')
 
 operator_registry.add(CPU64NoopOperator, Arm, 'noop', 'C')
 operator_registry.add(ArmOperator, Arm, 'advanced', 'C')
 operator_registry.add(ArmOpenMPOperator, Arm, 'advanced', 'openmp')
-operator_registry.add(CustomOperator, Arm, 'custom', 'C')
 
 operator_registry.add(CPU64NoopOperator, Power, 'noop', 'C')
 operator_registry.add(PowerOperator, Power, 'advanced', 'C')
 operator_registry.add(PowerOpenMPOperator, Power, 'advanced', 'openmp')
-operator_registry.add(CustomOperator, Power, 'custom', 'C')
+
+# Register Device Operators
+operator_registry.add(DeviceOpenMPCustomOperator, Device, 'custom', 'C')
+operator_registry.add(DeviceOpenMPCustomOperator, Device, 'custom', 'openmp')
+operator_registry.add(DeviceOpenACCCustomOperator, Device, 'custom', 'openacc')
 
 operator_registry.add(DeviceOpenMPNoopOperator, Device, 'noop', 'C')
 operator_registry.add(DeviceOpenMPOperator, Device, 'advanced', 'C')
-operator_registry.add(DeviceOpenMPCustomOperator, Device, 'custom', 'C')
 operator_registry.add(DeviceOpenMPNoopOperator, Device, 'noop', 'openmp')
 operator_registry.add(DeviceOpenMPOperator, Device, 'advanced', 'openmp')
-operator_registry.add(DeviceOpenMPCustomOperator, Device, 'custom', 'openmp')
 operator_registry.add(DeviceOpenACCNoopOperator, Device, 'noop', 'openacc')
 operator_registry.add(DeviceOpenACCOperator, Device, 'advanced', 'openacc')
-operator_registry.add(DeviceOpenACCCustomOperator, Device, 'custom', 'openacc')
 
 # The following used by backends.backendSelector
 from devito.core.operator import OperatorCore as Operator  # noqa
