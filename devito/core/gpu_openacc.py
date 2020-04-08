7a33b1f1ef371fb689cf100d5e2b24c1cc67c1d2
Fabio Luporini <f.luporini12@imperial.ac.uk>
2020-04-08 10:13:59 +0200

    accizer: Cast output value of acc_malloc

--- devito/core/gpu_openacc.py
+++ devito/core/gpu_openacc.py
@@ -91,10 +91,11 @@ class DeviceOpenACCDataManager(DeviceOpenMPDataManager):
         if obj in storage._high_bw_mem:
             return
 
-        decl = "(*%s)%s" % (obj.name, "".join("[%s]" % i for i in obj.symbolic_shape[1:]))
-        decl = c.Value(obj._C_typedata, decl)
-        size = prod(obj.symbolic_shape)
-        alloc = "acc_malloc(sizeof(%s[%s]))" % (obj._C_typedata, size)
+        size_trunkated = "".join("[%s]" % i for i in obj.symbolic_shape[1:])
+        decl = c.Value(obj._C_typedata, "(*%s)%s" % (obj.name, size_trunkated))
+        cast = "(%s (*)%s)" % (obj._C_typedata, size_trunkated)
+        size_full = prod(obj.symbolic_shape)
+        alloc = "%s acc_malloc(sizeof(%s[%s]))" % (cast, obj._C_typedata, size_full)
         init = c.Initializer(decl, alloc)
 
         free = c.Statement('acc_free(%s)' % obj.name)
