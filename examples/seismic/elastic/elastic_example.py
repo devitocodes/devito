920e5ce42519d2d707d681b7c04bf4bd446499bc
Rhodri Nelson <rhodri.nelson@gmail.com>
2020-04-09 11:36:01 +0100

    .

--- examples/seismic/elastic/elastic_example.py
+++ examples/seismic/elastic/elastic_example.py
@@ -38,7 +38,7 @@ def run(shape=(50, 50), spacing=(20.0, 20.0), tn=1000.0,
 def test_elastic():
     _, _, _, [rec1, rec2, v, tau] = run()
     norm = lambda x: np.linalg.norm(x.data.reshape(-1))
-    assert np.isclose(norm(rec1), 24.7273, atol=1e-3, rtol=0)
+    assert np.isclose(norm(rec1), 23.7273, atol=1e-3, rtol=0)
     assert np.isclose(norm(rec2), 0.99306, atol=1e-3, rtol=0)
 
 
