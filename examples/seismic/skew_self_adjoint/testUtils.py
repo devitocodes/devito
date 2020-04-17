17aae749ff8462acb31866f075fd2b49c9ebc7b5
wask <wask@chevron.com>
2020-04-17 16:18:02 -0500

    nb: ssa isotropic implementation 1 is in final form. added utils.py and testUtils.py.

--- examples/seismic/skew_self_adjoint/testUtils.py
+++ examples/seismic/skew_self_adjoint/testUtils.py
@@ -1,25 +1,68 @@
 import numpy as np
 import pytest
-from devito.logger import log
-
-from devito.logger import info
+from devito import Grid, Function, configuration
 from examples.seismic.skew_self_adjoint import *
+configuration['language'] = 'openmp'
+configuration['log-level'] = 'ERROR'
+
 
 class TestUtils(object):
 
-    @pytest.mark.parametrize('shape', [(200, 200), (200, 200, 200)])
-    @pytest.mark.parametrize('npad', [50, ])
+    @pytest.mark.parametrize('shape', [(40, 50), (40, 50, 60)])
+    @pytest.mark.parametrize('npad', [10, ])
+    @pytest.mark.parametrize('w', [2.0 * np.pi * 0.010, ])
     @pytest.mark.parametrize('qmin', [0.1, 1.0])
     @pytest.mark.parametrize('qmax', [10.0, 100.0])
     @pytest.mark.parametrize('sigma', [None, 11])
-    def test_setupWOverQ(self, shape, npad, qmin, qmax, sigma):
+    @pytest.mark.parametrize('dtype', [np.float32, ])
+    def test_setupWOverQ(self, shape, npad, w, qmin, qmax, sigma, dtype):
         """
         Test for the function that sets up the w/Q attenuation model.
-        This is not a correctness test, we just ensure that the output w/Q model 
-        had value w/Qmax in the interior, w/Qmin at the edge of the model, and 
-        monotonically intermediate value in between.
+        This is not a correctness test, we just ensure that the output model:
+            - value is bounded [w/Qmin, w/Qmax]
+            - value is w/Qmin in corners
+            - value is w/Qmax in center
         """
-        print("test_setupWOverQ(shape, npad, qmin, qmax, sigma)", 
-             shape, npad, qmin, qmax, sigma)
 
-#         assert np.isclose((term1 - term2)/term1, 0., atol=1.e-12)
+        tol = np.finfo(dtype).eps
+
+        # question: do we need to test for float32, float64?
+        if len(shape) == 2:
+            origin = (0.0, 0.0)
+            extent = (1.0, 1.0)
+            grid = Grid(extent=extent, shape=shape, origin=origin, dtype=dtype)
+            wOverQ = Function(name='wOverQ', grid=grid, space_order=8)
+            setup_wOverQ(wOverQ, w, qmin, qmax, npad, sigma=None)
+            q = (1 / (wOverQ.data / w))
+            assert np.isclose(np.min(q[:]), qmin, 10 * tol)
+            assert np.isclose(np.max(q[:]), qmax, 10 * tol)
+
+            nx, nz = q.data.shape
+            assert np.isclose(q.data[0, 0], qmin, atol=tol)
+            assert np.isclose(q.data[0, nz-1], qmin, atol=tol)
+            assert np.isclose(q.data[nx-1, 0], qmin, atol=tol)
+            assert np.isclose(q.data[nx-1, nz-1], qmin, atol=tol)
+
+            assert np.isclose(q.data[nx//2, nz//2], qmax, atol=tol)
+
+        else:
+            origin = (0.0, 0.0, 0.0)
+            extent = (1.0, 1.0, 1.0)
+            grid = Grid(extent=extent, shape=shape, origin=origin, dtype=np.float32)
+            wOverQ = Function(name='wOverQ', grid=grid, space_order=8)
+            setup_wOverQ(wOverQ, w, qmin, qmax, npad, sigma=None)
+            q = (1 / (wOverQ.data / w))
+            assert np.isclose(np.min(q), qmin, 10 * tol)
+            assert np.isclose(np.max(q), qmax, 10 * tol)
+
+            nx, ny, nz = q.data.shape
+            assert np.isclose(q.data[0, 0, 0], qmin, atol=tol)
+            assert np.isclose(q.data[0, 0, nz-1], qmin, atol=tol)
+            assert np.isclose(q.data[0, ny-1, 0], qmin, atol=tol)
+            assert np.isclose(q.data[0, ny-1, nz-1], qmin, atol=tol)
+            assert np.isclose(q.data[nx-1, 0, 0], qmin, atol=tol)
+            assert np.isclose(q.data[nx-1, 0, nz-1], qmin, atol=tol)
+            assert np.isclose(q.data[nx-1, ny-1, 0], qmin, atol=tol)
+            assert np.isclose(q.data[nx-1, ny-1, nz-1], qmin, atol=tol)
+
+            assert np.isclose(q.data[nx//2, ny//2, nz//2], qmax, atol=tol)
--- examples/seismic/skew_self_adjoint/utils.py
+++ examples/seismic/skew_self_adjoint/utils.py
@@ -6,7 +6,9 @@ from devito.builtins import gaussian_smooth
 def setup_wOverQ(wOverQ, w, qmin, qmax, npad, sigma=None):
     """
     Initialise spatially variable w/Q field used to implement attenuation and
-    absorb outgoing waves at the edges of the model.
+    absorb outgoing waves at the edges of the model. Uses an outer product
+    via numpy.ogrid[:n1, :n2] to speed up loop traversal for 2d and 3d.
+    TODO: stop wasting so much memory with 9 tmp arrays ...
 
     Parameters
     ----------
@@ -28,36 +30,49 @@ def setup_wOverQ(wOverQ, w, qmin, qmax, npad, sigma=None):
     sigma : float32
         sigma value for call to scipy gaussian smoother, default 5.
     """
+    # sanity checks
     assert w > 0, "supplied w value [%f] must be positive" % (w)
     assert qmin > 0, "supplied qmin value [%f] must be positive" % (qmin)
     assert qmax > 0, "supplied qmax value [%f] must be positive" % (qmax)
     assert npad > 0, "supplied npad value [%f] must be positive" % (npad)
-
-    sigma = sigma or npad//11
+    for n in wOverQ.data.shape:
+        if n - 2*npad < 1:
+            raise ValueError("2 * npad must not exceed dimension size!")
 
     t1 = timer()
+    sigma = sigma or npad//11
     lqmin = np.log(qmin)
     lqmax = np.log(qmax)
 
-    if len(wOverQ.shape) == 3:
-        nx, nz = wOverQ.shape()
+    if len(wOverQ.data.shape) == 2:
+        # 2d operations
+        nx, nz = wOverQ.data.shape
         kxMin, kzMin = np.ogrid[:nx, :nz]
         kxArr, kzArr = np.minimum(kxMin, nx-1-kxMin), np.minimum(kzMin, nz-1-kzMin)
         nval1 = np.minimum(kxArr, kzArr)
         nval2 = np.minimum(1, nval1/(npad))
         nval3 = np.exp(lqmin+nval2*(lqmax-lqmin))
         wOverQ.data[:, :] = w / nval3
+
     else:
-        nx, ny, nz = wOverQ.shape()
+        # 3d operations
+        nx, ny, nz = wOverQ.data.shape
         kxMin, kyMin, kzMin = np.ogrid[:nx, :ny, :nz]
-        kxArr, kzArr = np.minimum(kxMin, nx-1-kxMin), np.minimum(kzMin, nz-1-kzMin)
-        nval1 = np.minimum(kxArr, kzArr)
+        kxArr = np.minimum(kxMin, nx-1-kxMin)
+        kyArr = np.minimum(kyMin, ny-1-kyMin)
+        kzArr = np.minimum(kzMin, nz-1-kzMin)
+        nval1 = np.minimum(kxArr, np.minimum(kyArr, kzArr))
         nval2 = np.minimum(1, nval1/(npad))
         nval3 = np.exp(lqmin+nval2*(lqmax-lqmin))
-        wOverQ.data[:, :] = w / nval3
+        wOverQ.data[:, :, :] = w / nval3
 
+    # Note if we apply the gaussian smoother, renormalize output to [qmin,qmax]
     if sigma > 0:
-        wOverQ.data[:] = gaussian_smooth(wOverQ.data, sigma=sigma)
+        nval2[:] = gaussian_smooth(nval3, sigma=sigma)
+        nmin2, nmax2 = np.min(nval2), np.max(nval2)
+        nval3[:] = qmin + (qmax - qmin) * (nval2 - nmin2) / (nmax2 - nmin2)
+
+    wOverQ.data[:] = w / nval3
 
     # report min/max output Q value
     q1 = (np.min(1 / (wOverQ.data / w)))
