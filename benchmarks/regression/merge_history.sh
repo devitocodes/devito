366ef51f15e11a0c52d14d49e7ed8e263eef148b
Fabio Luporini <f.luporini12@imperial.ac.uk>
2020-04-23 11:49:53 +0200

    asv: Generate expanded hashes in merge_history.txt

--- benchmarks/regression/merge_history.sh
+++ benchmarks/regression/merge_history.sh
@@ -3,11 +3,11 @@
 touch merge_history.txt
 
 # Current branch tip
-git log --pretty=format:'%h' -n 1 >> merge_history.txt
+git log --pretty=format:'%H' -n 1 >> merge_history.txt
 
 echo ""  >> merge_history.txt
 
 # Last nine PRs merged into master
-git log --merges --first-parent master --pretty=format:"%h" -n 9 >> merge_history.txt
+git log --merges --first-parent origin/master --pretty=format:"%H" -n 9 >> merge_history.txt
 
 echo ""  >> merge_history.txt
