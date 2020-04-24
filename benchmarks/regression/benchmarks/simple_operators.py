80ebb2c8f7a1edb10899701469a3067b0b504268
Fabio Luporini <f.luporini12@imperial.ac.uk>
2020-04-24 12:04:43 +0200

    asv: Add time_laplacian() test

--- benchmarks/regression/benchmarks/simple_operators.py
+++ benchmarks/regression/benchmarks/simple_operators.py
@@ -5,10 +5,20 @@ repeat = 3
 
 
 def time_basic():
-    grid = Grid(shape=(16, 16, 16))
+    grid = Grid(shape=(400, 400, 400))
 
     f = TimeFunction(name='f', grid=grid)
 
     op = Operator(Eq(f.forward, f + 1))
 
-    op.apply(time_M=10)
+    op.apply(time_M=100)
+
+
+def time_laplacian():
+    grid = Grid(shape=(400, 400, 400))
+
+    f = TimeFunction(name='f', grid=grid, space_order=2)
+
+    op = Operator(Eq(f.forward, 1e-8*(f.laplace + 1)))
+
+    op.apply(time_M=100)
