6fea45f3f8187e905d70d75e0600abf94737efc1
Rhodri Nelson <rhodri.nelson@gmail.com>
2020-04-29 16:35:38 +0100

    workflows: asv updates.

---
+++ .github/azure/startVM.ps1
@@ -0,0 +1,47 @@
+# Start a (stopped) VM
+
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $True)]
+    [string]
+    $servicePrincipal,
+
+    [Parameter(Mandatory = $True)]
+    [string]
+    $servicePrincipalSecret,
+
+    [Parameter(Mandatory = $True)]
+    [string]
+    $servicePrincipalTenantId,
+
+    [Parameter(Mandatory = $True)]
+    [string]
+    $azureSubscriptionName,
+
+    [Parameter(Mandatory = $True)]
+    [string]
+    $resourceGroupName,
+
+    [Parameter(Mandatory = $True)]
+    [string]
+    $vmName
+)
+
+az login `
+    --service-principal `
+    --username $servicePrincipal `
+    --password $servicePrincipalSecret `
+    --tenant $servicePrincipalTenantId
+
+az account set --subscription $azureSubscriptionName
+
+Write-Output "Booting VM"
+try {
+    az vm start -g $resourceGroupName -n $vmName --verbose
+    Write-Output "VM running"
+    }
+catch {
+    Write-Output "VM already running"
+    }
+az vm list -d -o table --query "[?name=='$vmName']"
+Write-Output ""
