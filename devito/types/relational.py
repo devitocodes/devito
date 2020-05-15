ff7d1bd222d94455af2d5a7eacf476f9c3599181
George Bisbas <g.bisbas18@imperial.ac.uk>
2020-05-15 15:15:16 +0300

    relational: Add Ne "Not-Equal" class

--- devito/types/relational.py
+++ devito/types/relational.py
@@ -2,7 +2,7 @@
 
 import sympy
 
-__all__ = ['Le', 'Lt', 'Ge', 'Gt']
+__all__ = ['Le', 'Lt', 'Ge', 'Gt', 'Ne']
 
 
 class Le(sympy.Le):
@@ -34,8 +34,7 @@ class Le(sympy.Le):
     >>> grid = Grid(shape=(8, 8))
     >>> g = Function(name='g', shape=grid.shape, dimensions=grid.dimensions)
     >>> x, y = grid.dimensions
-    >>> cond = Le(g, 1)
-    >>> cond.canonical
+    >>> Le(g, 1)
     g(x, y) <= 1
     """
 
@@ -82,8 +81,7 @@ class Lt(sympy.Lt):
     >>> grid = Grid(shape=(8, 8))
     >>> g = Function(name='g', shape=grid.shape, dimensions=grid.dimensions)
     >>> x, y = grid.dimensions
-    >>> cond = Lt(g, 1)
-    >>> cond.canonical
+    >>> Lt(g, 1)
     g(x, y) < 1
     """
 
@@ -130,8 +128,7 @@ class Ge(sympy.Ge):
     >>> grid = Grid(shape=(8, 8))
     >>> g = Function(name='g', shape=grid.shape, dimensions=grid.dimensions)
     >>> x, y = grid.dimensions
-    >>> cond = Ge(g, 1)
-    >>> cond.canonical
+    >>> Ge(g, 1)
     g(x, y) >= 1
     """
 
@@ -179,8 +176,7 @@ class Gt(sympy.Gt):
     >>> grid = Grid(shape=(8, 8))
     >>> g = Function(name='g', shape=grid.shape, dimensions=grid.dimensions)
     >>> x, y = grid.dimensions
-    >>> cond = Gt(g, 1)
-    >>> cond.canonical
+    >>> Gt(g, 1)
     g(x, y) > 1
     """
 
@@ -196,4 +192,58 @@ class Gt(sympy.Gt):
     @property
     def subdomain(self):
         """The SubDomain in which the Gt is defined."""
+        return self._subdomain
+
+
+class Ne(sympy.Ne):
+    """
+    A not-equal ("!=") relation between two objects (see Notes below), the left-hand side
+    and the right-hand side. Can be used to build conditionals but not directly to
+    construct an Operator.
+
+    The left-hand side may be a Function or a SparseFunction. The right-hand
+    side may be any arbitrary expressions with numbers, Dimensions, Constants,
+    Functions and SparseFunctions as operands.
+
+    Parameters
+    ----------
+    lhs : Function or SparseFunction
+        The left-hand side.
+    rhs : expr-like, optional
+        The right-hand side. Defaults to 0.
+    subdomain : SubDomain, optional
+        To restrict the evalaution of the relation to a particular sub-region in the
+        computational domain.
+
+    Examples
+    --------
+    Gt may be used to express a relation.
+
+    >>> from devito import Grid, Function, ConditionalDimension, Eq, Operator
+    >>> from devito.types import Gt
+    >>> grid = Grid(shape=(8, 8))
+    >>> g = Function(name='g', shape=grid.shape, dimensions=grid.dimensions)
+    >>> x, y = grid.dimensions
+    >>> Ne(g, 0)
+    Ne(g(x, y), 0)
+
+    Notes
+    -----
+    This class is not the same as the != operator. The != operator tests for exact
+    structural equality between two expressions; this class compares expressions
+    mathematically. Source: https://docs.sympy.org/latest/modules/core.html
+    """
+
+    is_Relational = False
+
+    def __new__(cls, lhs, rhs=0, subdomain=None, **kwargs):
+        kwargs.update({'evaluate': False})
+        obj = sympy.Ne.__new__(cls, lhs, rhs, **kwargs)
+        obj._subdomain = subdomain
+        # import pdb; pdb.set_trace()
+        return obj
+
+    @property
+    def subdomain(self):
+        """The SubDomain in which the Ne is defined."""
         return self._subdomain
