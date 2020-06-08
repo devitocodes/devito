da65f20f8e72dcc85c35a178d82aff3762121a17
mloubout <mathias.louboutin@gmail.com>
2020-06-08 08:34:38 -0400

    indices: add test for float to int offsets

---
+++ tests/test_symbolics.py
@@ -0,0 +1,16 @@
+import sympy
+
+from devito import Grid, Function
+
+
+def test_float_indices():
+    """
+    Test that indices only contain Integers.
+    """
+    grid = Grid((10,))
+    x = grid.dimensions[0]
+    x0 = x + 1.0 * x.spacing
+    u = Function(name="u", grid=grid, space_order=2)
+    indices = u.subs({x: x0}).indexify().indices[0]
+    assert len(indices.atoms(sympy.Float)) == 0
+    assert indices == x + 1
