7983e82d90f161a070cd3ef0f8ff4e13f6c5f09d
Fabio Luporini <f.luporini12@imperial.ac.uk>
2020-06-17 10:44:54 +0200

    tools: Polish as_mapper

--- devito/tools/utils.py
+++ devito/tools/utils.py
@@ -57,10 +57,8 @@ def as_mapper(iterable, key=None, get=None):
     Rearrange an iterable into a dictionary of lists in which keys are
     produced by the function ``key``.
     """
-    if key is None:
-        key = lambda i: i
-    if get is None:
-        get = lambda i: i
+    key = key or (lambda i: i)
+    get = get or (lambda i: i)
     mapper = {}
     for i in iterable:
         mapper.setdefault(key(i), []).append(get(i))
