7b53e0fb909333bf2d2f3345c66f6da4b7ad216b
Giacomo La Scala <gl818@ic.ac.uk>
2020-07-09 21:33:18 +0200

    test_gpu_common: Implementation of get_gpu_info tests

---
+++ tests/test_gpu_common.py
@@ -0,0 +1,10 @@
+from devito.archinfo import get_gpu_info
+
+from conftest import skipif
+
+class TestGPUInfo(object):
+    
+    @skipif('nodevice')
+    def test_get_gpu_info(self):
+        info = get_gpu_info()
+        assert 'tesla' in info['architecture'].lower()
