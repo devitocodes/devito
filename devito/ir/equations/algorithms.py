55fa14d6d1a4771ab3b5e7e45d9a7c3fbfb9afde
Fabio Luporini <f.luporini12@imperial.ac.uk>
2020-09-25 15:57:22 +0200

    operator: Fix add_implicit

--- devito/ir/equations/algorithms.py
+++ devito/ir/equations/algorithms.py
@@ -81,6 +81,7 @@ def generate_implicit_exprs(expressions):
 
         * ``SubDomainSet``'s attached to input equations.
     """
+    found = {}
     processed = []
     for e in expressions:
         if e.subdomain:
@@ -90,9 +91,11 @@ def generate_implicit_exprs(expressions):
                 sub_dims.append(e.subdomain.implicit_dimension)
                 dims = [d for d in dims if d not in frozenset(sub_dims)]
                 dims.append(e.subdomain.implicit_dimension)
-                grid = list(retrieve_functions(e, mode='unique'))[0].grid
-                processed.extend([i.func(*i.args, implicit_dims=dims) for i in
-                                  e.subdomain._create_implicit_exprs(grid)])
+                if e.subdomain not in found:
+                    grid = list(retrieve_functions(e, mode='unique'))[0].grid
+                    found[e.subdomain] = [i.func(*i.args, implicit_dims=dims) for i in
+                                          e.subdomain._create_implicit_exprs(grid)]
+                processed.extend(found[e.subdomain])
                 dims.extend(e.subdomain.dimensions)
                 new_e = Eq(e.lhs, e.rhs, subdomain=e.subdomain, implicit_dims=dims)
                 processed.append(new_e)
