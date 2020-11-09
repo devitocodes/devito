e58602bef170689f9faa74fbfc80f93b2333c079
Victor Leite <leitevmd@gmail.com>
2020-11-09 11:28:01 -0300

    passes: Adjust code formatting (flake8)

--- devito/passes/iet/mpi.py
+++ devito/passes/iet/mpi.py
@@ -95,7 +95,8 @@ def _hoist_halospots(iet):
 
             for f, (loc_indices, _) in hs.fmapper.items():
                 loc_dims = frozenset().union([q for d in loc_indices
-                                                for q in d._defines])
+                                              for q in d._defines])
+
                 for n, i in enumerate(iters):
                     candidates = set().union(*[i.dim._defines for i in iters[n:]])
 
