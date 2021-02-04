f367a6f6fcb9dd5c3cd051545b72aa180af49d92
Fabio Luporini <f.luporini12@imperial.ac.uk>
2021-02-04 09:44:05 +0100

    types: Fix caching by changing BoundSymbol inheritance

--- tests/test_caching.py
+++ tests/test_caching.py
@@ -455,12 +455,24 @@ class TestCaching(object):
         for i in range(10):
             assert(len(_SymbolCache) == cache_size)
 
-            u = Function(name='u', grid=grid, space_order=2)
-            u._C_symbol
-            # All u, u(inds) and u.function through u._c_symbol added to cache
-            assert(len(_SymbolCache) == cache_size + 3)
-            del u
+            Function(name='u', grid=grid, space_order=2)
+            # Both u and u(inds) added to cache
+            assert(len(_SymbolCache) == cache_size + 2)
+
             clear_cache()
+
+    def test_clear_cache_with_Csymbol(self, operate_on_empty_cache, nx=1000, ny=1000):
+        grid = Grid(shape=(nx, ny), dtype=np.float64)
+        cache_size = len(_SymbolCache)
+
+        u = Function(name='u', grid=grid, space_order=2)
+        # Both u and u(inds) added to cache
+        assert(len(_SymbolCache) == cache_size + 2)
+
+        a = u._C_symbol
+        # Cache size won't change since _C_symbol isn't cached by devito to
+        # avoid circular references in the cache
+        assert(len(_SymbolCache) == cache_size + 2)
 
     def test_clear_cache_with_alive_symbols(self, operate_on_empty_cache,
                                             nx=1000, ny=1000):
