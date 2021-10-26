cda1d06e4f2e618ddc5b188ee238d1a0c0b3904b
Tobias Grosser <tobias@grosser.es>
2021-10-26 07:43:34 +0100

    Remove more code to focus discussion

--- devito/ir/ietxdsl/operations.py
+++ devito/ir/ietxdsl/operations.py
@@ -1,7 +1,7 @@
 from xdsl.ir import *
 from xdsl.irdl import *
 from xdsl.util import *
-from xdsl.dialects.builtin import IntegerType, Float32Type, IntegerAttr, FlatSymbolRefAttr
+from xdsl.dialects.builtin import IntegerType, IntegerAttr
 
 
 @dataclass
@@ -12,7 +12,6 @@ class IET:
         self.ctx.register_op(Constant)
 
         self.ctx.register_op(Addi)
-        self.ctx.register_op(Muli)
 
         self.i32 = IntegerType.get(32)
 
@@ -22,15 +21,7 @@ class IET:
             Constant, [], [self.i32],
             attributes={"value": IntegerAttr.get(val, self.i32)})
 
-    def constant_from_attr(self, attr: Attribute, typ: Attribute) -> Operation:
-        return Operation.with_result_types(Constant, [], [typ],
-                                           attributes={"value": attr})
-
     # TODO these operations should support all kinds of integer types
-    def muli(self, x: OpOrBlockArg, y: OpOrBlockArg) -> Operation:
-        return Operation.with_result_types(
-            Muli, [get_ssa_value(x), get_ssa_value(y)], [self.i32], {})
-
     def addi(self, x: OpOrBlockArg, y: OpOrBlockArg) -> Operation:
         return Operation.with_result_types(
             Addi, [get_ssa_value(x), get_ssa_value(y)], [self.i32], {})
@@ -50,19 +41,6 @@ class Constant:
 @irdl_op_definition
 class Addi:
     name: str = "iet.addi"
-    input1 = OperandDef(IntegerType)
-    input2 = OperandDef(IntegerType)
-    output = ResultDef(IntegerType)
-
-    # TODO replace with trait
-    def verify_(self) -> None:
-        if self.input1.typ != self.input2.typ or self.input2.typ != self.output.typ:
-            raise Exception("expect all input and output types to be equal")
-
-
-@irdl_op_definition
-class Muli:
-    name: str = "iet.muli"
     input1 = OperandDef(IntegerType)
     input2 = OperandDef(IntegerType)
     output = ResultDef(IntegerType)
