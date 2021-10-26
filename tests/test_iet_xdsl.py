6c3a9800e1417e5bc9d14dfaab1bd9277f00715b
Tobias Grosser <tobias@grosser.es>
2021-10-26 12:20:17 +0100

    Address flake8

--- tests/test_iet_xdsl.py
+++ tests/test_iet_xdsl.py
@@ -1,14 +1,12 @@
-import pytest
-
 from xdsl.dialects.builtin import *
-from xdsl.parser import Printer, Parser
+from xdsl.parser import Printer
 from xdsl.util import module
 from devito.ir.ietxdsl import *
 
 
 def test_expression():
     ctx = MLContext()
-    builtin = Builtin(ctx)
+    Builtin(ctx)
     iet = IET(ctx)
 
     cst42 = iet.constant(42)
@@ -19,13 +17,13 @@ def test_expression():
     # Maybe add sth like an IRBuilder?
     mod = module([cst42, cst3, addi])
 
-    result_string = """
-        module() {
-          %0 : !i32 = iet.constant() ["value" = 42 : !i32]
-          %1 : !i32 = iet.constant() ["value" = 3 : !i32]
-          %2 : !i32 = iet.addi(%0 : !i32, %1 : !i32)
-        }
-        """
+    # result_string = """
+    #    module() {
+    #      %0 : !i32 = iet.constant() ["value" = 42 : !i32]
+    #      %1 : !i32 = iet.constant() ["value" = 3 : !i32]
+    #      %2 : !i32 = iet.addi(%0 : !i32, %1 : !i32)
+    #    }
+    #    """
     # I would like to print the module to a string to be able to
     # compare it with the expected result. AFAIU xdsl.util.Printer
     # can only print to stdout, so this is not yet supported.
