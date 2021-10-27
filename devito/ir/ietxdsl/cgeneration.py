33bbc7b17543e29bbe2b0f53603ce523c489c03c
Tobias Grosser <tobias@grosser.es>
2021-10-27 07:22:53 +0100

    Add a couple of comments

--- devito/ir/ietxdsl/cgeneration.py
+++ devito/ir/ietxdsl/cgeneration.py
@@ -14,6 +14,19 @@ class CGeneration:
     def print(self, *args, **kwargs):
         print(*args, file=self.output, **kwargs)
 
+    # To translate code such as:
+    #
+    #   cst42 := iet.constant(42)
+    #   cst3 := iet.constant(3)
+    #   iet.addi(cst42, cst3)
+    #
+    # into a single-line expression such as:
+    #
+    #   42 + 3
+    #
+    # we look at the very last operation in the module and then walk iand
+    # recursively print the following tree expressed by the def-use chain of
+    # these operations.
     def printModule(self, module):
         # Get the last operation in the module
         self.printOperation(module.ops[-1])
