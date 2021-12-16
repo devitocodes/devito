25022e70e03808e9bda6467ea337aab4f9344085
Mathias Louboutin <mathias.louboutin@gmail.com>
2021-12-16 09:46:25 -0500

    examples: add adjoint, born, gradient, checkpointing to the tti example

--- examples/seismic/tti/tti_example.py
+++ examples/seismic/tti/tti_example.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from devito import info, norm
+from devito import Constant, Function, smooth, norm, info
 
 from examples.seismic import demo_model, setup_geometry, seismic_args
 from examples.seismic.tti import AnisotropicWaveSolver
@@ -23,20 +23,42 @@ def tti_setup(shape=(50, 50, 50), spacing=(20.0, 20.0, 20.0), tn=250.0,
 
 
 def run(shape=(50, 50, 50), spacing=(20.0, 20.0, 20.0), tn=250.0,
-        autotune=False, time_order=2, space_order=4, nbl=10,
-        kernel='centered', full_run=False, **kwargs):
+        autotune=False, space_order=4, nbl=10, preset='layers-tti',
+        kernel='centered', full_run=False, checkpointing=False, **kwargs):
 
     solver = tti_setup(shape=shape, spacing=spacing, tn=tn, space_order=space_order,
-                       nbl=nbl, kernel=kernel, **kwargs)
-    info("Applying Forward")
+                       nbl=nbl, kernel=kernel, preset=preset, **kwargs)
 
-    rec, u, v, summary = solver.forward(autotune=autotune)
+    info("Applying Forward")
+    # Whether or not we save the whole time history. We only need the full wavefield
+    # with 'save=True' if we compute the gradient without checkpointing, if we use
+    # checkpointing, PyRevolve will take care of the time history
+    save = full_run and not checkpointing
+    # Define receiver geometry (spread across x, just below surface)
+    rec, u, v, summary = solver.forward(save=save, autotune=autotune)
+
+    if preset == 'constant':
+        # With  a new m as Constant
+        v0 = Constant(name="v", value=2.0, dtype=np.float32)
+        solver.forward(save=save, vp=v0)
+        # With a new vp as a scalar value
+        solver.forward(save=save, vp=2.0)
 
     if not full_run:
         return summary.gflopss, summary.oi, summary.timings, [rec, u, v]
 
+    # Smooth velocity
+    initial_vp = Function(name='v0', grid=solver.model.grid, space_order=space_order)
+    smooth(initial_vp, solver.model.vp)
+    dm = np.float32(initial_vp.data**(-2) - solver.model.vp.data**(-2))
+
     info("Applying Adjoint")
     solver.adjoint(rec, autotune=autotune)
+    info("Applying Born")
+    solver.jacobian(dm, autotune=autotune)
+    info("Applying Gradient")
+    solver.jacobian_adjoint(rec, u, v, autotune=autotune, checkpointing=checkpointing)
+
     return summary.gflopss, summary.oi, summary.timings, [rec, u, v]
 
 
@@ -70,4 +92,5 @@ if __name__ == "__main__":
 
     run(shape=shape, spacing=spacing, nbl=args.nbl, tn=tn,
         space_order=args.space_order, autotune=args.autotune, dtype=args.dtype,
-        opt=args.opt, kernel=args.kernel, preset=preset, full_run=args.full)
+        opt=args.opt, kernel=args.kernel, preset=preset,
+        checkpointing=args.checkpointing, full_run=args.full)
