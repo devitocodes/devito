0e10d80901ddb8fa464bf89d73bc40d32c0b0ba9
George Bisbas <g.bisbas18@imperial.ac.uk>
2022-05-20 12:22:43 +0100

    mpi: Update script and notebooks

--- scripts/create_ipyparallel_mpi_profile.sh
+++ scripts/create_ipyparallel_mpi_profile.sh
@@ -6,11 +6,6 @@ IPYTHONDIR=~/.ipython
 # Create a new profile, called "mpi"
 ipython profile create --parallel --profile=mpi
 
-# Add the following line as per instructions from
-# https://ipyparallel.readthedocs.io/en/latest/process.html#using-ipcluster-in-mpiexec-mpirun-mode 
-# This is instructing `ipcluster` to use the MPI launchers
-echo "c.IPClusterEngines.engine_launcher_class = 'MPIEngineSetLauncher'" >> $IPYTHONDIR/profile_mpi/ipcluster_config.py
-
 ver=$(mpiexec --version)
 if [[ $ver == *"open-mpi"* ]]; then
   # OpenMPI need to be told that is allowed to oversubscribe cores
