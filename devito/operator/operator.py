60c9c895ece0cf9a35a5ae72e8b8e8500153f3e5
Gustavo Coelho <gustavo.coelho@fieb.org.br>
2022-06-13 10:57:29 -0300

    dsl: Adds the lookuptable header inside Devito and updates the path to it

--- devito/operator/operator.py
+++ devito/operator/operator.py
@@ -135,7 +135,7 @@ class Operator(Callable):
 
     _default_headers = [('_POSIX_C_SOURCE', '200809L')]
     _default_includes = ['stdlib.h', 'math.h', 'sys/time.h',
-                         f'{os.path.expanduser("~")}/lookuptable.h']
+                         f'{os.path.expanduser("~/devito")}/lookuptable.h']
     _default_globals = []
 
     def __new__(cls, expressions, **kwargs):
