1d060821b757ebd8dee10a4ca0474c4b932587c4
Peterson Nogueira <nogueirapeterson@gmail.com>
2022-06-14 22:38:40 -0300

    examples: Reorganize return of elastic adjoint stencil to obey flake8 rules

--- examples/seismic/elastic/operators.py
+++ examples/seismic/elastic/operators.py
@@ -120,9 +120,12 @@ def elastic_stencil(model, v, tau, forward=True):
         u_tauxy = Eq(tau[0, 1].backward, damp * solve(pde_tauxy, tau[0, 1].backward))
 
         if model.grid.dim == 3:
-            return [u_vx, u_vy, u_tauxx, u_tauyy, u_tauxy, u_vz, u_tauzz, u_tauxz, u_tauyz]
-        return [u_vx, u_vy, u_tauxx, u_tauyy, u_tauxy]
+            kernels = \
+                u_vx, u_vy, u_tauxx, u_tauyy, u_tauxy, u_vz, u_tauzz, u_tauxz, u_tauyz
+        else:
+            kernels = u_vx, u_vy, u_tauxx, u_tauyy, u_tauxy
 
+        return [kernels]
 
 
 def ForwardOperator(model, geometry, space_order=4, save=False, **kwargs):
