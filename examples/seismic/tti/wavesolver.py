74962326bf77847f7b3b685e6cc3cda4bccf0cbe
George Bisbas <g.bisbas18@imperial.ac.uk>
2022-06-16 12:55:25 +0300

    wip

--- examples/seismic/tti/wavesolver.py
+++ examples/seismic/tti/wavesolver.py
@@ -209,9 +209,9 @@ class AnisotropicWaveSolver(object):
             time_order = 2
             stagg_u = stagg_v = None
         # Source term is read-only, so re-use the default
-        src = src or self.geometry.src
+        # src = src or self.geometry.src
         # Create a new receiver object to store the result
-        rec = rec or self.geometry.rec
+        # rec = rec or self.geometry.rec
 
         # Create the forward wavefield if not provided
         if u is None:
@@ -236,8 +236,18 @@ class AnisotropicWaveSolver(object):
         model = model or self.model
 
         nx, ny, nz = self.model.grid.shape
-        u.data[0, int(nx/2), int(ny/2), 50] = 5
-        v.data[0, int(nx/2), int(ny/2), 50] = 5
+        u.data[:, int(nx/2), int(ny/2), int(nz/2)] = 1
+        v.data[:, int(nx/2), int(ny/2), int(nz/2)] = 1
+
+        import pdb;pdb.set_trace()
+        import matplotlib.pyplot as plt
+        from examples.cfd import plot_field
+        from examples.seismic import plot_image
+
+        plot_image(u.data[0, :, :, int(nz/2)], cmap="viridis")
+        plt.show()
+        plot_image(v.data[0, :, :, int(nz/2)], cmap="viridis")
+        plt.show()
 
         print(norm(u))
         print(norm(v))
@@ -249,8 +259,9 @@ class AnisotropicWaveSolver(object):
         # Execute operator and return wavefield and receiver data
         op = self.op_fwd_tb(save)
         # op = self.op_fwd(save)
-        # summary = op.apply(time_M=self.geometry.nt-1, u=u, v=v, dt=kwargs.pop('dt', self.dt), **{'time0_blk0_size': 64, 'x0_blk0_size': 32, 'x0_blk1_size': 4, 'y0_blk0_size': 32, 'y0_blk1_size': 4})
+        # summary = op.apply(time_M=self.geometry.nt-1, u=u, v=v, dt=kwargs.pop('dt', self.dt), **{'time0_blk0_size': 64, 'x0_blk0_size': 32, 'x0_blk1_size': 8, 'y0_blk0_size': 32, 'y0_blk1_size': 8})
         summary = op.apply(time_M=self.geometry.nt-1, u=u, v=v, dt=kwargs.pop('dt', self.dt))
+
         print(norm(u))
         print(norm(v))
         import pdb;pdb.set_trace()
@@ -258,9 +269,9 @@ class AnisotropicWaveSolver(object):
         from examples.cfd import plot_field
         from examples.seismic import plot_image
 
-        plot_image(u.data[0, :, :, 50], cmap="viridis")
+        plot_image(u.data[0, :, :, int(nz/2)], cmap="viridis")
         plt.show()
-        plot_image(v.data[0, :, :, 50], cmap="viridis")
+        plot_image(v.data[0, :, :, int(nz/2)], cmap="viridis")
         plt.show()
 
         return u, v, summary
