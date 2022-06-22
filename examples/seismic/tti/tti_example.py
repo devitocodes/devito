86c0f26af5f05870b1107e83da5174813a3be922
ofmla <o_mojical@hotmail.com>
2022-06-22 17:54:26 -0300

    examples: move test into model.py and check more cases

--- examples/seismic/tti/tti_example.py
+++ examples/seismic/tti/tti_example.py
@@ -64,29 +64,6 @@ def test_tti_stability(shape, kernel):
     assert np.isfinite(norm(rec))
 
 
-@pytest.mark.parametrize('shape', [(51, 51), (16, 16, 16)])
-def test_model_updt(shape):
-
-    # Layered model (tti)
-    tti_model = demo_model('layers-tti', shape=shape, spacing=[20. for _ in shape],
-                           space_order=4, nbl=10)
-    tpl1_set = set(tti_model.physical_parameters)
-
-    # Layered model (isotropic)
-    iso_model = demo_model('layers-isotropic', shape=shape, spacing=[20. for _ in shape],
-                           space_order=4, nbl=10)
-    tpl2_set = set(iso_model.physical_parameters)
-
-    # Physical parameters in either set but not in the intersection.
-    diff_phys_par = tuple(tpl1_set ^ tpl2_set)
-
-    # Convert iso model in tti model
-    slices = tuple(slice(tti_model.nbl, -tti_model.nbl) for _ in range(tti_model.dim))
-    for i in diff_phys_par:
-        iso_model.update(i, getattr(tti_model, i).data[slices])
-        assert np.array_equal(getattr(iso_model, i).data, getattr(tti_model, i).data)
-
-
 if __name__ == "__main__":
     description = ("Example script to execute a TTI forward operator.")
     parser = seismic_args(description)
