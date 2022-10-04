ce4994a490ed3d3acb7d8cf9bce7a4a25060dd2d
FabioLuporini <fabio@devitocodes.com>
2022-10-04 13:28:13 +0100

    builtins: Mute assign

--- devito/builtins/initializers.py
+++ devito/builtins/initializers.py
@@ -7,6 +7,7 @@ from devito.builtins.utils import abstract_args, nbl_to_padsize, pad_outhalo
 __all__ = ['assign', 'smooth', 'gaussian_smooth', 'initialize_function']
 
 
+@dv.switchconfig(log_level='ERROR')
 @abstract_args
 def assign(f, rhs=0, options=None, name='assign', argmap=None, **kwargs):
     """
