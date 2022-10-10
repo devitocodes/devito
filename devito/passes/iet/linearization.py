1e65b39761b53bb5b86cfd07933dc3082e1b9a1f
FabioLuporini <fabio@devitocodes.com>
2022-10-10 13:51:44 +0100

    compiler: Revamp linearization

--- devito/passes/iet/linearization.py
+++ devito/passes/iet/linearization.py
@@ -1,14 +1,16 @@
+from collections import defaultdict
+
 from functools import singledispatch
 
 import numpy as np
+from sympy import Add
 
 from devito.data import FULL
 from devito.ir import (BlankLine, Call, DummyExpr, Dereference, List, PointerCast,
-                       Transfer, FindNodes, FindSymbols, Transformer, Uxreplace,
-                       SymbolRegistry)
+                       Transfer, FindNodes, FindSymbols, Transformer, Uxreplace)
 from devito.passes.iet.engine import iet_pass
 from devito.symbolics import DefFunction, MacroArgument, ccode
-from devito.tools import Bunch, DefaultOrderedDict, filter_ordered, prod
+from devito.tools import Bunch, filter_ordered, prod
 from devito.types import Array, Symbol, FIndexed, Indexed, Wildcard
 from devito.types.basic import IndexedData
 from devito.types.dense import DiscreteFunction
@@ -20,138 +22,148 @@ __all__ = ['linearize']
 def linearize(graph, **kwargs):
     """
     Turn n-dimensional Indexeds into 1-dimensional Indexed with suitable index
-    access function, such as `a[i, j]` -> `a[i*n + j]`. The row-major format
+    access function, such as `a[i, j] -> a[i*n + j]`. The row-major format
     of the underlying Function objects is honored.
     """
-    # Simple data structure to avoid generation of duplicated code
-    track = DefaultOrderedDict(lambda: Bunch(stmts0=[], stmts1=[], held=set(), cbk=None))
+    mode = kwargs['mode']
+    if not mode:
+        return
 
-    linearization(graph, track=track, **kwargs)
+    # All information that need to be propagated across callables
+    tracker = Bunch(sizes={}, strides={}, undef=defaultdict(set), cbks={})
+
+    linearization(graph, tracker=tracker, **kwargs)
 
     # Sanity check
-    assert all(not v.held for v in track.values())
+    assert not tracker.undef
 
 
 @iet_pass
-def linearization(iet, **kwargs):
+def linearization(iet, mode=None, tracker=None, sregistry=None, **kwargs):
     """
     Carry out the actual work of `linearize`.
     """
-    mode = kwargs['mode']
-    sregistry = kwargs['sregistry']
-    track = kwargs['track']
-
-    # Pre-process the `mode` opt option
     # `mode` may be a callback describing what Function types, and under what
     # conditions, should linearization be applied
-    if not mode:
-        return iet, {}
-    elif callable(mode):
+    if callable(mode):
         key = lambda f: mode(f) and f.ndim > 1
     else:
         # Default
         key = lambda f: (f.is_DiscreteFunction or f.is_Array) and f.ndim > 1
 
-    iet, headers = linearize_accesses(iet, key, track, sregistry)
+    iet, headers = linearize_accesses(iet, key, tracker, sregistry)
     iet = linearize_pointers(iet, key)
     iet = linearize_transfers(iet, sregistry)
 
     return iet, {'headers': headers}
 
 
-def linearize_accesses(iet, key, track, sregistry):
+def key1(f, d):
+    """
+    A key for Function dimensions. The key is:
+
+        * False if not statically linearizable, that is not linearizable via
+          constant symbolic sizes and strides;
+        * A 3-tuple `(Dimension, halo size, grid)` otherwise.
+    """
+    if f.is_regular and f.is_compact:
+        # TODO: same grid + same halo => same padding, however this is not asserted
+        # during compilation... so maybe we should do it at `prepare_args` time?
+        return (d, f._size_halo[d], getattr(f, 'grid', None))
+    else:
+        return False
+
+
+def linearize_accesses(iet, key0, tracker, sregistry):
     """
     Turn Indexeds into FIndexeds and create the necessary access Macros.
     """
-    # The `candidates` are all Functions that may be linearized inside `iet`
+    # 1) What `iet` *needs*
     indexeds = FindSymbols('indexeds').visit(iet)
-    candidates = filter_ordered(i.function for i in indexeds if key(i.function))
-    candidates = sorted(candidates, key=lambda f: len(f.dimensions), reverse=True)
+    needs = filter_ordered(i.function for i in indexeds if key0(i.function))
+    needs = sorted(needs, key=lambda f: len(f.dimensions), reverse=True)
 
-    # For some of these candidates, a linearization may have already been
-    # produced in another IET
-    selected = [f for f in candidates if f not in track]
-
-    # Find unique sizes (unique -> minimize necessary registers)
-    mapper = DefaultOrderedDict(list)
-    for f in selected:
+    # Update unique sizes table
+    # E.g. `{(x, 8, grid) -> x_fsz0}`
+    for f in needs:
         # NOTE: the outermost dimension is unnecessary
         for d in f.dimensions[1:]:
-            # TODO: same grid + same halo => same padding, however this is
-            # never asserted throughout the compiler yet... maybe should do
-            # it when in debug mode at `prepare_arguments` time, ie right
-            # before jumping to C?
-            mapper[(d, f._size_halo[d], getattr(f, 'grid', None))].append(f)
+            k = key1(f, d)
+            if k and k not in tracker.sizes:
+                name = sregistry.make_name(prefix='%s_fsz' % d.name)
+                fsz = Symbol(name=name, dtype=np.int64, is_const=True)
+                tracker.sizes[k] = fsz
 
-    # For all unseen Functions, build the size exprs. For example:
-    # `x_fsz0 = u_vec->size[1]`
-    imapper = DefaultOrderedDict(dict)
-    dummyregistry = SymbolRegistry()
-    for (d, halo, _), v in mapper.items():
-        for f in list(v):
-            if _generate_fsz(f, d, dummyregistry) is None:
-                v.remove(f)
-        try:
-            expr = _generate_fsz(v[0], d, sregistry)
-        except IndexError:
-            continue
-        for f in v:
-            imapper[f][d] = expr.write
-            track[f].stmts0.append(expr)
-
-    # For all unseen Functions, build the stride exprs. For example:
-    # `y_stride0 = y_fsz0*z_fsz0`
-    built = {}
-    mapper = DefaultOrderedDict(dict)
-    for f, v in imapper.items():
-        for d in v:
-            n = f.dimensions.index(d)
-            expr = prod(v[i] for i in f.dimensions[n:])
+    # Update unique strides table
+    # E.g., `{x_stride0 -> (y_fsz0, z_fsz0)}`
+    mapper = defaultdict(dict)
+    for f in needs:
+        for n, d in enumerate(f.dimensions[1:], 1):
             try:
-                stmt = built[expr]
+                k = tuple(tracker.sizes[key1(f, d1)] for d1 in f.dimensions[n:])
             except KeyError:
+                continue
+            if k not in tracker.strides:
                 name = sregistry.make_name(prefix='%s_stride' % d.name)
-                s = Symbol(name=name, dtype=np.int64, is_const=True)
-                stmt = built[expr] = DummyExpr(s, expr, init=True)
-            mapper[f][d] = stmt.write
-            track[f].stmts1.append(stmt)
+                stride = Symbol(name=name, dtype=np.int64, is_const=True)
+                tracker.strides[k] = stride
+            mapper[f][d] = tracker.strides[k]
 
-    # For all unseen Functions, build the access macros. For example:
-    # `#define uL(t, x, y, z) u[(t)*t_stride0 + (x)*x_stride0 + (y)*y_stride0 + (z)]`
+    # Update unique access macros
+    # E.g. `{f(x, y) -> foo}`, `foo(Indexed) -> f[(x)*y_stride0 + (y)]`
     headers = []
-    for f in selected:
-        if track[f].cbk is None:
-            header, track[f].cbk = _generate_macro(f, mapper[f], sregistry)
-            headers.append(header)
+    for f in needs:
+        if f in tracker.cbks:
+            continue
+        header, tracker.cbks[f] = _generate_macro(f, mapper[f], sregistry)
+        headers.append(header)
 
-    # Turn all Indexeds into "functional" Indexeds. For example:
-    # `u[t2, x+8, y+9, z+7] => uL(t2, x+8, y+9, z+7)`
-    mapper = {i: track[i.function].cbk(i) for i in indexeds if i.function in track}
-    iet = Uxreplace(mapper).visit(iet)
+    # Turn Indexeds into FIndexeds
+    # E.g. `u[t2, x+8, y+9, z+7] -> uL(t2, x+8, y+9, z+7)`
+    subs = {i: tracker.cbks[i.function](i) for i in indexeds if i.function in needs}
+    iet = Uxreplace(subs).visit(iet)
 
-    # All Functions that can actually be linearized in `iet`
+    # 2) What `iet` *offers*
+    # E.g. `{x_fsz0 -> u_vec->size[1]}`
     defines = FindSymbols('defines-aliases').visit(iet)
+    offers = filter_ordered(f for f in defines if key0(f))
+    instances = {}
+    for f in offers:
+        for d in f.dimensions[1:]:
+            k = key1(f, d)
+            try:
+                fsz = tracker.sizes[k]
+                v = _generate_fsz(f, d, fsz, sregistry)
+            except KeyError:
+                v = None
+            if v is not None:
+                instances[fsz] = v
 
-    # All Callables for which `iet` may produce a linearization
-    calls = {i.name for i in FindNodes(Call).visit(iet)}
+    # 3) What we need to construct are `iet`'s needs plus the callee delegations
+    candidates = set().union(*[v.values() for v in mapper.values()])
+    for n in FindNodes(Call).visit(iet):
+        candidates.update(tracker.undef.pop(n.name, []))
 
-    # Place the linearization expressions or delegate to ancestor efunc
-    stmts0 = []
-    stmts1 = []
-    for f, v in track.items():
-        release = calls & v.held
-        v.held.difference_update(release)
-        if f in candidates or release:
-            if f in defines:
-                stmts0.extend(v.stmts0)
-                stmts1.extend(v.stmts1)
-            else:
-                v.held.add(iet.name)
+    # 4) Construct what needs to *and* can be constructed
+    mapper = dict(zip(tracker.strides.values(), tracker.strides.keys()))
+    stmts0, stmts1 = [], []
+    for stride, sizes in mapper.items():
+        if stride not in candidates:
+            continue
+        try:
+            stmts0.extend([instances[size] for size in sizes])
+        except KeyError:
+            # `stride` would be needed by `iet`, but we don't have the means
+            # to define it, hence we delegate to the caller
+            tracker.undef[iet.name].add(stride)
+            continue
+        stmts1.append(DummyExpr(stride, prod(sizes), init=True))
+
+    # 5) Attach `stmts0` and `stmts1` to `iet`
     if stmts0:
         assert len(stmts1) > 0
-        stmts0 = filter_ordered(stmts0) + [BlankLine]
-        stmts1 = filter_ordered(stmts1) + [BlankLine]
-        body = iet.body._rebuild(body=tuple(stmts0) + tuple(stmts1) + iet.body.body)
+        stmts = filter_ordered(stmts0) + [BlankLine] + stmts1 + [BlankLine]
+        body = iet.body._rebuild(body=tuple(stmts) + iet.body.body)
         iet = iet._rebuild(body=body)
     else:
         assert len(stmts1) == 0
@@ -160,43 +172,45 @@ def linearize_accesses(iet, key, track, sregistry):
 
 
 @singledispatch
-def _generate_fsz(f, d, sregistry):
+def _generate_fsz(f, d, fsz, sregistry):
     return
 
 
 @_generate_fsz.register(DiscreteFunction)
-def _(f, d, sregistry):
-    name = sregistry.make_name(prefix='%s_fsz' % d.name)
-    s = Symbol(name=name, dtype=np.int64, is_const=True)
-    return DummyExpr(s, f._C_get_field(FULL, d).size, init=True)
+def _(f, d, fsz, sregistry):
+    return DummyExpr(fsz, f._C_get_field(FULL, d).size, init=True)
 
 
 @_generate_fsz.register(Array)
-def _(f, d, sregistry):
-    name = sregistry.make_name(prefix='%s_fsz' % d.name)
-    s = Symbol(name=name, dtype=np.int64, is_const=True)
-    return DummyExpr(s, f.symbolic_shape[d], init=True)
+def _(f, d, fsz, sregistry):
+    return DummyExpr(fsz, f.symbolic_shape[d], init=True)
 
 
 @singledispatch
-def _generate_macro(f, szs, sregistry):
-    return
+def _generate_macro(f, strides, sregistry):
+    return None, None
 
 
 @_generate_macro.register(DiscreteFunction)
 @_generate_macro.register(Array)
-def _(f, szs, sregistry):
-    assert len(szs) == len(f.dimensions) - 1
-
+def _(f, strides, sregistry):
     pname = sregistry.make_name(prefix='%sL' % f.name)
-    cbk = lambda i, pname=pname: FIndexed(i, pname, strides=tuple(szs.values()))
 
-    expr = sum([MacroArgument(d0.name)*szs[d1]
-                for d0, d1 in zip(f.dimensions, f.dimensions[1:])])
-    expr += MacroArgument(f.dimensions[-1].name)
-    expr = Indexed(IndexedData(f.name, None, f), expr)
-    define = DefFunction(pname, f.dimensions)
+    # Generate e.g. `usave[(time)*xi_slc0 + (xi)*yi_slc0 + (yi)]`
+    assert len(strides) == len(f.dimensions) - 1
+    macroargnames = [d.name for d in f.dimensions]
+    macroargs = [MacroArgument(i) for i in macroargnames]
+
+    items = [m*strides[d] for m, d in zip(macroargs, f.dimensions[1:])]
+    items.append(MacroArgument(f.dimensions[-1].name))
+
+    value = Add(*items, evaluate=False)
+    expr = Indexed(IndexedData(f.name, None, f), value)
+    define = DefFunction(pname, macroargnames)
     header = (ccode(define), ccode(expr))
+
+    def cbk(i):
+        return FIndexed(i, pname, strides=tuple(strides.values()))
 
     return header, cbk
 
