72c516ee948f7b00f2d1cdce366a564f828bbeeb
FabioLuporini <fabio@devitocodes.com>
2022-10-14 15:50:09 +0100

    compiler: Refactor buffering

--- devito/passes/clusters/buffering.py
+++ devito/passes/clusters/buffering.py
@@ -219,23 +219,14 @@ class Buffering(Queue):
                 expr = lower_exprs(uxreplace(Eq(lhs, rhs), b.subdims_mapper))
                 ispace = b.written
 
-                # Buffering creates a storage-related dependence along the
-                # contracted dimensions
-                properties = dict(c.properties)
-                for d in b.contraction_mapper:
-                    d = ispace[d].dim  # E.g., `time_sub -> time`
-                    properties[d] = normalize_properties(properties[d], {SEQUENTIAL})
-
-                processed.append(
-                    c.rebuild(exprs=expr, ispace=ispace, properties=properties)
-                )
+                processed.append(downgrade(c.rebuild(exprs=expr, ispace=ispace), d))
 
             # Substitute buffered Functions with the newly created buffers
             exprs = [uxreplace(e, subs) for e in c.exprs]
             ispace = c.ispace
             for b in buffers:
                 ispace = ispace.augment(b.sub_iterators)
-            processed.append(c.rebuild(exprs=exprs, ispace=ispace))
+            processed.append(downgrade(c.rebuild(exprs=exprs, ispace=ispace), d))
 
             # Also append the copy-back if `e` is the last-write of some buffers
             # E.g., `usave[time + 1, x] = ub[sb1, x]`
@@ -254,16 +245,7 @@ class Buffering(Queue):
                 expr = lower_exprs(uxreplace(Eq(lhs, rhs), b.subdims_mapper))
                 ispace = b.written
 
-                # Buffering creates a storage-related dependence along the
-                # contracted dimensions
-                properties = dict(c.properties)
-                for d in b.contraction_mapper:
-                    d = ispace[d].dim  # E.g., `time_sub -> time`
-                    properties[d] = normalize_properties(properties[d], {SEQUENTIAL})
-
-                processed.append(
-                    c.rebuild(exprs=expr, ispace=ispace, properties=properties)
-                )
+                processed.append(downgrade(c.rebuild(exprs=expr, ispace=ispace), d))
 
         # Lift {write,read}-only buffers into separate IterationSpaces
         if self.options['buf-fuse-tasks']:
@@ -706,3 +688,16 @@ def offset_from_centre(d, indices):
             offset = p - vmin(*[Vector(i) for i in indices])[0]
 
     return p, offset
+
+
+def downgrade(c, d):
+    """
+    Buffering induces a storage-related dependence along `c`'s contracted
+    Dimensions. This rebuilds `c` such that its contracted Dimension `d`
+    is tagged SEQUENTIAL.
+    """
+    # Buffering creates a storage-related dependence along the
+    # contracted dimensions
+    properties = dict(c.properties)
+    properties[d] = normalize_properties(properties[d], {SEQUENTIAL})
+    return c.rebuild(properties=properties)
