db1038d93312afe71c8074654892a4c60fe4d231
FabioLuporini <fabio@devitocodes.com>
2022-12-07 13:32:04 +0000

    compiler: Apply linearization to all regular Functions

--- devito/passes/iet/linearization.py
+++ devito/passes/iet/linearization.py
@@ -77,7 +77,7 @@ def key1(f, d):
           constant symbolic sizes and strides;
         * A 3-tuple `(Dimension, halo size, grid)` otherwise.
     """
-    if f.is_regular and f.is_compact:
+    if f.is_regular:
         # TODO: same grid + same halo => same padding, however this is not asserted
         # during compilation... so maybe we should do it at `prepare_args` time?
         return (d, f._size_halo[d], getattr(f, 'grid', None))
@@ -155,7 +155,7 @@ def linearize_accesses(iet, key0, tracker=None, sregistry=None, options=None,
                     v = DummyExpr(fsz, val, init=True)
                 else:
                     v = None
-            except KeyError:
+            except (AttributeError, KeyError):
                 v = None
             if v is not None:
                 instances[fsz] = v
