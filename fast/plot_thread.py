9636e1177b61acd7b93866e969d221b911b2c15d
Papy Chacal <papychacal@gmail.com>
2023-05-16 17:11:32 +0200

    Now thread_scale benchmarks EITHER devito or xdsl. (To get out of a rabbit hole, devito seemed to have side effects on the xDSL executable's OpenMP settings) Update plotting script accordingly.

--- fast/plot_thread.py
+++ fast/plot_thread.py
@@ -4,6 +4,7 @@ from math import floor, log
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
+import thread_scale
 
 dims = {"2d5pt": 2, "3d_diff": 3}
 
@@ -15,65 +16,59 @@ if len(sys.argv) < 2 or len(sys.argv) > 3:
 benchmark = sys.argv[1]
 first = int(sys.argv[2]) if len(sys.argv) > 2 else 0
 
-csv_name = f"{benchmark}_thread_runtimes.csv"
-svg_name = f"devito_{benchmark}_nthreads_cpu.pdf"
 
-def human_format(number: int):
-    units = ['', 'K', 'M', 'G', 'T', 'P']
-    k = 1000.0
-    magnitude = int(floor(log(number, k)))
-    if magnitude == 0 :
-        return str(number)
-    return '%.2f%s' % (number / k**magnitude, units[magnitude])
-try:
-    with open(csv_name, "r") as f:
-        lines = f.read().split("\n")[:-1]
-        header_line = lines[0].split(",")
-        x_label = header_line[0]
-        labels = header_line[1:]
+pdf_name = f"devito_{benchmark}_nthreads_cpu.pdf"
 
-        def split_line(line: str):
-            split = line.split(",")
-            size = int(split[0])
-            runtimes = [float(t) for t in split[1:]]
-            return (size, runtimes)
-            # runtimes = 
-        lines = list(map(literal_eval, lines[1+first:]))
+labels:list[str] = []
+nthreads: set[int] | list[int] = set()
+x_label = "Threads"
+values:dict[str, list[float]] = {}
+for mode in thread_scale.modes:
+    csv_name = f"{benchmark}_threads_{mode}.csv"
+    try:
+        with open(csv_name, "r") as f:
+            lines = f.read().split("\n")[:-1]
 
-        nthreads: list[int] = []
-        values:dict[str, list[float]] = {}
-        for label in labels:
-            values[label] = []
-        for line in lines:
-            nthreads.append(line[0])
-            for i, label in enumerate(labels):
-                print(f"line {line} i {i}")
-                # line[i+1] is the list of runtimes (Here for Devito/xDSL then Devito.)
+            labels.append(mode)
+
+            lines = list(map(literal_eval, lines[1+first:]))
+            if mode not in values.keys():
+                values[mode] = []
+            for line in lines:
+                nthreads.add(line[0])
+                print(f"line {line}")
+                # line[1] is the list of runtimes
                 # Here I'm just plotting the average!
-                values[label].append(sum(line[i+1])/len(line[i+1]))
+                values[mode].append(sum(line[1])/len(line[1]))
 
-        x = np.arange(len(nthreads))  #type: ignore
-        width = 0.25  # the width of the bars
-        multiplier = 0
+    except FileNotFoundError:
+        print(f'{csv_name} not found! Try running "python grid_scale.py {sys.argv[1]} {mode}" to generate it." ')
+        sys.exit(1)
+nthreads = list(nthreads)
+nthreads.sort()
 
-        fig, ax = plt.subplots(layout="constrained") #type: ignore
+print(nthreads)
+print(labels)
+print(values)
+x = np.arange(len(nthreads))  #type: ignore
+width = 0.25  # the width of the bars
+multiplier = 0
 
-        for attribute, measurement in values.items():
-            offset = width * multiplier
-            rects = ax.bar(x + offset, measurement, width, label=attribute) #type: ignore
-            # ax.bar_label(rects, padding=3)
-            multiplier += 1
+fig, ax = plt.subplots(layout="constrained") #type: ignore
 
-        # Add some text for labels, title and custom x-axis tick labels, etc.
-        ax.set_ylabel("Time (s)") #type: ignore
-        ax.set_xlabel(x_label) # type: ignore
-        ax.set_xticks(x + width, nthreads)
-        ax.legend(loc="upper left", ncols=3) #type: ignore
-        fig.autofmt_xdate()
+for attribute, measurement in values.items():
+    offset = width * multiplier
+    rects = ax.bar(x + offset, measurement, width, label=attribute) #type: ignore
+    # ax.bar_label(rects, padding=3)
+    multiplier += 1
 
-        print(f"saving to {svg_name}")
-        plt.savefig(svg_name, format="pdf") #type: ignore
-        plt.show() #type: ignore
-except FileNotFoundError:
-    print(f'{csv_name} not found! Try running "python grid_scale.py {sys.argv[1]}" to generate it." ')
-    sys.exit(1)
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel("Time (s)") #type: ignore
+ax.set_xlabel(x_label) # type: ignore
+ax.set_xticks(x + width, nthreads)
+ax.legend(loc="upper left", ncols=3) #type: ignore
+fig.autofmt_xdate()
+
+print(f"saving to {pdf_name}")
+plt.savefig(pdf_name, format="pdf") #type: ignore
+plt.show() #type: ignore
--- fast/thread_scale.py
+++ fast/thread_scale.py
@@ -1,7 +1,7 @@
 dims = {"2d5pt": 2, "3d_diff": 3}
 
 import argparse
-import os, sys
+import os
 from devito.operator.operator import Operator
 from devito.operator.xdsl_operator import XDSLOperator
 from devito.parameters import configuration
@@ -11,7 +11,10 @@ import fast_benchmarks
 
 parser = argparse.ArgumentParser(description="Process arguments.")
 
+
 parser.add_argument('benchmark_name', choices=["2d5pt", "3d_diff"])
+modes = ["xdsl", "devito"]
+parser.add_argument('benchmark_mode', choices=modes)
 parser.add_argument('-i', '--init_threads', type=int, default=1, help="Initial (lowest) number of threads")
 parser.add_argument('-m', '--max_threads', type=int, default=os.cpu_count(), help=f"Maximum number of threads. Defaults to the detected {os.cpu_count()}")
 parser.add_argument(
@@ -24,53 +27,69 @@ parser.add_argument(
     )
 parser.add_argument('-p', '--points', type=int, default=10, help="Number of measurements to make for each number of threads.")
 
-args = parser.parse_args()
+if __name__ == "__main__":
+    args = parser.parse_args()
 
-size = args.shape
-bench_name = args.benchmark_name
-init_threads = args.init_threads
-max_threads = args.max_threads
-points = args.points
-csv_name = f"{bench_name}_thread_runtimes.csv"
+    size = args.shape
+    bench_name = args.benchmark_name
+    init_threads = args.init_threads
+    max_threads = args.max_threads
+    points = args.points
+    mode = args.benchmark_mode
+    csv_name = f"{bench_name}_threads_{mode}.csv"
 
 
 
-def get_runtimes_for_threads(threads: int, eq: Eq) -> tuple[int, list[float], list[float]]:
-    print(f"Running for {threads} threads")
-    os.environ["OMP_NUM_THREADS"] = str(threads)
+    def get_runtimes_for_threads(threads: int, eq: Eq) -> tuple[int, list[float]]:
+        print(f"Running for {threads} threads")
+        os.environ["OMP_NUM_THREADS"] = str(threads)
+        match mode:
+            case "xdsl":
+                runs = [fast_benchmarks.run_kernel(bench_name) for _ in range(points)]
+            case "devito":
+                grid, u, eq0, dt = fast_benchmarks.get_equation(bench_name, size, 2, 1, 10)
+                op = Operator([eq0])
+                runs = [fast_benchmarks.run_operator(op, nt, dt) for _ in range(points)]
+            case _:
+                raise Exception("Unknown mode!")
+        return (threads, runs)
+
+
+
+    os.environ["OMP_PLACES"] = "threads"
+    configuration['language'] = 'openmp'
+
+
+    threads = init_threads
+
     grid, u, eq0, dt = fast_benchmarks.get_equation(bench_name, size, 2, 1, 10)
-    op = Operator([eq0])
-    xdsl_runs = [fast_benchmarks.run_kernel(bench_name) for _ in range(points)]
-    devito_runs = [fast_benchmarks.run_operator(op, nt, dt) for _ in range(points)]
-    return (threads, xdsl_runs, devito_runs)
+    fast_benchmarks.dump_input(u, bench_name)
+    xop = XDSLOperator([eq0])
+    nt = 100
+
+    fast_benchmarks.compile_interop(bench_name, True)
+    fast_benchmarks.compile_main(bench_name, grid, u, xop, dt, nt)
+    fast_benchmarks.compile_kernel(bench_name, xop.mlircode, fast_benchmarks.XDSL_CPU_PIPELINE, fast_benchmarks.OPENMP_PIPELINE)
+    fast_benchmarks.link_kernel(bench_name)
 
 
 
-os.environ["OMP_PLACES"] = "threads"
-configuration['language'] = 'openmp'
+    label:str
+    match mode:
+        case "xdsl":
+            label = "Devito/xDSL"
+        case "devito":
+            label = "Devito"
+        case _:
+            raise Exception("Unknown mode!")
 
-
-threads = init_threads
-
-grid, u, eq0, dt = fast_benchmarks.get_equation(bench_name, size, 2, 1, 10)
-fast_benchmarks.dump_input(u, bench_name)
-xop = XDSLOperator([eq0])
-nt = 100
-
-fast_benchmarks.compile_interop(bench_name, True)
-fast_benchmarks.compile_main(bench_name, grid, u, xop, dt, nt)
-fast_benchmarks.compile_kernel(bench_name, xop.mlircode, fast_benchmarks.XDSL_CPU_PIPELINE, fast_benchmarks.OPENMP_PIPELINE)
-fast_benchmarks.link_kernel(bench_name)
-
-
-
-
-with open(csv_name, "w") as f:
-    f.write("Threads,Devito/xDSL,Devito\n")
-    f.flush()
-
-    while threads <= max_threads:
-        runtime = get_runtimes_for_threads(threads, eq0)
-        f.write(f"{runtime}\n")
+    with open(csv_name, "w") as f:
+        
+        f.write(f"Threads,{label}\n")
         f.flush()
-        threads *= 2
+
+        while threads <= max_threads:
+            runtime = get_runtimes_for_threads(threads, eq0)
+            f.write(f"{runtime}\n")
+            f.flush()
+            threads *= 2
