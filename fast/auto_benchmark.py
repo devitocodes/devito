38c5bd34dc11fb47a26e0ef64445316511ad71f5
Anton Lydike <me@antonlydike.de>
2023-06-23 18:33:39 +0100

    add mpi thread scale

--- fast/auto_benchmark.py
+++ fast/auto_benchmark.py
@@ -78,7 +78,47 @@ def mpi_scale_2d5pt(qos="standard"):
         w = csv.writer(f)
         w.writerow(['ranks', 'implementation', 'time'])
         w.writerows(result_lines)
-    
+
+    # write raw data as json lines
+    with open(f'2d5pt_mpi_runtimes.json', 'w') as f:
+        for r in raw_results:
+            f.write(r.to_json() + '\n')
+
+def mpi_thread_scale_2d5pt(qos="standard"):
+    flags = '-nt 100 --xdsl --devito --openmp --mpi'
+    ranks = 16
+    env = f"OMP_PLACES=cores OMP_NUM_THREADS={cpus_per_rank} DEVITO_MPI=1"
+    shape = [20000, 20000]
+    runs = 5
+
+    raw_results = []
+
+    result_lines = []
+
+    for cpus_per_rank in (1, 2, 4, 8, 16, 32):
+        results = run_benchmark(
+            ranks=ranks, 
+            cpus_per_rank=cpus_per_rank,
+            name='2d5pt',
+            shape=shape,
+            flags=flags,
+            runs=runs,
+            env=env,
+            qos=qos,
+        )
+        raw_results.extend(results)
+
+        for res in results:
+            res.report()
+            result_lines.append((
+                ranks, res.impl_name, sum(res.times) / len(res.times)
+            ))
+    # write csv file
+    with open(f'2d5pt_mpi_runtimes.csv', 'w') as f:
+        w = csv.writer(f)
+        w.writerow(['ranks', 'implementation', 'time'])
+        w.writerows(result_lines)
+
     # write raw data as json lines
     with open(f'2d5pt_mpi_runtimes.json', 'w') as f:
         for r in raw_results:
