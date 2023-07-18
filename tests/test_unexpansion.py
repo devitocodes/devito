25463cc1f70bb90445e277d2935c3fac2e4dfd08
Fabio Luporini <fabio@devitocodes.com>
2023-07-18 16:18:00 +0000

    compiler: Support 2-pass impls w unexpasion

--- tests/test_unexpansion.py
+++ tests/test_unexpansion.py
@@ -5,7 +5,7 @@ from devito import Buffer, Eq, Function, TimeFunction, Grid, Operator, cos, sin
 from devito.types import Symbol
 
 
-class TestBasic(object):
+class Test1Pass(object):
 
     def test_v0(self):
         grid = Grid(shape=(10, 10, 10))
@@ -85,7 +85,8 @@ class TestBasic(object):
 
         op0 = Operator(eqns)
         op1 = Operator(eqns, opt=('advanced', {'expand': False,
-                                               'blocklevels': 0}))
+                                               'blocklevels': 0,
+                                               'cire-mingain': 200}))
 
         # Check generated code -- expect maximal fusion!
         assert_structure(op1,
@@ -110,7 +111,8 @@ class TestBasic(object):
                 Eq(v.forward, (v + u.dx.dy + 1.))]
 
         op0 = Operator(eqns)
-        op1 = Operator(eqns, opt=('advanced', {'expand': False}))
+        op1 = Operator(eqns, opt=('advanced', {'expand': False,
+                                               'cire-mingain': 200}))
 
         # Check generated code -- redundant IndexDerivatives have been caught!
         op1._profiler._sections['section0'].sops == 65
@@ -123,63 +125,12 @@ class TestBasic(object):
 
     def test_v4(self):
         grid = Grid(shape=(16, 16, 16))
-        t = grid.stepping_dim
-        x, y, z = grid.dimensions
 
-        so = 4
-
-        a = Function(name='a', grid=grid, space_order=so)
-        f = Function(name='f', grid=grid, space_order=so)
-        e = Function(name='e', grid=grid, space_order=so)
-        r = Function(name='r', grid=grid, space_order=so)
-        p0 = TimeFunction(name='p0', grid=grid, time_order=2, space_order=so)
-        m0 = TimeFunction(name='m0', grid=grid, time_order=2, space_order=so)
-
-        def g1(field, r, e):
-            return (cos(e) * cos(r) * field.dx(x0=x+x.spacing/2) +
-                    cos(e) * sin(r) * field.dy(x0=y+y.spacing/2) -
-                    sin(e) * field.dz(x0=z+z.spacing/2))
-
-        def g2(field, r, e):
-            return - (sin(r) * field.dx(x0=x+x.spacing/2) -
-                      cos(r) * field.dy(x0=y+y.spacing/2))
-
-        def g3(field, r, e):
-            return (sin(e) * cos(r) * field.dx(x0=x+x.spacing/2) +
-                    sin(e) * sin(r) * field.dy(x0=y+y.spacing/2) +
-                    cos(e) * field.dz(x0=z+z.spacing/2))
-
-        def g1_tilde(field, r, e):
-            return ((cos(e) * cos(r) * field).dx(x0=x-x.spacing/2) +
-                    (cos(e) * sin(r) * field).dy(x0=y-y.spacing/2) -
-                    (sin(e) * field).dz(x0=z-z.spacing/2))
-
-        def g2_tilde(field, r, e):
-            return - ((sin(r) * field).dx(x0=x-x.spacing/2) -
-                      (cos(r) * field).dy(x0=y-y.spacing/2))
-
-        def g3_tilde(field, r, e):
-            return ((sin(e) * cos(r) * field).dx(x0=x-x.spacing/2) +
-                    (sin(e) * sin(r) * field).dy(x0=y-y.spacing/2) +
-                    (cos(e) * field).dz(x0=z-z.spacing/2))
-
-        update_p = t.spacing**2 * a**2 / f * \
-            (g1_tilde(f * g1(p0, r, e), r, e) +
-             g2_tilde(f * g2(p0, r, e), r, e) +
-             g3_tilde(f * g3(p0, r, e) + f * g3(m0, r, e), r, e)) + \
-            (2 - t.spacing * a)
-
-        update_m = t.spacing**2 * a**2 / f * \
-            (g1_tilde(f * g1(m0, r, e), r, e) +
-             g2_tilde(f * g2(m0, r, e), r, e) +
-             g3_tilde(f * g3(m0, r, e) + f * g3(p0, r, e), r, e)) + \
-            (2 - t.spacing * a)
-
-        eqns = [Eq(p0.forward, update_p),
-                Eq(m0.forward, update_m)]
+        eqns = tti_sa_eqns(grid)
 
         op = Operator(eqns, subs=grid.spacing_map,
-                      opt=('advanced', {'expand': False}))
+                      opt=('advanced', {'expand': False,
+                                        'cire-mingain': 400}))
 
         # Check code generation
         assert op._profiler._sections['section1'].sops == 1442
@@ -203,7 +154,8 @@ class TestBasic(object):
                 Eq(m0.forward, m0.dx.dx + m0.backward)]
 
         op = Operator(eqns, subs=grid.spacing_map,
-                      opt=('advanced', {'expand': False}))
+                      opt=('advanced', {'expand': False,
+                                        'cire-mingain': 200}))
 
         # Check code generation
         assert op._profiler._sections['section0'].sops == 127
@@ -227,10 +179,124 @@ class TestBasic(object):
                 Eq(m0.forward, (m0.dx + s0).dx + f*m0.backward)]
 
         op = Operator(eqns, subs=grid.spacing_map,
-                      opt=('advanced', {'expand': False}))
+                      opt=('advanced', {'expand': False,
+                                        'cire-mingain': 200}))
 
         # Check code generation
         assert op._profiler._sections['section0'].sops == 133
         assert_structure(op, ['t,x,y', 't,x,y,i1', 't,x,y,i1,i0'], 't,x,y,i1,i0')
 
         op.cfunction
+
+
+class Test2Pass(object):
+
+    def test_v0(self):
+        grid = Grid(shape=(10, 10, 10))
+
+        u = TimeFunction(name='u', grid=grid, space_order=4)
+        v = TimeFunction(name='v', grid=grid, space_order=4)
+        u1 = TimeFunction(name='u', grid=grid, space_order=4)
+        v1 = TimeFunction(name='v', grid=grid, space_order=4)
+
+        eqns = [Eq(u.forward, (u.dx.dy + v*u + 1.)),
+                Eq(v.forward, (v + u.dx.dy + 1.))]
+
+        op0 = Operator(eqns)
+        op1 = Operator(eqns, opt=('advanced', {'expand': False,
+                                               'openmp': True}))
+
+        # Check generated code
+        op1._profiler._sections['section0'].sops == 65
+        assert_structure(op1, ['t',
+                               't,x0_blk0,y0_blk0,x,y,z',
+                               't,x0_blk0,y0_blk0,x,y,z,i0',
+                               't,x0_blk0,y0_blk0,x,y,z',
+                               't,x0_blk0,y0_blk0,x,y,z,i1'],
+                         't,x0_blk0,y0_blk0,x,y,z,i0,y,z,i1')
+
+        op0.apply(time_M=5)
+        op1.apply(time_M=5, u=u1, v=v1)
+
+        assert np.allclose(u.data, u1.data, rtol=10e-3)
+        assert np.allclose(v.data, v1.data, rtol=10e-3)
+
+    def test_v1(self):
+        grid = Grid(shape=(16, 16, 16))
+
+        eqns = tti_sa_eqns(grid)
+
+        op = Operator(eqns, subs=grid.spacing_map,
+                      opt=('advanced', {'expand': False,
+                                        'openmp': False}))
+
+        # Check code generation
+        assert op._profiler._sections['section1'].sops == 190
+        assert_structure(op, ['x,y,z',
+                              't,x0_blk0,y0_blk0,x,y,z',
+                              't,x0_blk0,y0_blk0,x,y,z,i0',
+                              't,x0_blk0,y0_blk0,x,y,z',
+                              't,x0_blk0,y0_blk0,x,y,z,i1'],
+                         'x,y,z,t,x0_blk0,y0_blk0,x,y,z,i0,x,y,z,i1')
+
+        op.cfunction
+
+
+def tti_sa_eqns(grid):
+    t = grid.stepping_dim
+    x, y, z = grid.dimensions
+
+    so = 4
+
+    a = Function(name='a', grid=grid, space_order=so)
+    f = Function(name='f', grid=grid, space_order=so)
+    e = Function(name='e', grid=grid, space_order=so)
+    r = Function(name='r', grid=grid, space_order=so)
+    p0 = TimeFunction(name='p0', grid=grid, time_order=2, space_order=so)
+    m0 = TimeFunction(name='m0', grid=grid, time_order=2, space_order=so)
+
+    def g1(field, r, e):
+        return (cos(e) * cos(r) * field.dx(x0=x+x.spacing/2) +
+                cos(e) * sin(r) * field.dy(x0=y+y.spacing/2) -
+                sin(e) * field.dz(x0=z+z.spacing/2))
+
+    def g2(field, r, e):
+        return - (sin(r) * field.dx(x0=x+x.spacing/2) -
+                  cos(r) * field.dy(x0=y+y.spacing/2))
+
+    def g3(field, r, e):
+        return (sin(e) * cos(r) * field.dx(x0=x+x.spacing/2) +
+                sin(e) * sin(r) * field.dy(x0=y+y.spacing/2) +
+                cos(e) * field.dz(x0=z+z.spacing/2))
+
+    def g1_tilde(field, r, e):
+        return ((cos(e) * cos(r) * field).dx(x0=x-x.spacing/2) +
+                (cos(e) * sin(r) * field).dy(x0=y-y.spacing/2) -
+                (sin(e) * field).dz(x0=z-z.spacing/2))
+
+    def g2_tilde(field, r, e):
+        return - ((sin(r) * field).dx(x0=x-x.spacing/2) -
+                  (cos(r) * field).dy(x0=y-y.spacing/2))
+
+    def g3_tilde(field, r, e):
+        return ((sin(e) * cos(r) * field).dx(x0=x-x.spacing/2) +
+                (sin(e) * sin(r) * field).dy(x0=y-y.spacing/2) +
+                (cos(e) * field).dz(x0=z-z.spacing/2))
+
+    update_p = t.spacing**2 * a**2 / f * \
+        (g1_tilde(f * g1(p0, r, e), r, e) +
+         g2_tilde(f * g2(p0, r, e), r, e) +
+         g3_tilde(f * g3(p0, r, e) + f * g3(m0, r, e), r, e)) + \
+        (2 - t.spacing * a)
+
+    update_m = t.spacing**2 * a**2 / f * \
+        (g1_tilde(f * g1(m0, r, e), r, e) +
+         g2_tilde(f * g2(m0, r, e), r, e) +
+         g3_tilde(f * g3(m0, r, e) + f * g3(p0, r, e), r, e)) + \
+        (2 - t.spacing * a)
+
+    eqns = [Eq(p0.forward, update_p),
+            Eq(m0.forward, update_m)]
+
+
+    return eqns
