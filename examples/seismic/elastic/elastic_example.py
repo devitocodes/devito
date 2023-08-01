dcb8ea6bee0b0158e7777bbeb2fcf22db3bf86a2
mloubout <mathias.louboutin@gmail.com>
2023-08-01 13:31:38 -0400

    CI: restore pytest config and options ignored since #2079

--- examples/seismic/elastic/elastic_example.py
+++ examples/seismic/elastic/elastic_example.py
@@ -40,7 +40,7 @@ def test_elastic(dtype):
     assert np.isclose(norm(rec2), 0.627606, atol=1e-3, rtol=0)
 
 
-@pytest.mark.parametrize('shape', [(101,), (51, 51), (16, 16, 16)])
+@pytest.mark.parametrize('shape', [(51, 51), (16, 16, 16)])
 def test_elastic_stability(shape):
     spacing = tuple([20]*len(shape))
     _, _, _, [rec1, rec2, v, tau] = run(shape=shape, spacing=spacing, tn=20000.0, nbl=0)
