baa38fb6946c87e631314e036135c99dca287dcf
Emilien Bauer <bauer.emilien@gmail.com>
2023-08-09 15:43:03 +0100

    Use the boilerplate flag to not always copy to GPU.

--- devito/operator/xdsl_operator.py
+++ devito/operator/xdsl_operator.py
@@ -111,7 +111,7 @@ class XDSLOperator(Operator):
                 raise RuntimeError("Cannot run OMP+GPU!")
 
             # specialize the code for the specific apply parameters
-            finalize_module_with_globals(self._module, self._jit_kernel_constants, gpu_boilerplate=True)
+            finalize_module_with_globals(self._module, self._jit_kernel_constants, gpu_boilerplate=is_gpu)
 
             # print module as IR
             module_str = StringIO()
