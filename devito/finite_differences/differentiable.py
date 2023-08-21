e981c407b50e3f5f06853e8bf3c6945d48ef4b5c
Fabio Luporini <fabio@devitocodes.com>
2023-08-21 07:49:26 +0000

    compiler: Patch Weights __eq__ and hashing

--- devito/finite_differences/differentiable.py
+++ devito/finite_differences/differentiable.py
@@ -613,15 +613,15 @@ class Weights(Array):
 
     def __eq__(self, other):
         return (isinstance(other, Weights) and
-                self.dimension is other.dimension and
                 self.name == other.name and
+                self.dimension == other.dimension and
                 self.indices == other.indices and
                 self.weights == other.weights)
 
     __hash__ = sympy.Basic.__hash__
 
     def _hashable_content(self):
-        return super()._hashable_content() + (self.name,) + tuple(self.weights)
+        return (self.name, self.dimension, hash(tuple(self.weights)))
 
     @property
     def dimension(self):
