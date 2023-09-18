e72f8e9bc86fc40dde5bcb7f8fa803aa486dd795
Fabio Luporini <fabio@devitocodes.com>
2023-09-18 15:02:58 +0000

    compiler: Speedup ordering of IndexDerivatives

--- devito/finite_differences/differentiable.py
+++ devito/finite_differences/differentiable.py
@@ -7,6 +7,7 @@ import numpy as np
 import sympy
 from sympy.core.add import _addsort
 from sympy.core.mul import _keep_coeff, _mulsort
+from sympy.core.core import ordering_of_classes
 from sympy.core.decorators import call_highest_priority
 from sympy.core.evalf import evalf_table
 
@@ -668,6 +669,16 @@ class IndexDerivative(IndexSum):
     def _hashable_content(self):
         return super()._hashable_content() + (self.mapper,)
 
+    def compare(self, other):
+        if self is other:
+            return 0
+        n1 = self.__class__
+        n2 = other.__class__
+        if n1.__name__ == n2.__name__:
+            return self.base.compare(other.base)
+        else:
+            return super().compare(other)
+
     @cached_property
     def base(self):
         return self.expr.func(*[a for a in self.expr.args if a is not self.weights])
@@ -698,6 +709,10 @@ class IndexDerivative(IndexSum):
         expr = expr.xreplace(mapper)
 
         return expr
+
+
+ordering_of_classes.insert(ordering_of_classes.index('Derivative') + 1,
+                           'IndexDerivative')
 
 
 class EvalDerivative(DifferentiableOp, sympy.Add):
