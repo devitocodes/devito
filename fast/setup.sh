081af18b9053f75b88b664234cf86045cf24efc8
Emilien Bauer <bauer.emilien@gmail.com>
2023-10-17 12:14:32 +0100

    setup.sh cleanup and full threads usage.

--- fast/setup.sh
+++ fast/setup.sh
@@ -7,12 +7,8 @@ unset OMP_NUM_THREADS
 
 export OMP_PLACES=cores
 export OMP_PROC_BIND=true
+unset OMP_NUM_THREADS
 
-get_runtime() {
-    $@ |& grep 'Operator.*ran' | rev | cut -d ' ' -f2 | rev
-}
-
-echo bench_name,so,Devito,xDSL
 for bench in "setup_wave2d.py -d 8192 8192 --nt 1024" "setup_wave3d.py -d 512 512 512 --nt 512"
 do
   for so in 2 4 8
