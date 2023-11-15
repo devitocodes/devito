0761781ee7debd695c639959e66932f49c60200c
George Bisbas <g.bisbas18@imperial.ac.uk>
2023-11-15 10:22:16 +0000

    tests: Add tests

--- tests/test_xdsl_base.py
+++ tests/test_xdsl_base.py
@@ -1,12 +1,37 @@
 from devito import Grid, TimeFunction, Eq, XDSLOperator
 
 
-def test_simple_xdsl_operator():
-
+def test_xdsl_I():
     # Define a simple Devito Operator
-    grid = Grid(shape=(3, 3, 3))
+    grid = Grid(shape=(3,))
     u = TimeFunction(name='u', grid=grid)
+
     eq = Eq(u.forward, u + 1)
-    
     op = XDSLOperator([eq], opt=None)
     op.apply(time_M=1)
+    assert (u.data[1, :] == 1.).all()
+    assert (u.data[0, :] == 2.).all()
+
+
+def test_xdsl_II():
+    # Define a simple Devito Operator
+    grid = Grid(shape=(4, 4))
+    u = TimeFunction(name='u', grid=grid)
+
+    eq = Eq(u.forward, u + 1)
+    op = XDSLOperator([eq], opt=None)
+    op.apply(time_M=1)
+    assert (u.data[1, :] == 1.).all()
+    assert (u.data[0, :] == 2.).all()
+
+
+def test_xdsl_III():
+    # Define a simple Devito Operator
+    grid = Grid(shape=(5, 5, 5))
+    u = TimeFunction(name='u', grid=grid)
+
+    eq = Eq(u.forward, u + 1)
+    op = XDSLOperator([eq], opt=None)
+    op.apply(time_M=1)
+    assert (u.data[1, :] == 1.).all()
+    assert (u.data[0, :] == 2.).all()
