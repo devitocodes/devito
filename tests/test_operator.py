758e8baf1633c4758c79781e25916a10764c5f80
George Bisbas <g.bisbas18@imperial.ac.uk>
2023-11-23 17:04:11 +0000

    tests: enhance indirection testing

--- tests/test_operator.py
+++ tests/test_operator.py
@@ -2134,8 +2134,11 @@ class TestLoopScheduling:
 
 class TestInternals:
 
-    def test_indirection(self):
-        nt = 10
+    @pytest.mark.parametrize('nt, offset, epass',
+                             ([1, 1, True], [1, 2, False],
+                              [5, 1, True], [3, 5, False],
+                              [4, 1, True], [5, 10, False]))
+    def test_indirection(self, nt, offset, epass):
         grid = Grid(shape=(4, 4))
         time = grid.time_dim
         x, y = grid.dimensions
@@ -2143,7 +2146,7 @@ class TestInternals:
         f = TimeFunction(name='f', grid=grid, save=nt)
         g = TimeFunction(name='g', grid=grid)
 
-        idx = time + 1
+        idx = time + offset
         s = Indirection(name='ofs0', mapped=idx)
 
         eqns = [
@@ -2154,13 +2157,18 @@ class TestInternals:
         op = Operator(eqns)
 
         assert op._dspace[time].lower == 0
-        assert op._dspace[time].upper == 1
-        assert op.arguments()['time_M'] == nt - 2
+        assert op._dspace[time].upper == offset
 
         op()
 
         assert np.all(f.data[0] == 0.)
         assert np.all(f.data[i] == 3. for i in range(1, 10))
+
+        if epass:
+            assert op.arguments()['time_M'] == nt - offset - 1
+            op()
+            assert np.all(f.data[0] == 0.)
+            assert np.all(f.data[i] == 3. for i in range(1, nt))
 
 
 class TestEstimateMemory:
