a11f49992b76615ab7bdf36a1d94e0b880523acd
Emilien Bauer <bauer.emilien@gmail.com>
2023-12-20 13:53:43 +0000

    Fix diffusion_3D Devito operator.

--- fast/diffusion_2D_wBCs.py
+++ fast/diffusion_2D_wBCs.py
@@ -70,7 +70,7 @@ if args.devito:
     # To measure Devito at its best on GPU, we have to set the tile siwe manually
     opt = 'advanced'
     if configuration['platform'].name == 'nvidiaX':
-        opt = ('xdsl', {'par-tile': (32, 4, 8)})
+        opt = ('advanced', {'par-tile': (32, 4, 8)})
 
     op = Operator([eq_stencil], name='DevitoDiffusionOperator', opt=opt)
     op.apply(time=nt, dt=dt, a=nu)
