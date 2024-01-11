3f9391b9b5354faf8731d06283c392c19a2c7a5d
Edward Caunt <ed@devitocodes.com>
2024-01-11 11:05:28 +0000

    dsl: Work on index_glb_to_loc

--- devito/mpi/distributed.py
+++ devito/mpi/distributed.py
@@ -10,7 +10,7 @@ from cached_property import cached_property
 import numpy as np
 from cgen import Struct, Value
 
-from devito.data import LEFT, CENTER, RIGHT, Decomposition, SubDecomposition
+from devito.data import LEFT, CENTER, RIGHT, Decomposition
 from devito.parameters import configuration
 from devito.tools import EnrichedTuple, as_tuple, ctypes_to_cstr, filter_ordered, frozendict
 from devito.types import CompositeObject, Object
