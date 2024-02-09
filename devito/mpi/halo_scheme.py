89857990e32af43504248b502aaf2ae0bf40d1e3
George Bisbas <g.bisbas18@imperial.ac.uk>
2024-02-09 16:51:37 +0000

    mpi: Merge halowait and haloupdate

--- devito/mpi/halo_scheme.py
+++ devito/mpi/halo_scheme.py
@@ -291,9 +291,7 @@ class HaloScheme:
                  for d in filter_sorted(self.dimensions)]
 
         processed = []
-
         for item in product(*items):
-            print(item)
 
             where = []
             mapper = {}
