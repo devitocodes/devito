89b6fead9a676d109c174c720bda99ef040d8b91
mloubout <mathias.louboutin@gmail.com>
2024-05-31 09:58:54 -0400

    compiler: fix internal language specific types and cast
    wip

--- devito/arch/compiler.py
+++ devito/arch/compiler.py
@@ -252,7 +252,7 @@ class Compiler(GCCToolchain):
     @property
     def _complex_ctype(self):
         """
-        Type definition for complex numbers. THese two cases cover 99% of the cases since
+        Type definition for complex numbers. These two cases cover 99% of the cases since
         - Hip is now using std::complex
 https://rocm.docs.amd.com/en/docs-5.1.3/CHANGELOG.html#hip-api-deprecations-and-warnings
         - Sycl supports std::complex
@@ -1031,6 +1031,7 @@ _compiler_registry = {
     'nvc++': NvidiaCompiler,
     'nvidia': NvidiaCompiler,
     'cuda': CudaCompiler,
+    'nvcc': CudaCompiler,
     'osx': ClangCompiler,
     'intel': OneapiCompiler,
     'icx': OneapiCompiler,
