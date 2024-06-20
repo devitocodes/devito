e8fc1bd0fd0c0c3cfeedda3fc711e41115085aa9
mloubout <mathias.louboutin@gmail.com>
2024-06-20 10:28:38 -0400

    compiler: rework dtype lowering

--- devito/passes/iet/languages/C.py
+++ devito/passes/iet/languages/C.py
@@ -1,9 +1,16 @@
+import numpy as np
+
 from devito.ir import Call
 from devito.passes.iet.definitions import DataManager
 from devito.passes.iet.orchestration import Orchestrator
 from devito.passes.iet.langbase import LangBB
+from devito.tools import CustomNpType
 
 __all__ = ['CBB', 'CDataManager', 'COrchestrator']
+
+
+CCFloat = CustomNpType('_Complex float', np.complex64)
+CCDouble = CustomNpType('_Complex double', np.complex128)
 
 
 class CBB(LangBB):
@@ -19,7 +26,10 @@ class CBB(LangBB):
         'host-free-pin': lambda i:
             Call('free', (i,)),
         'alloc-global-symbol': lambda i, j, k:
-            Call('memcpy', (i, j, k))
+            Call('memcpy', (i, j, k)),
+        # Complex
+        'header-complex': 'complex.h',
+        'types': {np.complex128: CCDouble, np.complex64: CCFloat},
     }
 
 
