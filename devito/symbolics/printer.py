symbolics: fix printer for half precision

devito/symbolics/printer.py
@@ -52,7 +52,14 @@ class CodePrinter(C99CodePrinter):
         if no_f and expr is not None:
             return False
         dtype = sympy_dtype(expr) if expr is not None else self.dtype
-        return any(issubclass(dtype, d) for d in [np.float32, np.float16, np.complex64])
+        return any(issubclass(dtype, d) for d in [np.float32, np.complex64])
+
+    def half_prec(self, expr=None, with_f=False):
+        no_f = self.compiler._cpp and not with_f
+        if no_f and expr is not None:
+            return False
+        dtype = sympy_dtype(expr) if expr is not None else self.dtype
+        return issubclass(dtype, np.float16)
 
     def complex_prec(self, expr=None):
         if self.compiler._cpp:
@@ -126,7 +133,7 @@ class CodePrinter(C99CodePrinter):
         if cname not in self._prec_funcs:
             return super()._print_math_func(expr, nest=nest, known=known)
 
-        if self.single_prec(expr):
+        if self.single_prec(expr) or self.half_prec(expr):
             cname = '%sf' % cname
         if self.complex_prec(expr):
             cname = 'c%s' % cname
@@ -251,6 +258,8 @@ class CodePrinter(C99CodePrinter):
 
         if self.single_prec():
             rv = '%sF' % rv
+        elif self.half_prec():
+            rv = '%sF16' % rv
 
         return rv
 
@@ -258,6 +267,8 @@ class CodePrinter(C99CodePrinter):
         if self.compiler._cpp:
             if self.single_prec(with_f=True):
                 return '1if'
+            elif self.half_prec(with_f=True):
+                return '1if16'
             else:
                 return '1i'
         else:
@@ -315,7 +326,7 @@ class CodePrinter(C99CodePrinter):
     def _print_TrigonometricFunction(self, expr):
         func_name = str(expr.func)
 
-        if self.single_prec():
+        if self.single_prec() or self.half_prec():
             func_name = '%sf' % func_name
         if self.complex_prec():
             func_name = 'c%s' % func_name