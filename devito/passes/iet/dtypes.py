48288504804f9d4eb0fcebe476945d9b6672f4de
enwask <enwask@ucf.edu>
2024-07-11 13:51:55 +0100

    compiler: implement float16 support

--- devito/passes/iet/dtypes.py
+++ devito/passes/iet/dtypes.py
@@ -2,8 +2,44 @@ import numpy as np
 import ctypes
 
 from devito.ir import FindSymbols, Uxreplace
+from devito.ir.iet.nodes import Dereference
+from devito.tools.utils import as_tuple
+from devito.types.basic import Symbol
 
-__all__ = ['lower_complex']
+__all__ = ['lower_scalar_half', 'lower_complex']
+
+
+def lower_scalar_half(iet, lang, sregistry):
+    """
+    Lower half float scalars to pointers (special case, since we can't
+    pass them directly for lack of a ctypes equivalent)
+    """
+    if lang.get('half_types') is None:
+        return iet, {}
+    
+    # dtype mappings for float16
+    half, half_p = lang['half_types']
+
+    body = []  # derefs to prepend to the body
+    body_mapper = {}
+    params_mapper = {}
+
+    for s in FindSymbols('scalars').visit(iet):
+        if s.dtype != np.float16 or s not in iet.parameters:
+            continue
+
+        ptr = s._rebuild(dtype=half_p)
+        val = Symbol(name=sregistry.make_name(prefix='hf'), dtype=half, is_const=True)
+
+        params_mapper[s] = ptr
+        body_mapper[s] = val
+        body.append(Dereference(val, ptr))  # val = *ptr
+
+    body.extend(as_tuple(Uxreplace(body_mapper).visit(iet.body)))
+    params = Uxreplace(params_mapper).visit(iet.parameters)
+
+    iet = iet._rebuild(body=body, parameters=params)
+    return iet, {}
 
 
 def lower_complex(iet, lang, compiler):
@@ -14,30 +50,28 @@ def lower_complex(iet, lang, compiler):
     types = {f.dtype for f in FindSymbols().visit(iet)
              if not issubclass(f.dtype, ctypes._Pointer)}
 
-    if not any(np.issubdtype(d, np.complexfloating) for d in types):
-        return iet, {}
-
-    lib = (lang['header-complex'],)
-
     metadata = {}
-    if lang.get('complex-namespace') is not None:
-        metadata['namespaces'] = lang['complex-namespace']
+    if any(np.issubdtype(d, np.complexfloating) for d in types):
+        lib = (lang['header-complex'],)
 
-    # Some languges such as c++11 need some extra arithmetic definitions
-    if lang.get('def-complex'):
-        dest = compiler.get_jit_dir()
-        hfile = dest.joinpath('complex_arith.h')
-        with open(str(hfile), 'w') as ff:
-            ff.write(str(lang['def-complex']))
-        lib += (str(hfile),)
+        if lang.get('complex-namespace') is not None:
+            metadata['namespaces'] = lang['complex-namespace']
 
-    iet = _complex_dtypes(iet, lang)
-    metadata['includes'] = lib
+        # Some languges such as c++11 need some extra arithmetic definitions
+        if lang.get('def-complex'):
+            dest = compiler.get_jit_dir()
+            hfile = dest.joinpath('complex_arith.h')
+            with open(str(hfile), 'w') as ff:
+                ff.write(str(lang['def-complex']))
+            lib += (str(hfile),)
 
+        metadata['includes'] = lib
+
+    iet = _lower_dtypes(iet, lang)
     return iet, metadata
 
 
-def _complex_dtypes(iet, lang):
+def _lower_dtypes(iet, lang):
     """
     Lower dtypes to language specific types
     """
