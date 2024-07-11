43b82dbae39e409cfe2a1fe3edb79b8f21617618
enwask <enwask@ucf.edu>
2024-07-11 17:57:50 +0100

    compiler: refactor float16 and lower_dtypes

--- devito/passes/iet/dtypes.py
+++ devito/passes/iet/dtypes.py
@@ -3,46 +3,52 @@ import ctypes
 
 from devito.ir import FindSymbols, Uxreplace
 from devito.ir.iet.nodes import Dereference
-from devito.tools.utils import as_tuple
+from devito.tools.utils import as_list
 from devito.types.basic import Symbol
 
-__all__ = ['lower_scalar_half', 'lower_complex']
+__all__ = ['lower_dtypes']
 
 
-def lower_scalar_half(iet, lang, sregistry):
+def lower_dtypes(iet, lang, compiler, sregistry):
     """
-    Lower half float scalars to pointers (special case, since we can't
-    pass them directly for lack of a ctypes equivalent)
+    Lower language-specific dtypes and add headers for complex arithmetic
     """
-    if lang.get('half_types') is None:
-        return iet, {}
+    # Include complex headers if needed (before we replace complex dtypes)
+    metadata = _complex_includes(iet, lang, compiler)
 
-    # dtype mappings for float16
-    half, half_p = lang['half_types']
-
-    body = []  # derefs to prepend to the body
+    body_prefix = []  # Derefs to prepend to the body
     body_mapper = {}
     params_mapper = {}
 
-    for s in FindSymbols('scalars').visit(iet):
-        if s.dtype != np.float16 or s not in iet.parameters:
-            continue
+    # Lower scalar float16s to pointers and dereference them
+    if lang.get('half_types') is not None:
+        half, half_p = lang['half_types']  # dtype mappings for half float
 
-        ptr = s._rebuild(dtype=half_p)
-        val = Symbol(name=sregistry.make_name(prefix='hf'), dtype=half, is_const=True)
+        for s in FindSymbols('scalars').visit(iet):
+            if s.dtype != np.float16 or s not in iet.parameters:
+                continue
 
-        params_mapper[s] = ptr
-        body_mapper[s] = val
-        body.append(Dereference(val, ptr))  # val = *ptr
+            ptr = s._rebuild(dtype=half_p, is_const=True)
+            val = Symbol(name=sregistry.make_name(prefix='hf'), dtype=half,
+                         is_const=s.is_const)
 
-    body.extend(as_tuple(Uxreplace(body_mapper).visit(iet.body)))
+            params_mapper[s], body_mapper[s] = ptr, val
+            body_prefix.append(Dereference(val, ptr))  # val = *ptr
+
+    # Lower remaining language-specific dtypes
+    for s in FindSymbols('indexeds|basics|symbolics').visit(iet):
+        if s.dtype in lang['types'] and s not in params_mapper:
+            body_mapper[s] = params_mapper[s] = s._rebuild(dtype=lang['types'][s.dtype])
+
+    # Apply the dtype replacements
+    body = body_prefix + as_list(Uxreplace(body_mapper).visit(iet.body))
     params = Uxreplace(params_mapper).visit(iet.parameters)
 
     iet = iet._rebuild(body=body, parameters=params)
-    return iet, {}
+    return iet, metadata
 
 
-def lower_complex(iet, lang, compiler):
+def _complex_includes(iet, lang, compiler):
     """
     Add headers for complex arithmetic
     """
@@ -50,39 +56,23 @@ def lower_complex(iet, lang, compiler):
     types = {f.dtype for f in FindSymbols().visit(iet)
              if not issubclass(f.dtype, ctypes._Pointer)}
 
+    if not any(np.issubdtype(d, np.complexfloating) for d in types):
+        return {}
+
     metadata = {}
-    if any(np.issubdtype(d, np.complexfloating) for d in types):
-        lib = (lang['header-complex'],)
+    lib = (lang['header-complex'],)
 
-        if lang.get('complex-namespace') is not None:
-            metadata['namespaces'] = lang['complex-namespace']
+    if lang.get('complex-namespace') is not None:
+        metadata['namespaces'] = lang['complex-namespace']
 
-        # Some languges such as c++11 need some extra arithmetic definitions
-        if lang.get('def-complex'):
-            dest = compiler.get_jit_dir()
-            hfile = dest.joinpath('complex_arith.h')
-            with open(str(hfile), 'w') as ff:
-                ff.write(str(lang['def-complex']))
-            lib += (str(hfile),)
+    # Some languges such as c++11 need some extra arithmetic definitions
+    if lang.get('def-complex'):
+        dest = compiler.get_jit_dir()
+        hfile = dest.joinpath('complex_arith.h')
+        with open(str(hfile), 'w') as ff:
+            ff.write(str(lang['def-complex']))
+        lib += (str(hfile),)
 
-        metadata['includes'] = lib
+    metadata['includes'] = lib
 
-    iet = _lower_dtypes(iet, lang)
-    return iet, metadata
-
-
-def _lower_dtypes(iet, lang):
-    """
-    Lower dtypes to language specific types
-    """
-    mapper = {}
-
-    for s in FindSymbols('indexeds|basics|symbolics').visit(iet):
-        if s.dtype in lang['types']:
-            mapper[s] = s._rebuild(dtype=lang['types'][s.dtype])
-
-    body = Uxreplace(mapper).visit(iet.body)
-    params = Uxreplace(mapper).visit(iet.parameters)
-    iet = iet._rebuild(body=body, parameters=params)
-
-    return iet
+    return metadata
