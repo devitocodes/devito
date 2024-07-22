6bfeb88628ee52b830a378d92332998670df1b25
mloubout <mathias.louboutin@gmail.com>
2024-07-22 10:31:41 -0400

    arch: fix switchconfig with new setup

--- tests/test_arch.py
+++ tests/test_arch.py
@@ -1,6 +1,7 @@
 import pytest
 
-from devito.arch.compiler import sniff_compiler_version, compiler_registry
+from devito import switchconfig, configuration
+from devito.arch.compiler import sniff_compiler_version, compiler_registry, GNUCompiler
 
 
 @pytest.mark.parametrize("cc", [
@@ -15,3 +16,16 @@ def test_sniff_compiler_version(cc):
 @pytest.mark.parametrize("cc", ['gcc-4.9', 'gcc-11', 'gcc', 'gcc-14', 'gcc-123'])
 def test_gcc(cc):
     assert cc in compiler_registry
+
+
+def test_switcharch():
+    old_compiler = configuration['compiler']
+    with switchconfig(compiler='gcc-4.9'):
+        tmp_comp = configuration['compiler']
+        assert isinstance(tmp_comp, GNUCompiler)
+        assert tmp_comp.suffix == '4.9'
+
+    tmp_comp = configuration['compiler']
+    assert isinstance(tmp_comp, old_compiler.__class__)
+    assert old_compiler.suffix == tmp_comp.suffix
+    assert old_compiler.name == tmp_comp.name
