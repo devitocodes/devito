da15832cefcbfe796a8d607e0cb6bd1d902aacde
Edward Caunt <ed@devitocodes.com>
2024-07-30 13:31:04 +0100

    compiler: Remove unneeded machinery from concretize_subdims

--- devito/ir/equations/algorithms.py
+++ devito/ir/equations/algorithms.py
@@ -238,10 +238,6 @@ def _(d, mapper, rebuilt, sregistry):
             # Substitute into condition
             kwargs['condition'] = d.condition.subs(mapper)
 
-    if d._factor is None:
-        # Note: this is needed due to a bug in ConditionalDimension reconstruction
-        kwargs['factor'] = None
-
     if kwargs:
         # Rebuild if parent or condition need replacing
         mapper[d] = d._rebuild(**kwargs)
