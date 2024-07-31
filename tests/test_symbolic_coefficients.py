a14fdae7ec292f7646d9c7ae7df10ccf54e32f5e
mloubout <mathias.louboutin@gmail.com>
2024-07-31 10:14:33 -0400

    api: revamp custom coefficients API to be passed to derivatives directly

--- tests/test_symbolic_coefficients.py
+++ tests/test_symbolic_coefficients.py
@@ -2,9 +2,12 @@ import numpy as np
 import sympy as sp
 import pytest
 
-from devito import (Grid, Function, TimeFunction, Eq, Coefficient, Substitutions,
+from devito import (Grid, Function, TimeFunction, Eq,
                     Dimension, solve, Operator, NODE)
 from devito.finite_differences import Differentiable
+from devito.finite_differences.coefficients import Coefficient, Substitutions
+from devito.finite_differences.finite_difference import _PRECISION
+from devito.symbolics import retrieve_derivatives
 from devito.tools import as_tuple
 from devito.passes.equations.linearity import factorize_derivatives, aggregate_coeffs
 
@@ -16,29 +19,6 @@ class TestSC:
     Class for testing symbolic coefficients functionality
     """
 
-    @pytest.mark.parametrize('order', [2, 6])
-    @pytest.mark.parametrize('stagger', [True, False])
-    def test_default_rules(self, order, stagger):
-        """
-        Test that the default replacement rules return the same
-        as standard FD.
-        """
-        grid = Grid(shape=(20, 20))
-        if stagger:
-            staggered = grid.dimensions[0]
-        else:
-            staggered = None
-        u0 = TimeFunction(name='u', grid=grid, time_order=order, space_order=order,
-                          staggered=staggered)
-        u1 = TimeFunction(name='u', grid=grid, time_order=order, space_order=order,
-                          staggered=staggered, coefficients='symbolic')
-
-        eq0 = Eq(u0.dt-u0.dx)
-        eq1 = Eq(u1.dt-u1.dx)
-
-        assert(eq0.evaluate.evalf(_PRECISION).__repr__() ==
-               eq1.evaluate.evalf(_PRECISION).__repr__())
-
     @pytest.mark.parametrize('expr, sorder, dorder, dim, weights, expected', [
         ('u.dx2', 2, 2, 0, (-0.6, 0.1, 0.6),
          '0.1*u - 0.6*u._subs(x, x - h_x) + 0.6*u._subs(x, x + h_x)'),
@@ -48,7 +28,7 @@ class TestSC:
     def test_coefficients(self, expr, sorder, dorder, dim, weights, expected):
         """Test that custom coefficients return the expected result"""
         grid = Grid(shape=(10, 10))
-        u = Function(name='u', grid=grid, space_order=sorder, coefficients='symbolic')
+        u = Function(name='u', grid=grid, space_order=sorder)
         x, y = grid.dimensions
         h_x, h_y = grid.spacing_symbols  # noqa
 
@@ -59,14 +39,17 @@ class TestSC:
         coeffs = Coefficient(order, u, dim, weights)
 
         eq = Eq(eval(expr), coefficients=Substitutions(coeffs))
+        deriv = retrieve_derivatives(eq.lhs)[0]
+        assert np.all(deriv.weights == weights)
+        
         assert isinstance(eq.lhs, Differentiable)
-        assert sp.simplify(eval(expected) - eq.evaluate.lhs) == 0
+        assert sp.simplify(eval(expected).evalf(_PRECISION) - eq.evaluate.lhs) == 0
 
     def test_function_coefficients(self):
         """Test that custom function coefficients return the expected result"""
         so = 2
         grid = Grid(shape=(4, 4))
-        f0 = TimeFunction(name='f0', grid=grid, space_order=so, coefficients='symbolic')
+        f0 = TimeFunction(name='f0', grid=grid, space_order=so)
         f1 = TimeFunction(name='f1', grid=grid, space_order=so)
         x, y = grid.dimensions
 
@@ -86,11 +69,7 @@ class TestSC:
         w.data[:, :, 1] = -1.0/grid.spacing[0]
         w.data[:, :, 2] = 1.0/grid.spacing[0]
 
-        f_x_coeffs = Coefficient(1, f0, x, w)
-
-        subs = Substitutions(f_x_coeffs)
-
-        eq0 = Eq(f0.dt + f0.dx, 1, coefficients=subs)
+        eq0 = Eq(f0.dt + f0.dx(weights=w), 1)
         eq1 = Eq(f1.dt + f1.dx, 1)
 
         stencil0 = solve(eq0.evaluate, f0.forward)
@@ -107,147 +86,21 @@ class TestSC:
     def test_coefficients_w_xreplace(self):
         """Test custom coefficients with an xreplace before they are applied"""
         grid = Grid(shape=(4, 4))
-        u = Function(name='u', grid=grid, space_order=2, coefficients='symbolic')
+        u = Function(name='u', grid=grid, space_order=2)
         x = grid.dimensions[0]
         h_x = x.spacing
 
         dorder = 2
         weights = np.array([-0.6, 0.1, 0.6])
 
-        coeffs = Coefficient(dorder, u, x, weights)
-
         c = sp.Symbol('c')
 
-        eq = Eq(u.dx2+c, coefficients=Substitutions(coeffs))
+        eq = Eq(u.dx2(weights=weights) + c)
         eq = eq.xreplace({c: 2})
 
         expected = 0.1*u - 0.6*u._subs(x, x - h_x) + 0.6*u.subs(x, x + h_x) + 2
 
-        assert sp.simplify(expected - eq.evaluate.lhs) == 0
-
-    @pytest.mark.parametrize('order', [1, 2, 6, 8])
-    @pytest.mark.parametrize('extent', [1., 10., 100.])
-    @pytest.mark.parametrize('conf', [{'l': 'NODE', 'r1': 'x', 'r2': None},
-                                      {'l': 'NODE', 'r1': 'y', 'r2': None},
-                                      {'l': 'NODE', 'r1': '(x, y)', 'r2': None},
-                                      {'l': 'x', 'r1': 'NODE', 'r2': None},
-                                      {'l': 'y', 'r1': 'NODE', 'r2': None},
-                                      {'l': '(x, y)', 'r1': 'NODE', 'r2': None},
-                                      {'l': 'NODE', 'r1': 'x', 'r2': 'y'}])
-    def test_default_rules_vs_standard(self, order, extent, conf):
-        """
-        Test that equations containing default symbolic coefficients evaluate to
-        the same expressions as standard coefficients for the same function.
-        """
-        def function_setup(name, grid, order, stagger):
-            x, y = grid.dimensions
-            if stagger == 'NODE':
-                staggered = NODE
-            elif stagger == 'x':
-                staggered = x
-            elif stagger == 'y':
-                staggered = y
-            elif stagger == '(x, y)':
-                staggered = (x, y)
-            else:
-                raise ValueError("Invalid stagger in configuration")
-
-            f_std = Function(name=name, grid=grid, space_order=order,
-                             staggered=staggered)
-            f_sym = Function(name=name, grid=grid, space_order=order,
-                             staggered=staggered, coefficients='symbolic')
-
-            return f_std, f_sym
-
-        def get_eq(u, a, b, conf):
-            if conf['l'] == 'x' or conf['r1'] == 'x':
-                a_deriv = a.dx
-            elif conf['l'] == 'y' or conf['r1'] == 'y':
-                a_deriv = a.dy
-            elif conf['l'] == '(x, y)' or conf['r1'] == '(x, y)':
-                a_deriv = a.dx + a.dy
-            else:
-                raise ValueError("Invalid configuration")
-
-            if conf['r2'] == 'y':
-                b_deriv = b.dy
-            elif conf['r2'] == '(x, y)':
-                b_deriv = b.dx + b.dy
-            elif conf['r2'] is None:
-                b_deriv = 0.
-            else:
-                raise ValueError("Invalid configuration")
-
-            return Eq(u, a_deriv + b_deriv)
-
-        grid = Grid(shape=(11, 11), extent=(extent, extent))
-
-        # Set up functions as specified
-        u_std, u_sym = function_setup('u', grid, order, conf['l'])
-        a_std, a_sym = function_setup('a', grid, order, conf['r1'])
-        a_std.data[::2, ::2] = 1.
-        a_sym.data[::2, ::2] = 1.
-        if conf['r2'] is not None:
-            b_std, b_sym = function_setup('b', grid, order, conf['r2'])
-            b_std.data[::2, ::2] = 1.
-            b_sym.data[::2, ::2] = 1.
-        else:
-            b_std, b_sym = 0., 0.
-
-        eq_std = get_eq(u_std, a_std, b_std, conf)
-        eq_sym = get_eq(u_sym, a_sym, b_sym, conf)
-
-        evaluated_std = eq_std.evaluate.evalf(_PRECISION)
-        evaluated_sym = eq_sym.evaluate.evalf(_PRECISION)
-
-        assert str(evaluated_std) == str(evaluated_sym)
-
-        Operator(eq_std)()
-        Operator(eq_sym)()
-
-        assert np.all(np.isclose(u_std.data - u_sym.data, 0.0, atol=1e-5, rtol=0))
-
-    @pytest.mark.parametrize('so, expected', [(1, '-f(x)/h_x + f(x + h_x)/h_x'),
-                                              (4, '-9*f(x)/(8*h_x) + f(x - h_x)'
-                                                  '/(24*h_x) + 9*f(x + h_x)/(8*h_x)'
-                                                  ' - f(x + 2*h_x)/(24*h_x)'),
-                                              (6, '-75*f(x)/(64*h_x)'
-                                                  ' - 3*f(x - 2*h_x)/(640*h_x)'
-                                                  ' + 25*f(x - h_x)/(384*h_x)'
-                                                  ' + 75*f(x + h_x)/(64*h_x)'
-                                                  ' - 25*f(x + 2*h_x)/(384*h_x)'
-                                                  ' + 3*f(x + 3*h_x)/(640*h_x)')])
-    def test_default_rules_vs_string(self, so, expected):
-        """
-        Test that default_rules generates correct symbolic expressions when used
-        with staggered grids.
-        """
-        grid = Grid(shape=(11,), extent=(10.,))
-        x = grid.dimensions[0]
-        f = Function(name='f', grid=grid, space_order=so, staggered=NODE,
-                     coefficients='symbolic')
-        g = Function(name='g', grid=grid, space_order=so, staggered=x,
-                     coefficients='symbolic')
-        eq = Eq(g, f.dx)
-        assert str(eq.evaluate.rhs) == expected
-
-    @pytest.mark.parametrize('so', [1, 4, 6])
-    @pytest.mark.parametrize('offset', [1, -1])
-    def test_default_rules_deriv_offset(self, so, offset):
-        """
-        Test that default_rules generates correct derivatives when derivatives
-        are evaluated at offset x0.
-        """
-        grid = Grid(shape=(11,), extent=(10.,))
-        x = grid.dimensions[0]
-        h_x = x.spacing
-        f_std = Function(name='f', grid=grid, space_order=so)
-        f_sym = Function(name='f', grid=grid, space_order=so, coefficients='symbolic')
-        g = Function(name='g', grid=grid, space_order=so)
-
-        eval_std = str(Eq(g, f_std.dx(x0=x+offset*h_x/2)).evaluate.evalf(_PRECISION))
-        eval_sym = str(Eq(g, f_sym.dx(x0=x+offset*h_x/2)).evaluate.evalf(_PRECISION))
-        assert eval_std == eval_sym
+        assert sp.simplify(expected.evalf(_PRECISION) - eq.evaluate.lhs) == 0
 
     @pytest.mark.parametrize('order', [2, 4, 6])
     def test_staggered_array(self, order):
@@ -255,19 +108,15 @@ class TestSC:
         grid = Grid(shape=(11,), extent=(10.,))
         x = grid.dimensions[0]
 
-        f = Function(name='f', grid=grid, space_order=order,
-                     coefficients='symbolic')
-        g = Function(name='g', grid=grid, space_order=order,
-                     coefficients='symbolic', staggered=x)
+        f = Function(name='f', grid=grid, space_order=order)
+        g = Function(name='g', grid=grid, space_order=order, staggered=x)
         f.data[::2] = 1
         g.data[::2] = 1
 
         weights = np.ones(order+1)/grid.spacing[0]**2
-        coeffs_f = Coefficient(2, f, x, weights)
-        coeffs_g = Coefficient(2, g, x, weights)
 
-        eq_f = Eq(f, f.dx2, coefficients=Substitutions(coeffs_f))
-        eq_g = Eq(g, g.dx2, coefficients=Substitutions(coeffs_g))
+        eq_f = Eq(f, f.dx2(weights=weights))
+        eq_g = Eq(g, g.dx2(weights=weights))
 
         op = Operator([eq_f, eq_g])
         op()
@@ -280,10 +129,8 @@ class TestSC:
         grid = Grid(shape=(11,), extent=(10.,))
         x = grid.dimensions[0]
 
-        f = Function(name='f', grid=grid, space_order=order,
-                     coefficients='symbolic')
-        g = Function(name='g', grid=grid, space_order=order,
-                     coefficients='symbolic', staggered=x)
+        f = Function(name='f', grid=grid, space_order=order)
+        g = Function(name='g', grid=grid, space_order=order, staggered=x)
         f.data[::2] = 1
         g.data[::2] = 1
 
@@ -296,11 +143,8 @@ class TestSC:
         w = Function(name='w', dimensions=wdims, shape=wshape)
         w.data[:] = 1.0/grid.spacing[0]**2
 
-        coeffs_f = Coefficient(2, f, x, w)
-        coeffs_g = Coefficient(2, g, x, w)
-
-        eq_f = Eq(f, f.dx2, coefficients=Substitutions(coeffs_f))
-        eq_g = Eq(g, g.dx2, coefficients=Substitutions(coeffs_g))
+        eq_f = Eq(f, f.dx2(weights=w))
+        eq_g = Eq(g, g.dx2(weights=w))
 
         Operator([eq_f, eq_g])()
 
@@ -310,18 +154,15 @@ class TestSC:
         grid = Grid(shape=(11,), extent=(10.,))
         x = grid.dimensions[0]
 
-        f = Function(name='f', grid=grid, space_order=2,
-                     coefficients='symbolic')
-        g = Function(name='g', grid=grid, space_order=2,
-                     coefficients='symbolic', staggered=x)
+        f = Function(name='f', grid=grid, space_order=2)
+        g = Function(name='g', grid=grid, space_order=2, staggered=x)
 
-        w = Function(name='w', space_order=0, shape=(3,),
+        w = Function(name='w', space_order=0, shape=(2,),
                      dimensions=(Dimension("p"),))
-        coeffs_g = Coefficient(2, g, x, w)
 
-        eq_fg = Eq(f, g.dx2, coefficients=Substitutions(coeffs_g))
+        eq_fg = Eq(f, g.dx2(weights=w))
 
-        expected = 'Eq(f(x), g(x - h_x/2)*w[0] + g(x + h_x/2)*w[1])'
+        expected = 'Eq(f(x), g(x - h_x/2)*w(0) + g(x + h_x/2)*w(1))'
 
         assert str(eq_fg.evaluate) == expected
 
@@ -333,15 +174,13 @@ class TestSC:
         grid = Grid(shape=(11,), extent=(10.,))
         x = grid.dimensions[0]
 
-        f = Function(name='f', grid=grid, space_order=2,
-                     coefficients='symbolic', staggered=x)
+        f = Function(name='f', grid=grid, space_order=2, staggered=x)
 
         weights = np.array([1, -2, 1])/grid.spacing[0]**2
-        coeffs_f = Coefficient(2, f, x, weights)
 
-        eq_f = Eq(f, f.dx2, coefficients=Substitutions(coeffs_f))
+        eq_f = Eq(f, f.dx2(weights=weights))
 
-        expected = 'Eq(f(x + h_x/2), f(x - h_x/2) - 2.0*f(x + h_x/2) + f(x + 3*h_x/2))'
+        expected = 'Eq(f(x + h_x/2), 1.0*f(x - h_x/2) - 2.0*f(x + h_x/2) + 1.0*f(x + 3*h_x/2))'
         assert(str(eq_f.evaluate) == expected)
 
     @pytest.mark.parametrize('stagger', [True, False])
@@ -355,96 +194,31 @@ class TestSC:
             staggered = None
 
         f = TimeFunction(name='f', grid=grid, space_order=2, staggered=staggered)
-        g = TimeFunction(name='g', grid=grid, space_order=2, staggered=staggered,
-                         coefficients='symbolic')
+        g = TimeFunction(name='g', grid=grid, space_order=2, staggered=staggered)
 
         f.data[:, ::2] = 1
         g.data[:, ::2] = 1
 
         weights = np.array([-1, 2, -1])/grid.spacing[0]**2
-        coeffs = Coefficient(2, g, x, weights)
 
         eq_f = Eq(f.forward, f.dx2)
-        eq_g = Eq(g.forward, g.dx2, coefficients=Substitutions(coeffs))
+        eq_g = Eq(g.forward, g.dx2(weights=weights))
 
         Operator([eq_f, eq_g])(t_m=0, t_M=1)
 
         assert np.allclose(f.data[-1], -g.data[-1], atol=1e-7)
-
-    def test_collect_w_custom_coeffs(self):
-        grid = Grid(shape=(11, 11, 11))
-        p = TimeFunction(name='p', grid=grid, space_order=8, time_order=2,
-                         coefficients='symbolic')
-
-        q = TimeFunction(name='q', grid=grid, space_order=8, time_order=2,
-                         coefficients='symbolic')
-
-        expr = p.dx2 + q.dx2
-        collected = factorize_derivatives(expr)
-        assert collected == expr
-        assert collected.is_Add
-        Operator([Eq(p.forward, expr)])(time_M=2)  # noqa
-
-    def test_aggregate_w_custom_coeffs(self):
-        grid = Grid(shape=(11, 11, 11))
-        q = TimeFunction(name='q', grid=grid, space_order=8, time_order=2,
-                         coefficients='symbolic')
-
-        expr = 0.5 * q.dx2
-        aggregated = aggregate_coeffs(expr, {})
-
-        assert aggregated == expr
-        assert aggregated.is_Mul
-        assert aggregated.args[0] == .5
-        assert aggregated.args[1] == q.dx2
-
-        Operator([Eq(q.forward, expr)])(time_M=2)  # noqa
-
-    def test_cross_derivs(self):
-        grid = Grid(shape=(11, 11, 11))
-
-        q = TimeFunction(name='q', grid=grid, space_order=8, time_order=2,
-                         coefficients='symbolic')
-        q0 = TimeFunction(name='q', grid=grid, space_order=8, time_order=2)
-
-        eq0 = Eq(q0.forward, q0.dx.dy)
-        eq1 = Eq(q.forward, q.dx.dy)
-
-        assert(eq0.evaluate.evalf(_PRECISION).__repr__() ==
-               eq1.evaluate.evalf(_PRECISION).__repr__())
-
-    def test_cross_derivs_imperfect(self):
-        grid = Grid(shape=(11, 11, 11))
-
-        p = TimeFunction(name='p', grid=grid, space_order=4, time_order=2,
-                         coefficients='symbolic')
-        q = TimeFunction(name='q', grid=grid, space_order=4, time_order=2,
-                         coefficients='symbolic')
-
-        p0 = TimeFunction(name='p', grid=grid, space_order=4, time_order=2)
-        q0 = TimeFunction(name='q', grid=grid, space_order=4, time_order=2)
-
-        eq0 = Eq(q0.forward, (q0.dx + p0.dx).dy)
-        eq1 = Eq(q.forward, (q.dx + p.dx).dy)
-
-        assert(eq0.evaluate.evalf(_PRECISION).__repr__() ==
-               eq1.evaluate.evalf(_PRECISION).__repr__())
 
     def test_nested_subs(self):
         grid = Grid(shape=(11, 11))
         x, y = grid.dimensions
         hx, hy = grid.spacing_symbols
 
-        p = TimeFunction(name='p', grid=grid, space_order=2,
-                         coefficients='symbolic')
+        p = TimeFunction(name='p', grid=grid, space_order=2)
 
         coeffs0 = np.array([100, 100, 100])
         coeffs1 = np.array([200, 200, 200])
 
-        subs = Substitutions(Coefficient(2, p, x, coeffs0),
-                             Coefficient(2, p, y, coeffs1))
-
-        eq = Eq(p.forward, p.dx2.dy2, coefficients=subs)
+        eq = Eq(p.forward, p.dx2(weights=coeffs0).dy2(weights=coeffs1))
 
         mul = lambda e: sp.Mul(e, 200, evaluate=False)
         term0 = mul(p*100 +
@@ -466,14 +240,11 @@ class TestSC:
         hx, = grid.spacing_symbols
 
         f = Function(name='f', grid=grid, space_order=2)
-        p = TimeFunction(name='p', grid=grid, space_order=2,
-                         coefficients='symbolic')
+        p = TimeFunction(name='p', grid=grid, space_order=2)
 
         coeffs0 = np.array([100, 100, 100])
 
-        subs = Substitutions(Coefficient(2, p, x, coeffs0))
-
-        eq = Eq(p.forward, (f*p).dx2, coefficients=subs)
+        eq = Eq(p.forward, (f*p).dx2(weights=coeffs0))
 
         term0 = f*p*100
         term1 = (f*p*100).subs(x, x-hx)
@@ -488,16 +259,12 @@ class TestSC:
         hx, hy = grid.spacing_symbols
 
         f = Function(name='f', grid=grid, space_order=2)
-        p = TimeFunction(name='p', grid=grid, space_order=2,
-                         coefficients='symbolic')
+        p = TimeFunction(name='p', grid=grid, space_order=2)
 
         coeffs0 = np.array([100, 100, 100])
         coeffs1 = np.array([200, 200, 200])
 
-        subs = Substitutions(Coefficient(2, p, x, coeffs0),
-                             Coefficient(2, p, y, coeffs1))
-
-        eq = Eq(p.forward, (f*p.dx2).dy2, coefficients=subs)
+        eq = Eq(p.forward, (f*p.dx2(weights=coeffs0)).dy2(weights=coeffs1))
 
         mul = lambda e, i: sp.Mul(f.subs(y, y+i*hy), e, 200, evaluate=False)
         term0 = mul(p*100 +
@@ -512,10 +279,3 @@ class TestSC:
 
         # `str` simply because some objects are of type EvalDerivative
         assert sp.simplify(eq.evaluate.rhs - (term0 + term1 + term2)) == 0
-
-    def test_priority(self):
-        grid = Grid(shape=(11,))
-        m = Function(name='m', grid=grid, space_order=2, coefficients='symbolic')
-        p = Function(name='p', grid=grid, space_order=2)
-
-        assert (p*m).coefficients == 'taylor'
