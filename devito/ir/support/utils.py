7232dfca6742f7661084fed2470e94db9b73091e
Fabio Luporini <fabio@devitocodes.com>
2024-08-20 14:32:02 +0000

    compiler: Relax detect_accesses

--- devito/ir/support/utils.py
+++ devito/ir/support/utils.py
@@ -186,7 +186,11 @@ def detect_accesses(exprs):
     other_dims = set()
     for e in as_tuple(exprs):
         other_dims.update(i for i in e.free_symbols if isinstance(i, Dimension))
-        other_dims.update(e.implicit_dims or {})
+        try:
+            other_dims.update(e.implicit_dims or {})
+        except AttributeError:
+            # Not a types.Eq
+            pass
     other_dims = filter_sorted(other_dims)
     mapper[None] = Stencil([(i, 0) for i in other_dims])
 
