9d04368cd63b5ea8d2624b188183b897aa482ccc
Fabio Luporini <fabio@devitocodes.com>
2024-08-29 08:29:51 +0000

    compiler: Fixup symbol caching

--- devito/finite_differences/tools.py
+++ devito/finite_differences/tools.py
@@ -223,7 +223,7 @@ def make_stencil_dimension(expr, _min, _max):
     Create a StencilDimension for `expr` with unique name.
     """
     n = len(expr.find(StencilDimension))
-    return StencilDimension(name='i%d' % n, _min=_min, _max=_max)
+    return StencilDimension('i%d' % n, _min, _max)
 
 
 @cacheit
--- devito/types/basic.py
+++ devito/types/basic.py
@@ -1,4 +1,5 @@
 import abc
+import inspect
 from collections import namedtuple
 from ctypes import POINTER, _Pointer, c_char_p, c_char
 from functools import reduce, cached_property
@@ -489,6 +490,11 @@ class Symbol(AbstractSymbol, Cached):
 
         # From the kwargs
         key.update(kwargs)
+
+        # Any missing __rkwargs__ along with their default values
+        params = inspect.signature(cls.__init_finalize__).parameters
+        missing = [i for i in cls.__rkwargs__ if i in set(params).difference(key)]
+        key.update({i: params[i].default for i in missing})
 
         return frozendict(key)
 
