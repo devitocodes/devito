b7de2a08b05d89f788febb40118ca5540dd3df10
George Bisbas <g.bisbas18@imperial.ac.uk>
2024-09-17 13:58:44 +0300

    rebase: fix

--- devito/operator/operator.py
+++ devito/operator/operator.py
@@ -13,7 +13,7 @@ from devito.data import default_allocator
 from devito.exceptions import InvalidOperator, ExecutionError
 from devito.logger import (debug, info, perf, warning, is_log_enabled_for, set_log_level,
                            switch_log_level)
-from devito.ir.equations import LoweredEq, lower_exprs
+from devito.ir.equations import LoweredEq, lower_exprs, concretize_subdims
 from devito.ir.clusters import ClusterGroup, clusterize
 from devito.ir.iet import (Callable, CInterface, EntryFunction, FindSymbols, MetaCall,
                            derive_parameters, iet_build)
