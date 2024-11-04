38e667b582e80190677e72aac8025665613baf58
George Bisbas <g.bisbas18@imperial.ac.uk>
2024-11-04 19:55:30 +0200

    examples: Update with edge case of 03_PML notebook

--- devito/ir/iet/nodes.py
+++ devito/ir/iet/nodes.py
@@ -1460,7 +1460,6 @@ class HaloSpot(Node):
         fstrings = []
         for f in self.functions:
             loc_indices = set().union(*[self.halo_scheme.fmapper[f].loc_indices.values()])
-            loc_indices = list(loc_indices)
             loc_indices_str = str(list(loc_indices)) if loc_indices else ""
 
             fstrings.append(f"{f.name}{loc_indices_str}")
