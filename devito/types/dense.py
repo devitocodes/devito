5076ba10900e69880c11bb8a7a050b9309cb53e0
Fabio Luporini <fabio@devitocodes.com>
2024-12-17 08:19:44 +0000

    misc: Turn not-enough-memory warning into a MemoryError

--- devito/types/dense.py
+++ devito/types/dense.py
@@ -1374,10 +1374,10 @@ class TimeFunction(Function):
         available_mem = virtual_memory().available
         required_mem = np.dtype(self.dtype).itemsize * self.size
         if required_mem > available_mem:
-            warning("Trying to allocate more memory (%s) "
-                    % humanbytes(required_mem) + "for symbol %s " % self.name +
-                    "than available (%s) " % humanbytes(available_mem) +
-                    "on physical device, this will start swapping")
+            raise MemoryError(
+                "Trying to allocate more memory (%s) for `%s` than available (%s)"
+                % (humanbytes(required_mem), self.name, humanbytes(available_mem))
+            )
         if not isinstance(self.time_order, int):
             raise TypeError("`time_order` must be int")
 
