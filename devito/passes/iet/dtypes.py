98a9719ec8d5aed9502f01f352778c19079e7ee7
mloubout <mathias.louboutin@gmail.com>
2025-01-15 16:04:01 -0500

    compiler: process dtypes through printer

--- devito/passes/iet/dtypes.py
+++ devito/passes/iet/dtypes.py
@@ -2,11 +2,8 @@ import ctypes
 import numpy as np
 
 from devito.arch.compiler import Compiler
-from devito.ir import Callable, Dereference, FindSymbols, Node, SymbolRegistry, Uxreplace
+from devito.ir import Callable, FindSymbols, SymbolRegistry
 from devito.passes.iet.langbase import LangBB
-from devito.symbolics.extended_dtypes import Float16P
-from devito.tools import as_list
-from devito.types.basic import AbstractSymbol, Basic, Symbol
 
 __all__ = ['lower_dtypes']
 
@@ -17,34 +14,9 @@ def lower_dtypes(iet: Callable, lang: type[LangBB], compiler: Compiler,
     Lowers float16 scalar types to pointers since we can't directly pass their
     value. Also includes headers for complex arithmetic if needed.
     """
-
+    # Complex numbers
     iet, metadata = _complex_includes(iet, lang, compiler)
 
-    # Lower float16 parameters to pointers and dereference
-    prefix: list[Node] = []
-    params_mapper: dict[AbstractSymbol, AbstractSymbol] = {}
-    body_mapper: dict[AbstractSymbol, Symbol] = {}
-
-    params_set = set(iet.parameters)
-    s: AbstractSymbol
-    for s in FindSymbols('abstractsymbols').visit(iet):
-        if s.dtype != np.float16 or s not in params_set:
-            continue
-
-        # Replace the parameter with a pointer; replace occurences in the IET
-        # body with dereferenced symbol (using the original symbol's dtype)
-        ptr: AbstractSymbol = s._rebuild(dtype=Float16P, is_const=True)
-        val = Symbol(name=sregistry.make_name(prefix='hf'), dtype=s.dtype,
-                     is_const=s.is_const)
-
-        params_mapper[s], body_mapper[s] = ptr, val
-        prefix.append(Dereference(val, ptr))  # val = *ptr
-
-    # Apply the replacements
-    prefix.extend(as_list(Uxreplace(body_mapper).visit(iet.body)))
-    params: tuple[Basic] = Uxreplace(params_mapper).visit(iet.parameters)
-
-    iet = iet._rebuild(body=prefix, parameters=params)
     return iet, metadata
 
 
