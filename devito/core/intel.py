03cf03a58df9d9ffb72ef58b6a719fb76da2a4ec
mloubout <mathias.louboutin@gmail.com>
2025-01-27 10:41:47 -0500

    compiler: add all cxx target to operator registry

--- devito/core/intel.py
+++ devito/core/intel.py
@@ -1,11 +1,18 @@
 from devito.core.cpu import (Cpu64AdvCOperator, Cpu64AdvOmpOperator,
-                             Cpu64FsgCOperator, Cpu64FsgOmpOperator)
+                             Cpu64FsgCOperator, Cpu64FsgOmpOperator,
+                             Cpu64AdvCXXOperator, Cpu64CXXAdvOmpOperator,
+                             Cpu64CXXFsgCOperator, Cpu64CXXFsgOmpOperator)
 
 __all__ = ['Intel64AdvCOperator', 'Intel64AdvOmpOperator', 'Intel64FsgCOperator',
-           'Intel64FsgOmpOperator']
+           'Intel64FsgOmpOperator', 'Intel64CXXAdvCOperator', 'Intel64CXXAdvOmpOperator',
+           'Intel64CXXFsgCOperator', 'Intel64CXXFsgOmpOperator']
 
 
 Intel64AdvCOperator = Cpu64AdvCOperator
 Intel64AdvOmpOperator = Cpu64AdvOmpOperator
 Intel64FsgCOperator = Cpu64FsgCOperator
 Intel64FsgOmpOperator = Cpu64FsgOmpOperator
+Intel64CXXAdvCOperator = Cpu64AdvCXXOperator
+Intel64CXXAdvOmpOperator = Cpu64CXXAdvOmpOperator
+Intel64CXXFsgCOperator = Cpu64CXXFsgCOperator
+Intel64CXXFsgOmpOperator = Cpu64CXXFsgOmpOperator
