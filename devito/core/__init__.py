52fd61b6f68dd45bce451530329edac44b2f22eb
mloubout <mathias.louboutin@gmail.com>
2025-01-28 09:33:44 -0500

    compiler: cleanup operator class names

--- devito/core/__init__.py
+++ devito/core/__init__.py
@@ -2,19 +2,19 @@ from devito.arch import Cpu64, Intel64, Arm, Power, Device
 from devito.core.cpu import (Cpu64NoopCOperator, Cpu64NoopOmpOperator,
                              Cpu64AdvCOperator, Cpu64AdvOmpOperator,
                              Cpu64FsgCOperator, Cpu64FsgOmpOperator,
-                             Cpu64CustomOperator, Cpu64CXXCustomOperator,
+                             Cpu64CustomOperator, Cpu64CustomCXXOperator,
                              Cpu64CXXNoopCOperator, Cpu64CXXNoopOmpOperator,
-                             Cpu64AdvCXXOperator, Cpu64CXXAdvOmpOperator,
-                             Cpu64CXXFsgCOperator, Cpu64CXXFsgOmpOperator)
+                             Cpu64AdvCXXOperator, Cpu64AdvCXXOmpOperator,
+                             Cpu64FsgCXXOperator, Cpu64FsgCXXOmpOperator)
 
 from devito.core.intel import (Intel64AdvCOperator, Intel64AdvOmpOperator,
                                Intel64FsgCOperator, Intel64FsgOmpOperator,
-                               Intel64CXXAdvCOperator, Intel64CXXAdvOmpOperator,
-                               Intel64CXXFsgCOperator, Intel64CXXFsgOmpOperator)
+                               Intel64CXXAdvCOperator, Intel64AdvCXXOmpOperator,
+                               Intel64FsgCXXOperator, Intel64FsgCXXOmpOperator)
 from devito.core.arm import (ArmAdvCOperator, ArmAdvOmpOperator,
-                             ArmCXXAdvCOperator, ArmCXXAdvOmpOperator)
+                             ArmAdvCXXOperator, ArmAdvCXXOmpOperator)
 from devito.core.power import (PowerAdvCOperator, PowerAdvOmpOperator,
-                               PowerCXXAdvCOperator, PowerCXXAdvOmpOperator)
+                               PowerCXXAdvCOperator, PowerAdvCXXOmpOperator)
 from devito.core.gpu import (DeviceNoopOmpOperator, DeviceNoopAccOperator,
                              DeviceAdvOmpOperator, DeviceAdvAccOperator,
                              DeviceFsgOmpOperator, DeviceFsgAccOperator,
@@ -24,8 +24,8 @@ from devito.operator.registry import operator_registry
 # Register CPU Operators
 operator_registry.add(Cpu64CustomOperator, Cpu64, 'custom', 'C')
 operator_registry.add(Cpu64CustomOperator, Cpu64, 'custom', 'openmp')
-operator_registry.add(Cpu64CXXCustomOperator, Cpu64, 'custom', 'CXX')
-operator_registry.add(Cpu64CXXCustomOperator, Cpu64, 'custom', 'CXXopenmp')
+operator_registry.add(Cpu64CustomCXXOperator, Cpu64, 'custom', 'CXX')
+operator_registry.add(Cpu64CustomCXXOperator, Cpu64, 'custom', 'CXXopenmp')
 
 operator_registry.add(Cpu64NoopCOperator, Cpu64, 'noop', 'C')
 operator_registry.add(Cpu64NoopOmpOperator, Cpu64, 'noop', 'openmp')
@@ -35,32 +35,32 @@ operator_registry.add(Cpu64CXXNoopOmpOperator, Cpu64, 'noop', 'CXXopenmp')
 operator_registry.add(Cpu64AdvCOperator, Cpu64, 'advanced', 'C')
 operator_registry.add(Cpu64AdvOmpOperator, Cpu64, 'advanced', 'openmp')
 operator_registry.add(Cpu64AdvCXXOperator, Cpu64, 'advanced', 'CXX')
-operator_registry.add(Cpu64CXXAdvOmpOperator, Cpu64, 'advanced', 'CXXopenmp')
+operator_registry.add(Cpu64AdvCXXOmpOperator, Cpu64, 'advanced', 'CXXopenmp')
 
 operator_registry.add(Cpu64FsgCOperator, Cpu64, 'advanced-fsg', 'C')
 operator_registry.add(Cpu64FsgOmpOperator, Cpu64, 'advanced-fsg', 'openmp')
-operator_registry.add(Cpu64CXXFsgCOperator, Cpu64, 'advanced-fsg', 'CXX')
-operator_registry.add(Cpu64CXXFsgOmpOperator, Cpu64, 'advanced-fsg', 'CXXopenmp')
+operator_registry.add(Cpu64FsgCXXOperator, Cpu64, 'advanced-fsg', 'CXX')
+operator_registry.add(Cpu64FsgCXXOmpOperator, Cpu64, 'advanced-fsg', 'CXXopenmp')
 
 operator_registry.add(Intel64AdvCOperator, Intel64, 'advanced', 'C')
 operator_registry.add(Intel64AdvOmpOperator, Intel64, 'advanced', 'openmp')
 operator_registry.add(Intel64CXXAdvCOperator, Intel64, 'advanced', 'CXX')
-operator_registry.add(Intel64CXXAdvOmpOperator, Intel64, 'advanced', 'CXXopenmp')
+operator_registry.add(Intel64AdvCXXOmpOperator, Intel64, 'advanced', 'CXXopenmp')
 
 operator_registry.add(Intel64FsgCOperator, Intel64, 'advanced-fsg', 'C')
 operator_registry.add(Intel64FsgOmpOperator, Intel64, 'advanced-fsg', 'openmp')
-operator_registry.add(Intel64CXXFsgCOperator, Intel64, 'advanced-fsg', 'CXX')
-operator_registry.add(Intel64CXXFsgOmpOperator, Intel64, 'advanced-fsg', 'CXXopenmp')
+operator_registry.add(Intel64FsgCXXOperator, Intel64, 'advanced-fsg', 'CXX')
+operator_registry.add(Intel64FsgCXXOmpOperator, Intel64, 'advanced-fsg', 'CXXopenmp')
 
 operator_registry.add(ArmAdvCOperator, Arm, 'advanced', 'C')
 operator_registry.add(ArmAdvOmpOperator, Arm, 'advanced', 'openmp')
-operator_registry.add(ArmCXXAdvCOperator, Arm, 'advanced', 'CXX')
-operator_registry.add(ArmCXXAdvOmpOperator, Arm, 'advanced', 'CXXopenmp')
+operator_registry.add(ArmAdvCXXOperator, Arm, 'advanced', 'CXX')
+operator_registry.add(ArmAdvCXXOmpOperator, Arm, 'advanced', 'CXXopenmp')
 
 operator_registry.add(PowerAdvCOperator, Power, 'advanced', 'C')
 operator_registry.add(PowerAdvOmpOperator, Power, 'advanced', 'openmp')
 operator_registry.add(PowerCXXAdvCOperator, Power, 'advanced', 'CXX')
-operator_registry.add(PowerCXXAdvOmpOperator, Power, 'advanced', 'CXXopenmp')
+operator_registry.add(PowerAdvCXXOmpOperator, Power, 'advanced', 'CXXopenmp')
 
 # Register Device Operators
 operator_registry.add(DeviceCustomOmpOperator, Device, 'custom', 'C')
