47b74636a66b2881ee5ccde8e061d15a876616c2
ZoeLeibowitz <zoeleibowitz12@gmail.com>
2025-03-14 18:16:23 +0000

    dsl: Add separate_eqn and centre_stencil

---
+++ devito/symbolics/extraction.py
@@ -0,0 +1,109 @@
+from functools import singledispatch
+
+import sympy
+
+from devito.finite_differences.differentiable import Mul
+from devito.finite_differences.derivative import Derivative
+from devito.types import Eq, TimeFunction
+from devito.operations.solve import eval_time_derivatives
+from devito.symbolics import retrieve_functions
+
+
+def separate_eqn(eqn, target):
+    """
+    Separate the equation into two separate expressions,
+    where F(target) = b.
+    """
+    zeroed_eqn = Eq(eqn.lhs - eqn.rhs, 0)
+    zeroed_eqn = eval_time_derivatives(zeroed_eqn.lhs)
+    target_funcs = set(generate_targets(zeroed_eqn, target))
+    b, F_target = remove_targets(zeroed_eqn, target_funcs)
+    return -b, F_target, target_funcs
+
+
+def generate_targets(eq, target):
+    """
+    Extract all the functions that share the same time index as the target
+    but may have different spatial indices.
+    """
+    funcs = retrieve_functions(eq)
+    if isinstance(target, TimeFunction):
+        time_idx = target.indices[target.time_dim]
+        targets = [
+            f for f in funcs if f.function is target.function and time_idx
+            in f.indices
+        ]
+    else:
+        targets = [f for f in funcs if f.function is target.function]
+    return targets
+
+
+@singledispatch
+def remove_targets(expr, targets):
+    return (0, expr) if expr in targets else (expr, 0)
+
+
+@remove_targets.register(sympy.Add)
+def _(expr, targets):
+    if not any(expr.has(t) for t in targets):
+        return (expr, 0)
+
+    args_b, args_F = zip(*(remove_targets(a, targets) for a in expr.args))
+    return (expr.func(*args_b, evaluate=False), expr.func(*args_F, evaluate=False))
+
+
+@remove_targets.register(Mul)
+def _(expr, targets):
+    if not any(expr.has(t) for t in targets):
+        return (expr, 0)
+
+    args_b, args_F = zip(*[remove_targets(a, targets) if any(a.has(t) for t in targets)
+                           else (a, a) for a in expr.args])
+    return (expr.func(*args_b, evaluate=False), expr.func(*args_F, evaluate=False))
+
+
+@remove_targets.register(Derivative)
+def _(expr, targets):
+    return (0, expr) if any(expr.has(t) for t in targets) else (expr, 0)
+
+
+@singledispatch
+def centre_stencil(expr, target):
+    """
+    Extract the centre stencil from an expression. Its coefficient is what
+    would appear on the diagonal of the matrix system if the matrix were
+    formed explicitly.
+    """
+    return expr if expr == target else 0
+
+
+@centre_stencil.register(sympy.Add)
+def _(expr, target):
+    if not expr.has(target):
+        return 0
+
+    args = [centre_stencil(a, target) for a in expr.args]
+    return expr.func(*args, evaluate=False)
+
+
+@centre_stencil.register(Mul)
+def _(expr, target):
+    if not expr.has(target):
+        return 0
+
+    args = []
+    for a in expr.args:
+        if not a.has(target):
+            args.append(a)
+        else:
+            args.append(centre_stencil(a, target))
+
+    return expr.func(*args, evaluate=False)
+
+
+@centre_stencil.register(Derivative)
+def _(expr, target):
+    if not expr.has(target):
+        return 0
+    args = [centre_stencil(a, target) for a in expr.evaluate.args]
+    return expr.evaluate.func(*args)
