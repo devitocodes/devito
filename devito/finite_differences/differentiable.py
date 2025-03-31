171d0054115a67ae9a7b2d224e20363f35bacf10
Edward Caunt <ed@devitocodes.com>
2025-03-31 17:10:45 +0100

    dsl: Add Re and Im operators for taking real and imaginary parts of an expression

--- devito/finite_differences/differentiable.py
+++ devito/finite_differences/differentiable.py
@@ -22,7 +22,7 @@ from devito.types import Array, DimensionTuple, Evaluable, StencilDimension
 from devito.types.basic import AbstractFunction
 
 __all__ = ['Differentiable', 'DiffDerivative', 'IndexDerivative', 'EvalDerivative',
-           'Weights']
+           'Weights', 'Re', 'Im']
 
 
 class Differentiable(sympy.Expr, Evaluable):
@@ -642,6 +642,39 @@ class SafeInv(Differentiable, sympy.core.function.Application):
         return Pow(self.args[0], -1).__str__()
 
     __repr__ = __str__
+
+
+class ComplexPart(Differentiable, sympy.core.function.Application):
+    """Abstract class for `Re` or `Im` of an expression"""
+
+    def __new__(cls, *args, **kwargs):
+        if len(args) != 1:
+            raise ValueError(f"{cls.__name__} is constructed with exactly one arg;"
+                             f" {len(args)} were supplied.")
+
+        # Diffify any Add, Mul, etc which might be in the expression
+        new_args = (diffify(args[0]),)
+
+        if not np.issubdtype(new_args[0].dtype, np.complexfloating):
+            raise ValueError(f"{cls.__name__} requires a complex dtype,"
+                             f" not {new_args[0].dtype.__name__}.")
+
+        return super().__new__(cls, *new_args, **kwargs)
+
+    def __str__(self):
+        return f"{self.__class__.__name__}({self.args[0]})"
+
+    __repr__ = __str__
+
+
+class Re(ComplexPart):
+    """Get the real part of an expression"""
+    pass
+
+
+class Im(ComplexPart):
+    """Get the imaginary part of an expression"""
+    pass
 
 
 class IndexSum(sympy.Expr, Evaluable):
