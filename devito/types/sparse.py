0fc8f8f60d2180d521db88334f661a30d5bd1f75
Fabio Luporini <fabio@devitocodes.com>
2025-04-24 13:22:34 +0100

    api: Set SparseTimeFunction.is_regular=False

--- devito/types/sparse.py
+++ devito/types/sparse.py
@@ -989,6 +989,8 @@ class SparseTimeFunction(AbstractSparseTimeFunction, SparseFunction):
 
     is_SparseTimeFunction = True
 
+    is_regular = False
+
     __rkwargs__ = tuple(filter_ordered(AbstractSparseTimeFunction.__rkwargs__ +
                                        SparseFunction.__rkwargs__))
 
