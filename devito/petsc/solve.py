8a79fcde140004230b2bfe79323f1db7a17fc2b2
ZoeLeibowitz <zoeleibowitz12@gmail.com>
2025-06-03 22:33:35 +0100

    dsl: Move symbolic extraction functions outside of the PETSc module

--- devito/petsc/solve.py
+++ devito/petsc/solve.py
@@ -11,6 +11,8 @@ from devito.types import Eq, Symbol, SteppingDimension, TimeFunction
 from devito.types.equation import PetscEq
 from devito.operations.solve import eval_time_derivatives
 from devito.symbolics import retrieve_functions
+from devito.symbolics.extraction import (separate_eqn, centre_stencil,
+                                         generate_targets)
 from devito.tools import as_tuple, filter_ordered
 from devito.petsc.types import (LinearSolveExpr, PETScArray, DMDALocalInfo,
                                 FieldData, MultipleFieldData, Jacobian)
@@ -76,7 +78,6 @@ class InjectSolve:
             zip(*[self.build_function_eq(eq, target, arrays) for eq in eqns])
         )
 
-        # self._centre_stencils[arrays['x']].update(
         stencils = set()
         for eq in matvecs:
             if not isinstance(eq, EssentialBC):
@@ -362,33 +363,33 @@ class ZeroColumn(EssentialBC):
     pass
 
 
-def separate_eqn(eqn, target):
-    """
-    Separate the equation into two separate expressions,
-    where F(target) = b.
-    """
-    zeroed_eqn = Eq(eqn.lhs - eqn.rhs, 0)
-    zeroed_eqn = eval_time_derivatives(zeroed_eqn.lhs)
-    target_funcs = set(generate_targets(zeroed_eqn, target))
-    b, F_target = remove_targets(zeroed_eqn, target_funcs)
-    return -b, F_target, zeroed_eqn, target_funcs
+# def separate_eqn(eqn, target):
+#     """
+#     Separate the equation into two separate expressions,
+#     where F(target) = b.
+#     """
+#     zeroed_eqn = Eq(eqn.lhs - eqn.rhs, 0)
+#     zeroed_eqn = eval_time_derivatives(zeroed_eqn.lhs)
+#     target_funcs = set(generate_targets(zeroed_eqn, target))
+#     b, F_target = remove_targets(zeroed_eqn, target_funcs)
+#     return -b, F_target, zeroed_eqn, target_funcs
 
 
-def generate_targets(eq, target):
-    """
-    Extract all the functions that share the same time index as the target
-    but may have different spatial indices.
-    """
-    funcs = retrieve_functions(eq)
-    if isinstance(target, TimeFunction):
-        time_idx = target.indices[target.time_dim]
-        targets = [
-            f for f in funcs if f.function is target.function and time_idx
-            in f.indices
-        ]
-    else:
-        targets = [f for f in funcs if f.function is target.function]
-    return targets
+# def generate_targets(eq, target):
+#     """
+#     Extract all the functions that share the same time index as the target
+#     but may have different spatial indices.
+#     """
+#     funcs = retrieve_functions(eq)
+#     if isinstance(target, TimeFunction):
+#         time_idx = target.indices[target.time_dim]
+#         targets = [
+#             f for f in funcs if f.function is target.function and time_idx
+#             in f.indices
+#         ]
+#     else:
+#         targets = [f for f in funcs if f.function is target.function]
+#     return targets
 
 
 def targets_to_arrays(array, targets):
@@ -413,85 +414,6 @@ def targets_to_arrays(array, targets):
         array.subs(dict(zip(array.indices, i))) for i in space_indices
     ]
     return dict(zip(targets, array_targets))
-
-
-@singledispatch
-def remove_targets(expr, targets):
-    return (0, expr) if expr in targets else (expr, 0)
-
-
-@remove_targets.register(sympy.Add)
-def _(expr, targets):
-    if not any(expr.has(t) for t in targets):
-        return (expr, 0)
-
-    args_b, args_F = zip(*(remove_targets(a, targets) for a in expr.args))
-    return (expr.func(*args_b, evaluate=False), expr.func(*args_F, evaluate=False))
-
-
-@remove_targets.register(Mul)
-def _(expr, targets):
-    if not any(expr.has(t) for t in targets):
-        return (expr, 0)
-
-    args_b, args_F = zip(*[remove_targets(a, targets) if any(a.has(t) for t in targets)
-                           else (a, a) for a in expr.args])
-    return (expr.func(*args_b, evaluate=False), expr.func(*args_F, evaluate=False))
-
-
-@remove_targets.register(Derivative)
-def _(expr, targets):
-    return (0, expr) if any(expr.has(t) for t in targets) else (expr, 0)
-
-
-@singledispatch
-def centre_stencil(expr, target, as_coeff=False):
-    """
-    Extract the centre stencil from an expression. Its coefficient is what
-    would appear on the diagonal of the matrix system if the matrix were
-    formed explicitly.
-    Parameters
-    ----------
-    expr : the expression to extract the centre stencil from
-    target : the target function whose centre stencil we want
-    as_coeff : bool, optional
-        If True, return the coefficient of the centre stencil
-    """
-    if expr == target:
-        return 1 if as_coeff else expr
-    return 0
-
-
-@centre_stencil.register(sympy.Add)
-def _(expr, target, as_coeff=False):
-    if not expr.has(target):
-        return 0
-
-    args = [centre_stencil(a, target, as_coeff) for a in expr.args]
-    return expr.func(*args, evaluate=False)
-
-
-@centre_stencil.register(Mul)
-def _(expr, target, as_coeff=False):
-    if not expr.has(target):
-        return 0
-
-    args = []
-    for a in expr.args:
-        if not a.has(target):
-            args.append(a)
-        else:
-            args.append(centre_stencil(a, target, as_coeff))
-
-    return expr.func(*args, evaluate=False)
-
-
-@centre_stencil.register(Derivative)
-def _(expr, target, as_coeff=False):
-    if not expr.has(target):
-        return 0
-    args = [centre_stencil(a, target, as_coeff) for a in expr.evaluate.args]
-    return expr.evaluate.func(*args)
 
 
 def generate_time_mapper(funcs):
