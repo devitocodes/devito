83dfb04c7689586654dbdc06f021f38800a944f2
fernanvr <fernanvr@ime.usp.br>
2025-06-25 09:41:01 -0300

    3rd PR, updating tests and suggestions of 2nd PR revision

--- tests/test_multistage.py
+++ tests/test_multistage.py
@@ -174,22 +174,24 @@ def test_multistage_op_computing_directly(time_int='RK44'):
     src_spatial.data[1, 1] = 1
     src_temporal = (1 - 2 * (t*dt - 1)**2)
 
-    # PDE (2D heat eq.)
-    eq_rhs = (Derivative(u_multi_stage, (x, 2), fd_order=2) + Derivative(u_multi_stage, (y, 2), fd_order=2) +
+    # PDE
+    eq_rhs = (Derivative(u_multi_stage, (x, 2), fd_order=2) +
+              Derivative(u_multi_stage, (y, 2), fd_order=2) +
               src_spatial * src_temporal)
 
     # Time integration scheme
-    pde = [resolve_method(time_int)(eq_rhs, u_multi_stage)]
+    pde = [resolve_method(time_int)(u_multi_stage, eq_rhs)]
     op = Operator(pde, subs=grid.spacing_map)
     op(dt=0.01, time=1)
 
     # Solving now using Devito's standard time solver
     u = TimeFunction(name='u', grid=grid, space_order=2, time_order=1, dtype=float64)
-    eq_rhs = (Derivative(u, (x, 2), fd_order=2) + Derivative(u, (y, 2), fd_order=2) +
+    eq_rhs = (Derivative(u, (x, 2), fd_order=2) +
+              Derivative(u, (y, 2), fd_order=2) +
               src_spatial * src_temporal)
 
     # Time integration scheme
-    pde = Eq(u, solve(eq_rhs - u, u))
+    pde = Eq(u, eq_rhs)
     op = Operator(pde, subs=grid.spacing_map)
     op(dt=0.01, time=1)
 
