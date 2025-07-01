11d142951598486123bec26ec24874b12e87ffae
fernanvr <fernanvr@ime.usp.br>
2025-07-01 19:31:53 -0300

    including two more Runge-Kutta methods and improving tests: checking compatibility with pickles, and checking numerical convergence of three Runge-Kutta methods

--- devito/types/multistage.py
+++ devito/types/multistage.py
@@ -149,8 +149,6 @@ class RK44(RK):
     Classic 4th-order Runge-Kutta (RK4) time integration method.
 
     This class implements the classic explicit Runge-Kutta method of order 4 (RK44).
-    It uses four intermediate stages and specific Butcher coefficients to achieve
-    high accuracy while remaining explicit.
 
     Attributes
     ----------
@@ -169,5 +167,191 @@ class RK44(RK):
     c = [0, 1/2, 1/2, 1]
 
     def __init__(self, *args, **kwargs):
-        super().__init__(a=self.a, b=self.b, c=self.c)
+        super().__init__(a=self.a, b=self.b, c=self.c, **kwargs)
 
+
+@register_method
+class RK32(RK):
+    """
+    3 stages 2nd-order Runge-Kutta (RK32) time integration method.
+
+    This class implements the 3-stages explicit Runge-Kutta method of order 2 (RK32).
+
+    Attributes
+    ----------
+    a : list[list[float]]
+        Coefficients of the `a` matrix for intermediate stage coupling.
+    b : list[float]
+        Weights for final combination.
+    c : list[float]
+        Time positions of intermediate stages.
+    """
+    a = [[0, 0, 0],
+         [1/2, 0, 0],
+         [0, 1/2, 0]]
+    b = [0, 0, 1]
+    c = [0, 1/2, 1/2]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(a=self.a, b=self.b, c=self.c, **kwargs)
+
+
+@register_method
+class RK97(RK):
+    """
+    9 stages 7th-order Runge-Kutta (RK97) time integration method.
+
+    This class implements the 9-stages explicit Runge-Kutta method of order 7 (RK97).
+
+    Attributes
+    ----------
+    a : list[list[float]]
+        Coefficients of the `a` matrix for intermediate stage coupling.
+    b : list[float]
+        Weights for final combination.
+    c : list[float]
+        Time positions of intermediate stages.
+    """
+    a = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [4/63, 0, 0, 0, 0, 0, 0, 0, 0],
+         [1/42, 1/14, 0, 0, 0, 0, 0, 0, 0],
+         [1/28, 0, 3/28, 0, 0, 0, 0, 0, 0],
+         [12551/19652, 0, -48363/19652, 10976/4913, 0, 0, 0, 0, 0],
+         [-36616931/27869184, 0, 2370277/442368, -255519173/63700992, 226798819/445906944, 0, 0, 0, 0],
+         [-10401401/7164612, 0, 47383/8748, -4914455/1318761, -1498465/7302393, 2785280/3739203, 0, 0, 0],
+         [181002080831/17500000000, 0, -14827049601/400000000, 23296401527134463/857600000000000,
+          2937811552328081/949760000000000, -243874470411/69355468750, 2857867601589/3200000000000],
+         [-228380759/19257212, 0, 4828803/113948, -331062132205/10932626912, -12727101935/3720174304,
+          22627205314560/4940625496417, -268403949/461033608, 3600000000000/19176750553961]]
+    b = [95/2366, 0, 0, 3822231133/16579123200, 555164087/2298419200, 1279328256/9538891505,
+         5963949/25894400, 50000000000/599799373173, 28487/712800]
+    c = [0, 4/63, 2/21, 1/7, 7/17, 13/24, 7/9, 91/100, 1]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(a=self.a, b=self.b, c=self.c, **kwargs)
+
+
+@register_method
+class HORK(MultiStage):
+    # In construction
+    """
+    n stages Runge-Kutta (HORK) time integration method.
+
+    This class implements the arbitrary high-order explicit Runge-Kutta method.
+
+    Attributes
+    ----------
+    a : list[list[float]]
+        Coefficients of the `a` matrix for intermediate stage coupling.
+    b : list[float]
+        Weights for final combination.
+    c : list[float]
+        Time positions of intermediate stages.
+    """
+
+
+    def ssprk_alpha(mu=1, **kwargs):
+        """
+        Computes the coefficients for the Strong Stability Preserving Runge-Kutta (SSPRK) method.
+
+        Parameters:
+        mu : float
+            Theoretically, it should be the inverse of the CFL condition (typically mu=1 for best performance).
+            In practice, mu=1 works better.
+        degree : int
+            Degree of the polynomial used in the time-stepping scheme.
+
+        Returns:
+        numpy.ndarray
+            Array of SSPRK coefficients.
+        """
+        degree=kwargs.get('degree')
+
+        alpha = [0]*degree
+        alpha[0] = 1.0  # Initial coefficient
+
+        for i in range(1, degree):
+            alpha[i] = 1 / (mu * (i + 1)) * alpha[i - 1]
+            alpha[1:i] = 1 / (mu * list(range(1, i))) * alpha[:i - 1]
+            alpha[0] = 1 - sum(alpha[1:i + 1])
+
+        return alpha
+
+    def _evaluate(self, **kwargs):
+        """
+        Generate the stage-wise equations for a Runge-Kutta time integration method.
+
+        This method takes a single equation of the form `Eq(u.forward, rhs)` and
+        expands it into a sequence of intermediate stage evaluations and a final
+        update equation according to the Runge-Kutta coefficients `a`, `b`, and `c`.
+
+        Returns
+        -------
+        list of Eq
+            A list of SymPy Eq objects representing:
+            - `s` stage equations of the form `k_i = rhs evaluated at intermediate state`
+            - 1 final update equation of the form `u.forward = u + dt * sum(b_i * k_i)`
+        """
+
+        u = self.lhs.function
+        rhs = self.rhs
+        grid = u.grid
+        t = grid.time_dim
+        dt = t.spacing
+
+        an_eq = range(len(U0))
+
+        # Compute SSPRK coefficients
+        alpha = np.array(ssprk_alpha(mu, degree), dtype=np.float64)
+
+        # Initialize symbolic differentiation for source terms
+        t_var = sym.Symbol('t_var')
+        src_deriv = aux_fun.derivates_f(degree, f0)
+
+        # Expansion coefficients for stability control
+        e_p = [0] * degree
+        e_p[-1] = 1 / eta
+
+        # Initialize approximation and auxiliary variable
+        approx = [U0[i] * alpha[0] for i in n_eq]
+        aux = U0
+
+        # Perform Runge-Kutta steps
+        for i in range(1, degree - 1):
+            system_op, e_p = sys_op_extended(aux, x, y, z, param_fun, system, fd_order, src_spat, src_deriv, t, dt, t_var, e_p)
+            aux = [aux[j] + mu * dt * system_op[j] for j in n_eq]
+            approx = [approx[j] + aux[j] * alpha[i] for j in n_eq]
+
+        # Final Runge-Kutta updates
+        system_op, e_p = sys_op_extended(aux, x, y, z, param_fun, system, fd_order, src_spat, src_deriv, t, dt, t_var, e_p)
+        aux = [aux[i] + mu * dt * system_op[i] for i in n_eq]
+        system_op, e_p = sys_op_extended(aux, x, y, z, param_fun, system, fd_order, src_spat, src_deriv, t, dt, t_var, e_p)
+        aux = [aux[i] + mu * dt * system_op[i] for i in n_eq]
+
+        # Compute final approximation
+        approx = [approx[i] + aux[i] * alpha[degree - 1] for i in n_eq]
+
+        # Generate final PDE system
+        return [dv.Eq(U0[i].forward, approx[i]) for i in n_eq]
+
+        # Create temporary Functions to hold each stage
+        # k = [Array(name=f'{kwargs.get('sregistry').make_name(prefix='k')}', dimensions=grid.shape, grid=grid, dtype=u.dtype) for i in range(self.s)]  # Trying Array
+        k = [Function(name=f'{kwargs.get('sregistry').make_name(prefix='k')}', grid=grid, space_order=u.space_order, dtype=u.dtype)
+             for i in range(self.s)]
+
+        stage_eqs = []
+
+        # Build each stage
+        for i in range(self.s):
+            u_temp = u + dt * sum(aij * kj for aij, kj in zip(self.a[i][:i], k[:i]))
+            t_shift = t + self.c[i] * dt
+
+            # Evaluate RHS at intermediate value
+            stage_rhs = uxreplace(rhs, {u: u_temp, t: t_shift})
+            stage_eqs.append(Eq(k[i], stage_rhs))
+
+        # Final update: u.forward = u + dt * sum(b_i * k_i)
+        u_next = u + dt * sum(bi * ki for bi, ki in zip(self.b, k))
+        stage_eqs.append(Eq(u.forward, u_next))
+
+        return stage_eqs
--- tests/test_multistage.py
+++ tests/test_multistage.py
@@ -1,11 +1,13 @@
-from numpy import float64
+from numpy import float64, pi, max, abs
 
 from devito import (Grid, Function, TimeFunction,
-                    Derivative, Operator, solve)
+                    Derivative, Operator, solve, Eq)
 from devito.types.multistage import resolve_method, MultiStage
 from devito.ir.support import SymbolRegistry
 from devito.ir.equations import lower_multistage
 import pickle
+from sympy import exp
+import pytest
 
 
 def test_multistage_object(time_int='RK44'):
@@ -117,7 +119,6 @@ def test_multistage_lower_multistage(time_int='RK44'):
         print("There is an error when lowering the MultiStage object")
 
 
-
 def test_multistage_solve(time_int='RK44'):
     extent = (1, 1)
     shape = (3, 3)
@@ -212,4 +213,64 @@ def test_multistage_op_computing_directly(time_int='RK44'):
     pde = [resolve_method(time_int)(u_multi_stage, eq_rhs)]
     op = Operator(pde, subs=grid.spacing_map)
     op(dt=0.01, time=1)
+
+
+@pytest.mark.parametrize('time_int', ['RK44', 'RK32', 'RK97'])
+def test_multistage_methods_convergence(time_int):
+    extent = (1000, 1000)
+    shape = (201, 201)
+    origin = (0, 0)
+
+    # Grid setup
+    grid = Grid(origin=origin, extent=extent, shape=shape, dtype=float64)
+    x, y = grid.dimensions
+    dt = grid.stepping_dim.spacing
+    t = grid.time_dim
+    dx = extent[0] / (shape[0] - 1)
+
+    # Medium velocity model
+    vel = Function(name="vel", grid=grid, space_order=2, dtype=float64)
+    vel.data[:] = 1.0
+    vel.data[150:, :] = 1.3
+
+    # Source definition
+    src_spatial = Function(name="src_spat", grid=grid, space_order=2, dtype=float64)
+    src_spatial.data[100, 100] = 1/dx**2
+    f0 = 0.01
+    src_temporal = (1-2*(pi*f0*(t*dt-1/f0))**2)*exp(-(pi*f0*(t*dt-1/f0))**2)
+
+    # Time axis
+    t0, tn = 0.0, 500.0
+    dt0 = max(vel.data)/dx**2
+    nt = int((tn-t0)/dt0)
+    dt0 = tn/nt
+
+    # Time integrator solution
+    # Define wavefield unknowns: u (displacement) and v (velocity)
+    fun_labels = ['u', 'v']
+    U_multi_stage = [TimeFunction(name=name+'_multi_stage', grid=grid, space_order=2, time_order=1, dtype=float64) for name in fun_labels]
+
+    # PDE (2D acoustic)
+    eq_rhs = [U_multi_stage[1], (Derivative(U_multi_stage[0], (x, 2), fd_order=2) +
+                                 Derivative(U_multi_stage[0], (y, 2), fd_order=2) +
+                                 src_spatial * src_temporal) * vel**2]
+
+    # Time integration scheme
+    pdes = [resolve_method(time_int)(U_multi_stage[i], eq_rhs[i]) for i in range(len(fun_labels))]
+    op = Operator(pdes, subs=grid.spacing_map)
+    op(dt=dt0, time=nt)
+
+    # Devito's default solution
+    U = [TimeFunction(name=name, grid=grid, space_order=2, time_order=1, dtype=float64) for name in fun_labels]
+    # PDE (2D acoustic)
+    eq_rhs = [U[1], (Derivative(U[0], (x, 2), fd_order=2) + Derivative(U[0], (y, 2), fd_order=2) +
+                     src_spatial * src_temporal) * vel**2]
+
+    # Time integration scheme
+    pdes = [Eq(U[i].forward, solve(Eq(U[i].dt-eq_rhs[i]), U[i].forward)) for i in range(len(fun_labels))]
+    op = Operator(pdes, subs=grid.spacing_map)
+    op(dt=dt0, time=nt)
+    assert max(abs(U[0].data[0,:]-U_multi_stage[0].data[0,:]))<10**-5, "the method is not converging to the solution"
+
+
 
