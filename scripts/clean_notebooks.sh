5d69690119d052e3999bc66a66f7d4dac2230d6c
JDBetteridge <jack@devitocodes.com>
2025-07-23 17:33:22 +0100

    misc: Add script to clean notebooks

---
+++ scripts/clean_notebooks.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Clean your notebooks!
+
+# Clear all the output from all cells
+jupyter-nbconvert --clear-output $@
+
+# Run the whole notebook in order from start to end
+## We don't run with --ClearMetadataPreprocessor.enabled=True
+## as this is used to tag cells where nbval should skip checking
+jupyter-nbconvert \
+    --execute \
+    --allow-errors \
+    --to notebook \
+    --inplace \
+    $@
+
+# Strip superfluous metadata
+## NB: may need more extra-keys, these are just the current offenders
+nbstripout \
+    --keep-output \
+    --keep-count \
+    --extra-keys='cell.metadata.editable cell.metadata.slideshow' \
+    $@
