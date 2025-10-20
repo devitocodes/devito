b8004ada968ebee4be203317a4009042abf9011f
mloubout <mathias.louboutin@gmail.com>
2025-10-20 11:29:14 -0400

    installation: add devito images for arm64

--- tests/test_mpi.py
+++ tests/test_mpi.py
@@ -24,6 +24,12 @@ from examples.seismic.acoustic import acoustic_setup
 from test_dse import TestTTI
 
 
+# Main body in Operator IET, depending on ISA
+def body0(op):
+    bidx = 0 if 'sse' not in configuration['platform'].known_isas else 1
+    return op.body.body[bidx]
+
+
 class TestDistributor:
 
     @pytest.mark.parallel(mode=[2, 4])
@@ -1564,9 +1570,9 @@ class TestCodeGeneration:
         assert len(calls) == 2
         assert calls[0].arguments[3].args[0] is t.symbolic_min
 
-        assert len(FindNodes(HaloUpdateCall).visit(op.body.body[1].body[0])) == 1
-        assert len(FindNodes(HaloUpdateCall).visit(op.body.body[1].body[1])) == 1
-        assert len(FindNodes(HaloUpdateCall).visit(op.body.body[1].body[2])) == 0
+        assert len(FindNodes(HaloUpdateCall).visit(body0(op).body[0])) == 1
+        assert len(FindNodes(HaloUpdateCall).visit(body0(op).body[1])) == 1
+        assert len(FindNodes(HaloUpdateCall).visit(body0(op).body[2])) == 0
 
         op.apply(time_M=1)
 
@@ -3280,9 +3286,9 @@ class TestElasticLike:
 
         calls = [i for i in FindNodes(Call).visit(op) if isinstance(i, HaloUpdateCall)]
         assert len(calls) == 5
-        assert len(FindNodes(HaloUpdateCall).visit(op.body.body[1].body[1].body[0])) == 1
-        assert len(FindNodes(HaloUpdateCall).visit(op.body.body[1].body[1].body[1])) == 4
-        assert len(FindNodes(HaloUpdateCall).visit(op.body.body[1].body[1].body[2])) == 0
+        assert len(FindNodes(HaloUpdateCall).visit(body0(op).body[1].body[0])) == 1
+        assert len(FindNodes(HaloUpdateCall).visit(body0(op).body[1].body[1])) == 4
+        assert len(FindNodes(HaloUpdateCall).visit(body0(op).body[1].body[2])) == 0
 
         assert calls[0].arguments[0] is v[0]
         assert calls[0].arguments[1] is v[1]
@@ -3331,8 +3337,8 @@ class TestElasticLike:
         calls = [i for i in FindNodes(Call).visit(op0) if isinstance(i, HaloUpdateCall)]
 
         assert len(calls) == 3
-        assert len(FindNodes(HaloUpdateCall).visit(op0.body.body[1].body[1].body[0])) == 1
-        assert len(FindNodes(HaloUpdateCall).visit(op0.body.body[1].body[1].body[1])) == 2
+        assert len(FindNodes(HaloUpdateCall).visit(body0(op0).body[1].body[0])) == 1
+        assert len(FindNodes(HaloUpdateCall).visit(body0(op0).body[1].body[1])) == 2
         assert calls[0].arguments[0] is v
         assert calls[1].arguments[0] is tau
         assert calls[2].arguments[0] is v
@@ -3375,8 +3381,8 @@ class TestElasticLike:
         calls = [i for i in FindNodes(Call).visit(op2) if isinstance(i, HaloUpdateCall)]
 
         assert len(calls) == 5
-        assert len(FindNodes(HaloUpdateCall).visit(op2.body.body[1].body[1].body[0])) == 2
-        assert len(FindNodes(HaloUpdateCall).visit(op2.body.body[1].body[1].body[1])) == 3
+        assert len(FindNodes(HaloUpdateCall).visit(body0(op2).body[1].body[0])) == 2
+        assert len(FindNodes(HaloUpdateCall).visit(body0(op2).body[1].body[1])) == 3
         assert calls[0].arguments[0] is v
         assert calls[1].arguments[0] is v2
         assert calls[2].arguments[0] is tau
@@ -3410,8 +3416,8 @@ class TestElasticLike:
         calls = [i for i in FindNodes(Call).visit(op3) if isinstance(i, HaloUpdateCall)]
 
         assert len(calls) == 4
-        assert len(FindNodes(HaloUpdateCall).visit(op3.body.body[1].body[1].body[0])) == 1
-        assert len(FindNodes(HaloUpdateCall).visit(op3.body.body[1].body[1].body[1])) == 3
+        assert len(FindNodes(HaloUpdateCall).visit(body0(op3).body[1].body[0])) == 1
+        assert len(FindNodes(HaloUpdateCall).visit(body0(op3).body[1].body[1])) == 3
         assert calls[0].arguments[0] is v
         assert calls[1].arguments[0] is tau
         assert calls[1].arguments[1] is tau2
@@ -3459,8 +3465,8 @@ class TestElasticLike:
                  if isinstance(i, HaloUpdateCall)]
 
         assert len(calls) == 3
-        assert len(FindNodes(HaloUpdateCall).visit(op0.body.body[1].body[1].body[0])) == 1
-        assert len(FindNodes(HaloUpdateCall).visit(op0.body.body[1].body[1].body[1])) == 2
+        assert len(FindNodes(HaloUpdateCall).visit(body0(op0).body[1].body[0])) == 1
+        assert len(FindNodes(HaloUpdateCall).visit(body0(op0).body[1].body[1])) == 2
         assert calls[0].arguments[0] is v
         assert calls[0].arguments[3].args[0] is t.symbolic_max
         assert calls[1].arguments[0] is tau
