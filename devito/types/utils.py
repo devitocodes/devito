30227906dda1a08579b0bdd260a16bf057e608c0
Jack Betteridge <J.Betteridge@imperial.ac.uk>
2026-04-30 19:52:24 +0100

    dsl: Update repr for HierarchyLayer

--- devito/types/utils.py
+++ devito/types/utils.py
@@ -107,7 +107,7 @@ class HierarchyLayer:
         self.suffix = suffix
 
     def __repr__(self):
-        return f"HierarchyLayer({self.suffix!r})"
+        return f"{self.__class__.__name__}({self.suffix!r})"
 
     def __eq__(self, other):
         return (isinstance(other, HierarchyLayer) and
