756922181cbcb825bc728e16ee8be965b6ef49c1
Fabio Luporini <fabio@devitocodes.com>
2026-06-16 10:24:47 +0100

    compiler: Comply with ruff

--- devito/ir/support/space.py
+++ devito/ir/support/space.py
@@ -948,10 +948,7 @@ class IterationSpace(Space, CacheInstances):
         `d1`. Optionally, `d0` could be a mapper, in which case multiple Dimensions
         may be switched.
         """
-        if isinstance(d0, dict):
-            mapper = d0
-        else:
-            mapper = {d0: d1}
+        mapper = d0 if isinstance(d0, dict) else {d0: d1}
 
         intervals = self.intervals
         sub_iterators = dict(self.sub_iterators)
