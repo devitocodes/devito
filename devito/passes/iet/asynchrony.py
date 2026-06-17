18dba9d2e1d5ae8cd76219edd7cab78e95b67527
Fabio Luporini <fabio@devitocodes.com>
2026-06-17 15:47:19 +0100

    compiler: Fixup lower_async_objs

--- devito/passes/iet/asynchrony.py
+++ devito/passes/iet/asynchrony.py
@@ -38,7 +38,7 @@ def pthreadify(graph, **kwargs):
 AsyncMeta = namedtuple('AsyncMeta', 'sdata threads init shutdown')
 
 
-@iet_pass
+@iet_pass(updates_args=True)
 def lower_async_objs(iet, **kwargs):
     # Different actions depending on the Callable type
     iet, efuncs = _lower_async_objs(iet, **kwargs)
